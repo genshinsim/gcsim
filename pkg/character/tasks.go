8424fcc7d5b319d0b6801d5965f928b09e351278
Tsym <84653157+Tsym-or-Tysm@users.noreply.github.com>
2021-10-15 03:39:53 -0400

    Initial snapshot dynamic changes - core + Raiden

--- pkg/character/tasks.go
+++ pkg/character/tasks.go
@@ -15,3 +15,11 @@ func (t *Tmpl) QueueDmg(ds *core.Snapshot, delay int) {
 		t.Core.Combat.ApplyDamage(ds)
 	}, "dmg", delay)
 }
+
+// Helper/descriptive function to create a snapshot instance and queue up the damage on the same frame.
+// Best used for all abilities that do not snapshot in game (e.g. normal attacks, Hu Tao blood blossoms, etc.)
+func (t *Tmpl) QueueDmgDynamic(generateSnapshot func() *core.Snapshot, delay int) {
+	t.AddTask(func() {
+		t.Core.Combat.ApplyDamage(generateSnapshot())
+	}, "dmg", delay)
+}
