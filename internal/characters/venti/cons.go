9033728da21b0814ce3384a1d6e59e43e5e85421
srliao <906239+srliao@users.noreply.github.com>
2021-12-08 15:54:14 -0500

    fix venti

--- internal/characters/venti/cons.go
+++ internal/characters/venti/cons.go
@@ -2,23 +2,21 @@ package venti
 
 import "github.com/genshinsim/gcsim/pkg/core"
 
-func (c *char) applyC2(ds *core.Snapshot) {
-	ds.OnHitCallback = func(t core.Target) {
-		t.AddResMod("venti-c2-phys", core.ResistMod{
-			Ele:      core.Physical,
-			Value:    -0.12,
-			Duration: 600,
-		})
-		t.AddResMod("venti-c2-anemo", core.ResistMod{
-			Ele:      core.Physical,
-			Value:    -0.12,
-			Duration: 600,
-		})
-	}
+func c2cb(t core.Target, ae *core.AttackEvent) {
+	t.AddResMod("venti-c2-phys", core.ResistMod{
+		Ele:      core.Physical,
+		Value:    -0.12,
+		Duration: 600,
+	})
+	t.AddResMod("venti-c2-anemo", core.ResistMod{
+		Ele:      core.Physical,
+		Value:    -0.12,
+		Duration: 600,
+	})
 }
 
-func (c *char) applyC6(ds *core.Snapshot, ele core.EleType) {
-	ds.OnHitCallback = func(t core.Target) {
+func c6cb(ele core.EleType) func(t core.Target, ae *core.AttackEvent) {
+	return func(t core.Target, ae *core.AttackEvent) {
 		t.AddResMod("venti-c6-anemo", core.ResistMod{
 			Ele:      ele,
 			Value:    -0.20,
@@ -26,3 +24,28 @@ func (c *char) applyC6(ds *core.Snapshot, ele core.EleType) {
 		})
 	}
 }
+
+// func (c *char) applyC2(ds *core.Snapshot) {
+// 	ds.OnHitCallback = func(t core.Target) {
+// 		t.AddResMod("venti-c2-phys", core.ResistMod{
+// 			Ele:      core.Physical,
+// 			Value:    -0.12,
+// 			Duration: 600,
+// 		})
+// 		t.AddResMod("venti-c2-anemo", core.ResistMod{
+// 			Ele:      core.Physical,
+// 			Value:    -0.12,
+// 			Duration: 600,
+// 		})
+// 	}
+// }
+
+// func (c *char) applyC6(ds *core.Snapshot, ele core.EleType) {
+// 	ds.OnHitCallback = func(t core.Target) {
+// 		t.AddResMod("venti-c6-anemo", core.ResistMod{
+// 			Ele:      ele,
+// 			Value:    -0.20,
+// 			Duration: 600,
+// 		})
+// 	}
+// }
