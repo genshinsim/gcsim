ceb84569dbcb04814c78388057a14bd3e7779d58
srliao <906239+srliao@users.noreply.github.com>
2021-12-24 23:21:22 -0500

    fix diona burst cost

--- internal/characters/diona/diona.go
+++ internal/characters/diona/diona.go
@@ -21,8 +21,8 @@ func NewChar(s *core.Core, p core.CharacterProfile) (core.Character, error) {
 		return nil, err
 	}
 	c.Tmpl = t
-	c.Energy = 60
-	c.EnergyMax = 60
+	c.Energy = 80
+	c.EnergyMax = 80
 	c.Weapon.Class = core.WeaponClassBow
 	c.NormalHitNum = 5
 	c.BurstCon = 3
