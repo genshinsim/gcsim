89fb72ee8799e8ef7ed7be3ecd674d5413b2019d
srliao <906239+srliao@users.noreply.github.com>
2021-12-26 21:15:10 -0500

    move GA to index.html

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,24 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import GA4React from "ga-4-react";
 
-const ga4react = new GA4React("G-XXXXXXXXXX");
-
-(async (_) => {
-  await ga4react
-    .initialize()
-    .then((res) => console.log("Analytics Success."))
-    .catch((err) => console.log("Analytics Failure."))
-    .finally(() => {
-      ReactDOM.render(
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>,
-        document.getElementById("root")
-      );
-    });
-})();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
