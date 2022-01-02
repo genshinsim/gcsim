4ddd673bac60780ec76e1a1c61dc0260fbc451e8
srliao <906239+srliao@users.noreply.github.com>
2022-01-02 13:54:13 -0500

    fix hod

--- internal/artifacts/heartofdepth/heartofdepth.go
+++ internal/artifacts/heartofdepth/heartofdepth.go
@@ -26,21 +26,21 @@ func New(c core.Character, s *core.Core, count int, params map[string]int) {
 	if count >= 4 {
 		key := fmt.Sprintf("%v-hod-4pc", c.Name())
 
-		s.Events.Subscribe(core.PostSkill, func(args ...interface{}) bool {
-			s.Status.AddStatus(key, 900) //activate for 15 seoncds
-			return false
-		}, fmt.Sprintf("hod4-%v", c.Name()))
-
 		m := make([]float64, core.EndStatType)
 		m[core.DmgP] = 0.3
 
-		c.AddPreDamageMod(core.PreDamageMod{
-			Key: "hod-4pc",
-			Amount: func(atk *core.AttackEvent, t core.Target) ([]float64, bool) {
-				return m, (atk.Info.AttackTag == core.AttackTagNormal || atk.Info.AttackTag == core.AttackTagExtra)
-			},
-			Expiry: -1,
-		})
+		s.Events.Subscribe(core.PostSkill, func(args ...interface{}) bool {
+			s.Status.AddStatus(key, 900) //activate for 15 seoncds
+			//add stat mod here
+			c.AddPreDamageMod(core.PreDamageMod{
+				Key: "hod-4pc",
+				Amount: func(atk *core.AttackEvent, t core.Target) ([]float64, bool) {
+					return m, (atk.Info.AttackTag == core.AttackTagNormal || atk.Info.AttackTag == core.AttackTagExtra)
+				},
+				Expiry: s.F + 900,
+			})
+			return false
+		}, fmt.Sprintf("hod4-%v", c.Name()))
 
 	}
 	//add flat stat to char
