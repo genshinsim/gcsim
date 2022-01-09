1f7174e8a4d5bd0c6f339ddf2430f52241216ec0
Tsym-or-Tysm <84653157+Tsym-or-Tysm@users.noreply.github.com>
2022-01-09 15:47:36 -0500

    Bugfixes: Solar Pearl, Alley Hunter, Lisa, Logging, Lost Prayer (#157)
    * Fix solar pearl and make mod more descriptive
    
    * Fixes Alley Hunter parameter and stacking methodology (#156)
    
    * Fixes Lisa conductive stack naming, implements A4 (#154), fixes C6
    
    * Align Yun Jin buff tracking with Shenhe
    
    * Align Lost Prayer stacks param naming; remove React Mod logging

--- internal/weapons/catalyst/solar/solar.go
+++ internal/weapons/catalyst/solar/solar.go
@@ -14,38 +14,44 @@ func init() {
 //Normal Attack hits increase Elemental Skill and Elemental Burst DMG by 20/25/30/35/40% for 6s.
 //Likewise, Elemental Skill or Elmental Burst hits increase Normal Attack DMG by 20/25/30/35/40% for 6s.
 func weapon(char core.Character, c *core.Core, r int, param map[string]int) string {
-	skill := 0
-	attack := 0
+	val := make([]float64, core.EndStatType)
+	val[core.DmgP] = 0.15 + float64(r)*0.05
 
 	c.Events.Subscribe(core.OnDamage, func(args ...interface{}) bool {
 		atk := args[1].(*core.AttackEvent)
 		if atk.Info.ActorIndex != char.CharIndex() {
 			return false
 		}
-		if atk.Info.AttackTag == core.AttackTagElementalArt || atk.Info.AttackTag == core.AttackTagElementalBurst {
-			skill = c.F + 300
+		switch atk.Info.AttackTag {
+		case core.AttackTagElementalArt, core.AttackTagElementalBurst:
+			char.AddPreDamageMod(core.PreDamageMod{
+				Key:    "solar-na-buff",
+				Expiry: c.F + 6*60,
+				Amount: func(atk *core.AttackEvent, t core.Target) ([]float64, bool) {
+					switch atk.Info.AttackTag {
+					case core.AttackTagNormal:
+						return val, true
+					}
+					return nil, false
+				},
+			})
+		case core.AttackTagNormal:
+			char.AddPreDamageMod(core.PreDamageMod{
+				Key:    "solar-skill-burst-buff",
+				Expiry: c.F + 6*60,
+				Amount: func(atk *core.AttackEvent, t core.Target) ([]float64, bool) {
+					switch atk.Info.AttackTag {
+					case core.AttackTagElementalArt, core.AttackTagElementalBurst:
+						return val, true
+					}
+					return nil, false
+				},
+			})
+		default:
 			return false
-		}
-		if atk.Info.AttackTag == core.AttackTagNormal {
-			skill = c.F + 300
 		}
 		return false
 	}, fmt.Sprintf("solar-%v", char.Name()))
 
-	val := make([]float64, core.EndStatType)
-	val[core.DmgP] = 0.15 + float64(r)*0.05
-	char.AddPreDamageMod(core.PreDamageMod{
-		Key:    "solar",
-		Expiry: -1,
-		Amount: func(atk *core.AttackEvent, t core.Target) ([]float64, bool) {
-			if atk.Info.AttackTag == core.AttackTagElementalArt || atk.Info.AttackTag == core.AttackTagElementalBurst {
-				return val, attack > c.F
-			}
-			if atk.Info.AttackTag == core.AttackTagNormal {
-				return val, skill > c.F
-			}
-			return nil, false
-		},
-	})
 	return "solarpearl"
 }
