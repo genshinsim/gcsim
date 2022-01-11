1a140e967392f530422a1cce5d1cc472ea4b8be3
Tsym <84653157+Tsym-or-Tysm@users.noreply.github.com>
2022-01-11 09:14:59 -0500

    Move Redhorn/Cinnabar logging to align with Yun Jin/Shenhe

--- internal/weapons/sword/cinnabar/cinnnabar.go
+++ internal/weapons/sword/cinnabar/cinnnabar.go
@@ -27,9 +27,10 @@ func weapon(char core.Character, c *core.Core, r int, param map[string]int) stri
 		if effectDurationExpiry < c.F && c.F <= effectICDExpiry {
 			return false
 		}
-		atk.Info.FlatDmg += (atk.Snapshot.BaseDef*(1+atk.Snapshot.Stats[core.DEFP]) + atk.Snapshot.Stats[core.DEF]) * defPer
+		damageAdd := (atk.Snapshot.BaseDef*(1+atk.Snapshot.Stats[core.DEFP]) + atk.Snapshot.Stats[core.DEF]) * defPer
+		atk.Info.FlatDmg += damageAdd
 
-		c.Log.Debugw("Cinnabar Spindle proc dmg add", "frame", c.F, "event", core.LogCalc, "char", char.CharIndex(), "lastproc", effectLastProc, "effect_ends_at", effectDurationExpiry, "effect_icd_ends_at", effectICDExpiry)
+		c.Log.Debugw("Cinnabar Spindle proc dmg add", "frame", c.F, "event", core.LogPreDamageMod, "char", char.CharIndex(), "damage_added", damageAdd, "lastproc", effectLastProc, "effect_ends_at", effectDurationExpiry, "effect_icd_ends_at", effectICDExpiry)
 
 		// TODO: Assumes that the ICD starts after the last duration ends
 		effectICDExpiry = c.F + 6 + 90
