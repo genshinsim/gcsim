3e3966a5f01a89141475416b7ceda1e571e641c1
Shizuka <shizuka.alpha@gmail.com>
2022-02-04 10:35:19 +0400

    Move ATK buff inside event

--- internal/weapons/spear/calamity/calamity.go
+++ internal/weapons/spear/calamity/calamity.go
@@ -17,9 +17,44 @@ func init() {
 // When the character equipped with this weapon is not on the field, Consummation's ATK increase is doubled.
 func weapon(char core.Character, c *core.Core, r int, param map[string]int) string {
 
-	m := make([]float64, core.EndStatType)
 	dmg := .09 + float64(r)*.03
 	atkbonus := .024 + float64(r)*.008
+
+	skillInitF := -1
+
+	c.Events.Subscribe(core.PreSkill, func(args ...interface{}) bool {
+
+		// update init frame on first cast or after 20s from last cast
+		dur := 60 * 20
+		if skillInitF == -1 || (skillInitF+dur) < c.F {
+			skillInitF = c.F
+		}
+
+		char.AddMod(core.CharStatMod{
+			Key:    "calamity-consummation",
+			Expiry: c.F + dur,
+			Amount: func() ([]float64, bool) {
+				m := make([]float64, core.EndStatType)
+
+				stacks := (c.F - skillInitF) / 60
+				if stacks > 6 {
+					stacks = 6
+				}
+
+				atk := atkbonus * float64(stacks)
+				if c.ActiveChar != char.CharIndex() {
+					atk *= 2
+				}
+				m[core.ATKP] = atk
+
+				return m, true
+			},
+		})
+
+		return false
+	}, fmt.Sprintf("calamity-queller-%v", char.Name()))
+
+	m := make([]float64, core.EndStatType)
 	m[core.PyroP] = dmg
 	m[core.HydroP] = dmg
 	m[core.CryoP] = dmg
@@ -28,38 +63,10 @@ func weapon(char core.Character, c *core.Core, r int, param map[string]int) stri
 	m[core.GeoP] = dmg
 	m[core.DendroP] = dmg
 
-	expiry := -1
-	skillInitF := 0
-
-	c.Events.Subscribe(core.PreSkill, func(args ...interface{}) bool {
-
-		if expiry < c.F {
-			skillInitF = c.F
-		}
-		expiry = c.F + 60*20
-
-		return false
-	}, fmt.Sprintf("calamity-queller-%v", char.Name()))
-
 	char.AddMod(core.CharStatMod{
 		Key:    "calamity-queller",
 		Expiry: -1,
 		Amount: func() ([]float64, bool) {
-
-			stacks := 0
-			if expiry > c.F {
-				stacks = (c.F - skillInitF) / 60
-			}
-			if stacks > 6 {
-				stacks = 6
-			}
-
-			atk := atkbonus * float64(stacks)
-			if c.ActiveChar != char.CharIndex() {
-				atk *= 2
-			}
-			m[core.ATKP] = atk
-
 			return m, true
 		},
 	})
