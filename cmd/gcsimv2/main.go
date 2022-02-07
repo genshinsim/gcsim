90b3a9305c825df5a0ee61ee151a473250d058c1
srliao <srliao@users.noreply.github.com>
2022-02-07 18:12:15 -0500

    fix go mod

--- cmd/gcsimv2/main.go
+++ cmd/gcsimv2/main.go
@@ -6,12 +6,14 @@ import (
 	"log"
 
 	"github.com/genshinsim/gcsim/internal/simulator"
+	"github.com/pkg/profile"
 )
 
 type opts struct {
 	config string
 	out    string //file result name
 	gz     bool
+	prof   bool
 }
 
 //command line tool; following options are available:
@@ -21,8 +23,11 @@ func main() {
 	flag.StringVar(&opt.config, "c", "config.txt", "which profile to use; default config.txt")
 	flag.StringVar(&opt.out, "out", "", "output result to file? supply file path (otherwise empty string for disabled). default disabled")
 	flag.BoolVar(&opt.gz, "gz", false, "gzip json results; require out flag")
+	flag.BoolVar(&opt.prof, "p", false, "run cpu profile; default false")
 
-	// defer profile.Start(profile.ProfilePath("./"), profile.CPUProfile).Stop()
+	if opt.prof {
+		defer profile.Start(profile.ProfilePath("./"), profile.CPUProfile).Stop()
+	}
 
 	// defer profile.Start(profile.ProfilePath("./mem.pprof"), profile.MemProfileHeap).Stop()
 
