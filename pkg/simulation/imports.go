82894a1fa5b8d5ec1228aa277165de94b308f001
Han Du <han.du@uwaterloo.ca>
2022-02-15 22:50:22 -0500

    oathsworn

--- pkg/simulation/imports.go
+++ pkg/simulation/imports.go
@@ -100,6 +100,7 @@ import (
 	_ "github.com/genshinsim/gcsim/internal/weapons/catalyst/mappa"
 	_ "github.com/genshinsim/gcsim/internal/weapons/catalyst/memory"
 	_ "github.com/genshinsim/gcsim/internal/weapons/catalyst/moonglow"
+	_ "github.com/genshinsim/gcsim/internal/weapons/catalyst/oathsworneye"
 	_ "github.com/genshinsim/gcsim/internal/weapons/catalyst/perception"
 	_ "github.com/genshinsim/gcsim/internal/weapons/catalyst/prayer"
 	_ "github.com/genshinsim/gcsim/internal/weapons/catalyst/prototype"
