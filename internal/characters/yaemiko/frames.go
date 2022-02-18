5c279d7c4543393943e2662c126c95404e68c9dc
Han Du <han.du@uwaterloo.ca>
2022-02-18 18:58:08 -0500

    frame change from KQM

--- internal/characters/yaemiko/frames.go
+++ internal/characters/yaemiko/frames.go
@@ -9,20 +9,20 @@ func (c *char) ActionFrames(a core.ActionType, p map[string]int) (int, int) {
 		switch c.NormalCounter {
 		//TODO: need to add atkspd mod
 		case 0:
-			f = 24 //frames from keqing lib
+			f = 22 //frames from keqing lib
 		case 1:
-			f = 34
+			f = 46 - 22
 		case 2:
-			f = 70
+			f = 90 - 46 - 22
 		}
 		f = int(float64(f) / (1 + c.Stat(core.AtkSpd)))
 		return f, f
 	case core.ActionCharge:
 		return 114, 114 //frames from keqing lib
 	case core.ActionSkill:
-		return 22, 22 //ok
+		return 21, 21 //ok
 	case core.ActionBurst:
-		return 103, 103 //ok
+		return 111, 111 //ok
 	default:
 		c.Core.Log.NewEventBuildMsg(core.LogActionEvent, c.Index, "unknown action (invalid frames): ", a.String())
 		return 0, 0
