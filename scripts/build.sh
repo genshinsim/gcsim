ec4d2123883ccf98cbeb4e526f507586caaf30dc
srliao <srliao@users.noreply.github.com>
2022-02-21 23:05:06 -0500

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ cd "./cmd/gcsim"
 
 # notice how we avoid spaces in $now to avoid quotation hell in go build command
 now=$(date --utc +%FT%T%Z)
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now"
+GOOS=windows GOARCH=amd64 go build -o gcsim.exe -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now"
 
 mv gcsim.exe ../../
