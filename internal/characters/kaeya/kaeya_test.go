0cb3b7ca938e19e89c170a98900fdfc4fe902e15
srliao <906239+srliao@users.noreply.github.com>
2022-03-09 20:45:47 -0500

    fixed for - diluc - diona - eula - fischl - ganyu - gorou - hutao - ittou - jean - kaeya - kazuha

--- internal/characters/kaeya/kaeya_test.go
+++ internal/characters/kaeya/kaeya_test.go
@@ -19,3 +19,17 @@ func TestBasicAbilUsage(t *testing.T) {
 	}
 	testhelper.TestSwordCharacter(c, x)
 }
+
+func TestCD(t *testing.T) {
+	c := testhelper.NewTestCore()
+	prof := testhelper.CharProfile(core.Kaeya, core.Cryo, 6)
+	x, err := NewChar(c, prof)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	err = testhelper.TestSkillCDSingleCharge(c, x, 360+28)
+	if err != nil {
+		t.Error(err)
+	}
+}
