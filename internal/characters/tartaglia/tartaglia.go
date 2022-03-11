21923099e540cb193659b621e948f0664318a365
Shizuka <shizuka.alpha@gmail.com>
2022-03-11 09:02:51 +0400

    Implement Tartaglia C4 (#383)
    * cleanup tartaglia code
    * Implement Tartaglia C4

--- internal/characters/tartaglia/tartaglia.go
+++ internal/characters/tartaglia/tartaglia.go
@@ -16,10 +16,7 @@ type char struct {
 	*character.Tmpl
 	eCast         int // the frame tartaglia casts E to enter melee stance
 	rtParticleICD int
-	// rtFlashICD    []int
-	// rtSlashICD    []int
-	// rtExpiry      []int
-	mlBurstUsed bool // used for c6
+	mlBurstUsed   bool // used for c6
 }
 
 //constants for tags
@@ -55,9 +52,6 @@ func NewChar(s *core.Core, p core.CharacterProfile) (core.Character, error) {
 	}
 
 	c.rtParticleICD = 0
-	// c.rtFlashICD = make([]int, len(c.Core.Targets))
-	// c.rtSlashICD = make([]int, len(c.Core.Targets))
-	// c.rtExpiry = make([]int, len(c.Core.Targets))
 
 	c.Core.Flags.ChildeActive = true
 	c.onExitField()
@@ -122,39 +116,16 @@ func (c *char) onDefeatTargets() {
 	}, "tartaglia-on-enemy-death")
 }
 
-//apply riptide status to enemy hit
-// func (c *char) applyRT() {
-// 	c.Core.Events.Subscribe(core.OnDamage, func(args ...interface{}) bool {
-// 		atk := args[1].(*core.AttackEvent)
-// 		t := args[0].(core.Target)
-// 		crit := args[3].(bool)
+func (c *char) c4(t core.Target) {
+	if t.GetTag(riptideKey) < c.Core.F {
+		return
+	}
 
-// 		if c.Core.Status.Duration("tartagliamelee") > 0 {
-// 			if atk.Info.AttackTag != core.AttackTagNormal && atk.Info.AttackTag != core.AttackTagExtra {
-// 				return false
-// 			}
-// 			if !crit {
-// 				return false
-// 			}
+	if c.Core.Status.Duration("tartagliamelee") > 0 {
+		c.rtSlashTick(t)
+	} else {
+		c.rtFlashTick(t)
+	}
 
-// 			//dont log if it just refreshes riptide status
-// 			if c.rtExpiry[t.Index()] <= c.Core.F {
-// 				c.Core.Log.NewEvent("Tartaglia applied riptide", core.LogCharacterEvent, c.Index, "target", t.Index(), "rtExpiry", c.Core.F+riptideDuration)
-// 			}
-// 			c.rtExpiry[t.Index()] = c.Core.F + riptideDuration
-// 		} else {
-// 			if atk.Info.AttackTag != core.AttackTagElementalBurst && atk.Info.AttackTag != core.AttackTagExtra {
-// 				return false
-// 			}
-
-// 			//ranged burst or aim mode
-// 			//dont log if it just refreshes riptide status
-// 			if c.rtExpiry[t.Index()] <= c.Core.F {
-// 				c.Core.Log.NewEvent("Tartaglia applied riptide", core.LogCharacterEvent, c.Index, "target", t.Index(), "rtExpiry", c.Core.F+riptideDuration)
-// 			}
-// 			c.rtExpiry[t.Index()] = c.Core.F + riptideDuration
-// 		}
-
-// 		return false
-// 	}, "tartaglia-apply-riptide")
-// }
+	c.AddTask(func() { c.c4(t) }, "tartaglia-c4", 60*4)
+}
