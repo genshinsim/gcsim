2742ad607f3aef99d5936583541a1d6d992fe93c
tzwang1 <truman.wang1234@gmail.com>
2022-03-22 22:46:02 -0400

    Add filletblade to import.go

--- pkg/simulation/imports.go
+++ pkg/simulation/imports.go
@@ -167,6 +167,7 @@ import (
 	_ "github.com/genshinsim/gcsim/internal/weapons/sword/dullblade"
 	_ "github.com/genshinsim/gcsim/internal/weapons/sword/favonius"
 	_ "github.com/genshinsim/gcsim/internal/weapons/sword/festering"
+	_ "github.com/genshinsim/gcsim/internal/weapons/sword/filletblade"
 	_ "github.com/genshinsim/gcsim/internal/weapons/sword/flute"
 	_ "github.com/genshinsim/gcsim/internal/weapons/sword/freedom"
 	_ "github.com/genshinsim/gcsim/internal/weapons/sword/harbinger"
