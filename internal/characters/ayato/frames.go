261a18ab8d60d955aeb39258cd41d774c3ef37e4
Han Du <han.du@uwaterloo.ca>
2022-03-29 22:23:55 -0400

    ayato frames

--- internal/characters/ayato/frames.go
+++ internal/characters/ayato/frames.go
@@ -6,20 +6,25 @@ func (c *char) ActionFrames(a core.ActionType, p map[string]int) (int, int) {
 	switch a {
 	case core.ActionAttack:
 		f := 0
-		switch c.NormalCounter {
-		//TODO: need to add atkspd mod
-		case 0:
-			f = 8
-		case 1:
-			f = 20
-		case 2:
-			f = 28
-		case 3:
-			f = 43
-		case 4:
-			f = 37
+		if c.Core.Status.Duration("soukaikanka") > 0 {
+			f = 24
+		} else {
+			switch c.NormalCounter {
+			//TODO: need to add atkspd mod
+			case 0:
+				f = 8
+			case 1:
+				f = 20
+			case 2:
+				f = 28
+			case 3:
+				f = 43
+			case 4:
+				f = 37
+			}
+
+			f = int(float64(f) / (1 + c.Stat(core.AtkSpd)))
 		}
-		f = int(float64(f) / (1 + c.Stat(core.AtkSpd)))
 		return f, f
 	case core.ActionCharge:
 		return 53, 53
