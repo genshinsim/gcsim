a572ca5daf4aaa4007572322aff62fd60d1dfa37
Shizuka <shizuka.alpha@gmail.com>
2022-04-09 10:39:39 +0400

    Update Noelle frames

--- internal/characters/noelle/noelle.go
+++ internal/characters/noelle/noelle.go
@@ -34,9 +34,14 @@ func NewChar(s *core.Core, p core.CharacterProfile) (core.Character, error) {
 	c.Weapon.Class = core.WeaponClassClaymore
 	c.NormalHitNum = 4
 
-	c.a2()
-
 	return &c, nil
+}
+
+func (c *char) Init() {
+	c.Tmpl.Init()
+	c.InitCancelFrames()
+
+	c.a2()
 }
 
 /**
