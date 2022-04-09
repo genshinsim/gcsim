5c9125db593b5a4376a8a6a033f15a98bbb86d3c
Bryan <bryanck29@gmail.com>
2022-04-10 06:58:39 +0700

    bugfix(lisa burst) fix lisa's burst frame data for cooldown and the starting burst action frames

--- internal/characters/lisa/abil.go
+++ internal/characters/lisa/abil.go
@@ -227,9 +227,11 @@ func (c *char) Burst(p map[string]int) (int, int) {
 	//[8:11 PM] gimmeabreak: i guess single target it does nothing then?
 	//[8:12 PM] ArchedNosi | Lisa Unleashed: yeah single does nothing
 
+	//burst cd starts 52 frames after executed
+	//energy consumed the same time as the initial hit (64 frames)
 	c.ConsumeEnergy(64)
 	// c.CD[def.BurstCD] = c.Core.F + 1200
-	c.SetCDWithDelay(core.ActionBurst, 1200, 64)
+	c.SetCDWithDelay(core.ActionBurst, 1200, 52)
 	return f, a
 }
 
