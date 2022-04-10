1e7c6d9c7a516ac1f62d2ff2b89cf5e303fefae4
Jordan Lovato <jordanlovato@gmail.com>
2022-04-10 12:02:23 -0700

    Fix formatting

--- internal/artifacts/echoes/echoes.go
+++ internal/artifacts/echoes/echoes.go
@@ -30,32 +30,40 @@ func New(c core.Character, s *core.Core, count int, params map[string]int) {
 
 	if count >= 4 {
 		s.Events.Subscribe(core.OnAttackWillLand, func(args ...interface{}) bool {
+			// if the active char is not the equipped char then ignore
+			// if the active char is not the equipped char then ignore
 			if s.ActiveChar != c.CharIndex() {
 				return false
 			}
+
 			atk := args[1].(*core.AttackEvent)
+
+			// If this is not a normal attack then ignore
 			if atk.Info.AttackTag != core.AttackTagNormal {
 				return false
 			}
 
-			if s.F > icd {
-				if s.Rand.Float64() < prob {
-					dmgAdded := (atk.Snapshot.BaseAtk*(1+atk.Snapshot.Stats[core.ATKP]) + atk.Snapshot.Stats[core.ATK]) * 0.7
-					atk.Info.FlatDmg += dmgAdded
-					s.Log.NewEvent("echoes 4pc proc", core.LogArtifactEvent, c.CharIndex(),
-						"probability", prob,
-						"dmg_added", dmgAdded,
-					)
+			// If Artifact set effect is still on CD then ignore
+			if s.F < icd {
+				return false
+			}
 
-					prob = 0.36
-					icd = s.F + 0.2*60
-				} else {
-					prob += 0.2
-					icd = s.F + 0.2*60
+			if s.Rand.Float64() < prob {
+				dmgAdded := (atk.Snapshot.BaseAtk*(1+atk.Snapshot.Stats[core.ATKP]) + atk.Snapshot.Stats[core.ATK]) * 0.7
+				atk.Info.FlatDmg += dmgAdded
+				s.Log.NewEvent("echoes 4pc proc", core.LogArtifactEvent, c.CharIndex(),
+					"probability", prob,
+					"dmg_added", dmgAdded,
+				)
 
-					if prob > 1 {
-						prob = 1
-					}
+				prob = 0.36
+				icd = s.F + 0.2*60
+			} else {
+				prob += 0.2
+				icd = s.F + 0.2*60
+
+				if prob > 1 {
+					prob = 1
 				}
 			}
 
