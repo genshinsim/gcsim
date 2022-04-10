72f0a6369fd1153a27caf1f5ba988c53f0143818
Jordan Lovato <jordanlovato@gmail.com>
2022-04-10 11:56:32 -0700

    Tweak Echoes ICD
    * Fix icd on echoes set (previously could proc multiple times within 0.2s if you were lucky enough.

--- internal/artifacts/echoes/echoes.go
+++ internal/artifacts/echoes/echoes.go
@@ -14,7 +14,8 @@ func init() {
 
 func New(c core.Character, s *core.Core, count int, params map[string]int) {
 	prob := 0.36
-	probicd := 0
+	icd := 0
+
 	if count >= 2 {
 		m := make([]float64, core.EndStatType)
 		m[core.ATKP] = 0.18
@@ -37,25 +38,27 @@ func New(c core.Character, s *core.Core, count int, params map[string]int) {
 				return false
 			}
 
-			if s.Rand.Float64() < prob {
-				//TODO: need to check if this actually snapshots here
-				// snap := c.Snapshot(&atk.Info)
-				dmgAdded := (atk.Snapshot.BaseAtk*(1+atk.Snapshot.Stats[core.ATKP]) + atk.Snapshot.Stats[core.ATK]) * 0.7
-				atk.Info.FlatDmg += dmgAdded
-				s.Log.NewEvent("echoes 4pc proc", core.LogArtifactEvent, c.CharIndex(),
-					"probability", prob,
-					"dmg_added", dmgAdded,
-				)
-				prob = 0.36
-			} else {
-				if s.F > probicd {
+			if s.F > icd {
+				if s.Rand.Float64() < prob {
+					dmgAdded := (atk.Snapshot.BaseAtk*(1+atk.Snapshot.Stats[core.ATKP]) + atk.Snapshot.Stats[core.ATK]) * 0.7
+					atk.Info.FlatDmg += dmgAdded
+					s.Log.NewEvent("echoes 4pc proc", core.LogArtifactEvent, c.CharIndex(),
+						"probability", prob,
+						"dmg_added", dmgAdded,
+					)
+
+					prob = 0.36
+					icd = s.F + 0.2*60
+				} else {
 					prob += 0.2
-					probicd = s.F + 0.2*60
-				}
-				if prob > 1 {
-					prob = 1
+					icd = s.F + 0.2*60
+
+					if prob > 1 {
+						prob = 1
+					}
 				}
 			}
+
 			return false
 		}, fmt.Sprintf("echoes-4pc-%v", c.Name()))
 	}
