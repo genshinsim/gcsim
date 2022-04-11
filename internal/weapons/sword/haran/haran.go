9a3b8d67eca5454d393eca1c7c553fbaedab7304
T N <truong1320vv@gmail.com>
2022-04-11 21:19:35 +0700

    haran fix

--- internal/weapons/sword/haran/haran.go
+++ internal/weapons/sword/haran/haran.go
@@ -14,7 +14,7 @@ func init() {
 
 func weapon(char core.Character, c *core.Core, r int, param map[string]int) string {
 	m := make([]float64, core.EndStatType)
-	base := 0.12 + float64(r)*0.03
+	base := 0.09 + float64(r)*0.03
 	m[core.PyroP] = base
 	m[core.HydroP] = base
 	m[core.CryoP] = base
@@ -25,6 +25,7 @@ func weapon(char core.Character, c *core.Core, r int, param map[string]int) stri
 	wavespikeICD := 0
 	wavespikeStacks := 0
 	maxWavespikeStacks := 2
+	stackSnapshot := 0
 
 	char.AddMod(core.CharStatMod{
 		Key: "haran-ele-bonus",
@@ -34,25 +35,28 @@ func weapon(char core.Character, c *core.Core, r int, param map[string]int) stri
 		Expiry: -1,
 	})
 
-	c.Events.Subscribe(core.OnAttackWillLand, func(args ...interface{}) bool {
+	c.Events.Subscribe(core.PostSkill, func(args ...interface{}) bool {
 		if c.ActiveChar == char.CharIndex() {
 			return false
 		}
-		if c.F > wavespikeICD && args[1].(*core.AttackEvent).Info.AttackTag == core.AttackTagNormal {
+		if c.F > wavespikeICD {
 			wavespikeStacks++
 			if wavespikeStacks > maxWavespikeStacks {
 				wavespikeStacks = maxWavespikeStacks
 			}
+			c.Log.NewEvent("Haran gained a wavespike stack", core.LogWeaponEvent, char.CharIndex(), "stack", wavespikeStacks)
 			wavespikeICD = c.F + 0.3*60
 		}
 
 		return false
 	}, fmt.Sprintf("wavespike-%v", char.Name()))
+
 	val := make([]float64, core.EndStatType)
 	c.Events.Subscribe(core.PostSkill, func(args ...interface{}) bool {
 		if c.ActiveChar != char.CharIndex() {
 			return false
 		}
+		stackSnapshot = wavespikeStacks
 		char.AddPreDamageMod(core.PreDamageMod{
 			Key:    "ripping-upheaval",
 			Expiry: c.F + 60*8,
@@ -60,7 +64,7 @@ func weapon(char core.Character, c *core.Core, r int, param map[string]int) stri
 				if atk.Info.AttackTag != core.AttackTagNormal {
 					return nil, false
 				}
-				val[core.DmgP] = (0.2 * float64(r) * 0.04) * float64(wavespikeStacks)
+				val[core.DmgP] = (0.15 + float64(r)*0.05) * float64(stackSnapshot)
 				return val, true
 			},
 		})
