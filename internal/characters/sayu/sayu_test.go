a15733b684c042467b14cf79b411c0214f988eab
imring <vitaliyvorobets25@gmail.com>
2022-04-12 19:29:21 +0300

    fix skill tests

--- internal/characters/sayu/sayu_test.go
+++ internal/characters/sayu/sayu_test.go
@@ -1,6 +1,8 @@
 package sayu
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/genshinsim/gcsim/internal/testhelper"
@@ -28,8 +30,51 @@ func TestCD(t *testing.T) {
 		t.Error(err)
 		t.FailNow()
 	}
-	err = testhelper.TestSkillCDSingleCharge(c, x, 6*60)
-	if err != nil {
-		t.Error(err)
+
+	// skill press
+	cd := 6 * 60
+	p := make(map[string]int)
+	p["hold"] = 0
+	x.Skill(p)
+
+	// +1 = -1 + 2
+	testhelper.SkipFrames(c, 15+cd+1)
+	if x.ActionReady(core.ActionSkill, p) {
+		t.Error(errors.New("skill shouldn't be ready yet"))
+	}
+	v := x.Cooldown(core.ActionSkill)
+	if v != 1 {
+		t.Error(fmt.Errorf("expecting cooldown to be 1, got %v", v))
+	}
+	testhelper.SkipFrames(c, 1)
+	if !x.ActionReady(core.ActionSkill, p) {
+		t.Error(errors.New("skill should be ready now"))
+	}
+	v = x.Cooldown(core.ActionSkill)
+	if v != 0 {
+		t.Error(fmt.Errorf("expecting cooldown to be 0, got %v", v))
+	}
+
+	// skill hold
+	p["hold"] = 300
+	cd = 6*60 + 300 + 150 // 150 = 300 * 0.5
+	x.Skill(p)
+
+	// +1 = -1 + 2
+	testhelper.SkipFrames(c, 15+cd+1)
+	if x.ActionReady(core.ActionSkill, p) {
+		t.Error(errors.New("skill shouldn't be ready yet"))
+	}
+	v = x.Cooldown(core.ActionSkill)
+	if v != 1 {
+		t.Error(fmt.Errorf("expecting cooldown to be 1, got %v", v))
+	}
+	testhelper.SkipFrames(c, 1)
+	if !x.ActionReady(core.ActionSkill, p) {
+		t.Error(errors.New("skill should be ready now"))
+	}
+	v = x.Cooldown(core.ActionSkill)
+	if v != 0 {
+		t.Error(fmt.Errorf("expecting cooldown to be 0, got %v", v))
 	}
 }
