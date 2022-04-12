d1a623d16c853ed4e6fbd24750f9dd42e70fb037
Jordan <jordanlovato@gmail.com>
2022-04-12 08:57:13 -0700

    Tweak Echoes ICD (#454)
    * Tweak Echoes ICD
    
    * Fix icd on echoes set (previously could proc multiple times within 0.2s if you were lucky enough.
    
    * Fix formatting
    
    * Fix formatting
    
    * Move ICD assign to just below the ICD check for Echoes
    
    * Adjust Echoes 4pc impl
    
    Test: https://gcsim.app/viewer/share/37wTeuUdu03fk0r7dbakx
    * Add 2pc/4pc artifact set description as a comment in the Echoes of an offering artifact set
    * Add flat dmg as a core.StatType
    * Include core.Dmg as a factor in attack.go damage calculation + log
    * Update echoes-4pc implementation to add a flat dmg mod, then onDamage, apply a 3F expiry to the flat dmg mod.
    
    * Adjust Echoes 4pc impl
    
    Test: https://gcsim.app/viewer/share/Qor6YB6h9mCUZUl2cH0Jz
    * Use PreDamageMod implementation to manually adjust the flat damage on the attacks where the echoes set is active
    * Track attacks that proc the echoes set, so we do not accidentally buff the same attack twice (multi-hits still receive the extra damage on each hit, this just prevents any single hit from being buffed multiple times)
    
    * Use OnAttackWillLand hook, instead of onDamage
    
    * Remove preDamageMod, manually manage echoes proc expiry
    
    * Simplify diff
    
    * Apply srl's patch, change event type to OnAttackWillLand
    
    * add return false if buff currently active
    
    Co-authored-by: srl <906239+srliao@users.noreply.github.com>

--- internal/artifacts/echoes/echoes.go
+++ internal/artifacts/echoes/echoes.go
@@ -12,9 +12,17 @@ func init() {
 	core.RegisterSetFunc("echoes", New)
 }
 
+// 2pc - ATK +18%.
+// 4pc - When Normal Attacks hit opponents, there is a 36% chance that it will trigger Valley Rite, which will increase Normal Attack DMG by 70% of ATK.
+//  This effect will be dispelled 0.05s after a Normal Attack deals DMG.
+//  If a Normal Attack fails to trigger Valley Rite, the odds of it triggering the next time will increase by 20%.
+//  This trigger can occur once every 0.2s.
 func New(c core.Character, s *core.Core, count int, params map[string]int) {
 	prob := 0.36
-	probicd := 0
+	icd := 0
+	procDuration := 3 // 0.05s
+	procExpireF := 0
+
 	if count >= 2 {
 		m := make([]float64, core.EndStatType)
 		m[core.ATKP] = 0.18
@@ -27,35 +35,63 @@ func New(c core.Character, s *core.Core, count int, params map[string]int) {
 		})
 	}
 
+	var dmgAdded float64
+
 	if count >= 4 {
 		s.Events.Subscribe(core.OnAttackWillLand, func(args ...interface{}) bool {
+			// if the active char is not the equipped char then ignore
 			if s.ActiveChar != c.CharIndex() {
 				return false
 			}
+
 			atk := args[1].(*core.AttackEvent)
+
+			// If attack does not belong to the equipped character then ignore
+			if atk.Info.ActorIndex != c.CharIndex() {
+				return false
+			}
+
+			// If this is not a normal attack then ignore
 			if atk.Info.AttackTag != core.AttackTagNormal {
 				return false
 			}
 
-			if s.Rand.Float64() < prob {
-				//TODO: need to check if this actually snapshots here
-				// snap := c.Snapshot(&atk.Info)
-				dmgAdded := (atk.Snapshot.BaseAtk*(1+atk.Snapshot.Stats[core.ATKP]) + atk.Snapshot.Stats[core.ATK]) * 0.7
+			// if buff is already active then buff attack
+			if s.F < procExpireF {
+				dmgAdded = (atk.Snapshot.BaseAtk*(1+atk.Snapshot.Stats[core.ATKP]) + atk.Snapshot.Stats[core.ATK]) * 0.7
 				atk.Info.FlatDmg += dmgAdded
-				s.Log.NewEvent("echoes 4pc proc", core.LogArtifactEvent, c.CharIndex(),
-					"probability", prob,
+				s.Log.NewEvent("echoes 4pc adding dmg", core.LogArtifactEvent, c.CharIndex(),
+					"buff_expiry", procExpireF,
 					"dmg_added", dmgAdded,
 				)
-				prob = 0.36
-			} else {
-				if s.F > probicd {
-					prob += 0.2
-					probicd = s.F + 0.2*60
-				}
-				if prob > 1 {
-					prob = 1
-				}
+				return false
 			}
+
+			// If Artifact set effect is still on CD then ignore
+			if s.F < icd {
+				return false
+			}
+
+			if s.Rand.Float64() > prob {
+				prob += 0.2
+				return false
+			}
+
+			dmgAdded = (atk.Snapshot.BaseAtk*(1+atk.Snapshot.Stats[core.ATKP]) + atk.Snapshot.Stats[core.ATK]) * 0.7
+			atk.Info.FlatDmg += dmgAdded
+
+			procExpireF = s.F + procDuration
+			icd = s.F + 12 //0.2s
+
+			s.Log.NewEvent("echoes 4pc proc'd", core.LogArtifactEvent, c.CharIndex(),
+				"probability", prob,
+				"icd", icd,
+				"buff_expiry", procExpireF,
+				"dmg_added", atk.Info.FlatDmg,
+			)
+
+			prob = 0.36
+
 			return false
 		}, fmt.Sprintf("echoes-4pc-%v", c.Name()))
 	}
