f2d070e83bb6d5411e8c649c55e86a7869f6c8ca
srliao <906239+srliao@users.noreply.github.com>
2022-04-15 20:36:50 -0400

    move copy/send button to top

--- app/src/Components/Viewer/Summary.tsx
+++ app/src/Components/Viewer/Summary.tsx
@@ -3,58 +3,9 @@ import { SimResults } from "./DataType";
 import TeamView from "./Team/TeamView";
 import DPSOverTime from "./Graphs/DPSOverTime";
 import { Trans, useTranslation } from "react-i18next";
-import {
-  ButtonGroup,
-  Button,
-  Callout,
-  Checkbox,
-  Classes,
-  Dialog,
-} from "@blueprintjs/core";
-import React from "react";
-import { useLocation } from "wouter";
-import { updateCfg } from "~src/Pages/Sim";
-import { useAppDispatch } from "~src/store";
-import { AppToaster } from "./Share";
-
-const LOCALSTORAGE_KEY = "gcsim-viewer-cpy-cfg-settings";
 
 export default function Summary({ data }: { data: SimResults }) {
   let { t } = useTranslation();
-
-  const [open, setOpen] = React.useState<boolean>(false);
-  const dispatch = useAppDispatch();
-  const [_, setLocation] = useLocation();
-  const [keepExistingTeam, setKeepExistingTeam] = React.useState<boolean>(
-    () => {
-      const saved = localStorage.getItem(LOCALSTORAGE_KEY);
-      if (saved === "true") {
-        return true;
-      }
-      return false;
-    }
-  );
-
-  function copyToClipboard() {
-    navigator.clipboard.writeText(data.config_file).then(() => {
-      AppToaster.show({
-        message: t("viewer.copied_to_clipboard"),
-        intent: "success",
-      });
-    });
-    // TODO: Need to add a blueprintjs Toaster for ephemeral confirmation box
-  }
-
-  const openInSim = () => {
-    setOpen(false);
-    dispatch(updateCfg(data.config_file, keepExistingTeam));
-    setLocation("/simulator");
-  };
-
-  const handleToggleSelected = () => {
-    localStorage.setItem(LOCALSTORAGE_KEY, keepExistingTeam ? "false" : "true");
-    setKeepExistingTeam(!keepExistingTeam);
-  };
 
   //calculate per target damage
   let trgs: JSX.Element[] = [];
@@ -99,16 +50,6 @@ export default function Summary({ data }: { data: SimResults }) {
 
   return (
     <div className="wide:w-[70rem] ml-auto mr-auto">
-      <div className="flex flex-row justify-end m-2">
-        <ButtonGroup>
-          <Button onClick={copyToClipboard} icon="clipboard">
-            <Trans>viewer.copy</Trans>
-          </Button>
-          <Button onClick={() => setOpen(true)} icon="send-to">
-            <Trans>viewer.send_to_simulator</Trans>
-          </Button>
-        </ButtonGroup>
-      </div>
       <TeamView team={data.char_details} />
       <div className="bg-gray-600 relative rounded-md p-2 m-2 pt-10">
         <DPSOverTime data={data} />
@@ -183,32 +124,6 @@ export default function Summary({ data }: { data: SimResults }) {
       </div>
 
       <Graphs data={data} />
-
-      <Dialog isOpen={open} onClose={() => setOpen(false)}>
-        <div className={Classes.DIALOG_BODY}>
-          <Trans>viewer.load_this_configuration</Trans>
-          <Callout intent="warning" className="mt-2">
-            <Trans>viewer.this_will_overwrite</Trans>
-          </Callout>
-          <Checkbox
-            label="Copy action list only (ignore character stats)"
-            className="mt-2"
-            checked={keepExistingTeam}
-            onClick={handleToggleSelected}
-          />
-        </div>
-
-        <div className={Classes.DIALOG_FOOTER}>
-          <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-            <Button onClick={openInSim} intent="primary">
-              <Trans>viewer.continue</Trans>
-            </Button>
-            <Button onClick={() => setOpen(false)}>
-              <Trans>viewer.cancel</Trans>
-            </Button>
-          </div>
-        </div>
-      </Dialog>
     </div>
   );
 }
