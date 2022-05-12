1878fb673387b96c31fbbb534663048e26965605
kurt22i <streamratings2@gmail.com>
2022-05-11 21:05:33 -0400

    fix

--- pkg/simulation/run.go
+++ pkg/simulation/run.go
@@ -133,7 +133,7 @@ func (s *Simulation) AdvanceFrame() error {
 		}
 
 		//other wise we can add delay
-		if delay > 0 {
+		if delay > 0 && s.lastDelayAt < s.lastActionUsedAt {
 			s.C.Log.NewEvent(
 				"animation delay triggered",
 				core.LogActionEvent,
@@ -143,13 +143,17 @@ func (s *Simulation) AdvanceFrame() error {
 				"default_delays", s.C.Flags.Delays,
 			)
 			s.skip = delay - 1
+			s.lastDelayAt = s.C.F
 			return nil
 		}
 	}
 
 	s.skip, done, err = s.C.Action.Exec(s.queue[0])
 	//last action used should then be current frame + how much we are skipping (i.e. first frame queueable)
-	s.lastActionUsedAt = s.C.F + s.skip
+	//if skip is 0, the action either failed or was invalid.
+	if s.skip > 0 {
+		s.lastActionUsedAt = s.C.F + s.skip
+	}
 	if err != nil {
 		return err
 	}
