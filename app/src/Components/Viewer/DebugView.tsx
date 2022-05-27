fad39f70570c37f16172f23332ef966ac983158a
srl <906239+srliao@users.noreply.github.com>
2022-05-27 07:50:19 -0400

    fix searchbar location (#84)

--- app/src/Components/Viewer/DebugView.tsx
+++ app/src/Components/Viewer/DebugView.tsx
@@ -3,11 +3,7 @@ import { DebugItem, DebugRow } from "./parse";
 import { useVirtual } from "react-virtual";
 import AutoSizer from "react-virtualized-auto-sizer";
 import React from "react";
-import {
-  Button,
-  FormGroup,
-  InputGroup,
-} from "@blueprintjs/core";
+import { Button, FormGroup, InputGroup } from "@blueprintjs/core";
 
 type buffSetting = {
   start: number;
@@ -71,7 +67,15 @@ const Row = ({
 
 let lastSearchIndex: number = 0;
 
-export function Debugger({ data, team, searchable }: { data: DebugRow[]; team: string[], searchable: { [key: number]: any } }) {
+export function Debugger({
+  data,
+  team,
+  searchable,
+}: {
+  data: DebugRow[];
+  team: string[];
+  searchable: { [key: number]: any };
+}) {
   const parentRef = React.useRef<HTMLDivElement>(null!);
   const searchRef = React.useRef<HTMLInputElement>(null!);
   const [hl, sethl] = React.useState<buffSetting>({
@@ -119,15 +123,15 @@ export function Debugger({ data, team, searchable }: { data: DebugRow[]; team: s
         if (msg.indexOf(val) > -1) {
           console.log(index, lastSearchIndex);
           lastSearchIndex = index + 1;
-          rowVirtualizer.scrollToIndex(index, { align: 'start' });
+          rowVirtualizer.scrollToIndex(index, { align: "start" });
           return;
         }
       }
     }
-  }
+  };
 
   return (
-    <div className="h-full m-2 p-2 rounded-md bg-gray-600 text-xs flex flex-col min-w-[60rem] min-h-[20rem]">
+    <div className="flex flex-col h-full">
       <FormGroup label="Search" inline>
         <InputGroup
           type="text"
@@ -137,91 +141,96 @@ export function Debugger({ data, team, searchable }: { data: DebugRow[]; team: s
               <Button
                 icon="arrow-down"
                 intent="warning"
-                onClick={() => { searchAndScroll(searchRef.current.value) }}
+                onClick={() => {
+                  searchAndScroll(searchRef.current.value);
+                }}
               />
               <Button
                 icon="reset"
                 intent="warning"
-                onClick={() => { 
-                  searchRef.current.value = ''; lastSearchIndex = 0; rowVirtualizer.scrollToIndex(0) 
+                onClick={() => {
+                  searchRef.current.value = "";
+                  lastSearchIndex = 0;
+                  rowVirtualizer.scrollToIndex(0);
                 }}
               />
             </FormGroup>
           }
         />
       </FormGroup>
-
-      <AutoSizer defaultHeight={100}>
-        {({ height, width }) => (
-          <div
-            ref={parentRef}
-            style={{
-              minHeight: "100px",
-              height: height,
-              width: width,
-              overflow: "auto",
-              position: "relative",
-            }}
-            id="resize-inner"
-          >
-            <div className="flex flex-row debug-header">
-              <div
-                className={
-                  "font-medium text-lg text-gray-100 border-b-2 border-gray-500 text-right "
-                }
-                style={{ minWidth: "100px" }}
-              >
-                F | Sec
-              </div>
-              <div className="grid grid-cols-5 flex-grow">
-                <div className="font-medium text-lg text-gray-100 border-l-2 border-b-2 border-gray-500">
-                  Sim
-                </div>
-                {char}
-              </div>
-              <div style={{ width: "20px", minWidth: "20px" }} />
-            </div>
+      <div className="h-full ml-2 mr-2 p-2 rounded-md bg-gray-600 text-xs grow flex flex-col min-w-[60rem] min-h-[20rem]">
+        <AutoSizer defaultHeight={100}>
+          {({ height, width }) => (
             <div
-              className="ListInner"
+              ref={parentRef}
               style={{
-                // Set the scrolling inner div of the parent to be the
-                // height of all items combined. This makes the scroll bar work.
-                height: `${rowVirtualizer.totalSize}px`,
-                width: "100%",
+                minHeight: "100px",
+                height: height,
+                width: width,
+                overflow: "auto",
                 position: "relative",
               }}
+              id="resize-inner"
             >
-              {
-                // The meat and potatoes, an array of the virtual items
-                // we currently want to render and their index in the original data.
-              }
-              {rowVirtualizer.virtualItems.map((virtualRow) => (
+              <div className="flex flex-row debug-header">
                 <div
-                  key={virtualRow.index}
-                  // ref={virtualRow.measureRef}
-                  ref={(el) => virtualRow.measureRef(el)}
-                  style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    // Positions the virtual elements at the right place in container.
-                    // minHeight: `${virtualRow.size - 10}px`,
-                    transform: `translateY(${virtualRow.start}px)`,
-                  }}
-                  // id={"virtual-row-"+virtualRow.key}
+                  className={
+                    "font-medium text-lg text-gray-100 border-b-2 border-gray-500 text-right "
+                  }
+                  style={{ minWidth: "100px" }}
                 >
-                  <Row
-                    row={data[virtualRow.index]}
-                    highlight={hl}
-                    showBuffDuration={handleShowBuffDuration}
-                  />
+                  F | Sec
                 </div>
-              ))}
+                <div className="grid grid-cols-5 flex-grow">
+                  <div className="font-medium text-lg text-gray-100 border-l-2 border-b-2 border-gray-500">
+                    Sim
+                  </div>
+                  {char}
+                </div>
+                <div style={{ width: "20px", minWidth: "20px" }} />
+              </div>
+              <div
+                className="ListInner"
+                style={{
+                  // Set the scrolling inner div of the parent to be the
+                  // height of all items combined. This makes the scroll bar work.
+                  height: `${rowVirtualizer.totalSize}px`,
+                  width: "100%",
+                  position: "relative",
+                }}
+              >
+                {
+                  // The meat and potatoes, an array of the virtual items
+                  // we currently want to render and their index in the original data.
+                }
+                {rowVirtualizer.virtualItems.map((virtualRow) => (
+                  <div
+                    key={virtualRow.index}
+                    // ref={virtualRow.measureRef}
+                    ref={(el) => virtualRow.measureRef(el)}
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      // Positions the virtual elements at the right place in container.
+                      // minHeight: `${virtualRow.size - 10}px`,
+                      transform: `translateY(${virtualRow.start}px)`,
+                    }}
+                    // id={"virtual-row-"+virtualRow.key}
+                  >
+                    <Row
+                      row={data[virtualRow.index]}
+                      highlight={hl}
+                      showBuffDuration={handleShowBuffDuration}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        )}
-      </AutoSizer>
+          )}
+        </AutoSizer>
+      </div>
     </div>
   );
 }
