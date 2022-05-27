414842be5e9367016301c842c502eff8798dd3c3
Shizuka <shizuka.alpha@gmail.com>
2022-05-28 02:37:23 +0400

    Fix kokomi

---
+++ internal/template/characters/kokomi/attack.go
@@ -0,0 +1,56 @@
+package kokomi
+
+import (
+	"fmt"
+
+	"github.com/genshinsim/gcsim/internal/frames"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+)
+
+var attackFrames [][]int
+var attackHitmarks = []int{4, 12, 28}
+
+// Standard attack damage function
+// Has "travel" parameter, used to set the number of frames that the projectile is in the air (default = 10)
+func (c *char) Attack(p map[string]int) action.ActionInfo {
+	travel, ok := p["travel"]
+	if !ok {
+		travel = 10
+	}
+
+	ai := combat.AttackInfo{
+		ActorIndex: c.Index,
+		Abil:       fmt.Sprintf("Normal %v", c.NormalCounter),
+		AttackTag:  combat.AttackTagNormal,
+		ICDTag:     combat.ICDTagNormalAttack,
+		ICDGroup:   combat.ICDGroupDefault,
+		Element:    attributes.Hydro,
+		Durability: 25,
+		Mult:       attack[c.NormalCounter][c.TalentLvlAttack()],
+	}
+	ai.FlatDmg = c.burstDmgBonus(ai.AttackTag)
+
+	// TODO: Assume that this is not dynamic (snapshot on projectile release)
+	c.Core.QueueAttack(
+		ai,
+		combat.NewDefSingleTarget(1, combat.TargettableEnemy),
+		attackHitmarks[c.NormalCounter],
+		attackHitmarks[c.NormalCounter]+travel,
+	)
+	if c.NormalCounter == c.NormalHitNum-1 {
+		c.c1(attackHitmarks[c.NormalCounter], travel)
+	}
+
+	defer c.AdvanceNormalIndex()
+
+	return action.ActionInfo{
+		Frames:          frames.NewAttackFunc(c.Character, attackFrames),
+		AnimationLength: attackFrames[c.NormalCounter][action.InvalidAction],
+		CanQueueAfter:   attackHitmarks[c.NormalCounter],
+		Post:            attackHitmarks[c.NormalCounter],
+		State:           action.NormalAttackState,
+	}
+
+}
