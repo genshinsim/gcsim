c0a4085451975e4e4f3113cf6210ef40620c5999
Shizuka <shizuka.alpha@gmail.com>
2022-05-28 11:24:19 +0400

    Fix ganyu

--- internal/template/characters/ganyu/cons.go
+++ internal/template/characters/ganyu/cons.go
@@ -2,25 +2,42 @@ package ganyu
 
 import (
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/event"
 )
 
 func (c *char) c1() {
-	c.Core.Events.Subscribe(core.OnDamage, func(args ...interface{}) bool {
-		atk := args[1].(*core.AttackEvent)
-		t := args[0].(core.Target)
+	c.Core.Events.Subscribe(event.OnDamage, func(args ...interface{}) bool {
+		atk := args[1].(*combat.AttackEvent)
+		e, ok := args[0].(core.Enemy)
+		if !ok {
+			return false
+		}
 		if atk.Info.ActorIndex != c.Index {
 			return false
 		}
 		if atk.Info.Abil != "Frost Flake Arrow" {
 			return false
 		}
+
 		c.AddEnergy("ganyu-c1", 2)
-		t.AddResMod("ganyu-c1", core.ResistMod{
-			Ele:      core.Cryo,
-			Value:    -0.15,
-			Duration: 5 * 60,
-		})
+		e.AddResistMod("ganyu-c1", 5*60, attributes.Cryo, -0.15)
+
 		return false
 	}, "ganyu-c1")
+}
 
+func (c *char) c4() {
+	m := make([]float64, attributes.EndStatType)
+	for _, char := range c.Core.Player.Chars() {
+		char.AddAttackMod("ganyu-c4", -1, func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+			// reset stacks on expiry
+			if c.Core.F > t.GetTag("ganyuc4") {
+				c.c4Stacks = 0
+			}
+			m[attributes.DmgP] = float64(c.c4Stacks) * 0.05
+			return m, c.c4Stacks > 0
+		})
+	}
 }
--- internal/template/characters/ganyu/ganyu.go
+++ internal/template/characters/ganyu/ganyu.go
@@ -1,28 +1,36 @@
 package ganyu
 
 import (
-	"github.com/genshinsim/gcsim/internal/tmpl/character"
+	"github.com/genshinsim/gcsim/internal/frames"
+	tmpl "github.com/genshinsim/gcsim/internal/template/character"
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/core/player/weapon"
 )
 
+const normalHitNum = 6
+
 func init() {
-	core.RegisterCharFunc(core.Ganyu, NewChar)
+	initCancelFrames()
+	core.RegisterCharFunc(keys.Ganyu, NewChar)
 }
 
 type char struct {
-	*character.Tmpl
+	*tmpl.Character
 	a1Expiry int
 	c4Stacks int
 }
 
-func NewChar(s *core.Core, p core.CharacterProfile) (core.Character, error) {
+func NewChar(s *core.Core, w *character.CharWrapper, p character.CharacterProfile) error {
 	c := char{}
-	t, err := character.NewTemplateChar(s, p)
-	if err != nil {
-		return nil, err
-	}
-	c.Tmpl = t
-	c.Base.Element = core.Cryo
+	t := tmpl.New(s)
+	t.CharWrapper = w
+	c.Character = t
+
+	c.Base.Element = attributes.Cryo
 
 	e, ok := p.Params["start_energy"]
 	if !ok {
@@ -30,27 +38,53 @@ func NewChar(s *core.Core, p core.CharacterProfile) (core.Character, error) {
 	}
 	c.Energy = float64(e)
 	c.EnergyMax = 60
-	c.Weapon.Class = core.WeaponClassBow
-	c.NormalHitNum = 6
+	c.Weapon.Class = weapon.WeaponClassBow
+	c.NormalHitNum = normalHitNum
 	c.BurstCon = 3
 	c.SkillCon = 5
-	c.CharZone = core.ZoneLiyue
-	c.InitCancelFrames()
+	c.CharZone = character.ZoneLiyue
 
 	c.a1Expiry = -1
 	c.c4Stacks = 0
 
 	if c.Base.Cons >= 2 {
-		c.SetNumCharges(core.ActionSkill, 2)
+		c.SetNumCharges(action.ActionSkill, 2)
 	}
 
-	return &c, nil
+	w.Character = &c
+
+	return nil
 }
 
-func (c *char) Init() {
-	c.Tmpl.Init()
-
+func (c *char) Init() error {
 	if c.Base.Cons >= 1 {
 		c.c1()
 	}
+	if c.Base.Cons >= 4 {
+		c.c4()
+	}
+	return nil
+}
+
+func initCancelFrames() {
+	// NA cancels
+	attackFrames = make([][]int, normalHitNum)
+
+	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0], 19)
+	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1], 27)
+	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2], 38)
+	attackFrames[3] = frames.InitNormalCancelSlice(attackHitmarks[3], 37)
+	attackFrames[4] = frames.InitNormalCancelSlice(attackHitmarks[4], 28)
+	attackFrames[5] = frames.InitNormalCancelSlice(attackHitmarks[5], 59)
+
+	// aimed -> x
+	aimedFrames = frames.InitAbilSlice(113) // is this 103 or 113?
+
+	// skill -> x
+	skillFrames = frames.InitAbilSlice(28)
+	skillFrames[action.ActionSwap] = 27
+
+	// burst -> x
+	burstFrames = frames.InitAbilSlice(124)
+	burstFrames[action.ActionSwap] = 122
 }
