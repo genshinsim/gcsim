2c859118eea24f2dbe1ca4c8b1807c7b71ee0f97
imring <vitaliyvorobets25@gmail.com>
2022-05-31 01:43:27 +0300

    A4

--- internal/template/characters/razor/razor.go
+++ internal/template/characters/razor/razor.go
@@ -58,6 +58,8 @@ func (c *char) Init() error {
 	c.WolfBurst()
 	c.onSwapClearBurst()
 
+	c.a4()
+
 	return nil
 }
 
@@ -81,4 +83,17 @@ func (c *char) ActionStam(a action.Action, p map[string]int) float64 {
 		c.Core.Log.NewEvent("ActionStam not implemented", glog.LogActionEvent, c.Index, "action", a.String())
 		return 0
 	}
+}
+
+// When Razor's Energy is below 50%, increases Energy Recharge by 30%.
+func (c *char) a4() {
+	val := make([]float64, attributes.EndStatType)
+	val[attributes.ER] = 0.3
+	c.AddStatMod("er-sigil", -1, attributes.ER, func() ([]float64, bool) {
+		if c.Energy/c.EnergyMax < 0.5 {
+			return nil, false
+		}
+
+		return val, true
+	})
 }
