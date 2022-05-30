14669a5e765d9a20435b9666e7332daab0830175
imring <vitaliyvorobets25@gmail.com>
2022-05-31 02:00:56 +0300

    C2

--- internal/template/characters/razor/cons.go
+++ internal/template/characters/razor/cons.go
@@ -2,6 +2,7 @@ package razor
 
 import (
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
 	"github.com/genshinsim/gcsim/pkg/core/event"
 )
 
@@ -20,5 +21,18 @@ func (c *char) c1() {
 			return nil, false
 		}
 		return val, true
+	})
+}
+
+// Increases CRIT Rate against opponents with less than 30% HP by 10%.
+func (c *char) c2() {
+	m := make([]float64, attributes.EndStatType)
+	m[attributes.CR] = 0.1
+
+	c.AddAttackMod("razor-c2", -1, func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+		if t.HP()/t.MaxHP() < 0.3 {
+			return m, true
+		}
+		return nil, false
 	})
 }
