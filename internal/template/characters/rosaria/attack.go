178f0da4977a9f5d4fd8cde418c69ea7727aaff8
Shizuka <shizuka.alpha@gmail.com>
2022-05-31 05:45:22 +0400

    Fix rosaria

---
+++ internal/template/characters/rosaria/attack.go
@@ -0,0 +1,47 @@
+package rosaria
+
+import (
+	"fmt"
+
+	"github.com/genshinsim/gcsim/internal/frames"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+)
+
+var attackFrames [][]int
+var attackHitmarks = [][]int{{9}, {13}, {19, 28}, {32}, {26, 40}}
+
+// Normal attack damage queue generator
+// relatively standard with no major differences versus other characters
+func (c *char) Attack(p map[string]int) action.ActionInfo {
+	ai := combat.AttackInfo{
+		ActorIndex: c.Index,
+		Abil:       fmt.Sprintf("Normal %v", c.NormalCounter),
+		AttackTag:  combat.AttackTagNormal,
+		ICDTag:     combat.ICDTagNormalAttack,
+		ICDGroup:   combat.ICDGroupDefault,
+		Element:    attributes.Physical,
+		Durability: 25,
+	}
+
+	for i, mult := range attack[c.NormalCounter] {
+		ai.Mult = mult[c.TalentLvlAttack()]
+		c.Core.QueueAttack(
+			ai,
+			combat.NewDefCircHit(0.1, false, combat.TargettableEnemy),
+			attackHitmarks[c.NormalCounter][i],
+			attackHitmarks[c.NormalCounter][i],
+		)
+	}
+
+	defer c.AdvanceNormalIndex()
+
+	return action.ActionInfo{
+		Frames:          frames.NewAttackFunc(c.Character, attackFrames),
+		AnimationLength: attackFrames[c.NormalCounter][action.InvalidAction],
+		CanQueueAfter:   attackHitmarks[c.NormalCounter][len(attackHitmarks[c.NormalCounter])-1],
+		Post:            attackHitmarks[c.NormalCounter][len(attackHitmarks[c.NormalCounter])-1],
+		State:           action.NormalAttackState,
+	}
+}
