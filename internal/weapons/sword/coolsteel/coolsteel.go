9d0f19582a1af472d821fb9a1323702511ab9c4b
kurt22i <streamratings2@gmail.com>
2022-06-05 20:00:13 -0400

    implement cool steel
    its cope af

---
+++ internal/weapons/sword/coolsteel/coolsteel.go
@@ -0,0 +1,31 @@
+package coolsteel
+
+import (
+	"github.com/genshinsim/gcsim/pkg/core"
+)
+
+func init() {
+	core.RegisterWeaponFunc("coolsteel", weapon)
+}
+
+//Increases DMG against enemies affected by Hydro or Cryo by 12-24%.
+func weapon(char core.Character, c *core.Core, r int, param map[string]int) string {
+	dmg := 0.09 + float64(r)*0.03
+
+	char.AddPreDamageMod(core.PreDamageMod{
+		Key:    "coolsteel",
+		Expiry: -1,
+		Amount: func(atk *core.AttackEvent, t core.Target) ([]float64, bool) {
+			m := make([]float64, core.EndStatType)
+			if atk.Info.AttackTag > core.ReactionAttackDelim {
+				return nil, false
+			}
+			if t.AuraContains(core.Hydro, core.Cryo) {
+				m[core.DmgP] = dmg
+				return m, true
+			}
+			return nil, false
+		},
+	})
+	return "coolsteel"
+}
