629341937c42dbfa27d9851996a1d30c1fccafa1
imring <vitaliyvorobets25@gmail.com>
2022-06-10 17:43:40 +0300

    heartofdepth

--- internal/artifacts/heartofdepth/heartofdepth.go
+++ internal/artifacts/heartofdepth/heartofdepth.go
@@ -4,47 +4,60 @@ import (
 	"fmt"
 
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/event"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/artifact"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
 )
 
 func init() {
-	core.RegisterSetFunc("heart of depth", New)
-	core.RegisterSetFunc("heartofdepth", New)
+	core.RegisterSetFunc(keys.HeartOfDepth, NewSet)
 }
 
-func New(c core.Character, s *core.Core, count int, params map[string]int) {
+type Set struct {
+	key   string
+	Index int
+}
+
+func (s *Set) SetIndex(idx int) { s.Index = idx }
+func (s *Set) Init() error      { return nil }
+func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[string]int) (artifact.Set, error) {
+	s := Set{}
+	s.key = fmt.Sprintf("%v-hod-4pc", char.Base.Name)
+
 	if count >= 2 {
-		m := make([]float64, core.EndStatType)
-		m[core.HydroP] = 0.15
-		c.AddMod(core.CharStatMod{
-			Key: "hod-2pc",
-			Amount: func() ([]float64, bool) {
-				return m, true
-			},
-			Expiry: -1,
+		m := make([]float64, attributes.EndStatType)
+		m[attributes.HydroP] = 0.15
+		char.AddStatMod("hod-2pc", -1, attributes.HydroP, func() ([]float64, bool) {
+			return m, true
 		})
 	}
 	if count >= 4 {
-		key := fmt.Sprintf("%v-hod-4pc", c.Name())
+		m := make([]float64, attributes.EndStatType)
+		m[attributes.DmgP] = 0.30
 
-		m := make([]float64, core.EndStatType)
-		m[core.DmgP] = 0.3
-
-		s.Events.Subscribe(core.PostSkill, func(args ...interface{}) bool {
-			if s.ActiveChar != c.CharIndex() {
+		c.Events.Subscribe(event.PostSkill, func(args ...interface{}) bool {
+			if c.Player.Active() != char.Index {
 				return false
 			}
-			s.Status.AddStatus(key, 900) //activate for 15 seoncds
-			//add stat mod here
-			c.AddPreDamageMod(core.PreDamageMod{
-				Key: "hod-4pc",
-				Amount: func(atk *core.AttackEvent, t core.Target) ([]float64, bool) {
-					return m, (atk.Info.AttackTag == core.AttackTagNormal || atk.Info.AttackTag == core.AttackTagExtra)
+			c.Status.Add(s.key, 15*60)
+			// add stat mod here
+			char.AddAttackMod(
+				"hod-4pc",
+				c.F+900,
+				func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+					if atk.Info.AttackTag != combat.AttackTagNormal && atk.Info.AttackTag != combat.AttackTagExtra {
+						return nil, false
+					}
+					return m, true
 				},
-				Expiry: s.F + 900,
-			})
+			)
 			return false
-		}, fmt.Sprintf("hod4-%v", c.Name()))
+		}, s.key)
 
 	}
-	//add flat stat to char
+
+	return &s, nil
 }
