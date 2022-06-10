f7ddb7b6c798cc3835d9456a3ad0236d1e4c4ba4
imring <vitaliyvorobets25@gmail.com>
2022-06-10 18:43:06 +0300

    instructor

--- internal/artifacts/instructor/instructor.go
+++ internal/artifacts/instructor/instructor.go
@@ -1,63 +1,74 @@
 package instructor
 
 import (
+	"fmt"
+
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/event"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/artifact"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
 )
 
 func init() {
-	core.RegisterSetFunc("instructor", New)
+	core.RegisterSetFunc(keys.Instructor, NewSet)
 }
+
+type Set struct {
+	Index int
+}
+
+func (s *Set) SetIndex(idx int) { s.Index = idx }
+func (s *Set) Init() error      { return nil }
 
 // Implements Instructor artifact set:
 // 2-Piece Bonus: Increases Elemental Mastery by 80.
 // 4-Piece Bonus: Upon triggering an Elemental Reaction, increases all party members' Elemental Mastery by 120 for 8s.
-func New(c core.Character, s *core.Core, count int, params map[string]int) {
+func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[string]int) (artifact.Set, error) {
+	s := Set{}
+
 	if count >= 2 {
-		m := make([]float64, core.EndStatType)
-		m[core.EM] = 80
-		c.AddMod(core.CharStatMod{
-			Key: "instructor-2pc",
-			Amount: func() ([]float64, bool) {
-				return m, true
-			},
-			Expiry: -1,
+		m := make([]float64, attributes.EndStatType)
+		m[attributes.EM] = 80
+		char.AddStatMod("instructor-2pc", -1, attributes.EM, func() ([]float64, bool) {
+			return m, true
 		})
 	}
 	if count >= 4 {
-		m := make([]float64, core.EndStatType)
-		m[core.EM] = 120
+		m := make([]float64, attributes.EndStatType)
+		m[attributes.EM] = 120
 
 		add := func(args ...interface{}) bool {
-			atk := args[1].(*core.AttackEvent)
+			atk := args[1].(*combat.AttackEvent)
 			// Character must be on field to proc bonus
-			if s.ActiveChar != c.CharIndex() {
+			if c.Player.Active() != char.Index {
 				return false
 			}
 			// Source of elemental reaction must be the character with instructor
-			if atk.Info.ActorIndex != c.CharIndex() {
+			if atk.Info.ActorIndex != char.Index {
 				return false
 			}
 
 			// Add 120 EM to all characters except the one with instructor
-			for i, char := range s.Chars {
+			for i, this := range c.Player.Chars() {
 				// Skip the one with instructor
-				if i == c.CharIndex() {
+				if i == char.Index {
 					continue
 				}
 
-				char.AddMod(core.CharStatMod{
-					Key: "instructor-4pc",
-					Amount: func() ([]float64, bool) {
-						return m, true
-					},
-					Expiry: s.F + 480,
+				this.AddStatMod("instructor-4pc", c.F+480, attributes.EM, func() ([]float64, bool) {
+					return m, true
 				})
 			}
 			return false
 		}
 
-		for i := core.EventType(core.ReactionEventStartDelim + 1); i < core.ReactionEventEndDelim; i++ {
-			s.Events.Subscribe(i, add, "4ins"+c.Name())
+		for i := event.ReactionEventStartDelim + 1; i < event.ReactionEventEndDelim; i++ {
+			c.Events.Subscribe(i, add, fmt.Sprintf("instructor-4pc-%v", char.Base.Name))
 		}
 	}
+
+	return &s, nil
 }
