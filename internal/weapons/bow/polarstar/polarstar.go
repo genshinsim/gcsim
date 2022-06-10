533a799402ce3e8ad8f326fd12df8398bffe11eb
srliao <906239+srliao@users.noreply.github.com>
2022-06-09 22:03:18 -0400

    polarstar

--- internal/weapons/bow/polarstar/polarstar.go
+++ internal/weapons/bow/polarstar/polarstar.go
@@ -4,14 +4,32 @@ import (
 	"fmt"
 
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/event"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/core/player/weapon"
 )
 
 func init() {
-	core.RegisterWeaponFunc("polar star", weapon)
-	core.RegisterWeaponFunc("polarstar", weapon)
+
 }
 
-func weapon(char core.Character, c *core.Core, r int, param map[string]int) string {
+type Weapon struct {
+	Index int
+}
+
+func (w *Weapon) SetIndex(idx int) { w.Index = idx }
+func (w *Weapon) Init() error      { return nil }
+
+func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile) (weapon.Weapon, error) {
+	//Elemental Skill and Elemental Burst DMG increased by 12%. After a Normal Attack, Charged Attack,
+	//Elemental Skill or Elemental Burst hits an opponent, 1 stack of Ashen Nightstar will be gained for 12s.
+	//When 1/2/3/4 stacks of Ashen Nightstar are present, ATK is increased by 10/20/30/48%. The stack of Ashen
+	//Nightstar created by the Normal Attack, Charged Attack, Elemental Skill or Elemental Burst will be counted
+	//independently of the others.
+	w := &Weapon{}
+	r := p.Refine
 
 	dmg := .09 + float64(r)*.03
 	stack := .075 + float64(r)*.025
@@ -23,40 +41,36 @@ func weapon(char core.Character, c *core.Core, r int, param map[string]int) stri
 	burst := 0
 
 	mATK := make([]float64, attributes.EndStatType)
-	char.AddMod(core.CharStatMod{
-		Key:	"polar-star",
-		Expiry:	-1,
-		Amount: func() ([]float64, bool) {
-			count := 0
-			if normal > c.F {
-				count++
-			}
-			if charged > c.F {
-				count++
-			}
-			if skill > c.F {
-				count++
-			}
-			if burst > c.F {
-				count++
-			}
+	char.AddStatMod("polar-star", -1, attributes.NoStat, func() ([]float64, bool) {
+		count := 0
+		if normal > c.F {
+			count++
+		}
+		if charged > c.F {
+			count++
+		}
+		if skill > c.F {
+			count++
+		}
+		if burst > c.F {
+			count++
+		}
 
-			atkbonus := stack * float64(count)
-			if count >= 4 {
-				atkbonus += max
-			}
-			mATK[attributes.ATKP] = atkbonus
+		atkbonus := stack * float64(count)
+		if count >= 4 {
+			atkbonus += max
+		}
+		mATK[attributes.ATKP] = atkbonus
 
-			return mATK, true
-		},
+		return mATK, true
 	})
 
 	c.Events.Subscribe(event.OnDamage, func(args ...interface{}) bool {
 		atk := args[1].(*combat.AttackEvent)
-		if atk.Info.ActorIndex != char.CharIndex() {
+		if atk.Info.ActorIndex != char.Index {
 			return false
 		}
-		if c.ActiveChar != char.CharIndex() {
+		if c.Player.Active() != char.Index {
 			return false
 		}
 
@@ -73,19 +87,17 @@ func weapon(char core.Character, c *core.Core, r int, param map[string]int) stri
 		}
 
 		return false
-	}, fmt.Sprintf("polar-star-%v", char.Name()))
+	}, fmt.Sprintf("polar-star-%v", char.Base.Name))
 
 	mDmg := make([]float64, attributes.EndStatType)
 	mDmg[attributes.DmgP] = dmg
-	char.AddAttackMod("polar-star",
-		-1,
-		func(atk *combat.AttackEvent, t combat.Target,) ([]float64, bool) {
-			switch atk.Info.AttackTag {
-			case combat.AttackTagElementalArt, combat.AttackTagElementalArtHold, combat.AttackTagElementalBurst:
-				return mDmg, true
-			}
-			return nil, false
-		})
+	char.AddAttackMod("polar-star", -1, func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+		switch atk.Info.AttackTag {
+		case combat.AttackTagElementalArt, combat.AttackTagElementalArtHold, combat.AttackTagElementalBurst:
+			return mDmg, true
+		}
+		return nil, false
+	})
 
-	return "polarstar"
+	return w, nil
 }
