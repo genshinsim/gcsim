6e689fb3fc2248f312b34b9cd4942af3090c70a3
imring <vitaliyvorobets25@gmail.com>
2022-06-10 22:53:45 +0300

    wanderer

--- internal/artifacts/wanderer/wanderer.go
+++ internal/artifacts/wanderer/wanderer.go
@@ -2,42 +2,55 @@ package wanderer
 
 import (
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/artifact"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/core/player/weapon"
 )
 
 func init() {
-	core.RegisterSetFunc("wanderer's troupe", New)
-	core.RegisterSetFunc("wandererstroupe", New)
+	core.RegisterSetFunc(keys.WanderersTroupe, NewSet)
 }
 
-func New(c core.Character, s *core.Core, count int, params map[string]int) {
+type Set struct {
+	Index int
+}
+
+func (s *Set) SetIndex(idx int) { s.Index = idx }
+func (s *Set) Init() error      { return nil }
+func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[string]int) (artifact.Set, error) {
+	s := Set{}
+
 	if count >= 2 {
-		m := make([]float64, core.EndStatType)
-		m[core.EM] = 80
-		c.AddMod(core.CharStatMod{
-			Key: "wt-2pc",
-			Amount: func() ([]float64, bool) {
-				return m, true
-			},
-			Expiry: -1,
+		m := make([]float64, attributes.EndStatType)
+		m[attributes.EM] = 80
+		char.AddStatMod("wt-2pc", -1, attributes.CryoP, func() ([]float64, bool) {
+			return m, true
 		})
 	}
 	if count >= 4 {
-		switch c.WeaponClass() {
-		case core.WeaponClassCatalyst:
-		case core.WeaponClassBow:
+		switch char.Weapon.Class {
+		case weapon.WeaponClassCatalyst:
+		case weapon.WeaponClassBow:
 		default:
-			//don't add this mod if wrong weapon class
-			return
+			// don't add this mod if wrong weapon class
+			return &s, nil
 		}
-		m := make([]float64, core.EndStatType)
-		m[core.DmgP] = 0.35
-		c.AddPreDamageMod(core.PreDamageMod{
-			Key: "wt-4pc",
-			Amount: func(atk *core.AttackEvent, t core.Target) ([]float64, bool) {
-				return m, atk.Info.AttackTag == core.AttackTagNormal || atk.Info.AttackTag == core.AttackTagExtra
+		m := make([]float64, attributes.EndStatType)
+		m[attributes.DmgP] = 0.35
+		char.AddAttackMod(
+			"wt-4pc",
+			-1,
+			func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+				if atk.Info.AttackTag != combat.AttackTagNormal && atk.Info.AttackTag != combat.AttackTagExtra {
+					return nil, false
+				}
+				return m, true
 			},
-			Expiry: -1,
-		})
+		)
 	}
-	//add flat stat to char
+
+	return &s, nil
 }
