4ea8e8a9ce37712fb6a98507144c960cec4363ff
srliao <906239+srliao@users.noreply.github.com>
2022-06-10 07:46:12 -0400

    clean up kagura

--- internal/weapons/catalyst/kagura/kagura.go
+++ internal/weapons/catalyst/kagura/kagura.go
@@ -24,56 +24,63 @@ func (w *Weapon) SetIndex(idx int) { w.Index = idx }
 func (w *Weapon) Init() error      { return nil }
 
 func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile) (weapon.Weapon, error) {
+	//Gains the Kagura Dance effect when using an Elemental Skill, causing the
+	//Elemental Skill DMG of the character wielding this weapon to increase by
+	//12% for 16s. Max 3 stacks. This character will gain 12% All Elemental DMG
+	//Bonus when they possess 3 stacks.
 	w := &Weapon{}
 	r := p.Refine
-
 	stacks := 0
-	var ctick = func(char *character.CharWrapper, c *core.Core) func() {
-		return func() {
-			if c.Status.Duration("kaguradance-"+char.Base.Name) <= 0 {
-				stacks = 0
-				return
-			}
-		}
-	}
+	key := fmt.Sprintf("kaguradance-%v", char.Base.Name)
+	dmg := 0.12 + 0.03*float64(r-1)
+	val := make([]float64, attributes.EndStatType)
+	lastActiveUntil := -1
 
 	c.Events.Subscribe(event.PostSkill, func(args ...interface{}) bool {
 		if c.Player.Active() != char.Index {
 			return false
 		}
-		c.Status.Add("kaguradance-"+char.Base.Name, 16*60)
+		if c.F > lastActiveUntil {
+			//reset stacks back to 0
+			stacks = 0
+		}
+		lastActiveUntil = c.F + 960 //16 * 60
 		if stacks < 3 {
 			stacks++
 		}
-		c.Tasks.Add(ctick(char, c), 16*60)
-		return false
-
-	}, fmt.Sprintf("kaguradance-%v", char.Base.Name))
-
-	mod := float64(r - 1)
-	char.AddAttackMod("kagurasverity",
-
-		-1, func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+		//bonus ele damage if 3 stacks
+		if stacks == 3 {
+			val[attributes.PyroP] = dmg
+			val[attributes.HydroP] = dmg
+			val[attributes.CryoP] = dmg
+			val[attributes.ElectroP] = dmg
+			val[attributes.AnemoP] = dmg
+			val[attributes.GeoP] = dmg
+			val[attributes.PhyP] = dmg
+			val[attributes.DendroP] = dmg
+		} else {
+			//clean stacks ele dmg% otherwise
+			val[attributes.PyroP] = 0
+			val[attributes.HydroP] = 0
+			val[attributes.CryoP] = 0
+			val[attributes.ElectroP] = 0
+			val[attributes.AnemoP] = 0
+			val[attributes.GeoP] = 0
+			val[attributes.PhyP] = 0
+			val[attributes.DendroP] = 0
+		}
+		//add mod for duration, override last
+		char.AddAttackMod("kagurasverity", 960, func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
 			if atk.Info.ActorIndex != char.Index {
 				return nil, false
 			}
-			val := make([]float64, attributes.EndStatType)
-			if stacks == 3 {
-				val[attributes.PyroP] = 0.12 + 0.03*mod
-				val[attributes.HydroP] = 0.12 + 0.03*mod
-				val[attributes.CryoP] = 0.12 + 0.03*mod
-				val[attributes.ElectroP] = 0.12 + 0.03*mod
-				val[attributes.AnemoP] = 0.12 + 0.03*mod
-				val[attributes.GeoP] = 0.12 + 0.03*mod
-				val[attributes.PhyP] = 0.12 + 0.03*mod
-				val[attributes.DendroP] = 0.12 + 0.03*mod
-			}
 			if atk.Info.AttackTag == combat.AttackTagElementalArt || atk.Info.AttackTag == combat.AttackTagElementalArtHold {
-				val[attributes.DmgP] = float64(stacks) * (0.12 + mod*0.03)
+				val[attributes.DmgP] = dmg
 			}
-
 			return val, true
 		})
+		return false
+	}, key)
 
 	return w, nil
 
