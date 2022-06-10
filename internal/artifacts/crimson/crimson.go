65708263f68d0ef16995c31672e6a4fd4c873b1c
imring <vitaliyvorobets25@gmail.com>
2022-06-10 12:43:11 +0300

    crimson

--- internal/artifacts/crimson/crimson.go
+++ internal/artifacts/crimson/crimson.go
@@ -4,68 +4,78 @@ import (
 	"fmt"
 
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/event"
+	"github.com/genshinsim/gcsim/pkg/core/glog"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/artifact"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
 )
 
 func init() {
-	core.RegisterSetFunc("crimson witch of flames", New)
-	core.RegisterSetFunc("crimsonwitchofflames", New)
+	core.RegisterSetFunc(keys.CrimsonWitchOfFlames, NewSet)
 }
 
-func New(c core.Character, s *core.Core, count int, params map[string]int) {
-	stacks := 0
-	key := fmt.Sprintf("%v-cw-4pc", c.Name())
+type Set struct {
+	stacks int
+	key    string
+	Index  int
+}
+
+func (s *Set) SetIndex(idx int) { s.Index = idx }
+func (s *Set) Init() error      { return nil }
+func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[string]int) (artifact.Set, error) {
+	s := Set{}
+	s.stacks = 0
+	s.key = fmt.Sprintf("%v-cw-4pc", char.Base.Name)
+
 	if count >= 2 {
-		m := make([]float64, core.EndStatType)
-		c.AddMod(core.CharStatMod{
-			Key: "crimson-2pc",
-			Amount: func() ([]float64, bool) {
-				if s.Status.Duration(key) == 0 {
-					stacks = 0
-				}
-				mult := 0.5*float64(stacks) + 1
-				m[core.PyroP] = 0.15 * mult
-				if mult > 1 {
-					s.Log.NewEvent("crimson witch 4pc", core.LogArtifactEvent, c.CharIndex(), "mult", mult)
-				}
-				return m, true
-			},
-			Expiry: -1,
+		m := make([]float64, attributes.EndStatType)
+		char.AddStatMod("crimson-2pc", -1, attributes.PyroP, func() ([]float64, bool) {
+			if c.Status.Duration(s.key) == 0 {
+				s.stacks = 0
+			}
+			mult := 0.5*float64(s.stacks) + 1
+			m[attributes.PyroP] = 0.15 * mult
+			if mult > 1 {
+				c.Log.NewEvent("crimson witch 4pc", glog.LogArtifactEvent, char.Index, "mult", mult)
+			}
+
+			return m, true
 		})
 	}
-	if count >= 4 {
 
-		//post snap shot to increase stacks
-		s.Events.Subscribe(core.PreSkill, func(args ...interface{}) bool {
-			if s.ActiveChar != c.CharIndex() {
+	if count >= 4 {
+		// post snap shot to increase stacks
+		c.Events.Subscribe(event.PreSkill, func(args ...interface{}) bool {
+			if c.Player.Active() != char.Index {
 				return false
 			}
-			if s.Status.Duration(key) == 0 {
-				stacks = 0
+
+			// every exectuion, add 1 stack, to a max of 3, reset cd to 10 seconds
+			if c.Status.Duration(s.key) == 0 {
+				s.stacks = 0
 			}
-			//every exectuion, add 1 stack, to a max of 3, reset cd to 10 seconds
-			stacks++
-			if stacks > 3 {
-				stacks = 3
+			if s.stacks < 3 {
+				s.stacks++
 			}
-			s.Log.NewEvent("crimson witch 4pc adding stack", core.LogArtifactEvent, c.CharIndex(), "current stacks", stacks)
-			s.Status.AddStatus(key, 600)
+
+			c.Log.NewEvent("crimson witch 4pc adding stack", glog.LogArtifactEvent, char.Index, "current stacks", s.stacks)
+			c.Status.Add(s.key, 10*60)
 			return false
-		}, fmt.Sprintf("cw4s-%v", c.Name()))
+		}, s.key)
 
-		c.AddReactBonusMod(core.ReactionBonusMod{
-			Key:    "4cw",
-			Expiry: -1,
-			Amount: func(ai core.AttackInfo) (float64, bool) {
-				//overload dmg can't melt or vape so it's fine
-				if ai.AttackTag == core.AttackTagOverloadDamage {
-					return 0.4, false
-				}
-				if ai.Amped {
-					return 0.15, false
-				}
-				return 0, false
-			},
+		char.AddReactBonusMod("crimson-4pc", -1, func(ai combat.AttackInfo) (float64, bool) {
+			if ai.AttackTag == combat.AttackTagOverloadDamage {
+				return 0.4, false
+			}
+			if ai.Amped {
+				return 0.15, false
+			}
+			return 0, false
 		})
-
 	}
+
+	return &s, nil
 }
