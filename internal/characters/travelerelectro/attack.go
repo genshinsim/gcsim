ea8a8c5c5cc25f8993a1ebd5b500b755a0964e7f
Shizuka <shizuka.alpha@gmail.com>
2022-06-10 08:47:11 +0400

    Fix travelerelectro

---
+++ internal/characters/travelerelectro/attack.go
@@ -0,0 +1,44 @@
+package travelerelectro
+
+import (
+	"fmt"
+
+	"github.com/genshinsim/gcsim/internal/frames"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+)
+
+var attackFrames [][]int
+var attackHitmarks = []int{12, 24, 32, 51, 39}
+
+func (c *char) Attack(p map[string]int) action.ActionInfo {
+	ai := combat.AttackInfo{
+		ActorIndex: c.Index,
+		Abil:       fmt.Sprintf("Normal %v", c.NormalCounter),
+		AttackTag:  combat.AttackTagNormal,
+		ICDTag:     combat.ICDTagNormalAttack,
+		ICDGroup:   combat.ICDGroupDefault,
+		StrikeType: combat.StrikeTypeSlash,
+		Element:    attributes.Physical,
+		Durability: 25,
+		Mult:       attack[c.NormalCounter][c.TalentLvlAttack()],
+	}
+
+	c.Core.QueueAttack(
+		ai,
+		combat.NewDefCircHit(0.3, false, combat.TargettableEnemy),
+		attackHitmarks[c.NormalCounter],
+		attackHitmarks[c.NormalCounter],
+	)
+
+	defer c.AdvanceNormalIndex()
+
+	return action.ActionInfo{
+		Frames:          frames.NewAttackFunc(c.Character, attackFrames),
+		AnimationLength: attackFrames[c.NormalCounter][action.InvalidAction],
+		CanQueueAfter:   attackHitmarks[c.NormalCounter],
+		Post:            attackHitmarks[c.NormalCounter],
+		State:           action.NormalAttackState,
+	}
+}
--- internal/characters/travelerelectro/travelerelectro.go
+++ internal/characters/travelerelectro/travelerelectro.go
@@ -1,32 +1,41 @@
 package travelerelectro
 
 import (
-	"github.com/genshinsim/gcsim/internal/tmpl/character"
+	"github.com/genshinsim/gcsim/internal/frames"
+	tmpl "github.com/genshinsim/gcsim/internal/template/character"
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/core/player/weapon"
 )
 
+const normalHitNum = 5
+
+func init() {
+	initCancelFrames()
+	core.RegisterCharFunc(keys.TravelerElectro, NewChar)
+}
+
 type char struct {
-	*character.Tmpl
+	*tmpl.Character
 	abundanceAmulets      int
 	burstC6Hits           int
 	burstC6WillGiveEnergy bool
-	burstSnap             core.Snapshot
-	burstAtk              *core.AttackEvent
+	burstSnap             combat.Snapshot
+	burstAtk              *combat.AttackEvent
 	burstSrc              int
 }
 
-func init() {
-	core.RegisterCharFunc(core.TravelerElectro, NewChar)
-}
-
-func NewChar(s *core.Core, p core.CharacterProfile) (core.Character, error) {
+func NewChar(s *core.Core, w *character.CharWrapper, p character.CharacterProfile) error {
 	c := char{}
-	t, err := character.NewTemplateChar(s, p)
-	if err != nil {
-		return nil, err
-	}
-	c.Tmpl = t
-	c.Base.Element = core.Electro
+	t := tmpl.New(s)
+	t.CharWrapper = w
+	c.Character = t
+
+	c.Base.Element = attributes.Electro
 
 	e, ok := p.Params["start_energy"]
 	if !ok {
@@ -34,16 +43,42 @@ func NewChar(s *core.Core, p core.CharacterProfile) (core.Character, error) {
 	}
 	c.Energy = float64(e)
 	c.EnergyMax = 80
-	c.Weapon.Class = core.WeaponClassSword
+	c.Weapon.Class = weapon.WeaponClassSword
 	c.BurstCon = 3
 	c.SkillCon = 5
-	c.NormalHitNum = 5
+	c.NormalHitNum = normalHitNum
 
-	return &c, nil
+	w.Character = &c
+
+	return nil
 }
 
-func (c *char) Init() {
-	c.Tmpl.Init()
-
+func (c *char) Init() error {
 	c.burstProc()
+	return nil
+}
+
+func initCancelFrames() {
+	// NA cancels
+	attackFrames = make([][]int, normalHitNum)
+
+	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0], 13)
+	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1], 25)
+	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2], 33)
+	attackFrames[3] = frames.InitNormalCancelSlice(attackHitmarks[3], 52)
+	attackFrames[4] = frames.InitNormalCancelSlice(attackHitmarks[4], 40)
+	attackFrames[4][action.ActionCharge] = 500 //TODO: this action is illegal; need better way to handle it
+
+	// charge -> x
+	// TODO: charge not implemented
+	//chargeFrames = frames.InitAbilSlice(41)
+	//chargeFrames[action.ActionDash] = chargeHitmarks[len(chargeHitmarks)-1]
+	//chargeFrames[action.ActionJump] = chargeHitmarks[len(chargeHitmarks)-1]
+	//chargeFrames[action.ActionSwap] = chargeHitmarks[len(chargeHitmarks)-1]
+
+	// skill -> x
+	skillFrames = frames.InitAbilSlice(55)
+
+	// burst -> x
+	burstFrames = frames.InitAbilSlice(60)
 }
