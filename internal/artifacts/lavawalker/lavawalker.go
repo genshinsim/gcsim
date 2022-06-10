8b49296d16be814208cad9651130011147064698
imring <vitaliyvorobets25@gmail.com>
2022-06-10 20:18:12 +0300

    lavawalker

--- internal/artifacts/lavawalker/lavawalker.go
+++ internal/artifacts/lavawalker/lavawalker.go
@@ -2,30 +2,50 @@ package lavawalker
 
 import (
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/glog"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/artifact"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
 )
 
 func init() {
-	core.RegisterSetFunc("lavawalker", New)
+	core.RegisterSetFunc(keys.Lavawalker, NewSet)
 }
 
-func New(c core.Character, s *core.Core, count int, params map[string]int) {
+type Set struct {
+	Index int
+}
+
+func (s *Set) SetIndex(idx int) { s.Index = idx }
+func (s *Set) Init() error      { return nil }
+
+func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[string]int) (artifact.Set, error) {
+	s := Set{}
+
 	if count >= 2 {
-		s.Log.NewEvent("lavawalker 2 pc not implemented", core.LogArtifactEvent, c.CharIndex())
+		c.Log.NewEvent("lavawalker 2 pc not implemented", glog.LogArtifactEvent, char.Index)
 	}
 	if count >= 4 {
-		c.AddPreDamageMod(core.PreDamageMod{
-			Key:    "4lw",
-			Expiry: -1,
-			Amount: func(atk *core.AttackEvent, t core.Target) ([]float64, bool) {
-				m := make([]float64, core.EndStatType)
-				//frozen check first so we don't mistaken coexisting cryo
-				if t.AuraContains(core.Pyro) {
-					m[core.DmgP] = .35
+		m := make([]float64, attributes.EndStatType)
+		m[attributes.DmgP] = 0.35
+		char.AddAttackMod(
+			"lavawaler-4pc",
+			-1,
+			func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+				r, ok := t.(core.Reactable)
+				if !ok {
+					return nil, false
+				}
+
+				if r.AuraContains(attributes.Pyro) {
 					return m, true
 				}
 				return nil, false
 			},
-		})
+		)
 	}
-	//add flat stat to char
+
+	return &s, nil
 }
