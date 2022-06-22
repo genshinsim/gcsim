4b2dff0bb10cc8c516d0268823b36ed1afc40142
IvanHdzF <ivan083198@hotmail.com>
2022-06-21 21:07:48 -0700

    bugbugfix

--- internal/characters/kuki/kuki_test.go
+++ internal/characters/kuki/kuki_test.go
@@ -28,7 +28,7 @@ func TestCD(t *testing.T) {
 		t.Error(err)
 		t.FailNow()
 	}
-	err = testhelper.TestSkillCDSingleCharge(c, x, 360+28)
+	err = testhelper.TestSkillCDSingleCharge(c, x, 15*60+58)
 	if err != nil {
 		t.Error(err)
 	}
