786f8b22737a0f839e3c0ec84e5c81d681678911
imring <vitaliyvorobets25@gmail.com>
2022-06-23 21:16:44 +0300

    Xinyan Implementation

---
+++ internal/characters/xinyan/xinyan.go
@@ -0,0 +1,62 @@
+package xinyan
+
+import (
+	"github.com/genshinsim/gcsim/internal/frames"
+	tmpl "github.com/genshinsim/gcsim/internal/template/character"
+	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/core/player/weapon"
+)
+
+const normalHitNum = 4
+
+type char struct {
+	*tmpl.Character
+}
+
+func init() {
+	initCancelFrames()
+	core.RegisterCharFunc(keys.Xinyan, NewChar)
+}
+
+func NewChar(s *core.Core, w *character.CharWrapper, p character.CharacterProfile) error {
+	c := char{}
+	t := tmpl.New(s)
+	t.CharWrapper = w
+	c.Character = t
+
+	c.Base.Element = attributes.Pyro
+
+	e, ok := p.Params["start_energy"]
+	if !ok {
+		e = 60
+	}
+	c.Energy = float64(e)
+	c.EnergyMax = 60
+	c.Weapon.Class = weapon.WeaponClassClaymore
+	c.BurstCon = 5
+	c.SkillCon = 3
+	c.NormalHitNum = normalHitNum
+	c.CharZone = character.ZoneLiyue
+
+	w.Character = &c
+
+	return nil
+}
+
+func (c *char) Init() error {
+
+	return nil
+}
+
+// need to update frames
+func initCancelFrames() {
+	// NA cancels
+	attackFrames = make([][]int, normalHitNum)
+	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0], 25)
+	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1], 44)
+	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2], 70)
+	attackFrames[3] = frames.InitNormalCancelSlice(attackHitmarks[3], 65)
+}
