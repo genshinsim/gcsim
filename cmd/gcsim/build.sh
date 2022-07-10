e60cebf653b15333a23f33f959bf6220b9ae6976
Jiahao Li <reg@ljh.me>
2022-07-10 15:15:41 -0700

    Fixes build.sh for Mac

--- cmd/gcsim/build.sh
+++ cmd/gcsim/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # notice how we avoid spaces in $now to avoid quotation hell in go build command
-now=$(date --utc +%FT%T%Z)
+now=$(date -u +%FT%T%Z)
 go build -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now"
