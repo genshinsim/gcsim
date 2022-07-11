811d9797df5ddc25498d98ef6295a71da602c832
Vitaliy Vorobets <vitaliyvorobets25@gmail.com>
2022-07-11 16:19:57 +0300

    Xinyan Implementation (#617)
    * Xinyan Implementation
    
    * skill
    
    * burst
    
    * a4
    
    * c1
    
    * c2
    
    * c4
    
    * c6
    
    * fix

---
+++ internal/characters/xinyan/cons.go
@@ -0,0 +1,76 @@
+package xinyan
+
+import (
+	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/event"
+)
+
+func (c *char) c1() {
+	m := make([]float64, attributes.EndStatType)
+	m[attributes.AtkSpd] = 0.12
+
+	icd := -1
+	c.Core.Events.Subscribe(event.OnDamage, func(args ...interface{}) bool {
+		atk := args[1].(*combat.AttackEvent)
+		crit := args[3].(bool)
+		if atk.Info.ActorIndex != c.Index {
+			return false
+		}
+		// TODO: it works off field?
+		if c.Core.Player.Active() != c.Index {
+			return false
+		}
+		if !crit {
+			return false
+		}
+		if icd > c.Core.F {
+			return false
+		}
+
+		c.AddAttackMod(
+			"xinyan-c1",
+			5*60,
+			func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+				return m, true
+			},
+		)
+		icd = c.Core.F + 5*60
+
+		return false
+	}, "xinyan-c1")
+}
+
+// Riff Revolution's Physical DMG has its CRIT Rate increased by 100%, and will form a shield at Shield Level 3: Rave when cast.
+func (c *char) c2() {
+	m := make([]float64, attributes.EndStatType)
+	m[attributes.CR] = 1
+
+	c.AddAttackMod(
+		"xinyan-c2",
+		-1,
+		func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+			if atk.Info.AttackTag != combat.AttackTagElementalBurst {
+				return nil, false
+			}
+			return m, true
+		},
+	)
+}
+
+// Sweeping Fervor's swing DMG decreases opponent's Physical RES by 15% for 12s.
+func (c *char) c4(a combat.AttackCB) {
+	if c.Base.Cons < 4 {
+		return
+	}
+
+	e, ok := a.Target.(core.Enemy)
+	if !ok {
+		return
+	}
+	e.AddResistMod("xinyan-c4", 12*60, attributes.Physical, -0.15)
+}
+
+// Decreases the Stamina Consumption of Xinyan's Charged Attacks by 30%. Additionally, Xinyan's Charged Attacks gain an ATK Bonus equal to 50% of her DEF.
+// func (c *char) c6() {}
---
+++ internal/characters/xinyan/skill.go
@@ -0,0 +1,100 @@
+package xinyan
+
+import (
+	"github.com/genshinsim/gcsim/internal/frames"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/glog"
+	"github.com/genshinsim/gcsim/pkg/core/player/shield"
+)
+
+var skillFrames []int
+
+const skillHitmark = 65
+
+func (c *char) Skill(p map[string]int) action.ActionInfo {
+	ai := combat.AttackInfo{
+		ActorIndex: c.Index,
+		Abil:       "Sweeping Fervor",
+		AttackTag:  combat.AttackTagElementalArt,
+		ICDTag:     combat.ICDTagNone,
+		ICDGroup:   combat.ICDGroupDefault,
+		Element:    attributes.Pyro,
+		Durability: 25,
+		Mult:       skill[c.TalentLvlSkill()],
+	}
+	snap := c.Snapshot(&ai)
+
+	defFactor := snap.BaseDef*(1+snap.Stats[attributes.DEFP]) + snap.Stats[attributes.DEF]
+
+	hitOpponents := 0
+	cb := func(a combat.AttackCB) {
+		hitOpponents++
+
+		// including a1
+		if hitOpponents >= 2 && c.shieldLevel < 3 {
+			c.updateShield(3, defFactor)
+		} else if hitOpponents >= 1 && c.shieldLevel < 2 {
+			c.updateShield(2, defFactor)
+		}
+	}
+
+	if c.Core.Player.Shields.Get(shield.ShieldXinyanSkill) == nil {
+		c.updateShield(1, defFactor)
+	}
+	c.Core.QueueAttack(ai, combat.NewDefCircHit(0.5, false, combat.TargettableEnemy), skillHitmark, skillHitmark, cb, c.c4)
+
+	c.SetCDWithDelay(action.ActionSkill, 18*60, 6)
+
+	return action.ActionInfo{
+		Frames:          frames.NewAbilFunc(skillFrames),
+		AnimationLength: skillFrames[action.InvalidAction],
+		CanQueueAfter:   skillFrames[action.ActionDash], // earliest cancel
+		Post:            skillFrames[action.ActionDash], // earliest cancel
+		State:           action.SkillState,
+	}
+}
+
+func (c *char) shieldDot() func() {
+	return func() {
+		if c.Core.Player.Shields.Get(shield.ShieldXinyanSkill) == nil {
+			return
+		}
+		if c.shieldLevel != 3 {
+			return
+		}
+
+		ai := combat.AttackInfo{
+			ActorIndex: c.Index,
+			Abil:       "Sweeping Fervor (DoT)",
+			AttackTag:  combat.AttackTagElementalArt,
+			ICDTag:     combat.ICDTagNone,
+			ICDGroup:   combat.ICDGroupDefault,
+			Element:    attributes.Pyro,
+			Durability: 25,
+			Mult:       skillDot[c.TalentLvlSkill()],
+		}
+		c.Core.QueueAttack(ai, combat.NewDefCircHit(2, false, combat.TargettableEnemy), 1, 1)
+
+		c.Core.Tasks.Add(c.shieldDot(), 2*60)
+	}
+}
+
+func (c *char) updateShield(level int, defFactor float64) {
+	c.shieldLevel = level
+	shieldhp := 0.0
+
+	switch c.shieldLevel {
+	case 1:
+		shieldhp = shieldLevel1Flat[c.TalentLvlSkill()] + shieldLevel1[c.TalentLvlSkill()]*defFactor
+	case 2:
+		shieldhp = shieldLevel2Flat[c.TalentLvlSkill()] + shieldLevel2[c.TalentLvlSkill()]*defFactor
+	case 3:
+		shieldhp = shieldLevel3Flat[c.TalentLvlSkill()] + shieldLevel3[c.TalentLvlSkill()]*defFactor
+		c.Core.Tasks.Add(c.shieldDot(), 2*60)
+	}
+	shd := c.newShield(shieldhp, shield.ShieldXinyanSkill, 12*60)
+	c.Core.Player.Shields.Add(shd)
+	c.Core.Log.NewEvent("update shield level", glog.LogCharacterEvent, c.Index, "level", c.shieldLevel, "expiry", shd.Expiry())
+}
