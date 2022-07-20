37a02a8a6eb62cf7b53e9459d93dcc127770d9f2
skippi <jbtcao@gmail.com>
2022-07-20 09:19:27 -0500

    remove defense halt from first hit raiden CA

--- internal/characters/raiden/charge.go
+++ internal/characters/raiden/charge.go
@@ -76,8 +76,9 @@ func (c *char) swordCharge(p map[string]int) action.ActionInfo {
 		ax := ai
 		ax.Mult = mult[c.TalentLvlBurst()]
 		ax.Mult += resolveBonus[c.TalentLvlBurst()] * c.stacksConsumed
-		if i < len(chargeSword) - 1 { // Only the last hit has hitlag
+		if i == 0 { // Only the last hit has hitlag
 			ax.HitlagHaltFrames = 0
+			ax.CanBeDefenseHalted = false
 		}
 		if c.Base.Cons >= 2 {
 			ai.IgnoreDefPercent = .6
