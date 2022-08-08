51d8dcaff7f425efa3261fdde021a742b2baa37c
skippi <jbtcao@gmail.com>
2022-08-08 02:55:20 -0500

    add klee skill release cancel param
    This allows Klee to trigger Kagura/CW/etc without consuming the skill
    cooldown. There is a bug that allows this to be used without burst, but
    I will fix that in another commit.

--- internal/characters/klee/skill.go
+++ internal/characters/klee/skill.go
@@ -10,6 +10,7 @@ import (
 var skillFrames []int
 
 var bounceHitmarks = []int{71, 111, 140}
+
 const mineHitmark = 240
 
 func init() {
@@ -17,7 +18,7 @@ func init() {
 	skillFrames[action.ActionAttack] = 66
 	skillFrames[action.ActionCharge] = 69
 	skillFrames[action.ActionSkill] = 68
-	skillFrames[action.ActionBurst] = 34 // TODO: program early cancel for skill trigger
+	skillFrames[action.ActionBurst] = 34
 	skillFrames[action.ActionDash] = 37
 	skillFrames[action.ActionJump] = 35
 	skillFrames[action.ActionSwap] = 74
@@ -26,12 +27,36 @@ func init() {
 // Has two parameters, "bounce" determines the number of bounces that hit
 // "mine" determines the number of mines that hit the enemy
 func (c *char) Skill(p map[string]int) action.ActionInfo {
+	release, ok := p["release"]
+	if !ok {
+		release = 0
+	}
+
+	if release != 0 {
+		c.throwBomb(p)
+		c.SetCDWithDelay(action.ActionSkill, 1200, 33)
+	}
+
+	adjustedFrames := skillFrames
+	if release == 0 {
+		adjustedFrames := make([]int, len(skillFrames))
+		copy(adjustedFrames, skillFrames)
+		adjustedFrames[action.ActionBurst] = 5
+	}
+
+	return action.ActionInfo{
+		Frames:          frames.NewAbilFunc(adjustedFrames),
+		AnimationLength: adjustedFrames[action.InvalidAction],
+		CanQueueAfter:   0,
+		State:           action.SkillState,
+	}
+}
+
+func (c *char) throwBomb(p map[string]int) {
 	bounce, ok := p["bounce"]
 	if !ok {
 		bounce = 1
 	}
-
-	//mine lives for 5 seconds
 	for i := 0; i < bounce; i++ {
 		ai := combat.AttackInfo{
 			ActorIndex: c.Index,
@@ -50,7 +75,13 @@ func (c *char) Skill(p map[string]int) action.ActionInfo {
 			ai.Durability = 50
 		}
 
-		c.Core.QueueAttack(ai, combat.NewCircleHit(c.Core.Combat.Player(), 2, false, combat.TargettableEnemy), 0, bounceHitmarks[i], c.a1)
+		c.Core.QueueAttack(
+			ai,
+			combat.NewCircleHit(c.Core.Combat.Player(), 2, false, combat.TargettableEnemy),
+			0,
+			bounceHitmarks[i],
+			c.a1,
+		)
 	}
 
 	if bounce > 0 {
@@ -77,17 +108,14 @@ func (c *char) Skill(p map[string]int) action.ActionInfo {
 	}
 
 	for i := 0; i < minehits; i++ {
-		c.Core.QueueAttack(ai, combat.NewCircleHit(c.Core.Combat.Player(), 1, false, combat.TargettableEnemy), 0, mineHitmark, c.c2)
+		c.Core.QueueAttack(
+			ai,
+			combat.NewCircleHit(c.Core.Combat.Player(), 1, false, combat.TargettableEnemy),
+			0,
+			mineHitmark,
+			c.c2,
+		)
 	}
 
 	c.c1(bounceHitmarks[0])
-
-	c.SetCDWithDelay(action.ActionSkill, 1200, 33)
-
-	return action.ActionInfo{
-		Frames:          frames.NewAbilFunc(skillFrames),
-		AnimationLength: skillFrames[action.InvalidAction],
-		CanQueueAfter:   0,
-		State:           action.SkillState,
-	}
 }
