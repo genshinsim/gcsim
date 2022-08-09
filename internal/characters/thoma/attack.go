40513d6767b26ee4b8a3397cf413cfc40ac77c9c
skippi <jbtcao@gmail.com>
2022-08-09 18:50:48 -0500

    fix thoma attack frames

--- internal/characters/thoma/attack.go
+++ internal/characters/thoma/attack.go
@@ -9,27 +9,42 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/combat"
 )
 
-var attackFrames [][]int
-var attackHitmarks = [][]int{{11}, {38}, {27, 40}, {25}}
-var attackHitlagHaltFrame = [][]float64{{0.06}, {0.09}, {0, 0}, {0}}
-var attackDefHalt = [][]bool{{true}, {true}, {false, false}, {false}}
+var (
+	attackFrames          [][]int
+	attackHitmarks        = [][]int{{13}, {18}, {10, 13}, {13}}
+	attackHitlagHaltFrame = [][]float64{{0.06}, {0.09}, {0, 0}, {0}}
+	attackDefHalt         = [][]bool{{true}, {true}, {false, false}, {false}}
+)
 
 const normalHitNum = 4
 
 func init() {
 	attackFrames = make([][]int, normalHitNum)
 
-	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0][0], 11)
-	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1][0], 38)
-	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2][1], 40)
-	attackFrames[3] = frames.InitNormalCancelSlice(attackHitmarks[3][0], 25)
-	attackFrames[3][action.ActionCharge] = 500 //TODO: this action is illegal; need better way to handle it
+	// N1 -> x
+	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0][0], 21)
+	attackFrames[0][action.ActionAttack] = 20
+	attackFrames[0][action.ActionCharge] = 21
+
+	// N2 -> x
+	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1][0], 27)
+	attackFrames[1][action.ActionAttack] = 27
+	attackFrames[1][action.ActionCharge] = 25
+
+	// N3 -> x
+	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2][1], 32)
+	attackFrames[2][action.ActionAttack] = 31
+	attackFrames[2][action.ActionCharge] = 32
+
+	attackFrames[3] = frames.InitNormalCancelSlice(attackHitmarks[3][0], 58)
+	attackFrames[3][action.ActionAttack] = 58
+	attackFrames[3][action.ActionCharge] = 500 // TODO: this action is illegal; need better way to handle it
 }
 
 // Normal attack damage queue generator
 // relatively standard with no major differences versus other characters
 func (c *char) Attack(p map[string]int) action.ActionInfo {
-
+	lastMultiHit := 0
 	for i, mult := range attack[c.NormalCounter] {
 		ai := combat.AttackInfo{
 			ActorIndex:         c.Index,
@@ -44,6 +59,7 @@ func (c *char) Attack(p map[string]int) action.ActionInfo {
 			HitlagHaltFrames:   attackHitlagHaltFrame[c.NormalCounter][i] * 60,
 			CanBeDefenseHalted: attackDefHalt[c.NormalCounter][i],
 		}
+		hitmark := lastMultiHit + attackHitmarks[c.NormalCounter][i]
 		c.QueueCharTask(func() {
 			c.Core.QueueAttack(
 				ai,
@@ -51,7 +67,8 @@ func (c *char) Attack(p map[string]int) action.ActionInfo {
 				0,
 				0,
 			)
-		}, attackHitmarks[c.NormalCounter][i])
+		}, hitmark)
+		lastMultiHit = hitmark
 	}
 
 	defer c.AdvanceNormalIndex()
