553341c6f78691efe4f0450d881eb8127ae7230c
skippi <jbtcao@gmail.com>
2022-08-08 23:52:38 -0500

    refactor klee skill logic
    The skill can be simplified using the ActionQueue. We also moved the
    cancel frames into its own slice.

--- internal/characters/klee/skill.go
+++ internal/characters/klee/skill.go
@@ -7,11 +7,13 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/action"
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
 	"github.com/genshinsim/gcsim/pkg/core/combat"
-	"github.com/genshinsim/gcsim/pkg/core/event"
 	"github.com/genshinsim/gcsim/pkg/core/glog"
 )
 
-var skillFrames []int
+var (
+	skillFrames         []int
+	skillCanceledFrames []int
+)
 
 var bounceHitmarks = []int{71, 111, 140}
 
@@ -26,25 +28,14 @@ func init() {
 	skillFrames[action.ActionDash] = 37
 	skillFrames[action.ActionJump] = 35
 	skillFrames[action.ActionSwap] = 74
+	skillCanceledFrames = make([]int, len(skillFrames))
+	copy(skillCanceledFrames, skillFrames)
+	skillCanceledFrames[action.ActionBurst] = 5
 }
 
 // Has two parameters, "bounce" determines the number of bounces that hit
 // "mine" determines the number of mines that hit the enemy
 func (c *char) Skill(p map[string]int) action.ActionInfo {
-	release, ok := p["release"]
-	if !ok {
-		release = 1
-	}
-
-	earlyCancel := false
-	c.Core.Events.Subscribe(event.OnStateChange, func(args ...interface{}) bool {
-		next := args[1].(action.AnimationState)
-		if release == 0 && next == action.BurstState {
-			earlyCancel = true
-		}
-		return false
-	}, "klee-skill-cancel")
-
 	type attackData struct {
 		ai   combat.AttackInfo
 		snap combat.Snapshot
@@ -100,12 +91,30 @@ func (c *char) Skill(p map[string]int) action.ActionInfo {
 		}
 	}
 
-	cooldownDelay := 33
-	c.Core.Tasks.Add(func() {
-		c.Core.Events.Unsubscribe(event.OnStateChange, "klee-skill-cancel")
-		if earlyCancel {
-			return
+	release, ok := p["release"]
+	if !ok {
+		release = 1
+	}
+
+	adjustedFrames := skillFrames
+	if release == 0 {
+		adjustedFrames = skillCanceledFrames
+	}
+
+	canQueueAfter := math.MaxInt32
+	for _, f := range adjustedFrames {
+		if f < canQueueAfter {
+			canQueueAfter = f
 		}
+	}
+	actionInfo := action.ActionInfo{
+		Frames:          frames.NewAbilFunc(adjustedFrames),
+		AnimationLength: adjustedFrames[action.InvalidAction],
+		CanQueueAfter:   canQueueAfter,
+		State:           action.SkillState,
+	}
+	cooldownDelay := 33
+	actionInfo.QueueAction(func() {
 		if release == 0 {
 			c.Core.Log.NewEvent("attempted klee skill cancel without burst", glog.LogWarnings, -1)
 		}
@@ -129,24 +138,5 @@ func (c *char) Skill(p map[string]int) action.ActionInfo {
 		}
 		c.SetCD(action.ActionSkill, 1200)
 	}, cooldownDelay)
-
-	adjustedFrames := skillFrames
-	if release == 0 {
-		adjustedFrames = make([]int, len(skillFrames))
-		copy(adjustedFrames, skillFrames)
-		adjustedFrames[action.ActionBurst] = 5
-	}
-
-	canQueueAfter := math.MaxInt32
-	for _, f := range adjustedFrames {
-		if f < canQueueAfter {
-			canQueueAfter = f
-		}
-	}
-	return action.ActionInfo{
-		Frames:          frames.NewAbilFunc(adjustedFrames),
-		AnimationLength: adjustedFrames[action.InvalidAction],
-		CanQueueAfter:   canQueueAfter,
-		State:           action.SkillState,
-	}
+	return actionInfo
 }
