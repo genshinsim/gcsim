70f43ae1fe96d4ee30bc56e1e7a5276dea465c1c
srliao <906239+srliao@users.noreply.github.com>
2022-08-09 21:24:09 -0400

    fix workflows

--- scripts/build_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "./cmd/wasm"
-now=$(date --utc +%FT%T%Z)
-GOOS=js GOARCH=wasm go build -o main.wasm -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now"
-
-mv main.wasm ../../app/static
