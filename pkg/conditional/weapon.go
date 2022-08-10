68ce3692dd99aa76df455e41822e01a88f77b799
imring <vitaliyvorobets25@gmail.com>
2022-08-10 03:25:19 +0300

    correct warnings for weapon condition

--- pkg/conditional/weapon.go
+++ pkg/conditional/weapon.go
@@ -11,19 +11,19 @@ import (
 func evalWeapon(c *core.Core, fields []string) int64 {
 	// .weapon.fischl
 	if len(fields) < 2 {
-		c.Log.NewEvent("bad normal counter conditon: invalid num of fields", glog.LogWarnings, -1).Write("fields", fields)
+		c.Log.NewEvent("bad weapon conditon: invalid num of fields", glog.LogWarnings, -1).Write("fields", fields)
 		return 0
 	}
 
 	name := strings.TrimPrefix(fields[1], ".")
 	key, ok := shortcut.CharNameToKey[name]
 	if !ok {
-		c.Log.NewEvent("bad normal counter conditon: invalid character", glog.LogWarnings, -1).Write("fields", fields)
+		c.Log.NewEvent("bad weapon conditon: invalid character", glog.LogWarnings, -1).Write("fields", fields)
 		return 0
 	}
 	char, ok := c.Player.ByKey(key)
 	if !ok {
-		c.Log.NewEvent("bad normal counter conditon: invalid character", glog.LogWarnings, -1).Write("fields", fields)
+		c.Log.NewEvent("bad weapon conditon: invalid character", glog.LogWarnings, -1).Write("fields", fields)
 		return 0
 	}
 
