23c49f13ca04b2af9c9e1b2c00e52cf4017072f8
k0l11 <intensonom@yahoo.de>
2022-08-11 23:20:14 +0200

    fix yoimiya q hitlag - adjust q status name

--- internal/characters/yoimiya/burst.go
+++ internal/characters/yoimiya/burst.go
@@ -6,11 +6,14 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
 	"github.com/genshinsim/gcsim/pkg/core/combat"
 	"github.com/genshinsim/gcsim/pkg/core/event"
+	"github.com/genshinsim/gcsim/pkg/enemy"
 )
 
 var burstFrames []int
 
 const burstHitmark = 75
+const abDebuff = "aurous-blaze"
+const abIcdKey = "aurous-blaze-icd"
 
 func init() {
 	burstFrames = frames.InitAbilSlice(114)
@@ -32,25 +35,23 @@ func (c *char) Burst(p map[string]int) action.ActionInfo {
 		Durability: 50,
 		Mult:       burst[c.TalentLvlBurst()],
 	}
-	c.Core.QueueAttack(ai, combat.NewCircleHit(c.Core.Combat.Player(), 5, false, combat.TargettableEnemy), 0, burstHitmark)
+	// A4
+	c.Core.Tasks.Add(c.a4, burstHitmark)
 
-	//marker an opponent after first hit
-	//ignore the bouncing around for now (just assume it's always target 0)
-	//icd of 2s, removed if down
-	duration := 600
-	if c.Base.Cons >= 1 {
-		duration = 840
-	}
-	c.Core.Tasks.Add(func() {
-		c.Core.Status.Add("aurous", duration)
-		//attack buff
-		c.a4()
-	}, burstHitmark)
+	c.Core.QueueAttack(
+		ai,
+		combat.NewCircleHit(c.Core.Combat.Player(), 5, false, combat.TargettableEnemy),
+		0,
+		burstHitmark,
+		c.applyAB, // callback to apply Aurous Blaze
+	)
 
 	//add cooldown to sim
 	c.SetCD(action.ActionBurst, 15*60)
 	//use up energy
 	c.ConsumeEnergy(5)
+
+	c.abApplied = false
 
 	return action.ActionInfo{
 		Frames:          frames.NewAbilFunc(burstFrames),
@@ -60,20 +61,50 @@ func (c *char) Burst(p map[string]int) action.ActionInfo {
 	}
 }
 
+func (c *char) applyAB(a combat.AttackCB) {
+	// marker an opponent after first hit
+	// ignore the bouncing around for now (just assume it's always target 0)
+	// icd of 2s, removed if down
+
+	// do nothing if ab already applied on enemy
+	if c.abApplied {
+		return
+	}
+	c.abApplied = true
+
+	trg, ok := a.Target.(*enemy.Enemy)
+	// do nothing if not an enemy
+	if !ok {
+		return
+	}
+
+	duration := 600
+	if c.Base.Cons >= 1 {
+		duration = 840
+	}
+	trg.AddStatus(abDebuff, duration, true) // apply Aurous Blaze
+}
+
 func (c *char) burstHook() {
 	//check on attack landed for target 0
 	//if aurous active then trigger dmg if not on cd
 	c.Core.Events.Subscribe(event.OnDamage, func(args ...interface{}) bool {
 		ae := args[1].(*combat.AttackEvent)
-		if c.Core.Status.Duration("aurous") == 0 {
+		trg, ok := args[0].(*enemy.Enemy)
+		// ignore if not an enemy
+		if !ok {
 			return false
 		}
+		// ignore if debuff not on enemy
+		if !trg.StatusIsActive(abDebuff) {
+			return false
+		}
+		// ignore for self
 		if ae.Info.ActorIndex == c.Index {
-			//ignore for self
 			return false
 		}
 		//ignore if on icd
-		if c.Core.Status.Duration("aurousicd") > 0 {
+		if trg.StatusIsActive(abIcdKey) {
 			return false
 		}
 		//ignore if wrong tags
@@ -99,10 +130,9 @@ func (c *char) burstHook() {
 		}
 		c.Core.QueueAttack(ai, combat.NewCircleHit(c.Core.Combat.Player(), 3, false, combat.TargettableEnemy), 0, 1)
 
-		c.Core.Status.Add("aurousicd", 120) //2 sec icd
+		trg.AddStatus(abIcdKey, 120, true) // trigger Aurous Blaze ICD
 
-		//check for c4
-
+		// C4
 		if c.Base.Cons >= 4 {
 			c.ReduceActionCooldown(action.ActionSkill, 72)
 		}
@@ -115,7 +145,13 @@ func (c *char) burstHook() {
 		//add check for if yoimiya dies
 		c.Core.Events.Subscribe(event.OnCharacterHurt, func(_ ...interface{}) bool {
 			if c.HPCurrent <= 0 {
-				c.Core.Status.Delete("aurous")
+				// remove Aurous Blaze from target
+				for _, x := range c.Core.Combat.Targets() {
+					trg := x.(*enemy.Enemy)
+					if trg.StatusIsActive(abDebuff) {
+						trg.DeleteStatus(abDebuff)
+					}
+				}
 			}
 			return false
 		}, "yoimiya-died")
