8180a93eba85a1df98465a3e8e0c4c7bda7d74e4
k0l11 <intensonom@yahoo.de>
2022-08-13 12:59:52 +0200

    travelergeo frames update

--- internal/characters/travelergeo/skill.go
+++ internal/characters/travelergeo/skill.go
@@ -8,35 +8,79 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/construct"
 )
 
-var skillFrames []int
+var skillFrames [][][]int
 
-// isn't exactly hitmark
-const skillHitmark = 34
+// {Tap E, Short Hold E}
+var skillHitmark = []int{62, 29}
+var skillCDStart = []int{23, 25}
 
 func init() {
-	skillFrames = frames.InitAbilSlice(24)
+	skillFrames = make([][][]int, 2)
+
+	// Tap E
+	skillFrames[0] = make([][]int, 2)
+
+	// Male
+	skillFrames[0][0] = frames.InitAbilSlice(81) // Tap E -> N1/Q
+	skillFrames[0][0][action.ActionDash] = 25    // Tap E -> D
+	skillFrames[0][0][action.ActionJump] = 24    // Tap E -> J
+	skillFrames[0][0][action.ActionSwap] = 67    // Tap E -> Swap
+
+	// Female
+	skillFrames[0][1] = frames.InitAbilSlice(80) // Tap E -> Q
+	skillFrames[0][1][action.ActionAttack] = 79  // Tap E -> N1
+	skillFrames[0][1][action.ActionDash] = 23    // Tap E -> D
+	skillFrames[0][1][action.ActionJump] = 24    // Tap E -> J
+	skillFrames[0][1][action.ActionSwap] = 65    // Tap E -> Swap
+
+	// Short Hold E
+	skillFrames[1] = make([][]int, 2)
+
+	// Male
+	skillFrames[1][0] = frames.InitAbilSlice(54) // Short Hold E -> N1/Q
+	skillFrames[1][0][action.ActionDash] = 31    // Short Hold E -> D
+	skillFrames[1][0][action.ActionJump] = 31    // Short Hold E -> J
+	skillFrames[1][0][action.ActionSwap] = 39    // Short Hold E -> Swap
+
+	// Female
+	skillFrames[1][1] = frames.InitAbilSlice(54) // Short Hold E -> N1/Q
+	skillFrames[1][1][action.ActionDash] = 31    // Short Hold E -> D
+	skillFrames[1][1][action.ActionJump] = 32    // Short Hold E -> J
+	skillFrames[1][1][action.ActionSwap] = 40    // Short Hold E -> Swap
 }
 
 func (c *char) Skill(p map[string]int) action.ActionInfo {
+	short_hold, ok := p["short_hold"]
+	if !ok || short_hold < 0 {
+		short_hold = 0
+	}
+	if short_hold > 1 {
+		short_hold = 1
+	}
+
 	ai := combat.AttackInfo{
-		ActorIndex: c.Index,
-		Abil:       "Starfell Sword",
-		AttackTag:  combat.AttackTagElementalArt,
-		ICDTag:     combat.ICDTagElementalArt,
-		ICDGroup:   combat.ICDGroupDefault,
-		StrikeType: combat.StrikeTypeBlunt,
-		Element:    attributes.Geo,
-		Durability: 50,
-		Mult:       skill[c.TalentLvlSkill()],
+		ActorIndex:         c.Index,
+		Abil:               "Starfell Sword",
+		AttackTag:          combat.AttackTagElementalArt,
+		ICDTag:             combat.ICDTagElementalArt,
+		ICDGroup:           combat.ICDGroupDefault,
+		StrikeType:         combat.StrikeTypeBlunt,
+		Element:            attributes.Geo,
+		Durability:         50,
+		Mult:               skill[c.TalentLvlSkill()],
+		HitlagFactor:       0.05,
+		HitlagHaltFrames:   0.05 * 60,
+		CanBeDefenseHalted: true,
+		IsDeployable:       true,
 	}
 	// TODO: check snapshot timing
-	c.Core.QueueAttack(ai, combat.NewCircleHit(c.Core.Combat.Player(), 2, false, combat.TargettableEnemy), 24, skillHitmark)
+	c.Core.QueueAttack(ai, combat.NewCircleHit(c.Core.Combat.Player(), 2, false, combat.TargettableEnemy), 24, skillHitmark[short_hold])
 
 	var count float64 = 3
 	if c.Core.Rand.Float64() < 0.33 {
 		count = 4
 	}
-	c.Core.QueueParticle(c.Base.Key.String(), count, attributes.Geo, skillHitmark+c.Core.Flags.ParticleDelay)
+	c.Core.QueueParticle(c.Base.Key.String(), count, attributes.Geo, skillHitmark[short_hold]+c.Core.Flags.ParticleDelay)
 
 	c.Core.Tasks.Add(func() {
 		dur := 30 * 60
@@ -44,14 +88,14 @@ func (c *char) Skill(p map[string]int) action.ActionInfo {
 			dur += 600
 		}
 		c.Core.Constructs.New(c.newStone(dur), false)
-	}, skillHitmark)
+	}, skillHitmark[short_hold])
 
-	c.SetCD(action.ActionSkill, 360)
+	c.SetCDWithDelay(action.ActionSkill, 360, skillCDStart[short_hold])
 
 	return action.ActionInfo{
-		Frames:          frames.NewAbilFunc(skillFrames),
-		AnimationLength: skillFrames[action.InvalidAction],
-		CanQueueAfter:   skillFrames[action.ActionDash], // earliest cancel
+		Frames:          frames.NewAbilFunc(skillFrames[short_hold][c.female]),
+		AnimationLength: skillFrames[short_hold][c.female][action.InvalidAction],
+		CanQueueAfter:   skillFrames[short_hold][c.female][action.ActionDash], // earliest cancel
 		State:           action.SkillState,
 	}
 }
@@ -73,15 +117,19 @@ func (c *char) newStone(dur int) *stone {
 func (s *stone) OnDestruct() {
 	if s.char.Base.Cons >= 2 {
 		ai := combat.AttackInfo{
-			ActorIndex: s.char.Index,
-			Abil:       "Rockcore Meltdown",
-			AttackTag:  combat.AttackTagElementalArt,
-			ICDTag:     combat.ICDTagElementalArt,
-			ICDGroup:   combat.ICDGroupDefault,
-			StrikeType: combat.StrikeTypeBlunt,
-			Element:    attributes.Geo,
-			Durability: 50,
-			Mult:       skill[s.char.TalentLvlSkill()],
+			ActorIndex:         s.char.Index,
+			Abil:               "Rockcore Meltdown",
+			AttackTag:          combat.AttackTagElementalArt,
+			ICDTag:             combat.ICDTagElementalArt,
+			ICDGroup:           combat.ICDGroupDefault,
+			StrikeType:         combat.StrikeTypeBlunt,
+			Element:            attributes.Geo,
+			Durability:         50,
+			Mult:               skill[s.char.TalentLvlSkill()],
+			HitlagFactor:       0.05,
+			HitlagHaltFrames:   0.05 * 60,
+			CanBeDefenseHalted: true,
+			IsDeployable:       true,
 		}
 		s.char.Core.QueueAttack(ai, combat.NewCircleHit(s.char.Core.Combat.Player(), 2, false, combat.TargettableEnemy), 0, 0)
 	}
