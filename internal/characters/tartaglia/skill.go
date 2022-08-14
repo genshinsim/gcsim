53c40955ee5a87a0bbb6247e278ccb11a8adfda1
skippi <jbtcao@gmail.com>
2022-08-14 15:51:50 -0500

    add hitlag extension to tartaglia skill force disable
    When tartaglia hits the max skill duration is dependent on hitlag.

--- internal/characters/tartaglia/skill.go
+++ internal/characters/tartaglia/skill.go
@@ -134,7 +134,7 @@ func (c *char) Skill(p map[string]int) action.ActionInfo {
 	c.Core.QueueAttack(ai, combat.NewCircleHit(c.Core.Combat.Player(), 1, false, combat.TargettableEnemy), hitmark, hitmark)
 
 	src := c.eCast
-	c.Core.Tasks.Add(func() {
+	c.QueueCharTask(func() {
 		if src == c.eCast && c.StatusIsActive(meleeKey) {
 			c.onExitMeleeStance(0)
 			c.ResetNormalCounter()
