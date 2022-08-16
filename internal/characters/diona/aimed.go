133e1809a006949cc533ceafc15eab77f6be1003
k0l11 <intensonom@yahoo.de>
2022-08-17 01:03:22 +0200

    implement diona phys ca and arcc

--- internal/characters/diona/aimed.go
+++ internal/characters/diona/aimed.go
@@ -7,23 +7,45 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/combat"
 )
 
-var aimedFrames []int
+var aimedFrames [][]int
 var aimedC4Frames []int
 
-const aimedHitmark = 86
+var aimedHitmarks = []int{15 - 12, 15, 86}
+
 const aimedC4Hitmark = 50
 
 func init() {
-	aimedFrames = frames.InitAbilSlice(94)
-	aimedFrames[action.ActionDash] = aimedHitmark
-	aimedFrames[action.ActionJump] = aimedHitmark
+	aimedFrames = make([][]int, 3)
 
+	// Aimed Shot (ARCC)
+	aimedFrames[0] = frames.InitAbilSlice(23 - 12)
+	aimedFrames[0][action.ActionDash] = aimedHitmarks[0]
+	aimedFrames[0][action.ActionJump] = aimedHitmarks[0]
+
+	// Aimed Shot
+	aimedFrames[1] = frames.InitAbilSlice(23)
+	aimedFrames[1][action.ActionDash] = aimedHitmarks[1]
+	aimedFrames[1][action.ActionJump] = aimedHitmarks[1]
+
+	// Fully-Charged Aimed Shot
+	aimedFrames[2] = frames.InitAbilSlice(94)
+	aimedFrames[2][action.ActionDash] = aimedHitmarks[2]
+	aimedFrames[2][action.ActionJump] = aimedHitmarks[2]
+
+	// Fully-Charged Aimed Shot (C4)
 	aimedC4Frames = frames.InitAbilSlice(58)
 	aimedC4Frames[action.ActionDash] = aimedC4Hitmark
 	aimedC4Frames[action.ActionJump] = aimedC4Hitmark
 }
 
 func (c *char) Aimed(p map[string]int) action.ActionInfo {
+	hold, ok := p["hold"]
+	if !ok || hold < 0 {
+		hold = 2
+	}
+	if hold > 2 {
+		hold = 2
+	}
 	travel, ok := p["travel"]
 	if !ok {
 		travel = 10
@@ -32,25 +54,34 @@ func (c *char) Aimed(p map[string]int) action.ActionInfo {
 
 	ai := combat.AttackInfo{
 		ActorIndex:           c.Index,
-		Abil:                 "Aim (Charged)",
+		Abil:                 "Fully-Charged Aimed Shot",
 		AttackTag:            combat.AttackTagExtra,
 		ICDTag:               combat.ICDTagExtraAttack,
 		ICDGroup:             combat.ICDGroupDefault,
 		StrikeType:           combat.StrikeTypePierce,
 		Element:              attributes.Cryo,
 		Durability:           25,
-		Mult:                 aim[c.TalentLvlAttack()],
+		Mult:                 fullaim[c.TalentLvlAttack()],
 		HitWeakPoint:         weakspot == 1,
 		HitlagHaltFrames:     0.12 * 60,
 		HitlagFactor:         0.01,
 		HitlagOnHeadshotOnly: true,
 		IsDeployable:         true,
 	}
+	if hold < 2 {
+		ai.Abil = "Aimed Shot"
+		if hold == 0 {
+			ai.Abil += " (ARCC)"
+		}
+		ai.Element = attributes.Physical
+		ai.Mult = aim[c.TalentLvlAttack()]
+	}
 
 	var a action.ActionInfo
 
 	// TODO: assumes that Diona is always inside Q radius
-	if c.Base.Cons >= 4 && c.Core.Status.Duration("diona-q") > 0 {
+	if c.Base.Cons >= 4 && c.Core.Status.Duration("diona-q") > 0 && hold == 2 {
+		ai.Abil += " (C4)"
 		a = action.ActionInfo{
 			Frames:          frames.NewAbilFunc(aimedC4Frames),
 			AnimationLength: aimedC4Frames[action.InvalidAction],
@@ -59,12 +90,11 @@ func (c *char) Aimed(p map[string]int) action.ActionInfo {
 		}
 	} else {
 		a = action.ActionInfo{
-			Frames:          frames.NewAbilFunc(aimedFrames),
-			AnimationLength: aimedFrames[action.InvalidAction],
-			CanQueueAfter:   aimedHitmark,
+			Frames:          frames.NewAbilFunc(aimedFrames[hold]),
+			AnimationLength: aimedFrames[hold][action.InvalidAction],
+			CanQueueAfter:   aimedHitmarks[hold],
 			State:           action.AimState,
 		}
-
 	}
 
 	c.Core.QueueAttack(ai,
