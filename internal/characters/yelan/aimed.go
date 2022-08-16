db7d1c60c1561aa08581b65e419d5a658962521d
k0l11 <intensonom@yahoo.de>
2022-08-17 01:25:19 +0200

    implement yelan phys ca and arcc - fix wrong yelan barb cancel frames - fix wrong yelan fully aimed ca arrow scaling

--- internal/characters/yelan/aimed.go
+++ internal/characters/yelan/aimed.go
@@ -8,25 +8,46 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/glog"
 )
 
-var aimedFrames []int
+var aimedFrames [][]int
 var aimedBarbFrames []int
 
-const aimedHitmark = 86
+var aimedHitmarks = []int{15 - 12, 15, 86}
+
 const aimedBarbHitmark = 32
 
 func init() {
-	// TODO: confirm that aim->x is the same for all cancels
-	aimedFrames = frames.InitAbilSlice(96)
-	aimedFrames[action.ActionDash] = aimedHitmark
-	aimedFrames[action.ActionJump] = aimedHitmark
+	aimedFrames = make([][]int, 3)
 
+	// Aimed Shot (ARCC)
+	aimedFrames[0] = frames.InitAbilSlice(25 - 12)
+	aimedFrames[0][action.ActionDash] = aimedHitmarks[0]
+	aimedFrames[0][action.ActionJump] = aimedHitmarks[0]
+
+	// Aimed Shot
+	aimedFrames[1] = frames.InitAbilSlice(25)
+	aimedFrames[1][action.ActionDash] = aimedHitmarks[1]
+	aimedFrames[1][action.ActionJump] = aimedHitmarks[1]
+
+	// Fully-Charged Aimed Shot
+	aimedFrames[2] = frames.InitAbilSlice(96)
+	aimedFrames[2][action.ActionDash] = aimedHitmarks[2]
+	aimedFrames[2][action.ActionJump] = aimedHitmarks[2]
+
+	// Breakthrough Barb
 	aimedBarbFrames = frames.InitAbilSlice(42)
-	aimedBarbFrames[action.ActionDash] = aimedHitmark
-	aimedBarbFrames[action.ActionJump] = aimedHitmark
+	aimedBarbFrames[action.ActionDash] = aimedBarbHitmark
+	aimedBarbFrames[action.ActionJump] = aimedBarbHitmark
 }
 
 // Aimed charge attack damage queue generator
 func (c *char) Aimed(p map[string]int) action.ActionInfo {
+	hold, ok := p["hold"]
+	if !ok || hold < 0 {
+		hold = 2
+	}
+	if hold > 2 {
+		hold = 2
+	}
 	travel, ok := p["travel"]
 	if !ok {
 		travel = 10
@@ -59,21 +80,29 @@ func (c *char) Aimed(p map[string]int) action.ActionInfo {
 
 	ai := combat.AttackInfo{
 		ActorIndex:   c.Index,
-		Abil:         "Aim Charge Attack",
+		Abil:         "Fully-Charged Aimed Shot",
 		AttackTag:    combat.AttackTagExtra,
 		ICDTag:       combat.ICDTagNone,
 		ICDGroup:     combat.ICDGroupDefault,
 		Element:      attributes.Hydro,
 		Durability:   25,
-		Mult:         aimed[c.TalentLvlAttack()],
+		Mult:         fullaim[c.TalentLvlAttack()],
 		HitWeakPoint: weakspot == 1,
 	}
-	c.Core.QueueAttack(ai, combat.NewDefSingleTarget(c.Core.Combat.DefaultTarget, combat.TargettableEnemy), aimedHitmark, aimedHitmark+travel)
+	if hold < 2 {
+		ai.Abil = "Aimed Shot"
+		if hold == 0 {
+			ai.Abil += " (ARCC)"
+		}
+		ai.Element = attributes.Physical
+		ai.Mult = aim[c.TalentLvlAttack()]
+	}
+	c.Core.QueueAttack(ai, combat.NewDefSingleTarget(c.Core.Combat.DefaultTarget, combat.TargettableEnemy), aimedHitmarks[hold], aimedHitmarks[hold]+travel)
 
 	return action.ActionInfo{
-		Frames:          frames.NewAbilFunc(aimedFrames),
-		AnimationLength: aimedFrames[action.InvalidAction],
-		CanQueueAfter:   aimedHitmark,
+		Frames:          frames.NewAbilFunc(aimedFrames[hold]),
+		AnimationLength: aimedFrames[hold][action.InvalidAction],
+		CanQueueAfter:   aimedHitmarks[hold],
 		State:           action.AimState,
 	}
 }
