18ba7dcac5fd70be5e01840e25a831305003ae2a
k0l11 <intensonom@yahoo.de>
2022-08-17 01:21:43 +0200

    implement tartaglia phys ca and arcc

--- internal/characters/tartaglia/aimed.go
+++ internal/characters/tartaglia/aimed.go
@@ -8,14 +8,27 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/glog"
 )
 
-var aimedFrames []int
+var aimedFrames [][]int
 
-const aimedHitmark = 86
+var aimedHitmarks = []int{15 - 12, 15, 86}
 
 func init() {
-	aimedFrames = frames.InitAbilSlice(94)
-	aimedFrames[action.ActionDash] = aimedHitmark
-	aimedFrames[action.ActionJump] = aimedHitmark
+	aimedFrames = make([][]int, 3)
+
+	// Aimed Shot (ARCC)
+	aimedFrames[0] = frames.InitAbilSlice(23 - 12)
+	aimedFrames[0][action.ActionDash] = aimedHitmarks[0]
+	aimedFrames[0][action.ActionJump] = aimedHitmarks[0]
+
+	// Aimed Shot
+	aimedFrames[1] = frames.InitAbilSlice(23)
+	aimedFrames[1][action.ActionDash] = aimedHitmarks[1]
+	aimedFrames[1][action.ActionJump] = aimedHitmarks[1]
+
+	// Fully-Charged Aimed Shot
+	aimedFrames[2] = frames.InitAbilSlice(94)
+	aimedFrames[2][action.ActionDash] = aimedHitmarks[2]
+	aimedFrames[2][action.ActionJump] = aimedHitmarks[2]
 }
 
 // Once fully charged, deal Hydro DMG and apply the Riptide status.
@@ -30,7 +43,13 @@ func (c *char) Aimed(p map[string]int) action.ActionInfo {
 			State:           action.Idle,
 		}
 	}
-
+	hold, ok := p["hold"]
+	if !ok || hold < 0 {
+		hold = 2
+	}
+	if hold > 2 {
+		hold = 2
+	}
 	travel, ok := p["travel"]
 	if !ok {
 		travel = 10
@@ -39,35 +58,49 @@ func (c *char) Aimed(p map[string]int) action.ActionInfo {
 
 	ai := combat.AttackInfo{
 		ActorIndex:           c.Index,
-		Abil:                 "Aim (Charged)",
+		Abil:                 "Fully-Charged Aimed Shot",
 		AttackTag:            combat.AttackTagExtra,
 		ICDTag:               combat.ICDTagNone,
 		ICDGroup:             combat.ICDGroupDefault,
 		StrikeType:           combat.StrikeTypePierce,
 		Element:              attributes.Hydro,
 		Durability:           25,
-		Mult:                 aim[c.TalentLvlAttack()],
+		Mult:                 fullaim[c.TalentLvlAttack()],
 		HitWeakPoint:         weakspot == 1,
 		HitlagHaltFrames:     0.12 * 60, // deployable hitlag, but only on weakspot
 		HitlagFactor:         0.01,
 		HitlagOnHeadshotOnly: true,
 		IsDeployable:         true,
 	}
-
-	c.Core.QueueAttack(
-		ai,
-		combat.NewDefSingleTarget(c.Core.Combat.DefaultTarget, combat.TargettableEnemy),
-		aimedHitmark,
-		aimedHitmark+travel,
-		// TODO: what's the ordering on these 2 callbacks?
-		c.rtFlashCallback,   // call back for triggering slash
-		c.aimedApplyRiptide, // call back for applying riptide
-	)
+	if hold < 2 {
+		ai.Abil = "Aimed Shot"
+		if hold == 0 {
+			ai.Abil += " (ARCC)"
+		}
+		ai.Element = attributes.Physical
+		ai.Mult = aim[c.TalentLvlAttack()]
+		c.Core.QueueAttack(
+			ai,
+			combat.NewDefSingleTarget(c.Core.Combat.DefaultTarget, combat.TargettableEnemy),
+			aimedHitmarks[hold],
+			aimedHitmarks[hold]+travel,
+		)
+	} else {
+		c.Core.QueueAttack(
+			ai,
+			combat.NewDefSingleTarget(c.Core.Combat.DefaultTarget, combat.TargettableEnemy),
+			aimedHitmarks[hold],
+			aimedHitmarks[hold]+travel,
+			// TODO: what's the ordering on these 2 callbacks?
+			c.rtFlashCallback,   // call back for triggering slash
+			c.aimedApplyRiptide, // call back for applying riptide
+		)
+	}
 
 	return action.ActionInfo{
-		Frames:          frames.NewAbilFunc(aimedFrames),
-		AnimationLength: aimedFrames[action.InvalidAction],
-		CanQueueAfter:   aimedHitmark,
+		Frames:          frames.NewAbilFunc(aimedFrames[hold]),
+		AnimationLength: aimedFrames[hold][action.InvalidAction],
+		CanQueueAfter:   aimedHitmarks[hold],
 		State:           action.AimState,
 	}
 }
