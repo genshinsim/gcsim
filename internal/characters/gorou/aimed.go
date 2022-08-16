ec73eba23f6ba9f97dca2406ceb86b757b986723
k0l11 <intensonom@yahoo.de>
2022-08-17 01:18:41 +0200

    implement gorou phys ca and arcc

--- internal/characters/gorou/aimed.go
+++ internal/characters/gorou/aimed.go
@@ -7,18 +7,37 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/combat"
 )
 
-var aimedFrames []int
+var aimedFrames [][]int
 
-const aimedHitmark = 86
+var aimedHitmarks = []int{15 - 12, 15, 86}
 
 func init() {
-	aimedFrames = frames.InitAbilSlice(94)
-	aimedFrames[action.ActionDash] = aimedHitmark
-	aimedFrames[action.ActionJump] = aimedHitmark
+	aimedFrames = make([][]int, 3)
+
+	// Aimed Shot (ARCC)
+	aimedFrames[0] = frames.InitAbilSlice(23 - 12)
+	aimedFrames[0][action.ActionDash] = aimedHitmarks[0]
+	aimedFrames[0][action.ActionJump] = aimedHitmarks[0]
+
+	// Aimed Shot
+	aimedFrames[1] = frames.InitAbilSlice(23)
+	aimedFrames[1][action.ActionDash] = aimedHitmarks[1]
+	aimedFrames[1][action.ActionJump] = aimedHitmarks[1]
+
+	// Fully-Charged Aimed Shot
+	aimedFrames[2] = frames.InitAbilSlice(94)
+	aimedFrames[2][action.ActionDash] = aimedHitmarks[2]
+	aimedFrames[2][action.ActionJump] = aimedHitmarks[2]
 }
 
-// Aimed charge attack damage queue generator
 func (c *char) Aimed(p map[string]int) action.ActionInfo {
+	hold, ok := p["hold"]
+	if !ok || hold < 0 {
+		hold = 2
+	}
+	if hold > 2 {
+		hold = 2
+	}
 	travel, ok := p["travel"]
 	if !ok {
 		travel = 10
@@ -27,26 +46,34 @@ func (c *char) Aimed(p map[string]int) action.ActionInfo {
 
 	ai := combat.AttackInfo{
 		ActorIndex:           c.Index,
-		Abil:                 "Aim Charge Attack",
-		AttackTag:            combat.AttackTagExtra,
+		Abil:                 "Fully-Charged Aimed Shot",
+		AttackTag:            combat.AttackTagNone,
 		ICDTag:               combat.ICDTagNone,
 		ICDGroup:             combat.ICDGroupDefault,
 		StrikeType:           combat.StrikeTypeBlunt,
 		Element:              attributes.Geo,
 		Durability:           25,
-		Mult:                 aimed[c.TalentLvlAttack()],
+		Mult:                 fullaim[c.TalentLvlAttack()],
 		HitWeakPoint:         weakspot == 1,
 		HitlagHaltFrames:     .12 * 60,
 		HitlagOnHeadshotOnly: true,
 		IsDeployable:         true,
 	}
-
-	c.Core.QueueAttack(ai, combat.NewDefSingleTarget(c.Core.Combat.DefaultTarget, combat.TargettableEnemy), aimedHitmark, aimedHitmark+travel)
+	if hold < 2 {
+		ai.Abil = "Aimed Shot"
+		if hold == 0 {
+			ai.Abil += " (ARCC)"
+		}
+		ai.AttackTag = combat.AttackTagExtra
+		ai.Element = attributes.Physical
+		ai.Mult = aim[c.TalentLvlAttack()]
+	}
+	c.Core.QueueAttack(ai, combat.NewDefSingleTarget(c.Core.Combat.DefaultTarget, combat.TargettableEnemy), aimedHitmarks[hold], aimedHitmarks[hold]+travel)
 
 	return action.ActionInfo{
-		Frames:          frames.NewAbilFunc(aimedFrames),
-		AnimationLength: aimedFrames[action.InvalidAction],
-		CanQueueAfter:   aimedHitmark,
+		Frames:          frames.NewAbilFunc(aimedFrames[hold]),
+		AnimationLength: aimedFrames[hold][action.InvalidAction],
+		CanQueueAfter:   aimedHitmarks[hold],
 		State:           action.AimState,
 	}
 }
