4940092b142b19729f5d05e07c28658c2f13a712
k0l11 <intensonom@yahoo.de>
2022-08-16 17:43:56 +0200

    implement resolutionofsojourner

---
+++ internal/artifacts/sojourner/sojourner.go
@@ -0,0 +1,55 @@
+﻿package sojourner
+
+import (
+	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/artifact"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/modifier"
+)
+
+func init() {
+	core.RegisterSetFunc(keys.ResolutionOfSojourner, NewSet)
+}
+
+type Set struct {
+	Index int
+}
+
+func (s *Set) SetIndex(idx int) { s.Index = idx }
+func (s *Set) Init() error      { return nil }
+
+func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[string]int) (artifact.Set, error) {
+	s := Set{}
+
+	// 2 Piece: ATK +18%
+	if count >= 2 {
+		m := make([]float64, attributes.EndStatType)
+		m[attributes.ATKP] = 0.18
+		char.AddStatMod(character.StatMod{
+			Base:         modifier.NewBase("sojourner-2pc", -1),
+			AffectedStat: attributes.ATKP,
+			Amount: func() ([]float64, bool) {
+				return m, true
+			},
+		})
+	}
+	// 4 Piece: Increases Charged Attack CRIT Rate by 30%.
+	if count >= 4 {
+		m := make([]float64, attributes.EndStatType)
+		m[attributes.CR] = 0.30
+		char.AddAttackMod(character.AttackMod{
+			Base: modifier.NewBase("sojourner-4pc", -1),
+			Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+				if atk.Info.AttackTag != combat.AttackTagExtra {
+					return nil, false
+				}
+				return m, true
+			},
+		})
+	}
+
+	return &s, nil
+}
