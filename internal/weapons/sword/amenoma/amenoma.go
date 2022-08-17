9a97843463cbd3d3bd4585fd67afa79bd64dcb09
skippi <jbtcao@gmail.com>
2022-08-17 16:17:40 -0500

    add hitlag extension to amenoma seed expiry
    The 30s duration is hitlag extendable.

--- internal/weapons/sword/amenoma/amenoma.go
+++ internal/weapons/sword/amenoma/amenoma.go
@@ -26,11 +26,11 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile
 	w := &Weapon{}
 	r := p.Refine
 
-	seeds := make([]int, 3) //keep track the seeds
+	seeds := []string{"amenoma-seed-0", "amenoma-seed-1", "amenoma-seed-2"}
 	refund := 4.5 + 1.5*float64(r)
 	const icdKey = "amenoma-icd"
 
-	//TODO: this used to be on postskill. make sure nothing broke here
+	// TODO: this used to be on postskill. make sure nothing broke here
 	c.Events.Subscribe(event.OnSkill, func(args ...interface{}) bool {
 		if c.Player.Active() != char.Index {
 			return false
@@ -41,44 +41,40 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile
 		}
 		// find oldest seed to overwrite
 		index := 0
-		old := seeds[0]
-
-		for i, v := range seeds {
-			if v < old {
-				old = v
+		for i, s := range seeds {
+			if char.StatusExpiry(s) < char.StatusExpiry(seeds[index]) {
 				index = i
 			}
 		}
-		seeds[index] = c.F + 30*60
+		char.AddStatus(seeds[index], 30*60, true)
 
 		c.Log.NewEvent("amenoma proc'd", glog.LogWeaponEvent, char.Index).
-			Write("index", index).
-			Write("seeds", seeds)
+			Write("index", index)
 
-		char.AddStatus(icdKey, 300, true) //5 sec icd
+		char.AddStatus(icdKey, 300, true) // 5 sec icd
 
 		return false
 	}, fmt.Sprintf("amenoma-skill-%v", char.Base.Key.String()))
 
-	//TODO: this used to be on postburst. make sure nothing broke here
+	// TODO: this used to be on postburst. make sure nothing broke here
 	c.Events.Subscribe(event.OnBurst, func(args ...interface{}) bool {
 		if c.Player.Active() != char.Index {
 			return false
 		}
 		count := 0
-		for i, v := range seeds {
-			if v > c.F {
+		for _, s := range seeds {
+			if char.StatusIsActive(s) {
 				count++
 			}
-			seeds[i] = 0
+			char.DeleteStatus(s)
 		}
 		if count == 0 {
 			return false
 		}
-		//regen energy after 2 seconds
+		// regen energy after 2 seconds
 		char.QueueCharTask(func() {
 			char.AddEnergy("amenoma", refund*float64(count))
-		}, 120+60) //added 1 extra sec for burst animation but who knows if this is true
+		}, 120+60) // added 1 extra sec for burst animation but who knows if this is true
 
 		return false
 	}, fmt.Sprintf("amenoma-burst-%v", char.Base.Key.String()))
