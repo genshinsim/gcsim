a5eea8356a661cd93bec02271b0cd9fbcf0a63da
srliao <906239+srliao@users.noreply.github.com>
2022-08-18 09:18:56 -0400

    clean up worker code

--- services/worker/src/index.ts
+++ services/worker/src/index.ts
@@ -13,7 +13,6 @@ import { Router } from 'itty-router';
 import { handleAuth } from './auth';
 import { handleListDBChars, handleListDBSims } from './db';
 import { handleEnka } from './enka';
-import { handleOptions } from './options';
 import { handlePreview } from './preview';
 import { handleShare } from './share';
 import { handleListUserSims } from './sims/listByUser';
@@ -22,7 +21,7 @@ import { handleView } from './view';
 export const dbClient = new PostgrestClient(POSTGREST_ENDPOINT); //secrets?
 const router = Router();
 
-router.options('*', handleOptions);
+// router.options('*', handleOptions);
 
 router.get('/api/auth', handleAuth);
 
@@ -45,31 +44,6 @@ router.get('/api/db/:key', handleListDBSims);
 // user sims
 
 router.get('/api/:key/sims', handleListUserSims);
-
-router.get('/api/avatars', async () => {
-  const { data, error } = await dbClient.from('avatars').select();
-
-  if (error) {
-    console.log('error getting avatars: ', error);
-    return new Response(JSON.stringify({ error: error }), {
-      status: 500,
-      statusText: 'Error getting avatars',
-      headers: {
-        'content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Max-Age': '86400',
-      },
-    });
-  }
-
-  return new Response(JSON.stringify({ users: data }), {
-    headers: {
-      'content-type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Max-Age': '86400',
-    },
-  });
-});
 
 addEventListener('fetch', (event) => {
   event.respondWith(router.handle(event.request));
