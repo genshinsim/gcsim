0597a5205f06121f25bee2b6d985e10bee10b2c6
srliao <906239+srliao@users.noreply.github.com>
2022-08-22 07:46:25 -0400

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,5 @@
 # sudo apt-get install gcc-mingw-w64
 
 # notice how we avoid spaces in $now to avoid quotation hell in go build command
-cd "./cmd/wasm"
 now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-GOOS=windows GOARCH=amd64 go build -o ../../gcsim.exe -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now" ./cmd/gcsim/ 
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now" ./cmd/gcsim
