52c27bf0f3f60245c91e639665ba68458c44094f
k0l11 <intensonom@yahoo.de>
2022-08-22 11:31:51 +0200

    add missing sprint utility passives

--- internal/characters/kaeya/kaeya.go
+++ internal/characters/kaeya/kaeya.go
@@ -3,6 +3,7 @@ package kaeya
 import (
 	tmpl "github.com/genshinsim/gcsim/internal/template/character"
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/action"
 	"github.com/genshinsim/gcsim/pkg/core/keys"
 	"github.com/genshinsim/gcsim/pkg/core/player/character"
 	"github.com/genshinsim/gcsim/pkg/core/player/character/profile"
@@ -30,6 +31,13 @@ func NewChar(s *core.Core, w *character.CharWrapper, _ profile.CharacterProfile)
 }
 
 func (c *char) Init() error {
+	// make sure to use the same key everywhere so that these passives don't stack
+	c.Core.Player.AddStamPercentMod("utility-dash", -1, func(a action.Action) (float64, bool) {
+		if a == action.ActionDash && c.HPCurrent > 0 {
+			return -0.2, false
+		}
+		return 0, false
+	})
 	if c.Base.Cons > 0 {
 		c.c1()
 	}
--- internal/characters/razor/razor.go
+++ internal/characters/razor/razor.go
@@ -3,6 +3,7 @@ package razor
 import (
 	tmpl "github.com/genshinsim/gcsim/internal/template/character"
 	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/action"
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
 	"github.com/genshinsim/gcsim/pkg/core/keys"
 	"github.com/genshinsim/gcsim/pkg/core/player/character"
@@ -48,6 +49,13 @@ func (c *char) Init() error {
 
 	c.a4()
 
+	// make sure to use the same key everywhere so that these passives don't stack
+	c.Core.Player.AddStamPercentMod("utility-dash", -1, func(a action.Action) (float64, bool) {
+		if a == action.ActionDash && c.HPCurrent > 0 {
+			return -0.2, false
+		}
+		return 0, false
+	})
 	if c.Base.Cons >= 1 {
 		c.c1()
 	}
