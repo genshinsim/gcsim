6af95009e60fc1f10c2c753f4dc4976797b1d486
srliao <906239+srliao@users.noreply.github.com>
2022-08-22 07:18:35 -0400

    update github workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# sudo apt-get install gcc-multilib
+# sudo apt-get install gcc-mingw-w64
+
+# notice how we avoid spaces in $now to avoid quotation hell in go build command
 cd "./cmd/wasm"
 now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-GOOS=js GOARCH=wasm go build -o ../../app/static/main.wasm -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now"
+GOOS=windows GOARCH=amd64 go build -o ../../gcsim.exe -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now" ./cmd/gcsim/ 
