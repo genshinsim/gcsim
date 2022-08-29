8db30a6b27aab6be21abae7462a1b1e7c6012201
skippi <jbtcao@gmail.com>
2022-08-29 14:17:32 -0500

    fix sucrose burst doing extra absorb damage
    Sucrose elemental ticks only apply after the first hit, making a total
    of 3 elemental hits rather than 4.

--- internal/characters/sucrose/burst.go
+++ internal/characters/sucrose/burst.go
@@ -71,8 +71,9 @@ func (c *char) Burst(p map[string]int) action.ActionInfo {
 	for i := 137; i <= duration+5; i += 113 {
 		c.Core.QueueAttackWithSnap(ai, snap, combat.NewCircleHit(c.Core.Combat.PrimaryTarget(), 5, false, combat.TargettableEnemy), i, cb)
 
+		firstHit := i == 137
 		c.Core.Tasks.Add(func() {
-			if c.qInfused != attributes.NoElement {
+			if !firstHit && c.qInfused != attributes.NoElement {
 				aiAbs.Element = c.qInfused
 				c.Core.QueueAttackWithSnap(aiAbs, snapAbs, combat.NewCircleHit(c.Core.Combat.PrimaryTarget(), 5, false, combat.TargettableEnemy), 0)
 			}
