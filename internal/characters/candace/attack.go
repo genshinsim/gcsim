290f117ebdf4deddeec5de755322c8a3ad9aa024
skippi <jbtcao@gmail.com>
2022-09-30 23:51:47 -0500

    fix index out of bounds error on candace attack

--- internal/characters/candace/attack.go
+++ internal/characters/candace/attack.go
@@ -14,14 +14,14 @@ var (
 	attackHitmarks = [][]int{{22}, {32}, {52, 52}, {94}} // TODO add correct hitmarks
 )
 
-const normalHitNum = 5
+const normalHitNum = 4
 
 func init() {
 	attackFrames = make([][]int, normalHitNum)                               // TODO: add correct frame data
 	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0][0], 22) // N1 -> CA
 	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1][0], 32) // N2 -> CA
 	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2][1], 52) // N3 -> CA
-	attackFrames[3] = frames.InitNormalCancelSlice(attackHitmarks[3][1], 94) // N4 -> CA
+	attackFrames[3] = frames.InitNormalCancelSlice(attackHitmarks[3][0], 94) // N4 -> CA
 	attackFrames[3][action.ActionCharge] = 500                               // N5 -> CA, TODO: this action is illegal; need better way to handle it
 }
 
