54c19939b72b337a4c96671d384cac03a7113cf0
imring <vitaliyvorobets25@gmail.com>
2022-10-02 18:51:09 +0300

    new weapons

---
+++ internal/weapons/claymore/aquamarine/aquamarine.go
@@ -0,0 +1,75 @@
+package aquamarine
+
+import (
+	"fmt"
+
+	"github.com/genshinsim/gcsim/pkg/core"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/core/player/weapon"
+	"github.com/genshinsim/gcsim/pkg/modifier"
+)
+
+func init() {
+	core.RegisterWeaponFunc(keys.MakhairaAquamarine, NewWeapon)
+}
+
+type Weapon struct {
+	atkBuuff float64
+	core     *core.Core
+	char     *character.CharWrapper
+	Index    int
+}
+
+func (w *Weapon) SetIndex(idx int) { w.Index = idx }
+
+func (w *Weapon) Init() error {
+	w.updateStats()
+	return nil
+}
+
+// The following effect will trigger every 10s: The equipping character will gain 24%/30%/36%/42%/48% of
+// their Elemental Mastery as bonus ATK for 12s, with nearby party members gaining 30% of this buff for
+// the same duration. Multiple instances of this weapon can allow this buff to stack. This effect will
+// still trigger even if the character is not on the field.
+func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile) (weapon.Weapon, error) {
+	w := &Weapon{
+		core: c,
+		char: char,
+	}
+	r := p.Refine
+
+	w.atkBuuff = 0.18 + float64(r)*0.06
+	return w, nil
+}
+
+func (w *Weapon) updateStats() {
+	val := make([]float64, attributes.EndStatType)
+	val[attributes.ATK] = w.atkBuuff * w.char.Stat(attributes.EM)
+	w.char.AddStatMod(character.StatMod{
+		Base:         modifier.NewBase("aquamarine", 12*60),
+		AffectedStat: attributes.ATK,
+		Amount: func() ([]float64, bool) {
+			return val, true
+		},
+	})
+
+	valTeam := make([]float64, attributes.EndStatType)
+	valTeam[attributes.ATK] = val[attributes.ATK] * 0.3
+	for _, this := range w.core.Player.Chars() {
+		if this.Index == w.char.Index {
+			continue
+		}
+
+		this.AddStatMod(character.StatMod{
+			Base:         modifier.NewBase(fmt.Sprintf("aquamarine-%v", w.char.Base.Key.String()), 12*60),
+			AffectedStat: attributes.ATK,
+			Amount: func() ([]float64, bool) {
+				return valTeam, true
+			},
+		})
+	}
+
+	w.core.Tasks.Add(w.updateStats, 10*60)
+}
