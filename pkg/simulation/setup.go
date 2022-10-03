aafeafeb278db8f04c9d7574d8c869611d8c129f
srliao <906239+srliao@users.noreply.github.com>
2022-10-03 10:05:31 -0400

    enable dendro res

--- pkg/simulation/setup.go
+++ pkg/simulation/setup.go
@@ -230,8 +230,8 @@ func SetupResonance(s *core.Core) {
 					}
 					return false
 				}
-				// s.Events.Subscribe(event.OnBurning, twoEl, "dendro-res")
-				// s.Events.Subscribe(event.OnBloom, twoEl, "dendro-res")
+				s.Events.Subscribe(event.OnBurning, twoEl, "dendro-res")
+				s.Events.Subscribe(event.OnBloom, twoEl, "dendro-res")
 				s.Events.Subscribe(event.OnQuicken, twoEl, "dendro-res")
 
 				threeBuff := make([]float64, attributes.EndStatType)
