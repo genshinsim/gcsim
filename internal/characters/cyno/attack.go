77067e9fd624f1828667c3efb25dbf32cab54dc5
skippi <jbtcao@gmail.com>
2022-10-12 22:21:44 -0500

    fix cyno attack frames to match sheet

--- internal/characters/cyno/attack.go
+++ internal/characters/cyno/attack.go
@@ -9,33 +9,38 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/combat"
 )
 
-var attackFrames [][]int
-var attackEarliestCancel = []int{14, 16, 18, 12, 41}
-var attackHitmarks = [][]int{{14}, {16}, {12, 21}, {41}}
-var attackHitlagHaltFrame = []float64{0.1, 0.1, 0.1, 0.15, 0.15} //TODO:verify this with DM}
-var attackHitlagFactor = []float64{0.01, 0.01, 0.05, 0.01, 0.01}
+var (
+	attackFrames          [][]int
+	attackHitmarks        = [][]int{{14}, {17}, {13, 22}, {27}}
+	attackHitlagHaltFrame = []float64{0.1, 0.1, 0.1, 0.15, 0.15} // TODO:verify this with DM}
+	attackHitlagFactor    = []float64{0.01, 0.01, 0.05, 0.01, 0.01}
+)
 
 const normalHitNum = 4
-const BurstHitNum = 5 //Burst attack chains have 5
 
 func init() {
-	attackFrames = make([][]int, normalHitNum) // should be 4
+	attackFrames = make([][]int, normalHitNum)                               // should be 4
+	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0][0], 28) // N1 -> N2
+	attackFrames[0][action.ActionAttack] = 15
 
-	attackFrames[0] = frames.InitNormalCancelSlice(attackEarliestCancel[0], 18) // N1 -> N2
-	attackFrames[1] = frames.InitNormalCancelSlice(attackEarliestCancel[1], 37) // N2 -> N3
-	attackFrames[2] = frames.InitNormalCancelSlice(attackEarliestCancel[2], 37) // N3 -> N4
-	attackFrames[3] = frames.InitNormalCancelSlice(attackEarliestCancel[3], 34) // N4 -> N1
+	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1][0], 23) // N2 -> N3
+	attackFrames[1][action.ActionAttack] = 22
+
+	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2][1], 27) // N3 -> N4
+	attackFrames[2][action.ActionCharge] = 26
+
+	attackFrames[3] = frames.InitNormalCancelSlice(attackHitmarks[3][0], 58) // N4 -> N1
+	attackFrames[3][action.ActionCharge] = 500                               // impossible action
 }
 
-// TODO: Adjust the attack frame values (this ones are source: i made them the fuck up)
 func (c *char) Attack(p map[string]int) action.ActionInfo {
 	if c.StatusIsActive(burstKey) {
 		c.NormalHitNum = BurstHitNum
-		return c.attackB(p) //go to burst mode attacks
+		return c.attackB(p) // go to burst mode attacks
 	}
-	if c.NormalHitNum >= BurstHitNum { //this should avoid the panic error
+	if c.NormalHitNum >= BurstHitNum { // this should avoid the panic error
 		c.NormalHitNum = normalHitNum
-		c.ResetNormalCounter() //TODO:verify is cyno resets his attack string if burst expires
+		c.ResetNormalCounter() // TODO:verify is cyno resets his attack string if burst expires
 	}
 	c.NormalHitNum = normalHitNum
 	for i, mult := range attack[c.NormalCounter] {
@@ -66,26 +71,29 @@ func (c *char) Attack(p map[string]int) action.ActionInfo {
 	return action.ActionInfo{
 		Frames:          frames.NewAttackFunc(c.Character, attackFrames),
 		AnimationLength: attackFrames[c.NormalCounter][action.InvalidAction],
-		CanQueueAfter:   attackEarliestCancel[c.NormalCounter],
+		CanQueueAfter:   attackHitmarks[c.NormalCounter][len(attackHitmarks[c.NormalCounter])-1],
 		State:           action.NormalAttackState,
 	}
 }
 
-var attackBFrames [][]int
-var attackBEarliestCancel = []int{14, 16, 18, 12, 41}
-var attackBHitmarks = [][]int{{14}, {16}, {18}, {12, 21}, {41}}
+const BurstHitNum = 5 // Burst attack chains have 5
+
+var (
+	attackBFrames         [][]int
+	attackBEarliestCancel = []int{14, 16, 18, 12, 41}
+	attackBHitmarks       = [][]int{{14}, {16}, {18}, {12, 21}, {41}}
+)
 
 func init() {
 	// NA cancels (burst)
-	attackBFrames = make([][]int, BurstHitNum) //Should be 5
+	attackBFrames = make([][]int, BurstHitNum) // Should be 5
 
-	attackBFrames[0] = frames.InitNormalCancelSlice(attackEarliestCancel[0], 18) // N1 -> N2
-	attackBFrames[1] = frames.InitNormalCancelSlice(attackEarliestCancel[1], 37) // N2 -> N3
-	attackBFrames[2] = frames.InitNormalCancelSlice(attackEarliestCancel[2], 37) // N3 -> N4
-	attackBFrames[3] = frames.InitNormalCancelSlice(attackEarliestCancel[3], 34) // N4 -> N5
-	attackBFrames[4] = frames.InitNormalCancelSlice(attackEarliestCancel[4], 61) // N5 -> N1
-	//TODO: honestly idk what i am doing at dis point pls forgive me Koli
-
+	attackBFrames[0] = frames.InitNormalCancelSlice(attackBEarliestCancel[0], 18) // N1 -> N2
+	attackBFrames[1] = frames.InitNormalCancelSlice(attackBEarliestCancel[1], 37) // N2 -> N3
+	attackBFrames[2] = frames.InitNormalCancelSlice(attackBEarliestCancel[2], 37) // N3 -> N4
+	attackBFrames[3] = frames.InitNormalCancelSlice(attackBEarliestCancel[3], 34) // N4 -> N5
+	attackBFrames[4] = frames.InitNormalCancelSlice(attackBEarliestCancel[4], 61) // N5 -> N1
+	// TODO: honestly idk what i am doing at dis point pls forgive me Koli
 }
 
 // TODO: Adjust the attack frame values (this ones are source: i made them the fuck up)
@@ -106,7 +114,7 @@ func (c *char) attackB(p map[string]int) action.ActionInfo {
 		// i just copy pasted raiden code, why do we use ax:=ai i will never know
 		ax := ai
 		ax.Mult = mult[c.TalentLvlBurst()]
-		ax.FlatDmg = c.Stat(attributes.EM) * 1.5 //this is A4
+		ax.FlatDmg = c.Stat(attributes.EM) * 1.5 // this is A4
 
 		c.QueueCharTask(func() {
 			c.Core.QueueAttack(ax, combat.NewCircleHit(c.Core.Combat.Player(), 2, false, combat.TargettableEnemy), 0, 0)
