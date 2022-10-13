b16a1c90e2dd0a51bae9a0eabb59c0129c0c3434
Kyle Bartz <mail@kyle.fail>
2022-10-13 06:05:24 +0000

    New viewer routes with old viewer moved to legacy

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -10,8 +10,8 @@ import { DB } from './Pages/DB';
 import './i18n';
 import { Trans, useTranslation } from 'react-i18next';
 import { DiscordCallback } from './PageUserAccount/DiscordCallback';
-import { CharacterView, Database, TeamsList } from '~src/PageDatabase';
 import { PageUserAccount } from './PageUserAccount';
+import { ViewerLoader, ViewTypes } from './Pages/Viewer';
 import { RedirectDB } from './Pages/DB/RedirectDB';
 
 export default function App() {
@@ -30,61 +30,86 @@ export default function App() {
     <div className="bp4-dark h-screen flex flex-col">
       <Nav />
       <div className="flex flex-col flex-auto overflow-x-clip">
-      <Switch>
-        <Route path="/" component={Dash} />
-        <Route path="/simple">
-          <Redirect to="/simulator" />
-        </Route>
-        <Route path="/advanced">
-          <Redirect to="/simulator" />
-        </Route>
-        <Route path="/simulator">
-          <SimWrapper>
-            <Simple />
-          </SimWrapper>
-        </Route>
-        <Route path="/viewer/share/:id">
-          {(params) => <ViewerDash path={params.id} />}
-        </Route>
-        <Route path="/v3/viewer/share/:id">
-          {(params) => <ViewerDash path={params.id} next={true} />}
-        </Route>
-        <Route path="/viewer/local">
-          <ViewerDash path="local" />
-        </Route>
-        <Route path="/viewer">
-          <ViewerDash path="/" />
-        </Route>
-        {/* <Route path="/db">
-          <Database />
-        </Route>
-        <Route path="/db/:avatar">
-          {(params) => <CharacterView char={params.avatar} />}
-        </Route>
-        <Route path="/db/:avatar/:team">
-          {(params) => <TeamsList char={params.avatar} team={params.team} />}
-        </Route> */}
-        <Route path="/db">
-          <RedirectDB />
-        </Route>
-        <Route path="/old-db">
-          <DB />
-        </Route>
-        {/* <Route path="/db/:char">{({ char }) => <DbChar char={char} />}</Route> */}
-        <Route path="/account">
-          <PageUserAccount />
-        </Route>
-        <Route path="/auth/discord">
-          <DiscordCallback />
-        </Route>
-        <Route>
-          <div className="m-2 text-center">
-            <Trans>src.this_page_is</Trans>
-          </div>
-        </Route>
-      </Switch>
-      <div className="w-full pt-4 pb-4 md:pl-4">
-        <Footer />
+        <Switch>
+          <Route path="/" component={Dash} />
+          <Route path="/simple">
+            <Redirect to="/simulator" />
+          </Route>
+          <Route path="/advanced">
+            <Redirect to="/simulator" />
+          </Route>
+          <Route path="/simulator">
+            <SimWrapper>
+              <Simple />
+            </SimWrapper>
+          </Route>
+
+          {/* Viewer Routes */}
+          <Route path="/viewer">
+            <ViewerLoader type={ViewTypes.Landing} />
+          </Route>
+          <Route path="/viewer/upload">
+            <ViewerLoader type={ViewTypes.Upload} />
+          </Route>
+          <Route path="/viewer/web">
+            <ViewerLoader type={ViewTypes.Web} />
+          </Route>
+          <Route path="/viewer/local">
+            <ViewerLoader type={ViewTypes.Local} />
+          </Route>
+          <Route path="/viewer/share/:id">
+            {(params) => <ViewerLoader type={ViewTypes.Share} id={params.id} />}
+          </Route>
+          <Route path="/viewer/empty">
+            <ViewerLoader type={ViewTypes.Empty} />
+          </Route>
+
+          {/* reroute v3 -> new viewer */}
+          <Route path="/v3/viewer/share/:id">
+            {(params) => <Redirect to={"/viewer/share/" + params.id} />}
+          </Route>
+
+          {/* Legacy Viewer Routes */}
+          <Route path="/legacy/viewer">
+            <ViewerDash path="/" />
+          </Route>
+          <Route path="/legacy/viewer/local">
+            <ViewerDash path="local" />
+          </Route>
+          <Route path="/legacy/viewer/share/:id">
+            {(params) => <ViewerDash path={params.id} next={true} />}
+          </Route>
+
+          {/* <Route path="/db">
+            <Database />
+          </Route>
+          <Route path="/db/:avatar">
+            {(params) => <CharacterView char={params.avatar} />}
+          </Route>
+          <Route path="/db/:avatar/:team">
+            {(params) => <TeamsList char={params.avatar} team={params.team} />}
+          </Route> */}
+          <Route path="/db">
+            <RedirectDB />
+          </Route>
+          <Route path="/old-db">
+            <DB />
+          </Route>
+          {/* <Route path="/db/:char">{({ char }) => <DbChar char={char} />}</Route> */}
+          <Route path="/account">
+            <PageUserAccount />
+          </Route>
+          <Route path="/auth/discord">
+            <DiscordCallback />
+          </Route>
+          <Route>
+            <div className="m-2 text-center">
+              <Trans>src.this_page_is</Trans>
+            </div>
+          </Route>
+        </Switch>
+        <div className="w-full pt-4 pb-4 md:pl-4">
+          <Footer />
         </div>
       </div>
     </div>
