f6d2c65ed404fcd9253342cb6120824ed930eb8c
skippi <jbtcao@gmail.com>
2022-10-12 23:13:51 -0500

    add endseer a1 frame data to cyno skill
    Endseer skills have different frame data.

--- internal/characters/cyno/skill.go
+++ internal/characters/cyno/skill.go
@@ -15,8 +15,9 @@ var (
 )
 
 var (
-	skillFrames  []int
-	skillBFrames []int
+	skillFrames   []int
+	skillBFrames  []int
+	skillA1Frames []int
 )
 
 func init() {
@@ -25,11 +26,17 @@ func init() {
 	skillFrames[action.ActionJump] = 32
 	skillFrames[action.ActionSwap] = 42
 
-	// mortuary rite frames
+	// burst frames
 	skillBFrames = frames.InitAbilSlice(34)
 	skillBFrames[action.ActionDash] = 30
 	skillBFrames[action.ActionJump] = 31
 	skillBFrames[action.ActionSwap] = 33
+
+	// skill has different frame data with endseers
+	skillA1Frames = frames.InitAbilSlice(35)
+	skillA1Frames[action.ActionDash] = 30
+	skillA1Frames[action.ActionJump] = 31
+	skillA1Frames[action.ActionSwap] = 33
 }
 
 func (c *char) Skill(p map[string]int) action.ActionInfo {
@@ -144,8 +151,12 @@ func (c *char) skillB() action.ActionInfo {
 
 	c.SetCDWithDelay(action.ActionSkill, 180, 26)
 
+	f := skillBFrames
+	if c.StatusIsActive(a4key) {
+		f = skillA1Frames
+	}
 	return action.ActionInfo{
-		Frames:          frames.NewAbilFunc(skillBFrames),
+		Frames:          frames.NewAbilFunc(f),
 		AnimationLength: skillBFrames[action.InvalidAction],
 		CanQueueAfter:   skillBFrames[action.ActionJump], // earliest cancel is 3f before SkillBHitmark
 		State:           action.SkillState,
