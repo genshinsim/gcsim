c0e2817b655af4ce6f582c0230fcbc7f9b3bc35a
Kyle Bartz <mail@kyle.fail>
2022-10-16 04:45:47 +0000

    Fixed eslint and improved its rules

--- app/.prettierrc.js
+++ app/.prettierrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-  "printWidth": 80,
-  "singleQuote": true,
-  "trailingComma": "es5"
+  "printWidth": 100,
+  "singleQuote": false,
+  "semi": true,
+  "trailingComma": "es5",
+  "quoteProps": "preserve",
 }
