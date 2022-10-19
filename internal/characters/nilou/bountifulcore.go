2e8c2d521a41d460027d331f4a6696382847cecd
imring <vitaliyvorobets25@gmail.com>
2022-10-19 21:25:23 +0300

    remove todo comments

--- internal/characters/nilou/bountifulcore.go
+++ internal/characters/nilou/bountifulcore.go
@@ -31,7 +31,6 @@ func newBountifulCore(c *core.Core, x float64, y float64, a *combat.AttackEvent)
 		ai.FlatDmg = 0.05 * ai.FlatDmg
 		c.QueueAttack(ai, combat.NewCircleHit(b.Gadget, 6.5, true, combat.TargettablePlayer), -1, 1)
 	}
-	//TODO: should bloom do damage if it blows up due to limit reached?
 	b.Gadget.OnExpiry = explode
 	b.Gadget.OnKill = explode
 
