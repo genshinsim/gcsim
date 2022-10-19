9c111abc28e12dc8aaa51123b4f22b8612f085b6
skippi <jbtcao@gmail.com>
2022-10-19 18:01:07 -0500

    use StatMod for xiao a1
    We don't need AttackMod since we don't check for attack info.

--- internal/characters/xiao/asc.go
+++ internal/characters/xiao/asc.go
@@ -2,7 +2,6 @@ package xiao
 
 import (
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
-	"github.com/genshinsim/gcsim/pkg/core/combat"
 	"github.com/genshinsim/gcsim/pkg/core/player/character"
 	"github.com/genshinsim/gcsim/pkg/modifier"
 )
@@ -11,9 +10,10 @@ const a1Key = "xiao-a1"
 
 func (c *char) a1() {
 	m := make([]float64, attributes.EndStatType)
-	c.AddAttackMod(character.AttackMod{
-		Base: modifier.NewBase(a1Key, 900+burstStart),
-		Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+	c.AddStatMod(character.StatMod{
+		Base:         modifier.NewBaseWithHitlag(a1Key, 900+burstStart),
+		AffectedStat: attributes.DmgP,
+		Amount: func() ([]float64, bool) {
 			stacks := 1 + int((c.Core.F-c.qStarted)/180)
 			if stacks > 5 {
 				stacks = 5
