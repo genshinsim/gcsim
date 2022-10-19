d3b7b9090f2003b69491b0dd60f1f7f4aba85234
Charlie Zheng <charlie.x.3000@gmail.com>
2022-10-19 13:35:28 -0600

    Added ICD check to Lamp Gadget

--- internal/characters/travelerdendro/lealotus.go
+++ internal/characters/travelerdendro/lealotus.go
@@ -8,6 +8,7 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/combat"
 	"github.com/genshinsim/gcsim/pkg/core/event"
 	"github.com/genshinsim/gcsim/pkg/core/glog"
+	"github.com/genshinsim/gcsim/pkg/enemy"
 	"github.com/genshinsim/gcsim/pkg/gadget"
 	"github.com/genshinsim/gcsim/pkg/reactable"
 )
@@ -20,6 +21,11 @@ type LeaLotus struct {
 	targetingRadius float64
 	hitboxRadius    float64
 	r               reactable.Reactable
+	//icd related
+	icdTagOnTimer       [enemy.MaxTeamSize][combat.ICDTagLength]bool
+	icdTagCounter       [enemy.MaxTeamSize][combat.ICDTagLength]int
+	icdDamageTagOnTimer [enemy.MaxTeamSize][combat.ICDTagLength]bool
+	icdDamageTagCounter [enemy.MaxTeamSize][combat.ICDTagLength]int
 }
 
 func (s *LeaLotus) transfigInit() {
@@ -138,8 +144,15 @@ func (s *LeaLotus) transfig(ele attributes.Element) {
 }
 
 func (s *LeaLotus) Attack(a *combat.AttackEvent, evt glog.Event) (float64, bool) {
+
 	s.Core.Log.NewEvent(fmt.Sprintf("DMC lamp hit by %s", a.Info.Abil), glog.LogCharacterEvent, s.char.Index)
-	s.React(a)
+
+	s.ShatterCheck(a)
+	//check for ICD first
+	a.OnICD = !s.WillApplyEle(a.Info.ICDTag, a.Info.ICDGroup, a.Info.ActorIndex)
+	if a.Info.Durability > 0 && !a.OnICD && a.Info.Element != attributes.Physical {
+		s.React(a)
+	}
 	return 0, false
 }
 
@@ -154,13 +167,15 @@ func (s *LeaLotus) OnThinkInterval() {
 }
 
 func (s *LeaLotus) ApplyDamage(atk *combat.AttackEvent, damage float64) {
-	if atk.Info.Durability > 0 && !atk.Reacted && !atk.OnICD && atk.Info.Element != attributes.Physical {
+	if atk.Info.Durability > 0 && !atk.OnICD && atk.Info.Element != attributes.Physical {
 		if !atk.Reacted {
 			s.AttachOrRefill(atk)
+			s.Core.Log.NewEvent(fmt.Sprintf("Aura Applied: DMC lamp auras: %s", s.ActiveAuraString()), glog.LogCharacterEvent, s.char.Index)
 		}
-		s.Core.Log.NewEvent(fmt.Sprintf("DMC lamp auras: %s", s.ActiveAuraString()), glog.LogCharacterEvent, s.char.Index)
-	}
 
+		s.Core.Log.NewEvent(fmt.Sprintf("DMC lamp auras: %s", s.ActiveAuraString()), glog.LogCharacterEvent, s.char.Index)
+
+	}
 }
 
 func (s *LeaLotus) OnExpiry(*combat.AttackEvent, float64) {
@@ -180,5 +195,88 @@ func (l *LeaLotus) QueueAttack(delay int) {
 			delay,
 		)
 	}
+}
 
+// ICD code copied from enemy/icd.go
+func (t *LeaLotus) WillApplyEle(tag combat.ICDTag, grp combat.ICDGroup, char int) bool {
+
+	//no icd if no tag
+	if tag == combat.ICDTagNone {
+		return true
+	}
+
+	//check if we need to start timer
+	x := t.icdTagOnTimer[char][tag]
+	if !t.icdTagOnTimer[char][tag] {
+		t.icdTagOnTimer[char][tag] = true
+		t.ResetTagCounterAfterDelay(tag, grp, char)
+	}
+
+	val := t.icdTagCounter[char][tag]
+	//increment the counter
+	//if counter > length, then reset back to 0
+	t.icdTagCounter[char][tag]++
+	if t.icdTagCounter[char][tag] == len(combat.ICDGroupEleApplicationSequence[grp]) {
+		t.icdTagCounter[char][tag] = 0
+	}
+
+	t.Core.Log.NewEvent("ele icd check", glog.LogICDEvent, char).
+		Write("grp", grp).
+		Write("target", t.TargetIndex).
+		Write("tag", tag).
+		Write("counter", val).
+		Write("val", combat.ICDGroupEleApplicationSequence[grp][val]).
+		Write("group on timer", x)
+	//true if group seq is 1
+	return combat.ICDGroupEleApplicationSequence[grp][val] == 1
+}
+
+func (t *LeaLotus) GroupTagDamageMult(tag combat.ICDTag, grp combat.ICDGroup, char int) float64 {
+
+	//check if we need to start timer
+	if !t.icdDamageTagOnTimer[char][tag] {
+		t.icdDamageTagOnTimer[char][tag] = true
+		t.ResetDamageCounterAfterDelay(tag, grp, char)
+	}
+
+	val := t.icdDamageTagCounter[char][tag]
+	//increment the counter
+	//if counter > length, then reset back to 0
+	t.icdDamageTagCounter[char][tag]++
+	if t.icdDamageTagCounter[char][tag] == len(combat.ICDGroupDamageSequence[grp]) {
+		t.icdDamageTagCounter[char][tag] = 0
+	}
+
+	//true if group seq is 1
+	return combat.ICDGroupDamageSequence[grp][val]
+}
+
+func (t *LeaLotus) ResetDamageCounterAfterDelay(tag combat.ICDTag, grp combat.ICDGroup, char int) {
+	t.Core.Tasks.Add(func() {
+		//set the counter back to 0
+		t.icdDamageTagCounter[char][tag] = 0
+		t.icdDamageTagOnTimer[char][tag] = false
+		t.Core.Log.NewEvent("damage counter reset", glog.LogICDEvent, char).
+			Write("tag", tag).
+			Write("grp", grp)
+	}, combat.ICDGroupResetTimer[grp]-1)
+	t.Core.Log.NewEvent("damage reset timer set", glog.LogICDEvent, char).
+		Write("tag", tag).
+		Write("grp", grp).
+		Write("reset", t.Core.F+combat.ICDGroupResetTimer[grp]-1)
+}
+
+func (t *LeaLotus) ResetTagCounterAfterDelay(tag combat.ICDTag, grp combat.ICDGroup, char int) {
+	t.Core.Tasks.Add(func() {
+		//set the counter back to 0
+		t.icdTagCounter[char][tag] = 0
+		t.icdTagOnTimer[char][tag] = false
+		t.Core.Log.NewEvent("ele app counter reset", glog.LogICDEvent, char).
+			Write("tag", tag).
+			Write("grp", grp)
+	}, combat.ICDGroupResetTimer[grp]-1)
+	t.Core.Log.NewEvent("ele app reset timer set", glog.LogICDEvent, char).
+		Write("tag", tag).
+		Write("grp", grp).
+		Write("reset", t.Core.F+combat.ICDGroupResetTimer[grp]-1)
 }
