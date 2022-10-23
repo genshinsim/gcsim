d3e8dc1fef6bfec0afd8c8d316a0c2f8965fa2cc
soloxcx <crausch.cs@gmail.com>
2022-10-23 11:56:54 -0500

    amber and ayaka radius update

--- internal/characters/amber/bunny.go
+++ internal/characters/amber/bunny.go
@@ -30,7 +30,7 @@ func (c *char) makeBunny() {
 	snap := c.Snapshot(&ai)
 	b.ae = combat.AttackEvent{
 		Info:        ai,
-		Pattern:     combat.NewCircleHit(c.Core.Combat.Player(), 2, false, combat.TargettableEnemy, combat.TargettableGadget),
+		Pattern:     combat.NewCircleHit(c.Core.Combat.PrimaryTarget(), 3, false, combat.TargettableEnemy, combat.TargettableGadget),
 		SourceFrame: c.Core.F,
 		Snapshot:    snap,
 	}
