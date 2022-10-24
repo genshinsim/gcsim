1322630bd2eed45c3cd076131d99b0b3b056807b
soloxcx <crausch.cs@gmail.com>
2022-10-24 18:50:06 -0500

    barbara radius update

--- internal/characters/barbara/attack.go
+++ internal/characters/barbara/attack.go
@@ -72,7 +72,7 @@ func (c *char) Attack(p map[string]int) action.ActionInfo {
 	}
 
 	c.Core.QueueAttack(ai,
-		combat.NewCircleHit(c.Core.Combat.Player(), 0.1, false, combat.TargettableEnemy),
+		combat.NewCircleHit(c.Core.Combat.PrimaryTarget(), 1, false, combat.TargettableEnemy),
 		attackHitmarks[c.NormalCounter],
 		attackHitmarks[c.NormalCounter],
 		cb,
--- internal/characters/barbara/charge.go
+++ internal/characters/barbara/charge.go
@@ -74,7 +74,7 @@ func (c *char) ChargeAttack(p map[string]int) action.ActionInfo {
 
 	// TODO: Not sure of snapshot timing
 	c.Core.QueueAttack(ai,
-		combat.NewCircleHit(c.Core.Combat.Player(), 2, false, combat.TargettableEnemy),
+		combat.NewCircleHit(c.Core.Combat.PrimaryTarget(), 3, false, combat.TargettableEnemy),
 		chargeHitmark-windup,
 		chargeHitmark-windup,
 		cb,
