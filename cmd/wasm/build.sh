e8d32bc51013fe124d8b48d4cb60a57de2ae0763
Kyle Bartz <mail@kyle.fail>
2022-10-24 06:53:56 +0000

    Updated build scripts for new UI

--- cmd/wasm/build.sh
+++ cmd/wasm/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm -rf main.wasm
-
 # notice how we avoid spaces in $now to avoid quotation hell in go build command
-now=$(date +'%Y-%m-%d_%T')
+now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 GOOS=js GOARCH=wasm go build -o main.wasm -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now" $@
