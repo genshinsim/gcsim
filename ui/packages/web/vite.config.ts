2feb3411943b4afbe19953ebbd3fad676e03e73b
Kyle Bartz <mail@kyle.fail>
2022-10-25 01:08:23 +0000

    Reverted vite custom chunking

--- ui/packages/web/vite.config.ts
+++ ui/packages/web/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { visualizer } from "rollup-plugin-visualizer";
@@ -6,27 +6,28 @@ import { visualizer } from "rollup-plugin-visualizer";
 export default defineConfig({
   plugins: [
     react(),
+    splitVendorChunkPlugin(),
     tsconfigPaths(),
     visualizer()
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            if (id.includes("@blueprintjs") && id.includes("icons")) {
-              return "blueprint-icons";
-            }
-            if (id.includes("prismjs") || id.includes("pako")) {
-              return "core";
-            }
-            return "vendor";
-          }
-          return "core";
-        }
-      }
-    }
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: (id) => {
+  //         if (id.includes("node_modules")) {
+  //           if (id.includes("@blueprintjs") && id.includes("icons")) {
+  //             return "blueprint-icons";
+  //           }
+  //           if (id.includes("prismjs") || id.includes("pako")) {
+  //             return "core";
+  //           }
+  //           return "vendor";
+  //         }
+  //         return "core";
+  //       }
+  //     }
+  //   }
+  // },
   server: {
     proxy: {
       "/api": {
