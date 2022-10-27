76afe9d12a4b5dffdb5c299050d2bee2d6b0d9bf
ChinPTeo <58355994+ChinPTeo@users.noreply.github.com>
2022-10-28 02:24:39 +1100

    just element left

--- scripts/newcharstat/main.go
+++ scripts/newcharstat/main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
 	"github.com/genshinsim/gcsim/pkg/core/curves"
 	"github.com/genshinsim/gcsim/pkg/core/player/character/profile"
 	"github.com/genshinsim/gcsim/pkg/core/player/weapon"
@@ -62,9 +63,9 @@ type Avatars []struct {
 	HpBase          float64 `json:"hpBase"`
 	AttackBase      float64 `json:"attackBase"`
 	DefenseBase     float64 `json:"defenseBase"`
-	Critical        float64 `json:"critical"`
-	CriticalHurt    float64 `json:"criticalHurt"`
-	PropGrowCurves  []struct {
+	// Critical        float64 `json:"critical"`
+	// CriticalHurt    float64 `json:"criticalHurt"`
+	PropGrowCurves []struct {
 		Type      string `json:"type"`
 		GrowCurve string `json:"growCurve"`
 	} `json:"propGrowCurves"`
@@ -73,12 +74,9 @@ type Avatars []struct {
 	UseType         string `json:"useType,omitempty"`
 }
 type AvatarPromotes []struct {
-	AvatarPromoteID int `json:"avatarPromoteId"`
-	AddProps        []struct {
-		PropType string  `json:"propType"`
-		Value    float64 `json:"value"`
-	} `json:"addProps"`
-	PromoteLevel int `json:"promoteLevel,omitempty"`
+	AvatarPromoteID int       `json:"avatarPromoteId"`
+	AddProps        []AddProp `json:"addProps"`
+	PromoteLevel    int       `json:"promoteLevel,omitempty"`
 	// PromoteAudio    string `json:"promoteAudio"`
 	// CostItems       []struct {
 	// } `json:"costItems"`
@@ -87,17 +85,14 @@ type AvatarPromotes []struct {
 	// RequiredPlayerLevel int `json:"requiredPlayerLevel,omitempty"`
 }
 
+type AddProp struct {
+	PropType string  `json:"propType"`
+	Value    float64 `json:"value"`
+}
+
 type FetterInfo []struct {
 	AvatarAssocType string `json:"avatarAssocType"`
 	AvatarId        int    `json:"avatarId"`
-}
-
-type PromoData struct {
-	MaxLevel int
-	HP       float64
-	Atk      float64
-	Def      float64
-	Special  float64
 }
 
 func main() {
@@ -138,21 +133,18 @@ func main() {
 		specializedStatMap[v.AvatarPromoteID] = v.AddProps[3].PropType
 	}
 
-	fmt.Printf("%+v\n", specializedStatMap)
+	// fmt.Printf("%+v\n", specializedStatMap)
 
-	// reshape avatarPromotes to map of avatarPromoteId to PromoData
-	promoDataMap := make(map[int]PromoData)
+	// reshape avatarPromotes to map of avatarPromoteId to PromoData array of 7 elements
+	promoDataMap := make(map[int][]curves.PromoData)
 	for _, v := range avatarPromotes {
-		promoDataMap[v.AvatarPromoteID] = PromoData{
-			MaxLevel: v.PromoteLevel,
-			HP:       v.AddProps[0].Value,
-			Atk:      v.AddProps[1].Value,
-			Def:      v.AddProps[2].Value,
-			Special:  v.AddProps[3].Value,
-		}
+		partialPromoData := addPropArraytoPromoData(v.AddProps)
+
+		partialPromoData.MaxLevel = promoteLevelToMaxLevel(v.PromoteLevel)
+		promoDataMap[v.AvatarPromoteID] = append(promoDataMap[v.AvatarPromoteID], partialPromoData)
 	}
 
-	fmt.Printf("%+v\n", promoDataMap)
+	// fmt.Printf("%+v\n", promoDataMap)
 
 	characterArray := make([]curves.CharBase, len(avatars))
 
@@ -204,35 +196,72 @@ func main() {
 		switch locationMap[avatar.ID] {
 		case "ASSOC_TYPE_INAZUMA":
 			char.Region = profile.ZoneInazuma
-
 		case "ASSOC_TYPE_LIYUE":
 			char.Region = profile.ZoneInazuma
-
 		case "ASSOC_TYPE_MONDSTADT":
 			char.Region = profile.ZoneInazuma
 		case "ASSOC_TYPE_SUMERU":
 			char.Region = profile.ZoneSumeru
-
 		default:
 			char.Region = profile.ZoneUnknown
 		}
 
-		// switch avatar.
-
-		// characterArray[index].Region = profile.ZoneType(avatar.AvatarRegion)
+		switch specializedStatMap[avatar.AvatarPromoteID] {
+		case "FIGHT_PROP_CRITICAL_HURT":
+			char.Specialized = attributes.CD
+		case "FIGHT_PROP_HEAL_ADD":
+			char.Specialized = attributes.Heal
+		case "FIGHT_PROP_ATTACK_PERCENT":
+			char.Specialized = attributes.ATKP
+		case "FIGHT_PROP_ELEMENT_MASTERY":
+			char.Specialized = attributes.EM
+		case "FIGHT_PROP_HP_PERCENT":
+			char.Specialized = attributes.HPP
+		case "FIGHT_PROP_CHARGE_EFFICIENCY":
+			char.Specialized = attributes.ER
+		case "FIGHT_PROP_CRITICAL":
+			char.Specialized = attributes.CR
+		case "FIGHT_PROP_PHYSICAL_ADD_HURT":
+			char.Specialized = attributes.PhyP
+		case "FIGHT_PROP_ELEC_ADD_HURT":
+			char.Specialized = attributes.ElectroP
+		case "FIGHT_PROP_ROCK_ADD_HURT":
+			char.Specialized = attributes.GeoP
+		case "FIGHT_PROP_FIRE_ADD_HURT":
+			char.Specialized = attributes.PyroP
+		case "FIGHT_PROP_WATER_ADD_HURT":
+			char.Specialized = attributes.HydroP
+		case "FIGHT_PROP_DEFENSE_PERCENT":
+			char.Specialized = attributes.DEFP
+		case "FIGHT_PROP_ICE_ADD_HURT":
+			char.Specialized = attributes.CryoP
+		case "FIGHT_PROP_WIND_ADD_HURT":
+			char.Specialized = attributes.AnemoP
+		case "FIGHT_PROP_GRASS_ADD_HURT":
+			char.Specialized = attributes.DendroP
+		default:
+			log.Fatal("Unknown Specialized Stat")
+		}
 		// characterArray[index].Element = attributes.Element(avatar.AvatarElementalType)
-		char.HPCurve = curves.CharStatCurve(avatar.HpBase)
-		char.AtkCurve = curves.CharStatCurve(avatar.AttackBase)
-		char.DefCurve = curves.CharStatCurve(avatar.DefenseBase)
+		if strings.Contains(avatar.PropGrowCurves[0].GrowCurve, "S5") {
+			char.HPCurve = curves.GROW_CURVE_HP_S5
+			char.AtkCurve = curves.GROW_CURVE_ATTACK_S5
+			char.DefCurve = curves.GROW_CURVE_HP_S5
+		} else {
+			char.HPCurve = curves.GROW_CURVE_HP_S4
+			char.AtkCurve = curves.GROW_CURVE_ATTACK_S4
+			char.DefCurve = curves.GROW_CURVE_HP_S4
+		}
 		char.BaseHP = avatar.HpBase
 		char.BaseAtk = avatar.AttackBase
 		char.BaseDef = avatar.DefenseBase
-		// char.Specialized = attributes.Stat(avatar.Critical)
+		char.PromotionBonus = promoDataMap[avatar.AvatarPromoteID]
 
 		characterArray = append(characterArray, char)
 		//print out the character name
-		// fmt.Println(strings.Replace(avatar.IconName, "UI_AvatarIcon_", "", 1))
-		// fmt.Printf("%+v\n", char)
+		fmt.Println(strings.Replace(avatar.IconName, "UI_AvatarIcon_", "", 1))
+		fmt.Printf("%+v\n", char)
+		// fmt.Printf("%+v\n", avatar.PropGrowCurves)
 
 	}
 
@@ -299,3 +328,61 @@ var CharBaseMap = map[keys.Char]CharBase{
 }
 
 `
+
+var SpecKeyToStat = map[string]string{
+	"FIGHT_PROP_CRITICAL_HURT":     "attributes.CD",
+	"FIGHT_PROP_HEAL_ADD":          "attributes.Heal",
+	"FIGHT_PROP_ATTACK_PERCENT":    "attributes.ATKP",
+	"FIGHT_PROP_ELEMENT_MASTERY":   "attributes.EM",
+	"FIGHT_PROP_HP_PERCENT":        "attributes.HPP",
+	"FIGHT_PROP_CHARGE_EFFICIENCY": "attributes.ER",
+	"FIGHT_PROP_CRITICAL":          "attributes.CR",
+	"FIGHT_PROP_PHYSICAL_ADD_HURT": "attributes.PhyP",
+	"FIGHT_PROP_ELEC_ADD_HURT":     "attributes.ElectroP",
+	"FIGHT_PROP_ROCK_ADD_HURT":     "attributes.GeoP",
+	"FIGHT_PROP_FIRE_ADD_HURT":     "attributes.PyroP",
+	"FIGHT_PROP_WATER_ADD_HURT":    "attributes.HydroP",
+	"FIGHT_PROP_DEFENSE_PERCENT":   "attributes.DEFP",
+	"FIGHT_PROP_ICE_ADD_HURT":      "attributes.CryoP",
+	"FIGHT_PROP_WIND_ADD_HURT":     "attributes.AnemoP",
+	"FIGHT_PROP_GRASS_ADD_HURT":    "attributes.DendroP",
+}
+
+func promoteLevelToMaxLevel(level int) int {
+	switch level {
+	case 0:
+		return 20
+	case 1:
+		return 40
+	case 2:
+		return 50
+	case 3:
+		return 60
+	case 4:
+		return 70
+	case 5:
+		return 80
+	case 6:
+		return 90
+	default:
+		return 0
+	}
+}
+
+func addPropArraytoPromoData(addProps []AddProp) curves.PromoData {
+	var out curves.PromoData
+	for _, prop := range addProps {
+
+		switch prop.PropType {
+		case "FIGHT_PROP_BASE_HP":
+			out.HP = prop.Value
+		case "FIGHT_PROP_BASE_ATTACK":
+			out.Atk = prop.Value
+		case "FIGHT_PROP_BASE_DEFENSE":
+			out.Def = prop.Value
+		default:
+			out.Special = prop.Value
+		}
+	}
+	return out
+}
