dac40f8e76489ae2cc840ba35e978529d6ec925d
ChinPTeo <58355994+ChinPTeo@users.noreply.github.com>
2022-11-05 18:58:02 +1100

    swapping over to extracting from binoutput

--- scripts/newweapstat/main.go
+++ scripts/newweapstat/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 func main() {
-	getWeapon()
+	// var weapons []curves.WeaponBase
+	weaponConfigArray := getWeapon()
+
+	for _, weaponConfig := range weaponConfigArray {
+		// var weapon curves.WeaponBase
+		// weapon. = weaponConfig.Name
+		fmt.Println(weaponConfig.ID)
+		fmt.Printf("%+v\n", weaponConfig.WeaponProp)
+		// weapons = append(weapons, weapon)
+	}
+
+}
+
+func getJsonFromFile[V WeaponConfigs](path string) V {
+	jsonFile, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var configData V
+	json.Unmarshal(byteValue, &configData)
+
+	return configData
 }
