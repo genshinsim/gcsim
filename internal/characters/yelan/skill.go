2ba581311621650653809ee9d5cb16b52c6c6f07
k0l11 <intensonom@yahoo.de>
2022-11-12 16:23:39 +0100

    fix yelan striketype

--- internal/characters/yelan/skill.go
+++ internal/characters/yelan/skill.go
@@ -39,6 +39,7 @@ func (c *char) Skill(p map[string]int) action.ActionInfo {
 		AttackTag:  combat.AttackTagElementalArt,
 		ICDTag:     combat.ICDTagNone,
 		ICDGroup:   combat.ICDGroupDefault,
+		StrikeType: combat.StrikeTypeDefault,
 		Element:    attributes.Hydro,
 		Durability: 25,
 		Mult:       0,
