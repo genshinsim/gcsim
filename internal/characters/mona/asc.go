4c92fc0e086da9f7510ab3f524dcc6efd530073f
k0l11 <intensonom@yahoo.de>
2022-11-12 16:19:10 +0100

    fix mona striketype

--- internal/characters/mona/asc.go
+++ internal/characters/mona/asc.go
@@ -33,6 +33,7 @@ func (c *char) a1() func() {
 				AttackTag:  combat.AttackTagElementalArt,
 				ICDTag:     combat.ICDTagNone,
 				ICDGroup:   combat.ICDGroupDefault,
+				StrikeType: combat.StrikeTypeDefault,
 				Element:    attributes.Hydro,
 				Durability: 25,
 				Mult:       0.5 * skill[c.TalentLvlSkill()],
