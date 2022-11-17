78b4668c3a3fade62f3b59d36917b17f250449e8
srliao <906239+srliao@users.noreply.github.com>
2022-11-17 18:54:59 -0500

    initial skeleton proto definitions

--- generate.go
+++ generate.go
@@ -1,4 +1,3 @@
-//go:generate protoc --go-grpc_out=. proto/backend/result.proto
-//go:generate protoc --go_out=. proto/backend/result.proto
-//go:generate sh scripts/preview/build.sh
+//go:generate sh scripts/generate/generate_protos.sh
+//go:generate sh scripts/generate/build_preview.sh
 package gcsim
