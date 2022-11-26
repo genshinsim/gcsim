cb74385d3287cb140ea44bb0da524ffb31f04b97
Kyle Bartz <mail@kyle.fail>
2022-11-26 03:42:26 +0000

    Created initial results layout

--- ui/packages/ui/src/Pages/Viewer/Tabs/Results.tsx
+++ ui/packages/ui/src/Pages/Viewer/Tabs/Results.tsx
@@ -2,6 +2,9 @@ import Overview from "../Components/Results/Overview";
 import { SimResults } from "@gcsim/types";
 import TeamHeader from "../Components/Results/TeamHeader";
 import DistributionCard from "../Components/Results/DistributionCard";
+import { Card } from "@blueprintjs/core";
+import { ReactNode } from "react";
+import classNames from "classnames";
 
 type Props = {
   data: SimResults | null;
@@ -9,14 +12,152 @@ type Props = {
 
 export default ({ data }: Props) => {
   return (
-    <div className="w-full 2xl:mx-auto 2xl:container">
-      <div className="grid overflow-hidden grid-cols-2 md:grid-cols-5 auto-rows-auto gap-2 px-2">
+    <div className="w-full 2xl:mx-auto 2xl:container px-2">
+      {/* Overview */}
+      <Group>
         <TeamHeader data={data} />
         <Overview data={data} />
-        <div className="col-span-2 md:col-span-3 h-72 bg-bp4-black p-5">
-        </div>
+        <Card className="flex col-span-3 h-24 min-h-full">
+          Target info + sim metadata (num iterations)
+        </Card>
         <DistributionCard data={data} />
-      </div>
+      </Group>
+
+      {/* Damage */}
+      <Group>
+        <Heading text="Damage" />
+        <Card className="col-span-full h-64">
+          Damage Timeline (dps/time + cumu %)
+        </Card>
+        <Card className="flex col-span-2 h-64 min-h-full">
+          DPS per character (pie?)
+        </Card>
+        <Card className="flex col-span-2 h-64 min-h-full">
+          DPS per target (pie?)
+        </Card>
+        <Card className="flex col-span-2 h-64 min-h-full">
+          DPS per element (pie?)
+        </Card>
+        <Card className="flex col-span-full h-64 min-h-full">
+          DPS per character w/ element breakdown
+        </Card>
+        <Card className="flex col-span-full h-64 min-h-full">
+          Damage breakdown table
+        </Card>
+      </Group>
+
+      {/* Energy */}
+      <Group>
+        <Heading text="Energy" />
+        <Card className="flex col-span-full h-64 min-h-full">
+          Energy over time + cumu gained + cumu wasted
+        </Card>
+        <Card className="flex col-span-full h-64 min-h-full">
+          Energy produced by source
+        </Card>
+        <Card className="flex col-span-full h-64 min-h-full">
+          Incoming energy per character breakdown
+        </Card>
+      </Group>
+
+      {/* Reactions & Auras */}
+      <Group>
+        <Heading text="Reactions & Auras" />
+        <Card className="flex col-span-4 h-64 min-h-full">
+          Aura uptime timeline (worst, best, heatmap)
+        </Card>
+        <Card className="flex col-span-2 h-64 min-h-full">
+          Aura uptime (pie? vs bar?)
+        </Card>
+        <Card className="flex col-span-3 h-64 min-h-full">
+          Reactions triggered bar chart
+        </Card>
+        <Card className="flex col-span-3 h-64 min-h-full">
+          Reactions by source?
+        </Card>
+      </Group>
+
+      {/* Healing */}
+      {/* TODO: make optional? */}
+      <Group>
+        <Heading text="Healing" />
+        <Card className="flex col-span-full h-64 min-h-full">
+          Effective Healing Timeline (+ HP per char?)
+        </Card>
+        <Card className="flex col-span-3 h-64 min-h-full">
+          Healing by Src
+        </Card>
+        <Card className="flex col-span-3 h-64 min-h-full">
+          Healing by target
+        </Card>
+      </Group>
+
+      {/* Shields */}
+      {/* TODO: make optional? */}
+      <Group>
+        <Heading text="Shields" />
+        <Card className="flex col-span-full h-64 min-h-full">
+          Shield timeline (worst, best, heatmap)
+        </Card>
+        <Card className="flex col-span-3 h-[512px] min-h-full">
+          Shield uptime bar chart + pie
+        </Card>
+        <Card className="flex col-span-3 h-[512px] min-h-full">
+          Shield hp bar chart
+        </Card>
+        <Card className="flex col-span-full h-64 min-h-full">
+          Shield table
+        </Card>
+      </Group>
+
+      {/* Sim Metadata? */}
+      <Group>
+        <Heading text="Simulation Details" />
+        <Card className="flex col-span-2 h-64 min-h-full">
+          Character uptime (pie?)
+        </Card>
+        <Card className="flex col-span-4 h-64 min-h-full">
+          Character Uptime timeline (worst, best, heatmap)
+        </Card>
+        <Card className="flex col-span-4 h-64 min-h-full">
+          Failed actions bar graph
+        </Card>
+        <Card className="flex col-span-2 h-64 min-h-full">
+          Faied actions timeline (worst, best, heatmap)
+        </Card>
+        {/* tables? */}
+      </Group>
+    </div>
+  );
+};
+
+const Heading = ({ text }: { text: string }) => (
+  <h2 className="group flex whitespace-pre-wrap col-span-full text-xl font-semibold mt-4">
+    {text}
+    {/* currently does not work (wouter doesn't support hash links) */}
+    {/* <a
+        href={target}
+        className="ml-2 text-blue-500 opacity-0 transition-opacity group-hover:opacity-100">
+      #
+    </a> */}
+  </h2>
+);
+
+type GroupProps = {
+  children: ReactNode;
+  className?: string;
+}
+
+const Group = ({ children, className }: GroupProps) => {
+  const cls = classNames(
+      className,
+      "grid overflow-hidden",
+      "grid-cols-1 sm:grid-cols-6",
+      "gap-y-2", "sm:gap-2");
+
+  return (
+    <div className={cls}>
+      {children}
     </div>
   );
 };
