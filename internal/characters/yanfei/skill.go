34b4252e458b3dc270db69593eea34f50322bfa6
k0l11 <intensonom@yahoo.de>
2022-12-04 19:09:27 +0100

    fix yanfei hitboxes

--- internal/characters/yanfei/skill.go
+++ internal/characters/yanfei/skill.go
@@ -51,7 +51,18 @@ func (c *char) Skill(p map[string]int) action.ActionInfo {
 		Mult:       skill[c.TalentLvlSkill()],
 	}
 	// TODO: Not sure of snapshot timing
-	c.Core.QueueAttack(ai, combat.NewCircleHit(c.Core.Combat.Player(), 3.5), 0, skillHitmark, addSeal)
+	c.Core.QueueAttack(
+		ai,
+		combat.NewCircleHit(
+			c.Core.Combat.Player(),
+			c.Core.Combat.PrimaryTarget(),
+			nil,
+			3.5,
+		),
+		0,
+		skillHitmark,
+		addSeal,
+	)
 
 	c.Core.QueueParticle("yanfei", 3, attributes.Pyro, skillHitmark+c.ParticleDelay)
 
