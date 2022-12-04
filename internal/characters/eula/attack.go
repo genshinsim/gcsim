0c8cc12c685ab2380e8f3fb823d69ecb4de7c1c9
k0l11 <intensonom@yahoo.de>
2022-12-04 18:34:59 +0100

    fix eula hitboxes

--- internal/characters/eula/attack.go
+++ internal/characters/eula/attack.go
@@ -14,7 +14,9 @@ var (
 	attackHitmarks        = [][]int{{30}, {19}, {25, 42}, {17}, {29, 56}}
 	attackHitlagHaltFrame = [][]float64{{0.09}, {.12}, {0, .09}, {.09}, {0, .12}}
 	attackDefHalt         = [][]bool{{true}, {true}, {false, true}, {true}, {false, true}}
-	attackRadius          = []float64{2, 2, 2, 2, 1.8}
+	attackHitboxes        = [][]float64{{2}, {2}, {2}, {2}, {2, 3}}
+	attackOffsets         = []float64{0.5, 0.5, 0.5, 0, 0}
+	attackFanAngles       = []float64{270, 270, 360, 270, 360}
 )
 
 const normalHitNum = 5
@@ -46,14 +48,22 @@ func (c *char) Attack(p map[string]int) action.ActionInfo {
 			HitlagHaltFrames:   attackHitlagHaltFrame[c.NormalCounter][i] * 60,
 			CanBeDefenseHalted: attackDefHalt[c.NormalCounter][i],
 		}
-		radius := attackRadius[c.NormalCounter]
-		c.QueueCharTask(func() {
-			c.Core.QueueAttack(
-				ai,
-				combat.NewCircleHit(c.Core.Combat.Player(), radius),
-				0,
-				0,
+		ap := combat.NewCircleHitOnTargetFanAngle(
+			c.Core.Combat.Player(),
+			combat.Point{Y: attackOffsets[c.NormalCounter]},
+			attackHitboxes[c.NormalCounter][0],
+			attackFanAngles[c.NormalCounter],
+		)
+		if c.NormalCounter == 4 {
+			ap = combat.NewBoxHitOnTarget(
+				c.Core.Combat.Player(),
+				combat.Point{Y: attackOffsets[c.NormalCounter]},
+				attackHitboxes[c.NormalCounter][0],
+				attackHitboxes[c.NormalCounter][1],
 			)
+		}
+		c.QueueCharTask(func() {
+			c.Core.QueueAttack(ai, ap, 0, 0)
 		}, attackHitmarks[c.NormalCounter][i])
 	}
 
