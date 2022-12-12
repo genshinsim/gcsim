64a2319c17d1824b25c45dc5f171502b2f7a7284
Kyle Bartz <mail@kyle.fail>
2022-12-12 06:08:47 +0000

    Fixed image flickering on NoData view

--- ui/packages/ui/src/Pages/Viewer/Components/Util/NoData/index.tsx
+++ ui/packages/ui/src/Pages/Viewer/Components/Util/NoData/index.tsx
@@ -1,14 +1,29 @@
 import { NonIdealState } from "@blueprintjs/core";
+import { useEffect, useRef } from "react";
 import qiqi from "./images/qiqi.png";
 import kuki from "./images/kuki.png";
 import ayaka from "./images/ayaka.png";
 import xiao from "./images/xiao.png";
+import nahida from "./images/nahida.png";
+import venti from "./images/venti.png";
+import raiden from "./images/raiden.png";
+import collei from "./images/collei.png";
+import lisa from "./images/lisa.png";
+import ayato from "./images/ayato.png";
+import albedo from "./images/albedo.png";
 
 const images = [
   qiqi,
   kuki,
   xiao,
   ayaka,
+  nahida,
+  venti,
+  raiden,
+  collei,
+  lisa,
+  ayato,
+  albedo,
 ];
 
 // TODO: translation
@@ -24,10 +39,19 @@ export default ({}) => {
 let availableImages = [...images];
 
 const Icon = ({}) => {
-  const options = availableImages.length > 0 ? availableImages : [...images];
-  const img = options.splice(Math.floor(Math.random() * options.length), 1);
-  availableImages = options;
+  const img = useRef<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (img.current != null) {
+      return;
+    }
+
+    const options = availableImages.length > 0 ? availableImages : [...images];
+    img.current = options.splice(Math.floor(Math.random() * options.length), 1)[0];
+    availableImages = options;
+  }, []);
+
   return (
-    <img src={img[0]} className="h-24" />
+    <img src={img.current} className="h-24" />
   );
 };
