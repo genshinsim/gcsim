ee22daeae2217c559639d3bf0e531a5fa3f9caff
Kyle Bartz <mail@kyle.fail>
2022-12-22 05:36:39 +0000

    Added wasm compiler flags to reduce binary size

--- cmd/wasm/build.sh
+++ cmd/wasm/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # notice how we avoid spaces in $now to avoid quotation hell in go build command
+# reduces by ~2MB but makes really slow: -gcflags=all="-l -B -C -std"
 now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-GOOS=js GOARCH=wasm go build -o main.wasm $@
+GOOS=js GOARCH=wasm go build -o main.wasm -ldflags="-w -s -d" $@
