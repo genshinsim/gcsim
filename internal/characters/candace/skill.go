d7ce9e8fa97eb82d6bd788168257771193bd0f96
skippi <jbtcao@gmail.com>
2022-12-23 18:24:15 -0600

    add shield names for candace and yunjin

--- internal/characters/candace/skill.go
+++ internal/characters/candace/skill.go
@@ -83,6 +83,7 @@ func (c *char) Skill(p map[string]int) action.ActionInfo {
 	// Add shield until skill unleashed (treated as frame when attack hits)
 	c.Core.Player.Shields.Add(&shield.Tmpl{
 		Src:        c.Core.F,
+		Name:       "Candace Skill",
 		ShieldType: shield.ShieldCandaceSkill,
 		HP:         skillShieldPct[c.TalentLvlSkill()]*c.MaxHP() + skillShieldFlat[c.TalentLvlSkill()],
 		Ele:        attributes.Hydro,
