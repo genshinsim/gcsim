fd06262c461c86991c6db151db74cce542506c8b
srliao <906239+srliao@users.noreply.github.com>
2022-12-27 21:49:42 +0000

    docker fix attempt 2

--- backend/mongo-init.js
+++ backend/mongo-init.js
@@ -2,15 +2,4 @@ db.auth("root", "root-password");
 
 db = db.getSiblingDB("gcsim-database");
 
-db.createUser({
-  user: "gcsim",
-  pwd: "gcsim-password",
-  roles: [
-    {
-      role: "root",
-      db: "gcsim-database",
-    },
-  ],
-});
-
 db.createCollection("data");
--- start_backend.sh
+++
@@ -1,3 +0,0 @@
-docker compose build backend_image --target=preview
-docker compose build backend_image --target=backend
-docker compose up -d
