ef6e381e182d3c7808078ac54b5872265f1d537c
CPTeo <chinphin.teo@gmail.com>
2023-01-08 19:56:14 +1100

    basic dbentry work

--- ui/packages/db/src/Pages/Database/database.tsx
+++ ui/packages/db/src/Pages/Database/database.tsx
@@ -32,9 +32,52 @@ const mockData: model.IDBEntries["data"] = [
         maxLevel: 90,
       },
       {
-        name: "dehya",
+        name: "xingqiu",
         cons: 0,
-        element: "pyro",
+        element: "hydro",
+        level: 90,
+        weapon: {
+          level: 90,
+          name: "favoniuscodex",
+          maxLevel: 90,
+          refine: 5,
+        },
+        sets: {
+          wandererstroupe: 4,
+        },
+        talents: {
+          attack: 9,
+          burst: 9,
+          skill: 9,
+        },
+        maxLevel: 90,
+      },
+      {
+        name: "yelan",
+        cons: 0,
+        element: "hydro",
+        level: 90,
+        weapon: {
+          level: 90,
+          name: "favoniuscodex",
+          maxLevel: 90,
+          refine: 5,
+        },
+        sets: {
+          wandererstroupe: 2,
+          gildeddreams: 2,
+        },
+        talents: {
+          attack: 9,
+          burst: 9,
+          skill: 9,
+        },
+        maxLevel: 90,
+      },
+      {
+        name: "raiden",
+        cons: 0,
+        element: "cryo",
         level: 90,
         weapon: {
           level: 90,
@@ -71,11 +114,13 @@ const mockData: model.IDBEntries["data"] = [
     config: "config",
     hash: "hash",
     //indexing fields
-    charNames: ["dehya", "nahida"],
+    charNames: ["nahida", "xingqiu", "yelan", "raiden"],
     targetCount: 1,
     meanDpsPerTarget: 100,
     createDate: 20210101,
     runDate: 20210101,
+    description:
+      "  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut augue dapibus, interdum ante quis, congue nisi. Nulla ut sagittis lorem. Aliquam lobortis, urna sit amet fringilla porttitor, diam metus laoreet odio, rutrum maximus ligula ipsum ac nunc. Vivamus euismod nec neque at pharetra. Curabitur aliquam lectus diam. Suspendisse quis ultrices odio. Aenean eleifend condimentum nibh auctor ultrices. Curabitur sit amet erat vitae odio imperdiet consectetur. Suspendisse at massa bibendum, rutrum sem eu, laoreet magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin a maximus quam. Duis tempor viverra dui, at lacinia ligula Aliquam erat volutpat. Etiam pulvinar, elit a blandit sodales, erat libero bibendum turpis, quis sollicitudin felis ipsum ac velit. Nunc lobortis est eget lacus gravida consectetur. Morbi sed venenatis odio. Quisque vel finibus risus. Duis ut cursus magna. Donec egestas ante vitae neque finibus, at blandit ex porta. Proin molestie orci vitae velit ornare facilisis. Morbi auctor sodales maximus. Etiam eu posuere augue. Nunc vehicula ut est ac placerat. Pellentesque dignissim vitae lectus ac faucibus. Aliquam accumsan mi ut magna rutrum, commodo dapibus augue malesuada. Aenean vitae erat nec elit pharetra semper id sit amet orci. Nam ullamcorper euismod elit, nec elementum sem porta eu. Nam et velit dictum, varius odio sit amet, venenatis eros. ",
   },
 ];
 
