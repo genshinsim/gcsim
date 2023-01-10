7b28328cf9dce49e257747912d056c411b9b5a3d
Kyle <mail@kyle.fail>
2023-01-10 13:05:29 -0800

    Fixed direction set to work when no enemies (#1219)

--- pkg/target/target.go
+++ pkg/target/target.go
@@ -127,15 +127,17 @@ func (t *Target) SetDirection(trg combat.Point) {
 }
 func (t *Target) SetDirectionToClosestEnemy() {
 	src := t.Pos()
-	// calculate direction towards closest enemy
-	enemyIndex := t.Core.Combat.EnemyByDistance(src, combat.InvalidTargetKey)[0]
-	enemy := t.Core.Combat.Enemy(enemyIndex)
-	if enemy == nil {
-		panic("there should be an enemy to calculate direction")
+	// calculate direction towards closest enemy, or forward direction if none
+	enemies := t.Core.Combat.EnemyByDistance(src, combat.InvalidTargetKey)
+	if len(enemies) == 0 {
+		t.direction = combat.DefaultDirection()
+		return
 	}
+
+	enemy := t.Core.Combat.Enemy(enemies[0])
 	t.SetDirection(enemy.Pos())
 	t.Core.Combat.Log.NewEvent("set target direction to closest enemy", glog.LogDebugEvent, -1).
-		Write("enemy index", enemyIndex).
+		Write("enemy index", enemies[0]).
 		Write("enemy key", enemy.Key()).
 		Write("direction", t.direction)
 }
