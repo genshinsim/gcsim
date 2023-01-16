be7507ddca8b5b085720653cf7c48d68e0f7dd49
Charlie Zheng <charlie.x.3000@gmail.com>
2023-01-15 17:52:11 -0700

    Cleaned up code

--- internal/common/normalattackstate.go
+++ internal/common/normalattackstate.go
@@ -249,14 +249,14 @@ func (w *NAHook) checkState() bool {
 	state := w.Core.Player.CurrentState()
 	if state != action.NormalAttackState {
 		w.Core.Log.NewEvent(fmt.Sprintf("%v not triggered, not in normal state", w.AbilName), glog.LogCharacterEvent, w.C.Index).
-			Write("state", w.Core.Player.CurrentState())
+			Write("state", state)
 		return false
 	}
 	state_start := w.Core.Player.CurrentStateStart()
 	norm_counter := w.Core.Player.ActiveChar().NormalCounter
 	if (norm_counter == 1) && w.Core.F-state_start < w.DelayFunc(w.Core.Player.ActiveChar()) {
 		w.Core.Log.NewEvent(fmt.Sprintf("%v not triggered, not enough time since normal state start", w.AbilName), glog.LogCharacterEvent, w.C.Index).
-			Write("current_state", w.Core.Player.CurrentState()).
+			Write("current_state", state).
 			Write("state_start", state_start)
 		return false
 	}
