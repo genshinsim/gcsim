0da91a0c7cc6f50d20d5dcf5bd28fc60e4b178fe
k0l11 <intensonom@yahoo.de>
2023-01-26 19:54:20 +0100

    fix albedo q snapshot, a4 and c2 timing

--- internal/characters/albedo/burst.go
+++ internal/characters/albedo/burst.go
@@ -11,8 +11,8 @@ import (
 
 var burstFrames []int
 
-const burstHitmark = 75         // Initial Hit
-const fatalBlossomHitmark = 145 // Fatal Blossom
+const burstHitmark = 75                        // Initial Hit
+const fatalBlossomHitmark = 145 - burstHitmark // Fatal Blossom, accounting for task queuing
 
 func init() {
 	burstFrames = frames.InitAbilSlice(96) // Q -> N1/E
@@ -33,28 +33,54 @@ func (c *char) Burst(p map[string]int) action.ActionInfo {
 		Durability: 25,
 		Mult:       burst[c.TalentLvlBurst()],
 	}
-	snap := c.Snapshot(&ai)
 
-	//check stacks
-	if c.Base.Cons >= 2 && c.StatusIsActive(c2key) {
-		ai.FlatDmg += (snap.BaseDef*(1+snap.Stats[attributes.DEFP]) + snap.Stats[attributes.DEF]) * float64(c.c2stacks)
+	c2Count := 0
+	hasC2 := c.Base.Cons >= 2 && c.StatusIsActive(c2key)
+	// C2 damage for initial hit is calculated on burst start
+	if hasC2 {
+		c2Count = c.c2stacks
 		c.c2stacks = 0
+		ai.FlatDmg = (c.Base.Def*(1+c.Stat(attributes.DEFP)) + c.Stat(attributes.DEF)) * float64(c2Count)
 	}
 
 	// initial damage
-	// TODO: damage frame
-	c.Core.QueueAttackWithSnap(
+	c.Core.QueueAttack(
 		ai,
-		snap,
 		combat.NewCircleHitOnTargetFanAngle(c.Core.Combat.Player(), nil, 8, 120),
+		burstHitmark,
 		burstHitmark,
 	)
 
-	// Blossoms are generated on a slight delay from initial hit
-	// TODO: no precise frame data for time between Blossoms
-	if c.skillActive {
+	// A4 and fatal blossom
+	// delay fatal blossom triggering until burstHitmark because that's when it:
+	// - checks whether the skill is still active
+	// - recalculates C2 damage
+	c.Core.Tasks.Add(func() {
+		// A4
+		m := make([]float64, attributes.EndStatType)
+		m[attributes.EM] = 125
+		for _, char := range c.Core.Player.Chars() {
+			char.AddStatMod(character.StatMod{
+				Base:         modifier.NewBaseWithHitlag("albedo-a4", 600),
+				AffectedStat: attributes.EM,
+				Amount: func() ([]float64, bool) {
+					return m, true
+				},
+			})
+		}
+
+		// fatal blossom
+		if !c.skillActive {
+			return
+		}
 		ai.Abil = "Rite of Progeniture: Tectonic Tide (Blossom)"
 		ai.Mult = burstPerBloom[c.TalentLvlBurst()]
+
+		// C2 damage is recalculated once on burstHitmark
+		if hasC2 {
+			ai.FlatDmg = (c.Base.Def*(1+c.Stat(attributes.DEFP)) + c.Stat(attributes.DEF)) * float64(c2Count)
+		}
+
 		// generate 7 blossoms
 		maxBlossoms := 7
 		enemies := c.Core.Combat.RandomEnemiesWithinArea(c.skillArea, nil, maxBlossoms)
@@ -68,23 +94,16 @@ func (c *char) Burst(p map[string]int) action.ActionInfo {
 				// if a blossom has no enemy then it randomly spawns in the skill area
 				center = combat.CalcRandomPointFromCenter(c.skillArea.Shape.Pos(), 0.5, 9.5, c.Core.Rand)
 			}
-			c.Core.QueueAttackWithSnap(ai, c.bloomSnapshot, combat.NewCircleHitOnTarget(center, nil, 3), fatalBlossomHitmark+i*5)
+			// Blossoms are generated on a slight delay from initial hit
+			// TODO: no precise frame data for time between Blossoms
+			c.Core.QueueAttackWithSnap(
+				ai,
+				c.bloomSnapshot,
+				combat.NewCircleHitOnTarget(center, nil, 3),
+				fatalBlossomHitmark+i*5,
+			)
 		}
-	}
-
-	//Party wide EM buff
-	// a4: burst increase party em by 125 for 10s
-	m := make([]float64, attributes.EndStatType)
-	m[attributes.EM] = 125
-	for _, char := range c.Core.Player.Chars() {
-		char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBaseWithHitlag("albedo-a4", 600),
-			AffectedStat: attributes.EM,
-			Amount: func() ([]float64, bool) {
-				return m, true
-			},
-		})
-	}
+	}, burstHitmark)
 
 	c.SetCDWithDelay(action.ActionBurst, 720, 74)
 	c.ConsumeEnergy(77)
