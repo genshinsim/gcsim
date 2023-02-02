93d9ce4fe4e32ff1f8f44c048dbca180c21bfe96
k0l11 <intensonom@yahoo.de>
2023-02-02 23:07:59 +0100

    itto ascension refactor

--- internal/characters/itto/asc.go
+++ internal/characters/itto/asc.go
@@ -9,12 +9,17 @@ import (
 	"github.com/genshinsim/gcsim/pkg/modifier"
 )
 
-// A1:
 // When Arataki Itto uses consecutive Arataki Kesagiri, he obtains the following effects:
+//
 // - Each slash increases the ATK SPD of the next slash by 10%. Max ATK SPD increase is 30%.
-// - Increases his resistance to interruption.
+//
+// TODO: - Increases his resistance to interruption.
+//
 // These effects will be cleared once he stops performing consecutive slashes.
 func (c *char) a1() {
+	if c.Base.Ascension < 1 {
+		return
+	}
 	mAtkSpd := make([]float64, attributes.EndStatType)
 	c.AddStatMod(character.StatMod{
 		Base:         modifier.NewBase("itto-a1", -1),
@@ -30,6 +35,9 @@ func (c *char) a1() {
 }
 
 func (c *char) a1Update(curSlash SlashType) {
+	if c.Base.Ascension < 1 {
+		return
+	}
 	switch curSlash {
 	case SaichiSlash:
 		// reset a1 stacks if we are doing a CA0
@@ -51,9 +59,11 @@ func (c *char) a1Update(curSlash SlashType) {
 	// do nothing if we are doing a CAF
 }
 
-// A4:
 // Arataki Kesagiri DMG is increased by 35% of Arataki Itto's DEF.
 func (c *char) a4(ai *combat.AttackInfo) {
+	if c.Base.Ascension < 4 {
+		return
+	}
 	ai.FlatDmg = (c.Base.Def*(1+c.Stat(attributes.DEFP)) + c.Stat(attributes.DEF)) * 0.35
 	c.Core.Log.NewEvent("itto-a4 applied", glog.LogCharacterEvent, c.Index)
 }
