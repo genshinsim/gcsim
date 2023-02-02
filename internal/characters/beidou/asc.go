38243042aa46704ea8366f059f1e357c80f163aa
k0l11 <intensonom@yahoo.de>
2023-02-02 22:59:31 +0100

    beidou ascension refactor

--- internal/characters/beidou/asc.go
+++ internal/characters/beidou/asc.go
@@ -7,7 +7,17 @@ import (
 	"github.com/genshinsim/gcsim/pkg/modifier"
 )
 
+// A1 is not implemented:
+// TODO: Counterattacking with Tidecaller at the precise moment when the character is hit grants the maximum DMG Bonus.
+
+// Gain the following effects for 10s after unleashing Tidecaller with its maximum DMG Bonus:
+// - DMG dealt by Normal and Charged Attacks is increased by 15%. ATK SPD of Normal and Charged Attacks is increased by 15%.
+// TODO: - Greatly reduced delay before unleashing Charged Attacks.
 func (c *char) a4() {
+	if c.Base.Ascension < 4 {
+		return
+	}
+
 	mDmg := make([]float64, attributes.EndStatType)
 	mDmg[attributes.DmgP] = .15
 	c.AddAttackMod(character.AttackMod{
