060228004fe7d82e5081481d0400f96fc7a5c937
k0l11 <intensonom@yahoo.de>
2023-02-02 23:07:06 +0100

    fischl ascension refactor

--- internal/characters/fischl/asc.go
+++ internal/characters/fischl/asc.go
@@ -7,9 +7,17 @@ import (
 	"github.com/genshinsim/gcsim/pkg/gadget"
 )
 
-func (c *char) a4() {
-	last := 0
+// A1 is not implemented:
+// TODO: When Fischl hits Oz with a fully-charged Aimed Shot, Oz brings down Thundering Retribution, dealing AoE Electro DMG equal to 152.7% of the arrow's DMG.
 
+// If your current active character triggers an Electro-related Elemental Reaction when Oz is on the field,
+// the opponent shall be stricken with Thundering Retribution that deals Electro DMG equal to 80% of Fischl's ATK.
+func (c *char) a4() {
+	if c.Base.Ascension < 4 {
+		return
+	}
+
+	last := 0
 	// Hyperbloom comes from a gadget so it doesn't ignore gadgets
 	a4cb := func(args ...interface{}) bool {
 
