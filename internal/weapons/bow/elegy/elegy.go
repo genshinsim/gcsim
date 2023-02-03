8c0a0923b10e3d32f3857044c5e1796c8958818f
k0l11 <intensonom@yahoo.de>
2023-02-03 19:11:00 +0100

    fix stacking of millennial buffs of same type

--- internal/weapons/bow/elegy/elegy.go
+++ internal/weapons/bow/elegy/elegy.go
@@ -3,6 +3,7 @@ package elegy
 import (
 	"fmt"
 
+	"github.com/genshinsim/gcsim/internal/weapons/common"
 	"github.com/genshinsim/gcsim/pkg/core"
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
 	"github.com/genshinsim/gcsim/pkg/core/combat"
@@ -24,6 +25,15 @@ type Weapon struct {
 func (w *Weapon) SetIndex(idx int) { w.Index = idx }
 func (w *Weapon) Init() error      { return nil }
 
+// A part of the "Millennial Movement" that wanders amidst the winds.
+// Increases Elemental Mastery by 60. When the Elemental Skills or Elemental Bursts
+// of the character wielding this weapon hit opponents, that character gains a Sigil of Remembrance.
+// This effect can be triggered once every 0.2s and can be triggered even if said character
+// is not on the field. When you possess 4 Sigils of Remembrance, all of them will be consumed
+// and all nearby party members will obtain the "Millennial Movement: Farewell Song" effect for 12s.
+// "Millennial Movement: Farewell Song" increases Elemental Mastery by 100 and increases ATK by 20%.
+// Once this effect is triggered, you will not gain Sigils of Remembrance for 20s.
+// Of the many effects of the "Millennial Movement," buffs of the same type will not stack.
 func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile) (weapon.Weapon, error) {
 	w := &Weapon{}
 	r := p.Refine
@@ -38,16 +48,18 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile
 		},
 	})
 
-	val := make([]float64, attributes.EndStatType)
-	val[attributes.ATKP] = .15 + float64(r)*0.05
-	val[attributes.EM] = 75 + float64(r)*25
+	uniqueVal := make([]float64, attributes.EndStatType)
+	uniqueVal[attributes.EM] = 75 + float64(r)*25
+
+	sharedVal := make([]float64, attributes.EndStatType)
+	sharedVal[attributes.ATKP] = .15 + float64(r)*0.05
 
 	stacks := 0
-	buffDuration := 720 // 12s * 60
+	buffDuration := 12 * 60
 	const icdKey = "elegy-sigil-icd"
-	icd := 12 // 0.2s * 60
-	const cooldownKey = "elegy-cd"
-	cd := 1200 // 20s * 60
+	icd := int(0.2 * 60)
+	const cdKey = "elegy-cd"
+	cd := 20 * 60
 
 	c.Events.Subscribe(event.OnEnemyDamage, func(args ...interface{}) bool {
 		atk := args[1].(*combat.AttackEvent)
@@ -61,8 +73,7 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile
 		default:
 			return false
 		}
-
-		if char.StatusIsActive(cooldownKey) {
+		if char.StatusIsActive(cdKey) {
 			return false
 		}
 		if char.StatusIsActive(icdKey) {
@@ -73,13 +84,20 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile
 		stacks++
 		if stacks == 4 {
 			stacks = 0
-			char.AddStatus(cooldownKey, cd, true)
+			char.AddStatus(cdKey, cd, true)
 			for _, char := range c.Player.Chars() {
 				char.AddStatMod(character.StatMod{
 					Base:         modifier.NewBaseWithHitlag("elegy-proc", buffDuration),
-					AffectedStat: attributes.NoStat,
+					AffectedStat: attributes.EM,
 					Amount: func() ([]float64, bool) {
-						return val, true
+						return uniqueVal, true
+					},
+				})
+				char.AddStatMod(character.StatMod{
+					Base:         modifier.NewBaseWithHitlag(common.MillennialKey, buffDuration),
+					AffectedStat: attributes.ATKP,
+					Amount: func() ([]float64, bool) {
+						return sharedVal, true
 					},
 				})
 			}
