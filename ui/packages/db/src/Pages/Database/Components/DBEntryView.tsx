6ccb0ac206fb443995a84be7721d4c8908b218a8
CPTeo <chinphin.teo@gmail.com>
2023-02-05 00:46:05 +1100

    single line db entry

--- ui/packages/db/src/Pages/Database/Components/DBEntryView.tsx
+++ ui/packages/db/src/Pages/Database/Components/DBEntryView.tsx
@@ -11,7 +11,7 @@ export default function DBEntryView({ dbEntry }: { dbEntry: model.IDBEntry }) {
   const t = useTranslation();
   return (
     <div className="flex flex-row bg-slate-800  p-4 gap-4 w-full">
-      <div className="grid gap-2 grid-cols-2 min-w-fit ">
+      <div className="flex gap-2 flex-row min-w-fit ">
         {dbEntry.team &&
           dbEntry.team.map((char, index) => {
             return (
@@ -20,9 +20,9 @@ export default function DBEntryView({ dbEntry }: { dbEntry: model.IDBEntry }) {
           })}
       </div>
       <div className="flex flex-col grow ">
-        <div className="capitalize text-lg font-semibold ">
+        {/* <div className="capitalize text-lg font-semibold ">
           {dbEntry?.char_names?.toString().replaceAll(",", ", ")}
-        </div>
+        </div> */}
         <div>
           <div className="flex flex-col max-w-4xl">
             <DBEntryTags tags={dbEntry.tags} />
@@ -44,21 +44,23 @@ function DBEntryCharacterPortrait({
   cons,
 }: model.ICharacter) {
   return (
-    <div className="bg-slate-700 p-2 flex flex-col relative">
-      {name && (
-        <img
-          src={"https://gcsim.app/api/assets/avatar/" + name + ".png"}
-          alt={name}
-          className="w-24 h-24"
-        />
-      )}
-      <div className="absolute top-2 right-2 text-xs font-bold">
-        {(cons as number) ?? 0}
-      </div>
-      <div className="flex flex-row ">
+    <div className="bg-slate-700 p-2 flex flex-row w-20 h-20">
+      <div className="flex flex-col  min-h-min">
+        <div className=" relative">
+          {name && (
+            <img
+              src={"https://gcsim.app/api/assets/avatar/" + name + ".png"}
+              alt={name}
+            />
+          )}
+          <div className="absolute  right-0 bottom-0 text-xs font-bold">
+            {(cons as number) ?? 0}
+          </div>
+        </div>
         <PortraitWeaponComponent weapon={weapon} />
-        <PortraitArtifactsComponent artifactSet={sets} />
       </div>
+
+      <PortraitArtifactsComponent artifactSet={sets} />
     </div>
   );
 }
@@ -71,7 +73,7 @@ function PortraitWeaponComponent({
     return <div className="h-16 w-16">No weapon</div>;
   }
   return (
-    <div className=" relative max-h-min flex w-8 h-8">
+    <div className=" relative   ">
       <img
         src={"https://gcsim.app/api/assets/weapons/" + weapon.name + ".png"}
         alt={weapon.name}
@@ -96,11 +98,11 @@ function PortraitArtifactsComponent({
     return <div className="h-4 w-4">No artifact</div>;
   }
   return (
-    <div className="flex flex-row   ">
+    <div className="flex flex-col   ">
       {Object.entries(artifactSet).map(
         ([setName, setCount]) =>
           (setCount as number) > 0 && (
-            <div className=" relative h-8 w-8 ">
+            <div className=" relative  ">
               <img
                 src={
                   "https://gcsim.app/api/assets/artifacts/" +
@@ -154,22 +156,35 @@ function DBEntryDetails({
   mode,
   sim_duration,
   total_damage,
-  description,
-}: model.IDBEntry) {
+}: // total_damage,
+// description,
+model.IDBEntry) {
+  const t = useTranslation();
   return (
-    <div className="flex flex-col justify-center">
-      {mode && <div>Mode: {mode}</div>}
-      {target_count && <div>Target Count: {target_count}</div>}
-      {mean_dps_per_target && (
-        <div>Mean DPS Per Target: {mean_dps_per_target.toPrecision(8)}</div>
-      )}
-      {/* {total_damage && (
-        <div>Total Damage AVG: {total_damage.mean.toPrecision(8)}</div>
-      )} */}
-      {sim_duration && <div>Sim Duration AVG: {sim_duration.mean}s</div>}
-
-      {run_date && <div>Run Date: {JSON.stringify(run_date)}</div>}
-    </div>
+    <table className="table-auto table ">
+      <thead>
+        <tr className="">
+          <th className="pr-4">{t("Sim Mode")}</th>
+          <th className="px-4">{t("Target Count")}</th>
+          <th className="px-4">{t("DPS per Target")}</th>
+          <th className="px-4">{t("AVG Total Damage")}</th>
+          <th className="px-4">{t("AVG Sim Time")}</th>
+          <th className="px-4">{t("Run Date")}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className=" ">
+          <td className="pr-4">{mode ? t("Time to kill") : t(" Duration")}</td>
+          <td className="px-4">{target_count}</td>
+          <td className="px-4">{mean_dps_per_target?.toPrecision(8)}</td>
+          <td className="px-4">{total_damage?.mean?.toPrecision(8)}</td>
+          <td className="px-4">
+            {sim_duration?.mean?.toPrecision(3) ?? t("Unknown")}s
+          </td>
+          <td className="px-4">{run_date?.toString() ?? t("Unknown")}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
