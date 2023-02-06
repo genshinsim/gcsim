6e93eeb4c846b6fa919f2f7904bb9a5bba41849a
Kyle Bartz <mail@kyle.fail>
2023-02-06 05:52:53 +0000

    Fixed SimulationResult json name to be config_file

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
-//go:generate sh -c "protoc --go_out=module=github.com/genshinsim/gcsim:. --go-grpc_out=module=github.com/genshinsim/gcsim:. protos/**/*.proto"
+//go:generate sh -c "protoc --experimental_allow_proto3_optional --go_out=module=github.com/genshinsim/gcsim:. --go-grpc_out=module=github.com/genshinsim/gcsim:. protos/**/*.proto"
 //go:generate go run scripts/generate/bsontags/main.go -dir backend/pkg/services/db -verbose
 //go:generate go run scripts/generate/bsontags/main.go -dir pkg/model -verbose
 //go:generate sh scripts/generate/build_preview.sh
