4620eff5346ea7bfdc38b009cb6fca4205ec1b54
CPTeo <chinphin.teo@gmail.com>
2023-02-11 16:15:00 +1100

    refactored and improved portrait

--- ui/packages/db/src/Pages/Database/Components/DBEntryView.tsx
+++ ui/packages/db/src/Pages/Database/Components/DBEntryView.tsx
@@ -1,6 +1,6 @@
 import { model } from "@gcsim/types";
-import { Long } from "protobufjs";
 import { useState } from "react";
+import DBEntryPortrait from "./DBEntryViewComponents/DBEntryPortrait";
 
 function useTranslation() {
   return (text: string) => text;
@@ -21,17 +21,15 @@ export default function DBEntryView({ dbEntry }: { dbEntry: model.IDBEntry }) {
       <div className="flex gap-2 flex-row min-w-fit ">
         {team &&
           team.map((char, index) => {
-            return (
-              <DBEntryCharacterPortrait {...char} key={index.toString()} />
-            );
+            return <DBEntryPortrait {...char} key={index.toString()} />;
           })}
       </div>
       <div className="flex flex-col grow ">
         {/* <div className="capitalize text-lg font-semibold ">
           {dbEntry?.char_names?.toString().replaceAll(",", ", ")}
         </div> */}
-        <div>
-          <div className="flex flex-col max-w-4xl">
+        <div className="max-w-2xl">
+          <div className="flex flex-col ">
             <DBEntryTags tags={dbEntry.accepted_tags} />
             <span className="  overflow-hidden">{dbEntry?.description}</span>
           </div>
@@ -44,103 +42,13 @@ export default function DBEntryView({ dbEntry }: { dbEntry: model.IDBEntry }) {
   );
 }
 
-function DBEntryCharacterPortrait({
-  name,
-  sets,
-  weapon,
-  cons,
-}: model.ICharacter) {
-  if (!name) {
-    return <div className="bg-slate-700 p-2 flex flex-row w-20 h-20"></div>;
-  }
-  return (
-    <div className="bg-slate-700 p-2 flex flex-row w-20 h-20">
-      <div className="flex flex-col  min-h-min">
-        <div className=" relative">
-          {name && (
-            <img
-              src={"https://gcsim.app/api/assets/avatar/" + name + ".png"}
-              alt={name}
-            />
-          )}
-          <div className="absolute  right-0 bottom-0 text-xs font-bold">
-            {(cons as number) ?? 0}
-          </div>
-        </div>
-        <PortraitWeaponComponent weapon={weapon} />
-      </div>
-
-      <PortraitArtifactsComponent artifactSet={sets} />
-    </div>
-  );
-}
-function PortraitWeaponComponent({
-  weapon,
-}: {
-  weapon: model.IWeapon | undefined | null;
-}) {
-  if (!weapon || !weapon.name) {
-    return <div className="h-16 w-16">No weapon</div>;
-  }
-  return (
-    <div className=" relative   ">
-      <img
-        src={"https://gcsim.app/api/assets/weapons/" + weapon.name + ".png"}
-        alt={weapon.name}
-      />
-      <div className=" absolute bottom-0 right-0  text-xs  font-semibold">
-        R{weapon?.refine?.toString()}
-      </div>
-    </div>
-  );
-}
-function PortraitArtifactsComponent({
-  artifactSet,
-}: {
-  artifactSet:
-    | {
-        [k: string]: number | Long;
-      }
-    | undefined
-    | null;
-}) {
-  if (!artifactSet) {
-    return <div className="h-4 w-4">No artifact</div>;
-  }
-  return (
-    <div className="flex flex-col   ">
-      {Object.entries(artifactSet).map(
-        ([setName, setCount]) =>
-          (setCount as number) > 0 && (
-            <div className=" relative  ">
-              <div className=" h-16  bg-black -rotate-45 transform origin-top-left">
-                <img
-                  src={
-                    "https://gcsim.app/api/assets/artifacts/" +
-                    setName +
-                    "_flower.png"
-                  }
-                  alt={setName}
-                />
-              </div>
-
-              <div className=" absolute bottom-0 right-0  text-xs  font-semibold">
-                {setCount.toString()}
-              </div>
-            </div>
-          )
-      )}
-    </div>
-  );
-}
-
 function DBEntryTags({ tags }: { tags: string[] | undefined | null }) {
   const t = useTranslation();
   const [showAll, setShowAll] = useState(false);
   return (
     <div
       className={
-        "flex flex-row h-full flex-wrap  relative  max-w-md " +
+        "flex flex-row h-full flex-wrap  relative   " +
         (showAll ? " " : " truncate")
       }
     >
---
+++ ui/packages/db/src/Pages/Database/Components/DBEntryViewComponents/DBEntryPortrait.tsx
@@ -0,0 +1,126 @@
+import { model } from "@gcsim/types";
+import { Long } from "protobufjs";
+import kuki from "./images/kuki.png";
+
+export default function DBEntryPortrait({
+  name,
+  sets,
+  weapon,
+  cons,
+}: model.ICharacter) {
+  if (!name) {
+    return <div className="bg-slate-700 p-2 flex flex-row w-20 h-20"></div>;
+  }
+  return (
+    <div className="bg-slate-700 p-2 flex flex-row max-h-fit    w-20">
+      <div className="grid grid-cols-2 grid-rows-3 ">
+        <div className="col-span-2 row-span-2 border-b border-white/25">
+          <div className=" relative ">
+            {name && (
+              <img
+                src={"https://gcsim.app/api/assets/avatar/" + name + ".png"}
+                alt={name}
+              />
+            )}
+            <div className="absolute  right-0 bottom-0 text-xs font-bold opacity-80">
+              {(cons as number) ?? 0}
+            </div>
+          </div>
+        </div>
+
+        <PortraitArtifactsComponent artifactSet={{}} />
+        <PortraitWeaponComponent weapon={weapon} />
+      </div>
+    </div>
+  );
+}
+function PortraitWeaponComponent({
+  weapon,
+}: {
+  weapon: model.IWeapon | undefined | null;
+}) {
+  if (!weapon || !weapon.name) {
+    return <div className="h-16 w-16">No weapon</div>;
+  }
+  return (
+    <div className=" relative">
+      <img
+        src={"https://gcsim.app/api/assets/weapons/" + weapon.name + ".png"}
+        alt={weapon.name}
+      />
+      <div className=" absolute bottom-0 right-0  text-xs  font-semibold opacity-80">
+        R{weapon?.refine?.toString()}
+      </div>
+    </div>
+  );
+}
+
+function PortraitArtifactsComponent({
+  artifactSet,
+}: {
+  artifactSet:
+    | {
+        [k: string]: number | Long;
+      }
+    | undefined
+    | null;
+}) {
+  if (!artifactSet) {
+    return (
+      <div>
+        <img src={kuki} alt="kuki" className="relative max-h-full" />
+      </div>
+    );
+  }
+
+  const artifacts = Object.entries(artifactSet).filter(
+    ([, setCount]) => (setCount as number) >= 2
+  );
+
+  switch (artifacts.length) {
+    case 1:
+      return (
+        <div className="relative">
+          <img
+            src={
+              "https://gcsim.app/api/assets/artifacts/" +
+              artifacts[0][0] +
+              "_flower.png"
+            }
+            alt={artifacts[0][0]}
+          />
+        </div>
+      );
+    case 2:
+      return (
+        <div className="relative">
+          <img
+            src={
+              "https://gcsim.app/api/assets/artifacts/" +
+              artifacts[0][0] +
+              "_flower.png"
+            }
+            alt={artifacts[0][0]}
+            className="artifact-right-gap"
+          />
+          <img
+            src={
+              "https://gcsim.app/api/assets/artifacts/" +
+              artifacts[1][0] +
+              "_flower.png"
+            }
+            alt={artifacts[1][0]}
+            className="artifact-left-gap"
+          />
+        </div>
+      );
+
+    default:
+    case 0:
+      return (
+        <div>
+          <img src={kuki} alt="kuki" className="relative max-h-64" />
+        </div>
+      );
+  }
+}
