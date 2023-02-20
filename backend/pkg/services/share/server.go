047fc953871cb720732864befe6a0a57c000f79f
srliao <906239+srliao@users.noreply.github.com>
2023-02-20 14:45:53 +0000

    basic share server

--- backend/pkg/services/share/server.go
+++ backend/pkg/services/share/server.go
@@ -12,7 +12,11 @@ import (
 // ShareStore describes a database/service that can be used to store shares
 type ShareStore interface {
 	Create(context.Context, *ShareEntry) (string, error)
-	Get(context.Context, string) (*ShareEntry, error)
+	Read(context.Context, string) (*ShareEntry, error)
+	Update(context.Context, *ShareEntry) (string, error)
+	SetTTL(context.Context, string, uint64) (string, error)
+	Delete(context.Context, string) error
+	Random(context.Context) (*ShareEntry, error)
 }
 
 type Config struct {
@@ -78,4 +82,64 @@ func (s *Server) Create(ctx context.Context, req *CreateRequest) (*CreateRespons
 	return &CreateResponse{
 		Key: id,
 	}, nil
+}
+
+func (s *Server) Read(ctx context.Context, req *ReadRequest) (*ReadResponse, error) {
+	s.Log.Infow("share read request", "key", req.GetKey())
+
+	res, err := s.cfg.Store.Read(ctx, req.GetKey())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &ReadResponse{
+		Key:       req.GetKey(),
+		Result:    res.GetResult(),
+		ExpiresAt: res.GetExpiresAt(),
+	}, nil
+}
+
+func (s *Server) Update(ctx context.Context, req *UpdateRequest) (*UpdateResponse, error) {
+	s.Log.Infow("share update request", "key", req.GetKey())
+
+	_, err := s.cfg.Store.Update(ctx, &ShareEntry{
+		Id:        req.GetKey(),
+		Result:    req.GetResult(),
+		ExpiresAt: req.GetExpiresAt(),
+		Submitter: req.GetSubmitter(),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (s *Server) SetTTL(ctx context.Context, req *SetTTLRequest) (*SetTTLResponse, error) {
+	s.Log.Infow("share set ttl request", "key", req.GetKey(), "expires_at", req.GetExpiresAt())
+
+	_, err := s.cfg.Store.SetTTL(ctx, req.GetKey(), req.GetExpiresAt())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (s *Server) Delete(ctx context.Context, req *DeleteRequest) (*DeleteResponse, error) {
+	s.Log.Infow("share delete request", "key", req.GetKey())
+
+	err := s.cfg.Store.Delete(ctx, req.GetKey())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+func (s *Server) Random(ctx context.Context, req *RandomRequest) (*RandomResponse, error) {
+	return nil, nil
 }
