99aba71b5e04e2b77aaeee725a25a48643789c9a
imring <vitaliyvorobets25@gmail.com>
2023-02-26 13:18:33 +0200

    noelle burst

--- internal/characters/noelle/burst.go
+++ internal/characters/noelle/burst.go
@@ -34,8 +34,8 @@ func (c *char) Burst(p map[string]int) action.ActionInfo {
 		ActorIndex: c.Index,
 		Abil:       "Sweeping Time (Stat Snapshot)",
 	}
-	snapshot := c.Snapshot(&aiSnapshot)
-	burstDefSnapshot := snapshot.BaseDef*(1+snapshot.Stats[attributes.DEFP]) + snapshot.Stats[attributes.DEF]
+	c.Snapshot(&aiSnapshot)
+	burstDefSnapshot := c.Base.Def*(1+c.NonExtraStat(attributes.DEFP)) + c.NonExtraStat(attributes.DEF)
 	mult := defconv[c.TalentLvlBurst()]
 	if c.Base.Cons >= 6 {
 		mult += 0.5
