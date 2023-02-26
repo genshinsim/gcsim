2883035b32a0a7d9b264031b0a41a3780690948e
srliao <906239+srliao@users.noreply.github.com>
2023-02-26 22:53:08 +0000

    queue docker container

---
+++ backend/cmd/queue/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net"
+	"time"
+
+	"github.com/genshinsim/gcsim/backend/pkg/services/queue"
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	server, err := queue.NewQueue(queue.Config{
+		Timeout: 5 * time.Minute,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("failed to listen on port 3000: %v", err)
+	}
+
+	s := grpc.NewServer()
+	queue.RegisterWorkQueueServer(s, server)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
