903c3c9368f80a88f137ef9d54727ac060fe6290
Charlie Zheng <charlie.x.3000@gmail.com>
2023-03-04 18:44:43 -0700

    Added weapons to embed

--- ui/packages/embed/src/App.tsx
+++ ui/packages/embed/src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { parsed } from ".";
 
-const App = ({}) => {
+
+const App = ({ }) => {
   const [loaded, setLoaded] = React.useState(0);
   const [ready, setReady] = React.useState(false);
 
@@ -15,29 +16,64 @@ const App = ({}) => {
   //draw some cards
   const cards = parsed.character_details?.map((c) => {
     return (
-      <img
-        key={c.name}
-        src={`/api/assets/avatar/${c.name}.png`}
-        onLoad={handleLoaded}
-      />
+      <div className="card">
+        <div className="char">
+          <img
+            key={c.name}
+            src={`/api/assets/avatar/${c.name}.png`}
+            onLoad={handleLoaded}
+          />
+        </div>
+        <div className="equip">          
+          <svg key={c.weapon.name} width="91" height="91">
+            <filter id="outline">
+              <feMorphology in="SourceAlpha" result="expanded" operator="dilate" radius="2.5"/>
+              <feFlood flood-color="black"/>
+              <feComposite in2="expanded" operator="in"/>
+              <feGaussianBlur result="black" stdDeviation="1"/>
+              <feMorphology in="SourceAlpha" result="expanded1" operator="dilate" radius="1"/>
+              <feFlood flood-color="white"/>
+              <feComposite in2="expanded1" operator="in" result="white"/>
+              <feMerge>
+                <feMergeNode in="black"/>
+                <feMergeNode in="white"/>
+                <feMergeNode in="SourceGraphic"/>
+              </feMerge>
+            </filter>
+            <image filter="url(#outline)" href={`/api/assets/weapons/${c.weapon.name}.png`} height="85" width="85" x="3" y="3"/>
+          </svg>
+        </div>
+      </div>
+
     );
   });
 
   if (parsed.err) {
     return <div id="card">{parsed.err}</div>;
   }
-
+  const css = `
+  .card {
+    position: relative;
+  }
+  .equip {
+    position: absolute;
+    bottom: -10px;
+    right: -3px;
+    width: 91px;
+  }
+  `
   return (
     <div>
-      <div
-        id="card"
-        className={ready ? "grid grid-cols-4" : "grid grid-cols-4 disabled"}
-      >
+      <style>
+        {css}
+      </style>
+      <div id="cards" className={ready ? "grid grid-cols-4" : "grid grid-cols-4 disabled"}>
         {cards}
       </div>
       <div>{`DPS: ${parsed.statistics.dps.mean}`}</div>
     </div>
-  );
+    
+  );  
 };
 
 export default App;
