d85a3a30ed07b99a166b187aa4fa79a6ac6101a2
CPTeo <chinphin.teo@gmail.com>
2023-03-06 21:24:32 +1100

    ui update

--- ui/packages/db/src/SharedComponents/DBEntryViewComponents/DBEntryActions.tsx
+++ ui/packages/db/src/SharedComponents/DBEntryViewComponents/DBEntryActions.tsx
@@ -27,7 +27,24 @@ export default function DBEntryActions({
           rel="noreferrer"
         >
           <div className="md:flex hidden">{t("Open in Viewer")}</div>
-          <div className="block md:hidden">{"eheh"}</div>
+          <div className="flex md:hidden">
+            {
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                />
+              </svg>
+            }
+          </div>
         </a>
       </div>
     );
