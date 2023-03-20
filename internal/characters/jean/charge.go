2f727229597ce6d40fd73e8d55fb7bdc363453fa
David Ji <david.ji.8484@gmail.com>
2023-03-20 12:54:03 +1100

    fixed Jean CA ICD

--- internal/characters/jean/charge.go
+++ internal/characters/jean/charge.go
@@ -27,7 +27,7 @@ func (c *char) ChargeAttack(p map[string]int) action.ActionInfo {
 		ActorIndex: c.Index,
 		Abil:       "Charge",
 		AttackTag:  attacks.AttackTagExtra,
-		ICDTag:     attacks.ICDTagExtraAttack,
+		ICDTag:     attacks.ICDTagNormalAttack,
 		ICDGroup:   attacks.ICDGroupDefault,
 		StrikeType: attacks.StrikeTypeSlash,
 		Element:    attributes.Physical,
