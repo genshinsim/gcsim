6458eb4461c932d380bc3e030d800e4578dabba7
koli <98557316+k0l11@users.noreply.github.com>
2023-05-06 22:02:02 +0200

    mika bugfixes and cleanup (#1350)
    * remove redundant FramePausedOnHitlag
    
    * fix mika a1 stack spelling mistake
    
    * fix mika n1 -> n2 frames
    
    * fix mika ca hitlag
    
    * fix mika tap e hitlag
    
    * fix mika tap e earliest cancel
    
    * fix mika hold e icd
    
    * reduce mika a1 calls by moving it to init
    
    * fix mika a1 applying to off-field characters
    
    * fix mika q earliest cancel and q healing everyone
    
    * fix mika q duration being affected by hitlag
    
    * fix mika q heal icd not being per character

--- internal/characters/mika/charge.go
+++ internal/characters/mika/charge.go
@@ -33,10 +33,9 @@ func (c *char) ChargeAttack(p map[string]int) action.ActionInfo {
 		StrikeType:         attacks.StrikeTypeSpear,
 		Element:            attributes.Physical,
 		Durability:         25,
-		HitlagHaltFrames:   0.02 * 60,
+		Mult:               charge[c.TalentLvlAttack()],
 		HitlagFactor:       0.01,
 		CanBeDefenseHalted: true,
-		Mult:               charge[c.TalentLvlAttack()],
 	}
 
 	c.Core.QueueAttack(
