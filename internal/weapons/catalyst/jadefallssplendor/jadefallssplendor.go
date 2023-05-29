f0840d74a52610bcfffd1ac46cacddbad28fe75a
k0l11 <intensonom@yahoo.de>
2023-05-29 12:10:10 +0200

    add jadefall description

--- internal/weapons/catalyst/jadefallssplendor/jadefallssplendor.go
+++ internal/weapons/catalyst/jadefallssplendor/jadefallssplendor.go
@@ -25,6 +25,10 @@ type Weapon struct {
 func (w *Weapon) SetIndex(idx int) { w.Index = idx }
 func (w *Weapon) Init() error      { return nil }
 
+// For 3s after using an Elemental Burst or creating a shield, the equipping character can gain the Primordial Jade Regalia effect:
+// Restore 4.5/5/5.5/6/6.5 Energy every 2.5s, and gain 0.3/0.5/0.7/0.9/1.1% Elemental DMG Bonus
+// for their corresponding Elemental Type for every 1,000 Max HP they possess, up to 12/20/28/36/44%.
+// Primordial Jade Regalia will still take effect even if the equipping character is not on the field.
 func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile) (weapon.Weapon, error) {
 	w := &Weapon{}
 	r := p.Refine
