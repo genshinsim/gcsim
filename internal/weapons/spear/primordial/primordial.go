fef5262a1f0aad39410bd1c258502606ce45d846
k0l11 <intensonom@yahoo.de>
2023-06-13 10:43:30 +0200

    fix wrong pjws duration

--- internal/weapons/spear/primordial/primordial.go
+++ internal/weapons/spear/primordial/primordial.go
@@ -69,7 +69,7 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile
 
 		//refresh mod
 		char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBase(buffKey, 300),
+			Base:         modifier.NewBase(buffKey, 6*60),
 			AffectedStat: attributes.NoStat,
 			Amount: func() ([]float64, bool) {
 				return w.buff, true
