711b2beb7b5d8c8b4788bcb994f9a29b92af18f1
srliao <906239+srliao@users.noreply.github.com>
2023-07-05 19:03:27 -0400

    fix typescript errors

--- ui/packages/workers/src/proxy/index.ts
+++ ui/packages/workers/src/proxy/index.ts
@@ -1,4 +1,6 @@
-export async function proxyRequest(request: Request): Promise<Response> {
+import { IRequest } from "itty-router";
+
+export async function proxyRequest(request: IRequest): Promise<Response> {
   const x = new URL(request.url);
   return fetch(new Request(API_ENDPOINT + x.pathname + x.search, request));
 }
