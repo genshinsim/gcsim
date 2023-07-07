ec94fe039f96454deb9160a10da0afa7d4f5aa00
Athena <bqh1402003@gmail.com>
2023-07-07 15:40:20 +0700

    fix tidalshadow activate on teammate healing

--- internal/weapons/claymore/tidalshadow/tidalshadow.go
+++ internal/weapons/claymore/tidalshadow/tidalshadow.go
@@ -1,15 +1,15 @@
 package tidalshadow
 
 import (
+	"fmt"
+
 	"github.com/genshinsim/gcsim/pkg/core"
-	"github.com/genshinsim/gcsim/pkg/core/attacks"
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
-	"github.com/genshinsim/gcsim/pkg/core/combat"
 	"github.com/genshinsim/gcsim/pkg/core/event"
 	"github.com/genshinsim/gcsim/pkg/core/keys"
+	"github.com/genshinsim/gcsim/pkg/core/player"
 	"github.com/genshinsim/gcsim/pkg/core/player/character"
 	"github.com/genshinsim/gcsim/pkg/core/player/weapon"
-	"github.com/genshinsim/gcsim/pkg/core/player"
 	"github.com/genshinsim/gcsim/pkg/modifier"
 )
 
@@ -24,8 +24,8 @@ type Weapon struct {
 func (w *Weapon) SetIndex(idx int) { w.Index = idx }
 func (w *Weapon) Init() error      { return nil }
 
-//When the wielder is healed, ATK will be increased by 24/30/36/42/48% for 8s. 
-//This can be triggered even when the character is not on the field.
+// When the wielder is healed, ATK will be increased by 24/30/36/42/48% for 8s.
+// This can be triggered even when the character is not on the field.
 func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile) (weapon.Weapon, error) {
 	w := &Weapon{}
 	r := p.Refine
@@ -34,14 +34,19 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile
 	duration := 480 //60 * 8s
 	val := make([]float64, attributes.EndStatType)
 	val[attributes.ATKP] = atk
-	c.Events.Subscribe(event.OnHeal, func(args ...interface{}) bool) {
-		char.addStatMod(character.StatMod{
-			Base:			modifier.NewBase("tidal-shadow-atk-boost", duration)
-			AffectedStat:	attributes.NoStat,
+	c.Events.Subscribe(event.OnHeal, func(args ...interface{}) bool {
+		healInfo := args[0].(*player.HealInfo)
+		if healInfo.Target != -1 && healInfo.Target != char.Index {
+			return false
+		}
+		char.AddStatMod(character.StatMod{
+			Base:         modifier.NewBase("tidal-shadow-atk-boost", duration),
+			AffectedStat: attributes.NoStat,
 			Amount: func() ([]float64, bool) {
 				return val, true
 			},
 		})
-	}
+		return false
+	}, fmt.Sprintf("tidalshadow-%v", char.Base.Key.String()))
 	return w, nil
 }
