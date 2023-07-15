d0a4c3c7ec4a769d424f984a9bcdf902c8a15420
koli <98557316+k0l11@users.noreply.github.com>
2023-07-15 09:51:08 +0200

    add missing hitlag to docs (#1546)
    * add missing aimed shot hitlag to docs
    
    * fix mika ca not being deployable
    
    * add missing polearm ca hitlag to docs
    
    * add missing claymore ca hitlag to docs
    
    * add missing na hitlag to traveler docs

--- ui/packages/docs/src/components/Hitlag/HitlagTable.js
+++ ui/packages/docs/src/components/Hitlag/HitlagTable.js
@@ -52,10 +52,11 @@ function AbilHitlag({ data }) {
   );
 }
 
-const abils = ["normal", "charge", "skill", "burst", "asc", "cons"];
+const abils = ["normal", "charge", "aim", "skill", "burst", "asc", "cons"];
 const abilLabels = [
   "Normal",
   "Charge Attack",
+  "Aimed Shot",
   "Skill",
   "Burst",
   "Ascension",
