426bbb8fc86db0d3a9d3658f19d7e2800e843c1c
k0l11 <intensonom@yahoo.de>
2023-07-31 08:33:20 +0200

    split character actions with field time in viewer

--- ui/packages/ui/src/Pages/Viewer/Components/Miscellaneous/CharacterActionsBarChart/index.tsx
+++ ui/packages/ui/src/Pages/Viewer/Components/Miscellaneous/CharacterActionsBarChart/index.tsx
@@ -32,7 +32,7 @@ export const CharacterActionsCard = ({ data, running, names }: Props) => {
     : null;
 
   return (
-    <Card className="flex flex-col col-span-full h-96">
+    <Card className="flex flex-col col-span-3 h-96">
       <div className="flex flex-row justify-start gap-5">
         <div className="flex flex-col gap-2">
           <CardTitle title="Character Actions" tooltip="x" timer={timer} />
