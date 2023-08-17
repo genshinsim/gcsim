5a0aa6886903e2aae1354a5a9090bffc4a1784cc
k0l11 <intensonom@yahoo.de>
2023-08-17 17:24:26 +0200

    fix thefirstgreatmagic atk% buff calc

--- internal/weapons/bow/firstgreatmagic/firstgreatmagic.go
+++ internal/weapons/bow/firstgreatmagic/firstgreatmagic.go
@@ -17,38 +17,18 @@ func init() {
 
 type Weapon struct {
 	Index            int
+	core             *core.Core
+	char             *character.CharWrapper
+	atkStackVal      float64
 	sameElement      int
 	differentElement int
 }
 
 func (w *Weapon) SetIndex(idx int) { w.Index = idx }
-func (w *Weapon) Init() error      { return nil }
-
-// DMG dealt by Charged Attacks increased by 16/20/24/28/32%.
-// For every party member with the same Elemental Type as the wielder (including the wielder themselves), gain 1 Gimmick stack.
-// For every party member with a different Elemental Type from the wielder, gain 1 Theatrics stack.
-// When the wielder has 1/2/3 or more Gimmick stacks, ATK will be increased by 16%/32%/48% / 20%/40%/60% / 24%/48%/72% / 28%/56%/84% / 32%/64%/96%.
-// When the wielder has 1/2/3 or more Theatrics stacks, Movement SPD will be increased by 4%/7%/10% / 6%/9%/12% / 8%/11%/14% / 10%/13%/16% / 12%/15%/18%.
-func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile) (weapon.Weapon, error) {
-	w := &Weapon{}
-	r := p.Refine
-
-	// CA buff
-	mDmg := make([]float64, attributes.EndStatType)
-	mDmg[attributes.DmgP] = (0.12 + float64(r)*0.04)
-	char.AddAttackMod(character.AttackMod{
-		Base: modifier.NewBase("thefirstgreatmagic-dmg%", -1),
-		Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
-			if atk.Info.AttackTag != attacks.AttackTagExtra {
-				return nil, false
-			}
-			return mDmg, true
-		},
-	})
-
+func (w *Weapon) Init() error {
 	// calc Gimmick and Theatrics stacks
-	for _, x := range c.Player.Chars() {
-		if x.Base.Element == char.Base.Element { // includes wielder
+	for _, x := range w.core.Player.Chars() {
+		if x.Base.Element == w.char.Base.Element { // includes wielder
 			w.sameElement++
 			continue
 		}
@@ -64,8 +44,8 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile
 
 	// Gimmick buff
 	mAtk := make([]float64, attributes.EndStatType)
-	mAtk[attributes.ATKP] = (0.12 + float64(r)*0.04) * float64(w.sameElement)
-	char.AddStatMod(character.StatMod{
+	mAtk[attributes.ATKP] = w.atkStackVal * float64(w.sameElement)
+	w.char.AddStatMod(character.StatMod{
 		Base:         modifier.NewBase("thefirstgreatmagic-atk", -1),
 		AffectedStat: attributes.ATKP,
 		Amount: func() ([]float64, bool) {
@@ -75,6 +55,36 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile
 
 	// Theatrics buff
 	// TODO: movement speed is not implemented
+
+	return nil
+}
+
+// DMG dealt by Charged Attacks increased by 16/20/24/28/32%.
+// For every party member with the same Elemental Type as the wielder (including the wielder themselves), gain 1 Gimmick stack.
+// For every party member with a different Elemental Type from the wielder, gain 1 Theatrics stack.
+// When the wielder has 1/2/3 or more Gimmick stacks, ATK will be increased by 16%/32%/48% / 20%/40%/60% / 24%/48%/72% / 28%/56%/84% / 32%/64%/96%.
+// When the wielder has 1/2/3 or more Theatrics stacks, Movement SPD will be increased by 4%/7%/10% / 6%/9%/12% / 8%/11%/14% / 10%/13%/16% / 12%/15%/18%.
+func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile) (weapon.Weapon, error) {
+	w := &Weapon{
+		core: c,
+		char: char,
+	}
+	r := p.Refine
+
+	// CA buff
+	mDmg := make([]float64, attributes.EndStatType)
+	mDmg[attributes.DmgP] = (0.12 + float64(r)*0.04)
+	char.AddAttackMod(character.AttackMod{
+		Base: modifier.NewBase("thefirstgreatmagic-dmg%", -1),
+		Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+			if atk.Info.AttackTag != attacks.AttackTagExtra {
+				return nil, false
+			}
+			return mDmg, true
+		},
+	})
+
+	w.atkStackVal = (0.12 + float64(r)*0.04)
 
 	return w, nil
 }
