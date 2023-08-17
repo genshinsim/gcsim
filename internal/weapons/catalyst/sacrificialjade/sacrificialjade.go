e010309e07848d05abea3dda576fdabff138656c
imring <vitaliyvorobets25@gmail.com>
2023-08-17 21:52:55 +0300

    Fix Sacrificial Jade

--- internal/weapons/catalyst/sacrificialjade/sacrificialjade.go
+++ internal/weapons/catalyst/sacrificialjade/sacrificialjade.go
@@ -29,12 +29,15 @@ type Weapon struct {
 func (w *Weapon) SetIndex(idx int) { w.Index = idx }
 
 func (w *Weapon) Init() error {
-	w.c.Tasks.Add(w.updateBuff(w.lastSwap), 6*60)
+	if w.c.Player.Active() != w.char.Index {
+		w.lastSwap = w.c.F
+		w.c.Tasks.Add(w.getBuffs(w.lastSwap), 5*60)
+	}
 	return nil
 }
 
-// When not on the field for more than 6s, Max HP will be increased by 32% and Elemental Mastery will be increased by 40.
-// These effects will be canceled after the wielder has been on the field for 6s.
+// When not on the field for more than 5s, Max HP will be increased by 32% and Elemental Mastery will be increased by 40.
+// These effects will be canceled after the wielder has been on the field for 10s.
 func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile) (weapon.Weapon, error) {
 	w := &Weapon{
 		refine:   p.Refine,
@@ -55,33 +58,48 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile
 	c.Events.Subscribe(event.OnCharacterSwap, func(args ...interface{}) bool {
 		prev := args[0].(int)
 		next := args[1].(int)
-		if prev != char.Index && next != char.Index {
+		if prev == char.Index {
+			w.lastSwap = c.F
+			w.c.Tasks.Add(w.getBuffs(w.lastSwap), 5*60)
 			return false
 		}
-		w.lastSwap = c.F
-		c.Tasks.Add(w.updateBuff(c.F), 6*60)
+		if next == char.Index {
+			w.lastSwap = c.F
+			w.c.Tasks.Add(w.clearBuffs(w.lastSwap), 10*60)
+			return false
+		}
 		return false
 	}, fmt.Sprintf("sacrificial-jade-%v", char.Base.Key.String()))
 
 	return w, nil
 }
 
-func (w *Weapon) updateBuff(src int) func() {
+func (w *Weapon) getBuffs(src int) func() {
 	return func() {
 		if w.lastSwap != src {
 			return
 		}
-
-		if w.c.Player.Active() != w.char.Index {
-			w.buff[attributes.HPP] = 0.24 + 0.08*float64(w.refine)
-			w.buff[attributes.EM] = 30 + 10*float64(w.refine)
-			w.c.Log.NewEvent("sacrificial jade gained buffs", glog.LogWeaponEvent, w.char.Index)
-		} else {
-			w.buff[attributes.HPP] = 0
-			w.buff[attributes.EM] = 0
-			w.c.Log.NewEvent("sacrificial jade lost buffs", glog.LogWeaponEvent, w.char.Index)
+		if w.c.Player.Active() == w.char.Index {
+			return
 		}
 
-		w.c.Tasks.Add(w.updateBuff(src), 6*60)
+		w.buff[attributes.HPP] = 0.24 + 0.08*float64(w.refine)
+		w.buff[attributes.EM] = 30 + 10*float64(w.refine)
+		w.c.Log.NewEvent("sacrificial jade gained buffs", glog.LogWeaponEvent, w.char.Index)
+	}
+}
+
+func (w *Weapon) clearBuffs(src int) func() {
+	return func() {
+		if w.lastSwap != src {
+			return
+		}
+		if w.c.Player.Active() != w.char.Index {
+			return
+		}
+
+		w.buff[attributes.HPP] = 0
+		w.buff[attributes.EM] = 0
+		w.c.Log.NewEvent("sacrificial jade lost buffs", glog.LogWeaponEvent, w.char.Index)
 	}
 }
