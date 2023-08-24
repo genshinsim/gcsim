945979be7dfb197d6a13319b7f06ca1a0475fe60
srliao <906239+srliao@users.noreply.github.com>
2023-08-24 08:08:00 -0400

    fix new weapon merge conflicts

--- internal/weapons/spear/rightfulreward/rightfulreward.go
+++ internal/weapons/spear/rightfulreward/rightfulreward.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/genshinsim/gcsim/pkg/core"
 	"github.com/genshinsim/gcsim/pkg/core/event"
+	"github.com/genshinsim/gcsim/pkg/core/info"
 	"github.com/genshinsim/gcsim/pkg/core/keys"
 	"github.com/genshinsim/gcsim/pkg/core/player/character"
-	"github.com/genshinsim/gcsim/pkg/core/player/weapon"
 )
 
 func init() {
@@ -24,7 +24,7 @@ func (w *Weapon) Init() error      { return nil }
 // When the wielder is healed, restore 8/10/12/14/16 Energy.
 // This effect can be triggered once every 10s, and can occur even when the character is not on the field.
 
-func NewWeapon(c *core.Core, char *character.CharWrapper, p weapon.WeaponProfile) (weapon.Weapon, error) {
+func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) (info.Weapon, error) {
 	w := &Weapon{}
 	r := p.Refine
 
