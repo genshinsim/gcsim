97b5eb76e2de55e8435dbc004417905aa20c3bf6
srliao <906239+srliao@users.noreply.github.com>
2023-09-03 16:34:48 -0400

    add show/hide warning

--- ui/packages/db/src/SharedComponents/Warning.tsx
+++ ui/packages/db/src/SharedComponents/Warning.tsx
@@ -1,12 +1,37 @@
+import React from 'react';
 import tanuki from "images/tanuki.png";
+import { Button } from '@blueprintjs/core';
 
 export function Warning() {
+  const [hide, setHide] = React.useState<boolean>((): boolean => {
+    return localStorage.getItem('hide-warning') === "true"
+  });
+  React.useEffect(() => {
+    localStorage.setItem('hide-warning', hide.toString())
+  }, [hide])
+
+  if (hide) {
+    return (
+      <div className="flex flex-col py-0 max-w-xs sm:min-w-wsm md:min-w-wmd lg:min-w-wlg xl:min-w-wxl sm:max-w-sm md:max-w-2xl lg:max-w-4xl">
+        <div className='ml-auto'>
+          <Button small intent="success" onClick={() => { setHide(false) }}>Show Readme</Button>
+        </div>
+
+      </div>
+    )
+  }
+
   return (
-    <div className="relative bg-slate-900 px-5 sm:px-5 py-0 border border-blue-800 max-w-xs sm:min-w-wsm md:min-w-wmd lg:min-w-wlg xl:min-w-wxl sm:max-w-sm md:max-w-2xl lg:max-w-4xl">
+    <div className="relative flex flex-col gap-2 items-center bg-slate-900 px-5 py-0 border border-blue-800 max-w-xs sm:min-w-wsm md:min-w-wmd lg:min-w-wlg xl:min-w-wxl sm:max-w-sm md:max-w-2xl lg:max-w-4xl">
+      <div className='absolute top-1 right-1'>
+        <Button icon="cross" small intent="danger"
+          onClick={() => { setHide(true) }}
+        />
+      </div>
       <div className="inline-flex pt-4">
-        <img src={tanuki} className="w-15 h-10 mx-0"/>
+        <img src={tanuki} className="w-15 h-10 mx-0" />
         <div className="font-semibold px-3 pt-2 text-xl w-50 text-gray-200">Please read!</div>
-        <img src={tanuki} className="w-15 h-10 mx-0"/>
+        <img src={tanuki} className="w-15 h-10 mx-0" />
       </div>
       <div className="space-y-3 pb-3 text-s leading-5 text-gray-400">
         <p>Unless tagged otherwise, the sims in this database are here to provide examples of interesting, user-submitted configs.</p>
