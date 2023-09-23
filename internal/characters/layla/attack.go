a73f878e2f4d5b80cd724cb4345be6105a353736
srliao <906239+srliao@users.noreply.github.com>
2023-09-22 21:40:59 -0400

    layla

--- internal/characters/layla/attack.go
+++ internal/characters/layla/attack.go
@@ -37,7 +37,7 @@ func init() {
 	attackFrames[2][action.ActionCharge] = 500 // N3 -> CA, TODO: this action is illegal; need better way to handle it
 }
 
-func (c *char) Attack(p map[string]int) action.Info {
+func (c *char) Attack(p map[string]int) (action.Info, error) {
 	ai := combat.AttackInfo{
 		ActorIndex:         c.Index,
 		Abil:               fmt.Sprintf("Normal %v", c.NormalCounter),
@@ -74,5 +74,5 @@ func (c *char) Attack(p map[string]int) action.Info {
 		AnimationLength: attackFrames[c.NormalCounter][action.InvalidAction],
 		CanQueueAfter:   attackHitmarks[c.NormalCounter],
 		State:           action.NormalAttackState,
-	}
+	}, nil
 }
