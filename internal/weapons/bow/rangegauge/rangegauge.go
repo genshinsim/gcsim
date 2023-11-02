79d443aa461bf64c1e311b496e1825e91de60e2c
Athena <bqh1402003@gmail.com>
2023-11-02 08:42:17 +0700

    update consumeEnergy

--- internal/weapons/bow/rangegauge/rangegauge.go
+++ internal/weapons/bow/rangegauge/rangegauge.go
@@ -42,18 +42,12 @@ func (w *Weapon) Init() error      { return nil }
 
 func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) (info.Weapon, error) {
 	w := &Weapon{
-		core:   c,
-		char:   char,
-		refine: p.Refine,
-		buff:   make([]float64, attributes.EndStatType),
+		core:    c,
+		char:    char,
+		refine:  p.Refine,
+		atkp:    make([]float64, attributes.EndStatType),
+		eleDMGP: make([]float64, attributes.EndStatType),
 	}
-
-	atkp := 0.02 + 0.01*float64(r)
-	baseEleDMGP := 0.055 + 0.015*float64(r)
-	duration := 10 * 60
-
-	val := make([]float64, attributes.EndStatType)
-	m := make([]float64, attributes.EndStatType)
 
 	c.Events.Subscribe(event.OnHeal, func(args ...interface{}) bool {
 		source := args[0].(*player.HealInfo)
@@ -78,108 +72,20 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 		c.Log.NewEvent("dockhands-assistant proc'd", glog.LogWeaponEvent, char.Index).
 			Write("index", idx)
 
+		w.consumeEnergy()
 		return false
 	}, fmt.Sprintf("dockhands-assistant-heal-%v", char.Base.Key.String()))
 
-	c.Events.Subscribe(event.OnBurst, func(args ...interface{}) bool {
-		// check for active before deleting symbol
-		if char.StatusIsActive(icdKey) {
-			return false
-		}
-		if c.Player.Active() != char.Index {
-			return false
-		}
-		count := 0
-		for _, s := range symbol {
-			if char.StatusIsActive(s) {
-				count++
-			}
-			char.DeleteStatus(s)
-		}
-		if count == 0 {
-			return false
-		}
-		char.AddStatus(icdKey, 15*60, true)
-
-		val[attributes.ATKP] = atkp * float64(count)
-
-		// add atk buff
-		char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBaseWithHitlag("range-gauge-atk-boost", duration),
-			AffectedStat: attributes.ATKP,
-			Amount: func() ([]float64, bool) {
-				return val, true
-			},
-		})
-
-		eleDMGP := baseEleDMGP * float64(count)
-		for i := attributes.PyroP; i <= attributes.DendroP; i++ {
-			m[i] = eleDMGP
-		}
-
-		// add elemental dmg buff
-		char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBaseWithHitlag("range-gauge-eledmg-buff", duration),
-			AffectedStat: attributes.NoStat,
-			Amount: func() ([]float64, bool) {
-				return m, true
-			},
-		})
-
-		return false
-	}, fmt.Sprintf("range-gauge-struggle-%v", char.Base.Key.String()))
-
-	c.Events.Subscribe(event.OnSkill, func(args ...interface{}) bool {
-		// check for active before deleting symbol
-		if char.StatusIsActive(icdKey) {
-			return false
-		}
-		if c.Player.Active() != char.Index {
-			return false
-		}
-		count := 0
-		for _, s := range symbol {
-			if char.StatusIsActive(s) {
-				count++
-			}
-			char.DeleteStatus(s)
-		}
-		if count == 0 {
-			return false
-		}
-		char.AddStatus(icdKey, 15*60, true)
-		val[attributes.ATKP] = atkp * float64(count)
-
-		// add atk buff
-		char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBaseWithHitlag("range-gauge-atk-boost", duration),
-			AffectedStat: attributes.ATKP,
-			Amount: func() ([]float64, bool) {
-				return val, true
-			},
-		})
-
-		eleDMGP := baseEleDMGP * float64(count)
-		for i := attributes.PyroP; i <= attributes.DendroP; i++ {
-			m[i] = eleDMGP
-		}
-		// add elemental dmg buff
-		char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBaseWithHitlag("range-gauge-eledmg-buff", duration),
-			AffectedStat: attributes.NoStat,
-			Amount: func() ([]float64, bool) {
-				return m, true
-			},
-		})
-
-		return false
-	}, fmt.Sprintf("dockhands-assistant-roused-%v", char.Base.Key.String()))
+	key := fmt.Sprintf("range-gauge-roused-%v", char.Base.Key.String())
+	c.Events.Subscribe(event.OnBurst, w.consumeEnergy, key)
+	c.Events.Subscribe(event.OnSkill, w.consumeEnergy, key)
 	return w, nil
 }
 
 func (w *Weapon) consumeEnergy(args ...interface{}) bool {
-	em := 30 + 10*float64(w.refine)
-	refund := 1.5 + 0.5*float64(w.refine)
+	baseEleDMGP := 0.055 + 0.015*float64(w.refine)
+	duration := 10 * 60
+	atkp := 0.02 + 0.01*float64(w.refine)
 
 	// check for active before deleting symbol
 	if w.char.StatusIsActive(icdKey) {
@@ -188,7 +94,6 @@ func (w *Weapon) consumeEnergy(args ...interface{}) bool {
 	if w.core.Player.Active() != w.char.Index {
 		return false
 	}
-
 	count := 0
 	for _, s := range symbol {
 		if w.char.StatusIsActive(s) {
@@ -201,28 +106,28 @@ func (w *Weapon) consumeEnergy(args ...interface{}) bool {
 	}
 
 	w.char.AddStatus(icdKey, 15*60, true)
-	w.buff[attributes.EM] = em * float64(count)
+	w.atkp[attributes.ATKP] = atkp * float64(count)
 
 	// add atk buff
 	w.char.AddStatMod(character.StatMod{
 		Base:         modifier.NewBaseWithHitlag("range-gauge-atk-boost", duration),
 		AffectedStat: attributes.ATKP,
 		Amount: func() ([]float64, bool) {
-			return w.buff, true
+			return w.atkp, true
 		},
 	})
 
 	eleDMGP := baseEleDMGP * float64(count)
 	for i := attributes.PyroP; i <= attributes.DendroP; i++ {
-		m[i] = eleDMGP
+		w.eleDMGP[i] = eleDMGP
 	}
 
 	// add elemental dmg buff
-	char.AddStatMod(character.StatMod{
+	w.char.AddStatMod(character.StatMod{
 		Base:         modifier.NewBaseWithHitlag("range-gauge-eledmg-buff", duration),
 		AffectedStat: attributes.NoStat,
 		Amount: func() ([]float64, bool) {
-			return m, true
+			return w.eleDMGP, true
 		},
 	})
 
