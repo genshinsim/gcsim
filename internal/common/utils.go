d35d4b45c80e14a5a32b1ee9dc3c29aeb712b6fd
Charlie Zheng <charlie.x.3000@gmail.com>
2023-11-13 19:34:01 -0700

    Added missing util file

---
+++ internal/common/utils.go
@@ -0,0 +1,24 @@
+package common
+
+type Float interface {
+	~float32 | ~float64
+}
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+type Integer interface {
+	Signed | Unsigned
+}
+type Ordered interface {
+	Integer | Float | ~string
+}
+
+func Min[T Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
