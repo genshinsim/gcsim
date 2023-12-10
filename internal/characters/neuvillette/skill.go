a3bda709b23fa580145f415ced1218d3a5ed6506
Charlie Zheng <charlie.x.3000@gmail.com>
2023-12-10 12:56:45 -0700

    Refactored skill implementation to use particle ICD status, added droplet spawn lcoation, added aligned attack hitlag. Fixed canqueueafter.

--- internal/characters/neuvillette/skill.go
+++ internal/characters/neuvillette/skill.go
@@ -13,8 +13,18 @@ import (
 )
 
 var skillFrames []int
-var skillHitmarks = [2]int{23, 54}
-var skillHitbox = [2]float64{6.0, 4.5}
+var skillHitmarks = [2]int{23, 60}
+var skillDropletOffsets = [][][]float64{{{-1, 3}, {0, 3.8}, {1, 3}}, {{-2, 7}, {0, 8}, {2, 7}}, {{-3, 10}, {0, 11}, {3, 10}}}
+var skillDropletRandomRanges = [][][]float64{{{0.5, 1.5}, {0.5, 1.5}, {0.5, 1.5}}, {{1, 2.5}, {3.5, 4}, {1, 2.5}}, {{2, 3}, {2, 4}, {2, 3}}}
+
+const (
+	skillAlignedICD    = 10 * 60
+	skillAlignedICDKey = "neuvillette-aligned-icd"
+
+	particleCount  = 4
+	particleICD    = 0.3 * 60
+	particleICDKey = "neuvillette-particle-icd"
+)
 
 func init() {
 	skillFrames = frames.InitAbilSlice(42)
@@ -29,6 +39,7 @@ func init() {
 
 func (c *char) Skill(p map[string]int) (action.Info, error) {
 	c.chargeEarlyCancelled = false
+
 	ai := combat.AttackInfo{
 		ActorIndex: c.Index,
 		Abil:       "O Tears, I Shall Repay",
@@ -40,63 +51,103 @@ func (c *char) Skill(p map[string]int) (action.Info, error) {
 		Durability: 25,
 		FlatDmg:    skill[c.TalentLvlSkill()] * c.MaxHP(),
 	}
+	// TODO: if target is out of range then pos should be player pos + Y: 8 offset
+	skillPos := c.Core.Combat.PrimaryTarget().Pos()
 	c.Core.QueueAttack(
 		ai,
-		combat.NewCircleHitOnTarget(c.Core.Combat.PrimaryTarget(), geometry.Point{}, skillHitbox[0]),
+		combat.NewCircleHitOnTarget(skillPos, nil, 6),
 		skillHitmarks[0], //TODO: snapshot delay?
 		skillHitmarks[0],
-		c.skillcb,
+		c.makeDropletCB(),
+		c.particleCB,
 	)
-	// 10s Spiritbreath Thorn Interval
-	if c.Core.F-c.lastThorn > 600 {
-		c.lastThorn = c.Core.F
-		aiThorn := combat.AttackInfo{
-			// TODO: Apply Pneuma
-			ActorIndex: c.Index,
-			Abil:       "Spiritbreath Thorn",
-			AttackTag:  attacks.AttackTagElementalArt,
-			ICDTag:     attacks.ICDTagNone,
-			ICDGroup:   attacks.ICDGroupDefault,
-			StrikeType: attacks.StrikeTypeDefault,
-			Element:    attributes.Hydro,
-			Durability: 0,
-			Mult:       thorn[c.TalentLvlSkill()],
+
+	aiThorn := combat.AttackInfo{
+		// TODO: Apply Pneuma
+		ActorIndex:         c.Index,
+		Abil:               "Spiritbreath Thorn (" + c.Base.Key.Pretty() + ")",
+		AttackTag:          attacks.AttackTagElementalArt,
+		ICDTag:             attacks.ICDTagNone,
+		ICDGroup:           attacks.ICDGroupDefault,
+		StrikeType:         attacks.StrikeTypeSpear,
+		Element:            attributes.Hydro,
+		Durability:         0,
+		Mult:               thorn[c.TalentLvlSkill()],
+		CanBeDefenseHalted: true,
+	}
+	c.QueueCharTask(func() {
+		if c.StatusIsActive(skillAlignedICDKey) {
+			return
 		}
+		c.AddStatus(skillAlignedICDKey, skillAlignedICD, true)
+
 		c.Core.QueueAttack(
 			aiThorn,
-			combat.NewCircleHitOnTarget(c.Core.Combat.PrimaryTarget(), geometry.Point{}, skillHitbox[1]),
-			skillHitmarks[1], //TODO: snapshot delay?
-			skillHitmarks[1],
+			combat.NewCircleHitOnTarget(skillPos, nil, 4.5),
+			skillHitmarks[1]-skillHitmarks[0], // TODO: snapshot delay?
+			skillHitmarks[1]-skillHitmarks[0],
 		)
-	}
+	}, skillHitmarks[1])
+
 	c.SetCDWithDelay(action.ActionSkill, 12*60, 20)
 
 	return action.Info{
 		Frames:          frames.NewAbilFunc(skillFrames),
 		AnimationLength: skillFrames[action.InvalidAction],
-		CanQueueAfter:   skillFrames[action.ActionSwap], // earliest cancel
+		CanQueueAfter:   skillFrames[action.ActionCharge], // earliest cancel
 		State:           action.SkillState,
 	}, nil
 }
 
-func (c *char) skillcb(ac combat.AttackCB) {
-	if ac.Target.Type() != targets.TargettableEnemy {
+func (c *char) particleCB(a combat.AttackCB) {
+	if a.Target.Type() != targets.TargettableEnemy {
 		return
 	}
-	if c.Core.F-c.lastSkillParticle > 18 {
-		c.lastSkillParticle = c.Core.F
+	if c.StatusIsActive(particleICDKey) {
+		return
+	}
+	c.AddStatus(particleICDKey, particleICD, true)
 
-		circ, ok := ac.Target.Shape().(*geometry.Circle)
-		if !ok {
-			panic("rectangle target hurtbox is not supported for on target Sourcewater droplet spawning")
+	c.Core.QueueParticle(c.Base.Key.String(), particleCount, attributes.Hydro, c.ParticleDelay)
+}
+
+func (c *char) makeDropletCB() combat.AttackCBFunc {
+	done := false
+	return func(a combat.AttackCB) {
+		if a.Target.Type() != targets.TargettableEnemy {
+			return
 		}
-		for i := 0; i < 3; i++ {
-			// TODO: find the actual sourcewater droplet spawn radius for Neuv E
-			pos := geometry.CalcRandomPointFromCenter(ac.Target.Pos(), circ.Radius()+1.0, circ.Radius()+4.0, c.Core.Rand)
-			common.NewSourcewaterDroplet(c.Core, pos, combat.GadgetTypSourcewaterDropletNeuv)
+		if done {
+			return
+		}
+		done = true
+
+		// determine which droplet offset and random ranges to use based on distance to first target hit
+		player := c.Core.Combat.Player()
+		i := 2
+		if a.Target.IsWithinArea(combat.NewCircleHitOnTarget(player.Pos(), nil, 5)) {
+			i = 0
+		} else if a.Target.IsWithinArea(combat.NewCircleHitOnTarget(player.Pos(), nil, 10)) {
+			i = 1
+		}
+
+		for j := 0; j < 3; j++ {
+			common.NewSourcewaterDroplet(
+				c.Core,
+				geometry.CalcRandomPointFromCenter(
+					geometry.CalcOffsetPoint(
+						player.Pos(),
+						geometry.Point{X: skillDropletOffsets[i][j][0], Y: skillDropletOffsets[i][j][1]},
+						player.Direction(),
+					),
+					skillDropletRandomRanges[i][j][0],
+					skillDropletRandomRanges[i][j][1],
+					c.Core.Rand,
+				),
+				combat.GadgetTypSourcewaterDropletNeuv,
+			)
 		}
 		c.Core.Combat.Log.NewEvent("Spawned 3 droplets", glog.LogCharacterEvent, c.Index).
 			Write("src_action", "skill")
-		c.Core.QueueParticle(c.Base.Key.String(), 4, attributes.Hydro, c.ParticleDelay)
 	}
 }
