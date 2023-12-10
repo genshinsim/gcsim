7452d0ea06ca8ac0487c01597fe438f970f6f2d6
Charlie Zheng <charlie.x.3000@gmail.com>
2023-12-10 11:44:31 -0700

    Adjusted burst implementation for water droplet spawn location

--- internal/characters/neuvillette/burst.go
+++ internal/characters/neuvillette/burst.go
@@ -15,8 +15,12 @@ import (
 
 var burstFrames []int
 var burstHitmarks = [3]int{95, 95 + 40, 95 + 40 + 19}
-var dropletBurstSpawnCount = [3]int{3, 2, 1}
-var dropletBurstSpawnFrame = [3]int{93, 135, 152}
+
+var dropletPosOffsets = [][][]float64{{{0, 7}, {-1, 7.5}, {0.8, 6.5}}, {{-3.5, 7.5}, {-2.5, 6}}, {{3.3, 6}}}
+var dropletRandomRanges = [][]float64{{0.5, 2}, {0.5, 1.2}, {0.5, 1.2}}
+
+var defaultBurstAtkPosOffsets = [][]float64{{-3, 7.5}, {4, 6}}
+var burstTickTargetXOffsets = []float64{1.5, -1.5}
 
 func init() {
 	burstFrames = frames.InitAbilSlice(135)
@@ -32,7 +36,7 @@ func (c *char) Burst(p map[string]int) (action.Info, error) {
 	c.chargeEarlyCancelled = false
 	player := c.Core.Combat.Player()
 
-	aiIninitialHit := combat.AttackInfo{
+	aiInitialHit := combat.AttackInfo{
 		ActorIndex: c.Index,
 		Abil:       "O Tides, I Have Returned: Skill DMG",
 		AttackTag:  attacks.AttackTagElementalBurst,
@@ -43,7 +47,6 @@ func (c *char) Burst(p map[string]int) (action.Info, error) {
 		Durability: 25,
 		FlatDmg:    burst[c.TalentLvlBurst()] * c.MaxHP(),
 	}
-
 	aiWaterfall := combat.AttackInfo{
 		ActorIndex: c.Index,
 		Abil:       "O Tides, I Have Returned: Waterfall DMG",
@@ -55,29 +58,79 @@ func (c *char) Burst(p map[string]int) (action.Info, error) {
 		Durability: 25,
 		FlatDmg:    burstWaterfall[c.TalentLvlBurst()] * c.MaxHP(),
 	}
+	for i := 0; i < 3; i++ {
+		ix := i // avoid closure issue
 
-	apInitialHit := combat.NewCircleHitOnTarget(player, geometry.Point{}, 8)
-	apWaterfall := combat.NewCircleHitOnTarget(c.Core.Combat.PrimaryTarget(), geometry.Point{}, 5)
+		dropletCount := 3 - ix
+		ai := aiInitialHit
+		if ix > 0 {
+			ai = aiWaterfall
+		}
 
-	c.Core.QueueAttack(aiIninitialHit, apInitialHit, burstHitmarks[0], burstHitmarks[0])
-	c.Core.QueueAttack(aiWaterfall, apWaterfall, burstHitmarks[1], burstHitmarks[1])
-	c.Core.QueueAttack(aiWaterfall, apWaterfall, burstHitmarks[2], burstHitmarks[2])
+		c.QueueCharTask(func() {
+			// spawn droplets for current tick using random point from player pos with offset
+			for j := 0; j < dropletCount; j++ {
+				common.NewSourcewaterDroplet(
+					c.Core,
+					geometry.CalcRandomPointFromCenter(
+						geometry.CalcOffsetPoint(
+							player.Pos(),
+							geometry.Point{X: dropletPosOffsets[ix][j][0], Y: dropletPosOffsets[ix][j][1]},
+							player.Direction(),
+						),
+						dropletRandomRanges[ix][0],
+						dropletRandomRanges[ix][1],
+						c.Core.Rand,
+					),
+					combat.GadgetTypSourcewaterDropletNeuv,
+				)
+			}
+			c.Core.Combat.Log.NewEvent(fmt.Sprint("Spawned ", dropletCount, " droplets"), glog.LogCharacterEvent, c.Index).Write("src_action", "burst")
 
-	for i, f := range dropletBurstSpawnFrame {
-		i := i // need to make a copy or else the task func will use the actual i variable
-		c.Core.Tasks.Add(
-			func() {
-				for j := 0; j < dropletBurstSpawnCount[i]; j++ {
-					// TODO: find the actual sourcewater droplet spawn shape for Neuv Q
-					center := player.Pos().Add(player.Direction().Normalize().Mul(geometry.Point{X: 3.0, Y: 3.0}))
-					pos := geometry.CalcRandomPointFromCenter(center, 0, 2.5, c.Core.Rand)
-					common.NewSourcewaterDroplet(c.Core, pos, combat.GadgetTypSourcewaterDropletNeuv)
+			// determine attack pattern
+			// initial tick
+			ap := combat.NewCircleHitOnTarget(player, geometry.Point{Y: 1}, 8)
+			// 2nd and 3rd tick
+			if ix > 0 {
+				// determine attack pattern pos
+				// default assumption: no target in range -> ticks should spawn at specific offset from player
+				apPos := geometry.CalcOffsetPoint(
+					player.Pos(),
+					geometry.Point{
+						X: defaultBurstAtkPosOffsets[ix-1][0],
+						Y: defaultBurstAtkPosOffsets[ix-1][1],
+					},
+					player.Direction(),
+				)
+
+				// check if target is within range
+				target := c.Core.Combat.PrimaryTarget()
+				if target.IsWithinArea(combat.NewCircleHitOnTarget(player, nil, 10)) {
+					// target in range -> adjust pos
+					// pos is a point in random range from target pos + offset
+					// TODO: offset is not accurate because currently target is always looking in default direction
+					apPos = geometry.CalcRandomPointFromCenter(
+						geometry.CalcOffsetPoint(
+							target.Pos(),
+							geometry.Point{X: burstTickTargetXOffsets[ix-1]},
+							target.Direction(),
+						),
+						0,
+						1.5,
+						c.Core.Rand,
+					)
 				}
-				c.Core.Combat.Log.NewEvent(fmt.Sprint("Spawned ", dropletBurstSpawnCount[i], " droplets"), glog.LogCharacterEvent, c.Index).
-					Write("src_action", "burst")
-			},
-			f,
-		)
+				// create attack pattern for tick after determining pos
+				ap = combat.NewCircleHitOnTarget(apPos, nil, 5)
+			}
+
+			c.Core.QueueAttack(
+				ai,
+				ap,
+				0,
+				0,
+			)
+		}, burstHitmarks[ix])
 	}
 
 	c.SetCD(action.ActionBurst, 18*60)
