947e0dff0cbedc4d65e9c3dba46eebee2c2ddd10
Siwat-Keng <ssiwat.keng@gmail.com>
2023-12-16 13:59:50 +0700

    refactor nwew-4pc

--- internal/artifacts/nighttimewhispersintheechoingwoods/nighttimewhispersintheechoingwoods.go
+++ internal/artifacts/nighttimewhispersintheechoingwoods/nighttimewhispersintheechoingwoods.go
@@ -22,6 +22,7 @@ type Set struct {
 	Index int
 	core  *core.Core
 	char  *character.CharWrapper
+	lastF int
 }
 
 func (s *Set) SetIndex(idx int) { s.Index = idx }
@@ -29,8 +30,9 @@ func (s *Set) Init() error      { return nil }
 
 func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[string]int) (info.Set, error) {
 	s := Set{
-		core: c,
-		char: char,
+		core:  c,
+		char:  char,
+		lastF: 0,
 	}
 
 	if count >= 2 {
@@ -46,72 +48,84 @@ func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[stri
 	}
 
 	if count >= 4 {
-		lastF := 0
-		c.Events.Subscribe(event.OnShieldBreak, func(args ...interface{}) bool {
-			breaked := args[0].(shield.Shield)
-			shd := c.Player.Shields.Get(shield.Crystallize)
-			if shd != nil {
-				return false
-			}
-			if c.Player.Active() != char.Index {
-				return false
-			}
-			if breaked.Type() == shield.Crystallize {
-				lastF = c.F + 60
-				return false
-			}
-			return false
-		}, fmt.Sprintf("nwew-4pc-%v", char.Base.Key.String()))
-
-		c.Events.Subscribe(event.OnShielded, func(args ...interface{}) bool {
-			shd := args[0].(shield.Shield)
-			if c.Player.Active() != char.Index {
-				return false
-			}
-			if shd.Type() == shield.Crystallize {
-				lastF = shd.Expiry()
-				return false
-			}
-			return false
-		}, fmt.Sprintf("nwew-4pc-%v", char.Base.Key.String()))
-
-		c.Events.Subscribe(event.OnCharacterSwap, func(args ...interface{}) bool {
-			prev := args[0].(int)
-			active := args[1].(int)
-			shd := c.Player.Shields.Get(shield.Crystallize)
-			if shd == nil {
-				return false
-			}
-			if active == char.Index {
-				lastF = shd.Expiry()
-				return false
-			}
-			if prev == char.Index {
-				lastF = c.F + 60
-				return false
-			}
-			return false
-		}, fmt.Sprintf("nwew-4pc-%v", char.Base.Key.String()))
-
-		c.Events.Subscribe(event.OnSkill, func(args ...interface{}) bool {
-			if c.Player.Active() != char.Index {
-				return false
-			}
-			m := make([]float64, attributes.EndStatType)
-			char.AddAttackMod(character.AttackMod{
-				Base: modifier.NewBaseWithHitlag("nwew-4pc", 10*60),
-				Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
-					if c.F < lastF {
-						m[attributes.GeoP] = 0.2 * 2.5
-					} else {
-						m[attributes.GeoP] = 0.20
-					}
-					return m, true
-				},
-			})
-			return false
-		}, fmt.Sprintf("nwew-4pc-%v", char.Base.Key.String()))
+		c.Events.Subscribe(event.OnShielded, OnShielded(&s), fmt.Sprintf("nwew-4pc-%v", char.Base.Key.String()))
+		c.Events.Subscribe(event.OnShieldBreak, OnShieldBreak(&s), fmt.Sprintf("nwew-4pc-%v", char.Base.Key.String()))
+		c.Events.Subscribe(event.OnCharacterSwap, OnCharacterSwap(&s), fmt.Sprintf("nwew-4pc-%v", char.Base.Key.String()))
+		c.Events.Subscribe(event.OnSkill, OnSkill(&s), fmt.Sprintf("nwew-4pc-%v", char.Base.Key.String()))
 	}
 
 	return &s, nil
+}
+
+func OnShielded(s *Set) func(args ...interface{}) bool {
+	return func(args ...interface{}) bool {
+		shd := args[0].(shield.Shield)
+		if s.core.Player.Active() != s.char.Index {
+			return false
+		}
+		if shd.Type() == shield.Crystallize {
+			s.lastF = shd.Expiry()
+			return false
+		}
+		return false
+	}
+}
+
+func OnShieldBreak(s *Set) func(args ...interface{}) bool {
+	return func(args ...interface{}) bool {
+		breaked := args[0].(shield.Shield)
+		shd := s.core.Player.Shields.Get(shield.Crystallize)
+		if shd != nil {
+			return false
+		}
+		if s.core.Player.Active() != s.char.Index {
+			return false
+		}
+		if breaked.Type() == shield.Crystallize {
+			s.lastF = s.core.F + 60
+			return false
+		}
+		return false
+	}
+}
+
+func OnCharacterSwap(s *Set) func(args ...interface{}) bool {
+	return func(args ...interface{}) bool {
+		prev := args[0].(int)
+		active := args[1].(int)
+		shd := s.core.Player.Shields.Get(shield.Crystallize)
+		if shd == nil {
+			return false
+		}
+		if active == s.char.Index {
+			s.lastF = shd.Expiry()
+			return false
+		}
+		if prev == s.char.Index {
+			s.lastF = s.core.F + 60
+			return false
+		}
+		return false
+	}
+}
+
+func OnSkill(s *Set) func(args ...interface{}) bool {
+	return func(args ...interface{}) bool {
+		if s.core.Player.Active() != s.char.Index {
+			return false
+		}
+		m := make([]float64, attributes.EndStatType)
+		s.char.AddAttackMod(character.AttackMod{
+			Base: modifier.NewBaseWithHitlag("nwew-4pc", 10*60),
+			Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+				if s.core.F <= s.lastF {
+					m[attributes.GeoP] = 0.2 * 2.5
+				} else {
+					m[attributes.GeoP] = 0.20
+				}
+				return m, true
+			},
+		})
+		return false
+	}
 }
