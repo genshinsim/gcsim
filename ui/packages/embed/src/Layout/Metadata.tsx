1580e9996d9dda6c1099491cb084b6b3b12f82c7
srliao <906239+srliao@users.noreply.github.com>
2023-12-17 16:20:07 -0500

    add dps line

--- ui/packages/embed/src/Layout/Metadata.tsx
+++ ui/packages/embed/src/Layout/Metadata.tsx
@@ -7,6 +7,7 @@ import { Iterations } from "@gcsim/ui/src/Pages/Viewer/Components/Overview/Metad
 import { ModeItem } from "@gcsim/ui/src/Pages/Viewer/Components/Overview/Metadata/Mode";
 import { Standard } from "@gcsim/ui/src/Pages/Viewer/Components/Overview/Metadata/Standard";
 import { WarningItem } from "@gcsim/ui/src/Pages/Viewer/Components/Overview/Metadata/Warning";
+import { useTranslation } from "react-i18next";
 
 type Props = {
   data: SimResults;
@@ -20,15 +21,44 @@ export const Metadata = ({ data }: Props) => {
       </Card>
     );
   }
+  let dps: number | undefined = data?.statistics?.dps?.mean;
+  let count: number = data?.statistics?.dps_by_target?.length ?? 0;
+  if (count > 0 && dps != undefined) {
+    dps = dps / count;
+  } else {
+    dps = undefined;
+  }
 
   return (
     <Card className="flex flex-row flex-wrap !p-2 gap-2 justify-center">
       <Error signKey={data.key_type} modified={data.modified} />
+      {!data.modified && (data.key_type == null || data.key_type == "prod") ? (
+        <DPS dps={dps} />
+      ) : null}
       <WarningItem warnings={data?.statistics?.warnings} />
       <Standard standard={data?.standard} />
       <Iterations itr={data?.statistics?.iterations} />
       <ModeItem mode={data?.mode} />
     </Card>
+  );
+};
+
+type DPSProps = {
+  dps?: number;
+};
+
+export const DPS = ({ dps }: DPSProps) => {
+  const { i18n } = useTranslation();
+
+  if (dps == undefined) {
+    <Item title="dps/target" value={"n/a"} />;
+  }
+
+  return (
+    <Item
+      title="dps/target"
+      value={(dps ?? 0).toLocaleString(i18n.language, { notation: "compact" })}
+    />
   );
 };
 
