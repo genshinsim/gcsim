c22cf013727a515f54bb4746d3f9addfc57292d3
srliao <906239+srliao@users.noreply.github.com>
2023-12-17 15:37:25 -0500

    fix agg pkg typo

--- pkg/simulator/imports.go
+++ pkg/simulator/imports.go
@@ -6,7 +6,7 @@ import (
 	_ "github.com/genshinsim/gcsim/pkg/agg/aura"
 	_ "github.com/genshinsim/gcsim/pkg/agg/damage"
 	_ "github.com/genshinsim/gcsim/pkg/agg/energy"
-	_ "github.com/genshinsim/gcsim/pkg/agg/failiures"
+	_ "github.com/genshinsim/gcsim/pkg/agg/failures"
 	_ "github.com/genshinsim/gcsim/pkg/agg/fieldtime"
 	_ "github.com/genshinsim/gcsim/pkg/agg/metadata"
 	_ "github.com/genshinsim/gcsim/pkg/agg/overview"
