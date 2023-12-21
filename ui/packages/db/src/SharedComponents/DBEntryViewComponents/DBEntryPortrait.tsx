1504227b3dee77fc374f24da6d0e941b9a752468
koli <98557316+k0l11@users.noreply.github.com>
2023-12-21 20:54:30 +0100

    fix db entry size on mid size screens (#1825)
    - max-w-32 is not an actual tailwind css class

--- ui/packages/db/src/SharedComponents/DBEntryViewComponents/DBEntryPortrait.tsx
+++ ui/packages/db/src/SharedComponents/DBEntryViewComponents/DBEntryPortrait.tsx
@@ -62,7 +62,7 @@ function DBEntryMobilePortrait({ name, sets, weapon, cons }: model.ICharacter) {
     );
   }
   return (
-    <div className="bg-slate-700 flex flex-row max-h-fit max-w-32">
+    <div className="bg-slate-700 flex flex-row max-h-fit max-w-[128px]">
       <div className="grid grid-cols-3 grid-rows-2 bg-slate-500/10 gap-[2px]  ">
         <div className="col-span-2 row-span-2 bg-slate-700">
           <div className=" relative ">
