36a4e1d61a40ade76244851dee639c178b3a6b30
Charlie Zheng <charlie.x.3000@gmail.com>
2024-02-17 12:58:15 -0700

    Added ayato plunge
    (cherry picked from commit b0fc3d638baedec14f43f2a9eb3f71d6d4782f37)

---
+++ internal/characters/ayato/jump.go
@@ -0,0 +1,29 @@
+package ayato
+
+import (
+	"github.com/genshinsim/gcsim/internal/characters/xianyun"
+	"github.com/genshinsim/gcsim/internal/frames"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/player"
+)
+
+var xianyunJumpFrames []int
+
+func init() {
+	xianyunJumpFrames = frames.InitAbilSlice(58)
+	xianyunJumpFrames[action.ActionHighPlunge] = 6
+	xianyunJumpFrames[action.ActionLowPlunge] = 5
+}
+
+func (c *char) Jump(p map[string]int) (action.Info, error) {
+	if c.StatusIsActive(xianyun.StarwickerKey) {
+		c.Core.Player.SetAirborne(player.AirborneXianyun)
+		return action.Info{
+			Frames:          frames.NewAbilFunc(xianyunJumpFrames),
+			AnimationLength: xianyunJumpFrames[action.InvalidAction],
+			CanQueueAfter:   xianyunJumpFrames[action.ActionLowPlunge], // earliest cancel
+			State:           action.JumpState,
+		}, nil
+	}
+	return c.Character.Jump(p)
+}
