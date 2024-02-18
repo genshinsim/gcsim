db2444ad993ba28162aa3feb99a9ee668dea8b6b
Charlie Zheng <charlie.x.3000@gmail.com>
2024-02-18 09:19:40 -0700

    Changed jump source to source from player
    (cherry picked from commit 956949591da7f542add1d65366281aa4a38053db)

--- internal/characters/bennett/jump.go
+++ internal/characters/bennett/jump.go
@@ -1,7 +1,6 @@
 package bennett
 
 import (
-	"github.com/genshinsim/gcsim/internal/characters/xianyun"
 	"github.com/genshinsim/gcsim/internal/frames"
 	"github.com/genshinsim/gcsim/pkg/core/action"
 	"github.com/genshinsim/gcsim/pkg/core/player"
@@ -16,7 +15,7 @@ func init() {
 }
 
 func (c *char) Jump(p map[string]int) (action.Info, error) {
-	if c.StatusIsActive(xianyun.StarwickerKey) {
+	if c.StatusIsActive(player.XianyunAirborneBuff) {
 		c.Core.Player.SetAirborne(player.AirborneXianyun)
 		return action.Info{
 			Frames:          frames.NewAbilFunc(xianyunJumpFrames),
