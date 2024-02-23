fba636e5550a79c59004abbb6334c1eccd6b9704
srliao <906239+srliao@users.noreply.github.com>
2024-02-23 09:23:08 -0500

    add param for host + port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -12,9 +14,24 @@ var (
 	shareKey string
 )
 
+type opts struct {
+	host     string
+	port     string
+	shareKey string
+}
+
 func main() {
 	if shareKey == "" {
 		shareKey = os.Getenv("GCSIM_SHARE_KEY")
+	}
+
+	var opt opts
+	flag.StringVar(&opt.host, "host", "localhost", "host to listen to (default: localhost)")
+	flag.StringVar(&opt.port, "port", "54321", "port to listen on (default: 54321)")
+	flag.StringVar(&opt.shareKey, "sharekey", "", "share key to use (default: build flag OR GCSIM_SHARE_KEY env variable if not available)")
+
+	if opt.shareKey != "" {
+		shareKey = opt.shareKey
 	}
 
 	server, err := servermode.New(
@@ -26,5 +43,5 @@ func main() {
 		panic(err)
 	}
 
-	log.Fatal(http.ListenAndServe("localhost:54321", server.Router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%v:%v", opt.host, opt.port), server.Router))
 }
