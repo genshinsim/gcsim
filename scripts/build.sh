e32c383a650e3cf9b7c094aae119665333d15b62
k0l11 <intensonom@yahoo.de>
2024-03-07 20:20:53 +0100

    adjust build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,7 @@
 # TODO: get from file or env
 SHARE_KEY=$GCSIM_SHARE_KEY
 
-LDFLAGS=(
-  "-X 'main.shareKey=${SHARE_KEY}'"
-)
-
-GOOS=windows GOARCH=amd64 go build -ldflags="${LDFLAGS[*]}" ./cmd/gcsim
+GOOS=windows GOARCH=amd64 go build -ldflags="-X 'main.shareKey=${SHARE_KEY}' -X main.version=`git tag --sort=-version:refname | head -n 1`" ./cmd/gcsim
 GOOS=windows GOARCH=amd64 go build -ldflags "-X 'main.shareKey=${SHARE_KEY}'  -X main.version=`git tag --sort=-version:refname | head -n 1`" -o server_windows_amd64.exe ./cmd/server
 GOOS=darwin GOARCH=arm64 go build -ldflags "-X 'main.shareKey=${SHARE_KEY}' -X main.version=`git tag --sort=-version:refname | head -n 1`" -o server_darwin_arm64 ./cmd/server 
 GOOS=darwin GOARCH=amd64 go build -ldflags "-X 'main.shareKey=${SHARE_KEY}' -X main.version=`git tag --sort=-version:refname | head -n 1`" -o server_darwin_amd64 ./cmd/server 
