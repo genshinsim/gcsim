8de10d36ec853438ac409896b1837b461ca2aac5
k0l11 <intensonom@yahoo.de>
2024-03-15 18:52:27 +0100

    fix gaming na

--- internal/characters/gaming/attack.go
+++ internal/characters/gaming/attack.go
@@ -1,6 +1,5 @@
 package gaming
 
-// TODO : Entirely stolen from chongyun
 import (
 	"fmt"
 
@@ -14,10 +13,11 @@ import (
 
 var (
 	attackFrames          [][]int
-	attackHitmarks        = []int{26, 24, 41, 53}
-	attackHitlagHaltFrame = []float64{.1, .09, .12, .12}
-	attackHitboxes        = [][]float64{{2}, {2}, {2}, {2, 3}}
-	attackOffsets         = []float64{1, 1, 1, -0.5}
+	attackHitmarks        = []int{25, 18, 34, 48}
+	attackHitlagHaltFrame = []float64{0.10, 0.09, 0.12, 0.12}
+	attackPoiseDMG        = []float64{112.13300323486328, 105.6989974975586, 142.60800170898438, 171.09500122070312}
+	attackHitboxes        = [][]float64{{2}, {2}, {2.5, 4}, {2}}
+	attackOffsets         = []float64{1, 1, -1.7, 2}
 	attackFanAngles       = []float64{360, 270, 360, 360}
 )
 
@@ -26,10 +26,17 @@ const normalHitNum = 4
 func init() {
 	attackFrames = make([][]int, normalHitNum)
 
-	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0], 30)  // N1 -> N2
-	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1], 36)  // N2 -> N3
-	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2], 57)  // N3 -> N4
-	attackFrames[3] = frames.InitNormalCancelSlice(attackHitmarks[3], 101) // N4 -> N1
+	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0], 39)
+	attackFrames[0][action.ActionAttack] = 30
+
+	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1], 46)
+	attackFrames[1][action.ActionAttack] = 32
+
+	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2], 86)
+	attackFrames[2][action.ActionAttack] = 79
+
+	attackFrames[3] = frames.InitNormalCancelSlice(attackHitmarks[3], 89)
+	attackFrames[3][action.ActionAttack] = 87
 }
 
 func (c *char) Attack(p map[string]int) (action.Info, error) {
@@ -40,6 +47,7 @@ func (c *char) Attack(p map[string]int) (action.Info, error) {
 		ICDTag:             attacks.ICDTagNormalAttack,
 		ICDGroup:           attacks.ICDGroupDefault,
 		StrikeType:         attacks.StrikeTypeBlunt,
+		PoiseDMG:           attackPoiseDMG[c.NormalCounter],
 		Element:            attributes.Physical,
 		Durability:         25,
 		Mult:               attack[c.NormalCounter][c.TalentLvlAttack()],
@@ -53,7 +61,7 @@ func (c *char) Attack(p map[string]int) (action.Info, error) {
 		attackHitboxes[c.NormalCounter][0],
 		attackFanAngles[c.NormalCounter],
 	)
-	if c.NormalCounter == 3 {
+	if c.NormalCounter == 2 {
 		ap = combat.NewBoxHitOnTarget(
 			c.Core.Combat.Player(),
 			geometry.Point{Y: attackOffsets[c.NormalCounter]},
