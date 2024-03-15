4055e3e7127afd6f9a72706bb77b15c927642561
k0l11 <intensonom@yahoo.de>
2024-03-15 19:13:27 +0100

    fix gaming a4

--- internal/characters/gaming/asc.go
+++ internal/characters/gaming/asc.go
@@ -1,9 +1,6 @@
 package gaming
 
 import (
-	"strings"
-
-	"github.com/genshinsim/gcsim/pkg/core/attacks"
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
 	"github.com/genshinsim/gcsim/pkg/core/combat"
 	"github.com/genshinsim/gcsim/pkg/core/player"
@@ -48,45 +45,38 @@ func (c *char) a1Heal() {
 	c.QueueCharTask(c.a1Heal, 0.2*60)
 }
 
-/*
-When Gaming has less than 50% HP, he will receive a 20% Incoming Healing Bonus.
-When Gaming has 50% HP or more, Plunging Attack: Charmed Cloudstrider will deal 20% more DMG.
-TODO: Confirm this is 20% DMG and not a 1.2x multiplier to MVs
-*/
+// When Gaming has less than 50% HP, he will receive a 20% Incoming Healing Bonus.
+// When Gaming has 50% HP or more, Plunging Attack: Charmed Cloudstrider will deal 20% more DMG.
 func (c *char) a4() {
 	if c.Base.Ascension < 4 {
 		return
 	}
-	// Healing part
+
 	mHeal := make([]float64, attributes.EndStatType)
 	mHeal[attributes.Heal] = 0.2
 	c.AddStatMod(character.StatMod{
 		Base:         modifier.NewBase("gaming-a4-heal-bonus", -1),
 		AffectedStat: attributes.Heal,
 		Amount: func() ([]float64, bool) {
-			active := c.Core.Player.ActiveChar()
-			if active.CurrentHPRatio() < 0.5 {
-				return mHeal, true
+			if c.CurrentHPRatio() >= 0.5 {
+				return nil, false
 			}
-			return nil, false
+			return mHeal, true
 		},
 	})
 
-	a4Buff := make([]float64, attributes.EndStatType)
-	a4Buff[attributes.PyroP] = 0.2
+	mDmg := make([]float64, attributes.EndStatType)
+	mDmg[attributes.DmgP] = 0.2
 	c.AddAttackMod(character.AttackMod{
 		Base: modifier.NewBase("gaming-a4-dmg-bonus", -1),
 		Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
-			if atk.Info.AttackTag != attacks.AttackTagPlunge {
-				return nil, false
-			}
 			if c.CurrentHPRatio() < 0.5 {
 				return nil, false
 			}
-			if !strings.Contains(atk.Info.Abil, ePlungeKey) {
+			if atk.Info.Abil != ePlungeKey {
 				return nil, false
 			}
-			return a4Buff, true
+			return mDmg, true
 		},
 	})
 }
