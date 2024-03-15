59a644a5cb67b46adc8d8b54ed1edf46c8670d87
srl <906239+srliao@users.noreply.github.com>
2024-03-15 14:20:07 -0400

    change web code editor to use react-ace (#2060)
    - remove dependency on prismjs fork

--- ui/packages/ui/src/Pages/Viewer/Tabs/Config.tsx
+++ ui/packages/ui/src/Pages/Viewer/Tabs/Config.tsx
@@ -1,19 +1,26 @@
-import React, { Dispatch, SetStateAction, useEffect, useMemo, useState } from 'react';
-import Editor from "react-simple-code-editor";
-import { Button, Callout, Intent, NonIdealState, Spinner, SpinnerSize } from "@blueprintjs/core";
-import { SimResults } from '@gcsim/types';
-import ExecutorSettingsButton from '../../../Components/Buttons/ExecutorSettingsButton';
-import { Executor, ExecutorSupplier } from '@gcsim/executors';
-import { runSim } from '../../Simulator/Toolbox';
-import { useAppDispatch } from '../../../Stores/store';
-import { useConfigValidateListener } from '../../Simulator';
+import {
+  Button,
+  Callout,
+  Intent,
+  NonIdealState,
+  Spinner,
+  SpinnerSize,
+} from "@blueprintjs/core";
+import { Executor, ExecutorSupplier } from "@gcsim/executors";
+import { SimResults } from "@gcsim/types";
+import React, { useEffect, useMemo, useState } from "react";
+import AceEditor from "react-ace";
+import ExecutorSettingsButton from "../../../Components/Buttons/ExecutorSettingsButton";
+import { useAppDispatch } from "../../../Stores/store";
+import { useConfigValidateListener } from "../../Simulator";
+import { runSim } from "../../Simulator/Toolbox";
+//THESE IMPORTS NEEDS TO BE AFTER IMPORTING AceEditor
+import "ace-builds/src-noconflict/ext-language_tools";
+import "ace-builds/src-noconflict/theme-tomorrow_night";
+import "../../../util/mode-gcsim";
 
-//@ts-ignore
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-gcsim";
-import "prismjs/themes/prism-tomorrow.css";
-import { useHistory } from 'react-router';
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router";
 
 type UseConfigData = {
   cfg?: string;
@@ -23,7 +30,7 @@ type UseConfigData = {
   modified: boolean;
   exec: ExecutorSupplier<Executor>;
   setCfg: (cfg: string) => void;
-}
+};
 
 type ConfigProps = {
   config: UseConfigData;
@@ -46,62 +53,69 @@ const ConfigUI = ({ config, running, resetTab }: ConfigProps) => {
         <div className="flex gap-2 justify-center">
           <ExecutorSettingsButton />
           <Button
-              icon="refresh"
-              text={t<string>("viewer.rerun")}
-              intent={Intent.SUCCESS}
-              disabled={config.error !== "" || (!config.validated && config.modified)}
-              loading={!config.isReady || running}
-              className="basis-1/2"
-              onClick={() => {
-                dispatch(runSim(config.exec(), config.cfg ?? ""));
-                resetTab();
-                history.push("/web");
-              }} />
+            icon="refresh"
+            text={t<string>("viewer.rerun")}
+            intent={Intent.SUCCESS}
+            disabled={
+              config.error !== "" || (!config.validated && config.modified)
+            }
+            loading={!config.isReady || running}
+            className="basis-1/2"
+            onClick={() => {
+              dispatch(runSim(config.exec(), config.cfg ?? ""));
+              resetTab();
+              history.push("/web");
+            }}
+          />
         </div>
         <Error error={config.error} cfg={config.cfg} />
       </div>
       <div>
-        <Editor
-            value={config.cfg}
-            onValueChange={(c) => config.setCfg(c)}
-            textareaId="codeArea"
-            className="editor"
-            highlight={(code) =>
-              highlight(code, languages.gcsim)
-                .split("\n")
-                .map(
-                  (line: string, i: number) =>
-                    `<span class='editorLineNumber'>${i + 1}</span>${line}`
-                )
-                .join("\n")
-            }
-            insertSpaces
-            padding={10}
-            style={{
-              fontFamily: '"Fira code", "Fira Mono", monospace',
-              fontSize: 14,
-              backgroundColor: "rgb(45 45 45)",
-            }} />
+        <AceEditor
+          mode="gcsim"
+          theme="tomorrow_night"
+          width="100%"
+          onChange={config.setCfg}
+          value={config.cfg}
+          name="UNIQUE_ID_OF_DIV"
+          editorProps={{
+            $blockScrolling: true,
+          }}
+          setOptions={{
+            maxLines: Infinity,
+            fontSize: 14,
+            tabSize: 2,
+          }}
+        />
       </div>
     </div>
   );
 };
 
-const Error = ({ error, cfg }: { error: string, cfg: string}) => {
+const Error = ({ error, cfg }: { error: string; cfg: string }) => {
   const { t } = useTranslation();
   if (error === "" || cfg === "") {
     return null;
   }
   return (
     <div className="px-6 pt-4">
-      <Callout intent={Intent.DANGER} title={t<string>("viewer.error_encountered") + + t<string>("viewer.config_invalid")}>
+      <Callout
+        intent={Intent.DANGER}
+        title={
+          t<string>("viewer.error_encountered") +
+          +t<string>("viewer.config_invalid")
+        }
+      >
         <pre className="whitespace-pre-wrap pl-5">{error}</pre>
       </Callout>
     </div>
   );
 };
 
-export function useConfig(data: SimResults | null, exec: ExecutorSupplier<Executor>): UseConfigData {
+export function useConfig(
+  data: SimResults | null,
+  exec: ExecutorSupplier<Executor>
+): UseConfigData {
   const [cfg, setCfg] = useState(data?.config_file);
   const [isReady, setReady] = useState<boolean | null>(null);
   const [err, setErr] = useState("");
@@ -120,14 +134,21 @@ export function useConfig(data: SimResults | null, exec: ExecutorSupplier<Execut
   // check worker ready state every 250ms so run button becomes available when workers do
   useEffect(() => {
     const interval = setInterval(() => {
-      exec().ready().then(res => setReady(res))
+      exec()
+        .ready()
+        .then((res) => setReady(res));
     }, 250);
     return () => clearInterval(interval);
   }, [exec]);
 
   // will detect changes in the redux config and validate with the executor
   // validated == true means we had a successful validation check run, not that it is valid
-  const validated = useConfigValidateListener(exec, cfg ?? "", modified, setErr);
+  const validated = useConfigValidateListener(
+    exec,
+    cfg ?? "",
+    modified,
+    setErr
+  );
 
   return useMemo(() => {
     return {
