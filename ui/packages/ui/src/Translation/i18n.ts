f01e0e225cf21591af3bac4ac2b1ae75be20e1c0
srl <906239+srliao@users.noreply.github.com>
2024-03-15 14:46:02 -0400

    use generated names for translation (#2061)

--- ui/packages/ui/src/Translation/i18n.ts
+++ ui/packages/ui/src/Translation/i18n.ts
@@ -1,43 +1,45 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import { merge } from "lodash-es";
 import { initReactI18next } from "react-i18next";
 import Chinese from "./locales/Chinese.json";
 import English from "./locales/English.json";
-import IngameNames from "./locales/IngameNames.json";
+import German from "./locales/German.json";
 import Japanese from "./locales/Japanese.json";
 import Korean from "./locales/Korean.json";
+import names from "./locales/names.generated.json";
+import traveler_names from "./locales/names.traveler.json";
 import Russian from "./locales/Russian.json";
 import Spanish from "./locales/Spanish.json";
-import German from "./locales/German.json";
 
 const resources = {
   en: {
     translation: English,
-    game: IngameNames.English,
+    game: merge(names.English, traveler_names.English),
   },
   zh: {
     translation: Chinese,
-    game: IngameNames.Chinese,
+    game: merge(names.Chinese, traveler_names.Chinese),
   },
   ja: {
     translation: Japanese,
-    game: IngameNames.Japanese,
+    game: merge(names.Japanese, traveler_names.Japanese),
   },
   ko: {
     translation: Korean,
-    game: IngameNames.Korean,
+    game: merge(names.Korean, traveler_names.Korean),
   },
   es: {
     translation: Spanish,
-    game: IngameNames.Spanish,
+    game: merge(names.Spanish, traveler_names.Spanish),
   },
   ru: {
     translation: Russian,
-    game: IngameNames.Russian,
+    game: merge(names.Russian, traveler_names.Russian),
   },
   de: {
     translation: German,
-    game: IngameNames.German,
+    game: merge(names.German, traveler_names.German),
   },
 };
 
