315c923e3053a62c7ce9e75ddf05df63ec9ae740
k0l11 <intensonom@yahoo.de>
2024-03-16 18:13:35 +0100

    fix chiori na

--- internal/characters/chiori/attack.go
+++ internal/characters/chiori/attack.go
@@ -12,15 +12,15 @@ import (
 )
 
 var (
-	attackFrames   [][]int
-	attackHitmarks = [][]int{{25 - 1}, {20 - 1}, {20 - 1, 59 - 20 - 1}, {62 - 1}} //TODO:
-
-	//TODO: cp from alhaitham
+	attackFrames [][]int
+	// TODO: attack hitmark frames
+	attackHitmarks        = [][]int{{25 - 1}, {20 - 1}, {20 - 1, 59 - 20 - 1}, {62 - 1}}
 	attackHitlagHaltFrame = [][]float64{{0.03}, {0.03}, {0.06, 0}, {0}}
-	attackDefHalt         = [][]bool{{true}, {true}, {false, true}, {false}}
-	attackHitboxes        = [][]float64{{2.5}, {2}, {2, 3}, {3}}
-	attackOffsets         = []float64{-0.1, -0.1, 0, 0}
-	attackFanAngles       = []float64{180, 360, 360, 360}
+	attackHitlagFactor    = [][]float64{{0.01}, {0.01}, {0.01, 0.01}, {0.05}}
+	attackDefHalt         = [][]bool{{true}, {true}, {true, false}, {true}}
+	attackHitboxes        = [][]float64{{1.6}, {1.8}, {1.8, 3.6}, {2.8}}
+	attackOffsets         = []float64{0.5, 0.6, -0.6, 0.9}
+	attackFanAngles       = []float64{360, 360, 360, 360}
 )
 
 const normalHitNum = 4
@@ -28,6 +28,7 @@ const normalHitNum = 4
 func init() {
 	attackFrames = make([][]int, normalHitNum)
 
+	// TODO: attack cancel frames
 	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0][0], 25)
 	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1][0], 20)
 	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2][1], 59)
@@ -46,18 +47,25 @@ func (c *char) Attack(p map[string]int) (action.Info, error) {
 			Element:            attributes.Physical,
 			Durability:         25,
 			Mult:               mult[c.TalentLvlAttack()],
-			HitlagFactor:       0.01,
+			HitlagFactor:       attackHitlagFactor[c.NormalCounter][i],
 			HitlagHaltFrames:   attackHitlagHaltFrame[c.NormalCounter][i] * 60,
 			CanBeDefenseHalted: attackDefHalt[c.NormalCounter][i],
 		}
 
-		//TODO: wtf is this even
 		ap := combat.NewCircleHitOnTargetFanAngle(
 			c.Core.Combat.Player(),
 			geometry.Point{Y: attackOffsets[c.NormalCounter]},
 			attackHitboxes[c.NormalCounter][0],
 			attackFanAngles[c.NormalCounter],
 		)
+		if c.NormalCounter == 2 {
+			ap = combat.NewBoxHitOnTarget(
+				c.Core.Combat.Player(),
+				geometry.Point{Y: attackOffsets[c.NormalCounter]},
+				attackHitboxes[c.NormalCounter][0],
+				attackHitboxes[c.NormalCounter][1],
+			)
+		}
 
 		c.Core.Tasks.Add(func() {
 			snap := c.Snapshot(&ai)
