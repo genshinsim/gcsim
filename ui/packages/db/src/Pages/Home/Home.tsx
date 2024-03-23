70af6a9f0128941a2ad9d4d5cf96bbfc384ae9ca
srl <906239+srliao@users.noreply.github.com>
2024-03-23 11:15:08 -0400

    UI localization refactor (#2121)
    * refactor translation to sep dir + update db home page
    
    * fix enemy names import

--- ui/packages/db/src/Pages/Home/Home.tsx
+++ ui/packages/db/src/Pages/Home/Home.tsx
@@ -1,42 +1,44 @@
-import { Button, ButtonGroup } from "@blueprintjs/core"
-import { useLocation } from "wouter"
+import { Button, ButtonGroup } from "@blueprintjs/core";
+import { useTranslation } from "react-i18next";
 import tagData from "tags.json";
+import { useLocation } from "wouter";
 
 export const Home = () => {
-    const [_, to] = useLocation()
-    const sortedTagnames = Object.keys(tagData)
-        .filter((key) => {
-            return key !== "0" && key != "2";
-        })
-        .map((key) => {
-            let name = tagData[key]["display_name"]
-            if (key == "1") {
-                name = "(Not Tagged)"
-            }
-            return <li key={key}>
-                <span className="font-semibold text-rose-700">{name}</span>{`: ${tagData[key]["blurb"]}`}
-            </li>
-        });
-    return (
-        <div className="ml-2 mr-2 mt-2">
-            <div className="text-center text-lg font-semibold text-indigo-600">Welcome to Simpact</div>
-            <div className="mb-4">
-                <p className="m-2">
-                    Simpact is a database of gcsim simulations submitted and maintained by gcsim users.
-                </p>
-                <p className="m-2">
-                    The database consists of various tags, each tag representing a collection of sims. These collections are maintained by volunteers and each collection follows their own rules.
-                </p>
-                <p className="m-2">Below are the current available tags:</p>
-                <ul className="list-disc m-4 ml-8">
-                    {sortedTagnames}
-                </ul>
-            </div>
-            <ButtonGroup fill className="mb-4">
-                <Button intent="primary" onClick={() => to("/database")}>Get started</Button>
-            </ButtonGroup>
+  const { t } = useTranslation();
 
-        </div>
-    )
-
-} 
+  const [_, to] = useLocation();
+  const sortedTagnames = Object.keys(tagData)
+    .filter((key) => {
+      return key !== "0" && key != "2";
+    })
+    .map((key) => {
+      let name = tagData[key]["display_name"];
+      if (key == "1") {
+        name = "(Not Tagged)";
+      }
+      return (
+        <li key={key}>
+          <span className="font-semibold text-rose-700">{name}</span>
+          {`: ${t<string>("db.home.tag_desc_" + key)}`}
+        </li>
+      );
+    });
+  return (
+    <div className="ml-2 mr-2 mt-2">
+      <div className="text-center text-lg font-semibold text-indigo-600">
+        {t<string>("db.home_welcome")}{" "}
+      </div>
+      <div className="mb-4">
+        <p className="m-2">{t<string>("db.home.simpact_desc")}</p>
+        <p className="m-2">{t<string>("db.home.simpact_tag_desc")} </p>
+        <p className="m-2">{t<string>("db.home.simpact_tag_list_header")} </p>
+        <ul className="list-disc m-4 ml-8">{sortedTagnames}</ul>
+      </div>
+      <ButtonGroup fill className="mb-4">
+        <Button intent="primary" onClick={() => to("/database")}>
+          {t<string>("db.home.get_started")}
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
+};
