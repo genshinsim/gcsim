98c9f5748025bf12f950b7949f992e43a3a61190
srliao <906239+srliao@users.noreply.github.com>
2024-03-23 23:00:32 -0400

    add separators

--- ui/packages/components/src/common/ui/index.ts
+++ ui/packages/components/src/common/ui/index.ts
@@ -1,5 +1,6 @@
 export { Button } from "./button";
 export { Input } from "./input";
+export * from "./separator";
 export * from "./toast";
 export * from "./toaster";
 export * from "./use-toast";
