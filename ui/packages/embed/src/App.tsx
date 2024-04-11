138e73438bc9956ac6c41a53f803f1177d8af8ca
srliao <906239+srliao@users.noreply.github.com>
2024-04-11 15:38:56 +0000

    add image loaded checks to new embed

--- ui/packages/embed/src/App.tsx
+++ ui/packages/embed/src/App.tsx
@@ -1,44 +1,56 @@
-import { PreviewCard } from "@gcsim/components";
-import "@gcsim/components/src/index.css";
-import { SimulationResult } from "@gcsim/types/src/generated/index.model";
-import axios from "axios";
-import React from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import { Route, Switch } from "wouter";
+import {PreviewCard} from '@gcsim/components';
+import '@gcsim/components/src/index.css';
+import {SimulationResult} from '@gcsim/types/src/generated/index.model';
+import axios from 'axios';
+import React from 'react';
+import {ErrorBoundary} from 'react-error-boundary';
+import {Route, Switch} from 'wouter';
 
-function fallbackRender({ error }) {
+function fallbackRender({error}) {
   return (
     <div id="card" role="alert">
       <input id="has-error" disabled hidden value={JSON.stringify(error)} />
       <p className="text-white">Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
+      <pre style={{color: 'red'}}>{error.message}</pre>
     </div>
   );
 }
 
-const App = ({ id, src }: { id: string; src: string }) => {
-  const [err, setError] = React.useState<string>("");
+const App = ({id, src}: {id: string; src: string}) => {
+  const [err, setError] = React.useState<string>('');
   const [data, setData] = React.useState<SimulationResult | undefined>(
-    undefined
+    undefined,
   );
+  const [loaded, setLoaded] = React.useState(0);
+  const [completed, setCompleted] = React.useState(false);
   React.useEffect(() => {
     //https://gcsim.app/api/share/db/nFLhjtD9dfFN
+    const url = `/api/share/` + (src === 'db' ? 'db/' : '') + id;
     axios
-      .get("/api/share/" + src + "/" + id)
+      .get(url)
       .then((res) => {
         console.log(res);
         if (res.data) {
           setData(res.data);
         } else {
-          setError("unexpected no data");
+          setError('unexpected no data');
         }
       })
       .catch((e) => {
         setError(JSON.stringify(e));
       });
   }, []);
+  React.useEffect(() => {
+    if (loaded >= (data?.character_details?.length ?? 0)) {
+      setCompleted(true);
+    }
+  }, [loaded]);
 
-  if (err !== "") {
+  const handleOnImageLoaded = () => {
+    setLoaded(loaded + 1);
+  };
+
+  if (err !== '') {
     return (
       <>
         <input id="has-error" disabled hidden value={err} />
@@ -57,7 +69,12 @@ const App = ({ id, src }: { id: string; src: string }) => {
 
   return (
     <ErrorBoundary fallbackRender={fallbackRender}>
-      <PreviewCard data={data} />
+      {completed ? (
+        <span
+          className="hidden absolute top-0 left-0"
+          id="images_loaded"></span>
+      ) : null}
+      <PreviewCard data={data} onImageLoaded={handleOnImageLoaded} />
     </ErrorBoundary>
   );
 };
