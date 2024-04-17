89abda833a2ff9b4fd6cb5629cb3a2348dae3659
srliao <906239+srliao@users.noreply.github.com>
2024-04-17 12:44:08 -0400

    try a different image

--- containers/embedgenerator/ci/build-linux-amd64.sh
+++ containers/embedgenerator/ci/build-linux-amd64.sh
@@ -3,4 +3,4 @@ yarn workspace @gcsim/embed build
 cd ..
 ls -lh ./ui/packages/embed/dist
 cd cmd/services/embedgenerator
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build .
+GOOS=linux GOARCH=amd64 go build .
