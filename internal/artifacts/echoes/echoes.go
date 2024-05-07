6cfb8540f5d4ea31ca2520e06ac4276413581b2c
Yihua Liu <yihuajack@live.cn>
2024-05-07 18:51:01 +0800

    fix echoes icd bug

--- internal/artifacts/echoes/echoes.go
+++ internal/artifacts/echoes/echoes.go
@@ -98,9 +98,11 @@ func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[stri
 		}
 
 		if c.Rand.Float64() > s.prob {
+			s.icd = c.F + 12 // 0.2s
 			s.prob += 0.2
 			c.Log.NewEvent("echoes 4pc failed to proc due to chance", glog.LogArtifactEvent, char.Index).
-				Write("probabability_now", s.prob)
+				Write("probabability_now", s.prob).
+				Write("icd_up", s.icd)
 			return false
 		}
 
