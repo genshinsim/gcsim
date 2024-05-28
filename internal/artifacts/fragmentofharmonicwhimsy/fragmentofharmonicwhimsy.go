d63c04a64db3135ae6953a811900ff21d862a2ef
Siwat-Keng <ssiwat.keng@gmail.com>
2024-05-28 09:19:40 +0700

    update fohw condition

--- internal/artifacts/fragmentofharmonicwhimsy/fragmentofharmonicwhimsy.go
+++ internal/artifacts/fragmentofharmonicwhimsy/fragmentofharmonicwhimsy.go
@@ -17,22 +17,26 @@ func init() {
 }
 
 type Set struct {
-	counter int
-	Index   int
+	stacks int
+	Index  int
 }
 
 func (s *Set) SetIndex(idx int) { s.Index = idx }
 func (s *Set) Init() error      { return nil }
 
+const (
+	fohw2pc = "fragmentofharmonicwhimsy-2pc"
+	fohw4pc = "fragmentofharmonicwhimsy-4pc"
+)
+
 func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[string]int) (info.Set, error) {
-	s := Set{}
-	s.counter = 0
+	var s Set
 
 	if count >= 2 {
 		m := make([]float64, attributes.EndStatType)
 		m[attributes.ATKP] = 0.18
 		char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBase("fragmentofharmonicwhimsy-2pc", -1),
+			Base:         modifier.NewBase(fohw2pc, -1),
 			AffectedStat: attributes.ATKP,
 			Amount: func() ([]float64, bool) {
 				return m, true
@@ -42,19 +46,30 @@ func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[stri
 
 	if count >= 4 {
 		m := make([]float64, attributes.EndStatType)
-		m[attributes.DmgP] = 0.18
 		c.Events.Subscribe(event.OnHPDebt, func(args ...interface{}) bool {
 			index := args[0].(int)
 			amount := args[1].(float64)
-			if char.Index == index && amount != 0 {
-				char.AddStatMod(character.StatMod{
-					Base: modifier.NewBaseWithHitlag(fmt.Sprintf("fragmentofharmonicwhimsy-4pc-%v-stack", s.counter+1), 6*60),
-					Amount: func() ([]float64, bool) {
-						return m, true
-					},
-				})
-				s.counter = (s.counter + 1) % 3
+			if char.Index != index || amount == 0 {
+				return false
 			}
+
+			if !char.StatusIsActive(fohw4pc) {
+				s.stacks = 0
+			}
+
+			if s.stacks < 3 {
+				s.stacks++
+			}
+
+			m[attributes.DmgP] = 0.18 * float64(s.stacks)
+
+			char.AddStatMod(character.StatMod{
+				Base: modifier.NewBaseWithHitlag(fohw4pc, 6*60),
+				Amount: func() ([]float64, bool) {
+					return m, true
+				},
+			})
+
 			return false
 		}, fmt.Sprintf("fragmentofharmonicwhimsy-hp-debt-%v", char.Base.Key.String()))
 	}
