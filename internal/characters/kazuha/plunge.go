ecab91d2dd4269c330978c4288f35ed080e5dc5b
srl <906239+srliao@users.noreply.github.com>
2024-06-10 14:54:08 -0400

    fix linter issues from new linter version (#2175)

--- internal/characters/kazuha/plunge.go
+++ internal/characters/kazuha/plunge.go
@@ -79,13 +79,13 @@ func (c *char) LowPlungeAttack(p map[string]int) (action.Info, error) {
 
 	switch c.Core.Player.Airborne() {
 	case player.AirborneXianyun:
-		return c.lowPlungeXY(p)
+		return c.lowPlungeXY(p), nil
 	default:
 		return action.Info{}, errors.New("low_plunge can only be used while airborne")
 	}
 }
 
-func (c *char) lowPlungeXY(p map[string]int) (action.Info, error) {
+func (c *char) lowPlungeXY(p map[string]int) action.Info {
 	collision, ok := p["collision"]
 	if !ok {
 		collision = 0 // Whether or not collision hit
@@ -119,7 +119,7 @@ func (c *char) lowPlungeXY(p map[string]int) (action.Info, error) {
 		AnimationLength: lowPlungeFrames[action.InvalidAction],
 		CanQueueAfter:   lowPlungeFrames[action.ActionDash],
 		State:           action.PlungeAttackState,
-	}, nil
+	}
 }
 
 func (c *char) HighPlungeAttack(p map[string]int) (action.Info, error) {
@@ -130,7 +130,7 @@ func (c *char) HighPlungeAttack(p map[string]int) (action.Info, error) {
 
 	switch c.Core.Player.Airborne() {
 	case player.AirborneXianyun:
-		return c.highPlungeXY(p)
+		return c.highPlungeXY(p), nil
 	default:
 		return action.Info{}, errors.New("high_plunge can only be used while airborne")
 	}
@@ -230,7 +230,7 @@ func (c *char) skillPlunge(p map[string]int) (action.Info, error) {
 	return act, nil
 }
 
-func (c *char) highPlungeXY(p map[string]int) (action.Info, error) {
+func (c *char) highPlungeXY(p map[string]int) action.Info {
 	collision, ok := p["collision"]
 	if !ok {
 		collision = 0 // Whether or not collision hit
@@ -264,7 +264,7 @@ func (c *char) highPlungeXY(p map[string]int) (action.Info, error) {
 		AnimationLength: highPlungeFrames[action.InvalidAction],
 		CanQueueAfter:   highPlungeFrames[action.ActionDash],
 		State:           action.PlungeAttackState,
-	}, nil
+	}
 }
 
 // Plunge normal falling attack damage queue generator
