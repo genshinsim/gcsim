11ee78c1c81976053ecf043a947aa54901a2c9d7
srl <906239+srliao@users.noreply.github.com>
2024-06-17 15:56:18 -0400

    Fix Weapon.BaseAtk no longer used (#2182)
    * remove Weapon.BaseAtk
    
    * add TotalAtk and TotalDef helpers

--- internal/template/character/character.go
+++ internal/template/character/character.go
@@ -58,7 +58,7 @@ func (c *Character) Snapshot(a *combat.AttackInfo) combat.Snapshot {
 	s := combat.Snapshot{
 		CharLvl:     c.Base.Level,
 		ActorEle:    c.Base.Element,
-		BaseAtk:     c.Base.Atk + c.Weapon.BaseAtk,
+		BaseAtk:     c.Base.Atk,
 		BaseDef:     c.Base.Def,
 		BaseHP:      c.Base.HP,
 		SourceFrame: c.Core.F,
