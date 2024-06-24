62aef0b7ae74c83d81ffa65b3fff98433b6b55c8
srl <906239+srliao@users.noreply.github.com>
2024-06-24 14:39:25 -0400

    revert ads (#2187)

--- ui/packages/db/src/SharedComponents/ActionBar.tsx
+++ ui/packages/db/src/SharedComponents/ActionBar.tsx
@@ -1,16 +1,16 @@
-import { useTranslation } from "react-i18next";
-import { CharacterQuickSelect } from "./CharacterQuickSelect";
-import { Filter } from "./Filter";
+import {useTranslation} from 'react-i18next';
+import {CharacterQuickSelect} from './CharacterQuickSelect';
+import {Filter} from './Filter';
 
-export function ActionBar({ simCount }: { simCount: number | null }) {
-  const { t } = useTranslation();
+export function ActionBar({simCount}: {simCount: number | null}) {
+  const {t} = useTranslation();
   return (
-    <div className="flex flex-row justify-between items-center w-full max-w-[970px] min-[1300px]:w-[970px] gap-4">
+    <div className="flex flex-row justify-between items-center w-full max-w-7xl gap-4">
       <Filter />
       <CharacterQuickSelect />
 
       <div className="text-base  hidden md:block md:text-2xl">
-        {t<string>("db.showing_simulations", { i: simCount ?? 0 })}
+        {t('db.showing_simulations', {i: simCount ?? 0})}
       </div>
       {/* <Sorter /> */}
     </div>
