a02063929606a8193c16415b24ce6da282958655
imring <vitaliyvorobets25@gmail.com>
2024-08-10 13:54:11 +0300

    rework stacks for heartstrings

--- internal/weapons/bow/heartstrings/heartstrings.go
+++ internal/weapons/bow/heartstrings/heartstrings.go
@@ -8,6 +8,7 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
 	"github.com/genshinsim/gcsim/pkg/core/combat"
 	"github.com/genshinsim/gcsim/pkg/core/event"
+	"github.com/genshinsim/gcsim/pkg/core/glog"
 	"github.com/genshinsim/gcsim/pkg/core/info"
 	"github.com/genshinsim/gcsim/pkg/core/keys"
 	"github.com/genshinsim/gcsim/pkg/core/player/character"
@@ -19,19 +20,23 @@ func init() {
 }
 
 type Weapon struct {
-	Index int
+	char       *character.CharWrapper
+	core       *core.Core
+	prevStacks int
+	Index      int
 }
 
 func (w *Weapon) SetIndex(idx int) { w.Index = idx }
 func (w *Weapon) Init() error      { return nil }
 
 const (
-	bondKey    = "heartstrings-bond"
-	skillKey   = "heartstrings-skill"
-	healingKey = "heartstrings-healing"
+	bondKey          = "heartstrings-bond"
+	skillKey         = "heartstrings-skill"
+	healingKey       = "heartstrings-healing"
+	burstCRKey       = "heartstrings-cr"
+	burstCRKeyCancel = "heartstrings-cr-cancel"
 )
 
-// TODO: clear burst cr buff 4s after losing 3 stacks <-- I have no idea how to do this
 func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) (info.Weapon, error) {
 	// The equipping character can gain the Remedy effect.
 	// When they possess 1/2/3 Remedy stacks, Max HP will increase by 12%/24%/40%.
@@ -43,13 +48,29 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 	// Each stack's duration is counted independently.
 	// In addition, when 3 stacks are active, Elemental Burst CRIT Rate will be increased by 28%.
 	// This effect will be canceled 4s after falling under 3 stacks.
-	w := &Weapon{}
+	w := &Weapon{
+		char: char,
+		core: c,
+	}
 	r := p.Refine
 
-	stack := 0.09 + float64(r)*0.03
-	max := 0.04 + float64(r) - 1
+	hpStack := 0.09 + float64(r)*0.03
+	hpMaxStack := 0.03 + float64(r)*0.01
 
+	// Max HP increase
 	mHP := make([]float64, attributes.EndStatType)
+	char.AddStatMod(character.StatMod{
+		Base:         modifier.NewBase("heartstrings", -1),
+		AffectedStat: attributes.HPP,
+		Amount: func() ([]float64, bool) {
+			stacks := w.Stacks()
+			mHP[attributes.HPP] = hpStack * float64(stacks)
+			if stacks >= 3 {
+				mHP[attributes.HPP] += hpMaxStack
+			}
+			return mHP, true
+		},
+	})
 
 	// Using skill
 	c.Events.Subscribe(event.OnSkill, func(args ...interface{}) bool {
@@ -57,7 +78,7 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 			return false
 		}
 
-		char.AddStatus(skillKey, 25*60, true)
+		w.AddStack(skillKey, 25*60)
 		return false
 	}, fmt.Sprintf("heartstrings-%v", char.Base.Key.String()))
 
@@ -70,7 +91,7 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 			return false
 		}
 
-		char.AddStatus(bondKey, 25*60, true)
+		w.AddStack(bondKey, 25*60)
 		return false
 	}, fmt.Sprintf("heartstrings-%v", char.Base.Key.String()))
 
@@ -82,45 +103,64 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 			return false
 		}
 
-		char.AddStatus(healingKey, 20*60, true)
+		w.AddStack(healingKey, 20*60)
 		return false
 	}, fmt.Sprintf("heartstrings-%v", char.Base.Key.String()))
 
-	char.AddStatMod(character.StatMod{
-		Base:         modifier.NewBase("heartstrings", -1),
-		AffectedStat: attributes.NoStat,
-		Amount: func() ([]float64, bool) {
-			count := 0
-			if char.StatusIsActive(skillKey) {
-				count++
+	// Burst CR buff if 3 stacks
+	mCR := make([]float64, attributes.EndStatType)
+	mCR[attributes.CR] = 0.21 + float64(r)*0.07
+	char.AddAttackMod(character.AttackMod{
+		Base: modifier.NewBase(burstCRKey, -1),
+		Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+			if atk.Info.AttackTag != attacks.AttackTagElementalBurst {
+				return nil, false
 			}
-			if char.StatusIsActive(bondKey) {
-				count++
+			if w.Stacks() < 3 && !char.StatusIsActive(burstCRKeyCancel) {
+				return nil, false
 			}
-			if char.StatusIsActive(healingKey) {
-				count++
-			}
-			maxhpbonus := stack * float64(count)
-			if count >= 3 {
-				maxhpbonus += max
-
-				// should this be here
-				// Burst CR buff if 3 stacks
-				mCR := make([]float64, attributes.EndStatType)
-				mCR[attributes.CR] = 0.21 + float64(r)*0.07
-				char.AddAttackMod(character.AttackMod{
-					Base: modifier.NewBase("heartstrings-cr", -1),
-					Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
-						if atk.Info.AttackTag != attacks.AttackTagElementalBurst {
-							return nil, false
-						}
-						return mCR, true
-					},
-				})
-			}
-			mHP[attributes.HPP] = maxhpbonus
-			return mHP, true
+			return mCR, true
 		},
 	})
+
 	return w, nil
+}
+
+func (w *Weapon) AddStack(name string, duration int) {
+	w.char.AddStatus(name, duration, true)
+	w.char.QueueCharTask(func() {
+		if !w.char.StatusIsActive(name) {
+			w.OnUpdateStack()
+		}
+	}, duration+1)
+	w.OnUpdateStack()
+}
+
+func (w *Weapon) Stacks() int {
+	count := 0
+	if w.char.StatusIsActive(skillKey) {
+		count++
+	}
+	if w.char.StatusIsActive(bondKey) {
+		count++
+	}
+	if w.char.StatusIsActive(healingKey) {
+		count++
+	}
+	return count
+}
+
+func (w *Weapon) OnUpdateStack() {
+	stacks := w.Stacks()
+	w.core.Log.NewEvent("heartstrings update stacks", glog.LogWeaponEvent, w.char.Index).
+		Write("stacks", stacks).
+		Write("bol-stack", w.char.StatusIsActive(bondKey)).
+		Write("skill-stack", w.char.StatusIsActive(skillKey)).
+		Write("heal-stack", w.char.StatusIsActive(healingKey))
+
+	if w.prevStacks == 3 && stacks < 3 {
+		// Elemental Burst CRIT Rate effect will be canceled 4s after falling under 3 stacks.
+		w.char.AddStatus(burstCRKeyCancel, 4*60, true)
+	}
+	w.prevStacks = stacks
 }
