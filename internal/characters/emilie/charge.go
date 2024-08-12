0f50f2f885a7993563d1ab6f4525ffaffdafa95b
imring <vitaliyvorobets25@gmail.com>
2024-08-12 18:47:16 +0300

    add charge

---
+++ internal/characters/emilie/charge.go
@@ -0,0 +1,54 @@
+package emilie
+
+import (
+	"github.com/genshinsim/gcsim/internal/frames"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attacks"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/geometry"
+)
+
+var chargeFrames []int
+
+const chargeHitmark = 16
+
+func init() {
+	chargeFrames = frames.InitAbilSlice(45)
+	chargeFrames[action.ActionSkill] = 38
+	chargeFrames[action.ActionBurst] = 37
+	chargeFrames[action.ActionDash] = chargeHitmark
+	chargeFrames[action.ActionJump] = chargeHitmark
+	chargeFrames[action.ActionSwap] = 43
+}
+
+func (c *char) ChargeAttack(p map[string]int) (action.Info, error) {
+	ai := combat.AttackInfo{
+		ActorIndex:         c.Index,
+		Abil:               "Charge",
+		AttackTag:          attacks.AttackTagExtra,
+		ICDTag:             attacks.ICDTagExtraAttack,
+		ICDGroup:           attacks.ICDGroupDefault,
+		StrikeType:         attacks.StrikeTypeSlash,
+		Element:            attributes.Physical,
+		Durability:         25,
+		Mult:               charge[c.TalentLvlAttack()],
+		HitlagHaltFrames:   0.09 * 60,
+		HitlagFactor:       0.01,
+		CanBeDefenseHalted: true,
+	}
+
+	c.Core.QueueAttack(
+		ai,
+		combat.NewBoxHitOnTarget(c.Core.Combat.PrimaryTarget(), geometry.Point{Y: -1.2}, 3.3, 3.5),
+		chargeHitmark,
+		chargeHitmark,
+	)
+
+	return action.Info{
+		Frames:          frames.NewAbilFunc(chargeFrames),
+		AnimationLength: chargeFrames[action.InvalidAction],
+		CanQueueAfter:   chargeHitmark,
+		State:           action.ChargeAttackState,
+	}, nil
+}
