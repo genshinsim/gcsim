d0c9b77861d653bc0e478759dd5131d07cc99a63
Charlie-Zheng <Charlie-Zheng@users.noreply.github.com>
2024-08-17 07:34:32 -0600

    Yaoyao a4 (#2190)
    * Fix yaoyao a4 not healing
    
    * Fixed yaoyao a4 src checking being wrong.
    
    * Uncommented src check
    
    ---------
    
    Co-authored-by: imring <vitaliyvorobets25@gmail.com>

--- internal/characters/yaoyao/asc.go
+++ internal/characters/yaoyao/asc.go
@@ -62,12 +62,13 @@ func (c *char) a1Throw() {
 
 func (c *char) a4(index, src int) func() {
 	return func() {
-		if c.a4Srcs[index] != c.Core.F {
+		// TODO: Check if reapplying radish will reset heal timer or not?
+		if c.a4Srcs[index] != src {
 			return
 		}
 
 		char := c.Core.Player.ByIndex(index)
-		if char.StatusIsActive(a4Status) {
+		if !char.StatusIsActive(a4Status) {
 			return
 		}
 
