fa2e11c4aec121a76cfff39792090b76ea96eb82
Charlie Zheng <charlie.x.3000@gmail.com>
2024-09-03 18:55:47 -0600

    Apply Shizuka's patch. Cleans up functions. Adds character check to event subscription

--- internal/artifacts/scrolloftheheroofcindercity/scrolloftheheroofcindercity.go
+++ internal/artifacts/scrolloftheheroofcindercity/scrolloftheheroofcindercity.go
@@ -2,252 +2,106 @@ package scrolloftheheroofcindercity
 
 import (
 	"fmt"
+	"slices"
 
 	"github.com/genshinsim/gcsim/internal/template/nightsoul"
 	"github.com/genshinsim/gcsim/pkg/core"
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
 	"github.com/genshinsim/gcsim/pkg/core/event"
-	"github.com/genshinsim/gcsim/pkg/core/glog"
 	"github.com/genshinsim/gcsim/pkg/core/info"
 	"github.com/genshinsim/gcsim/pkg/core/keys"
 	"github.com/genshinsim/gcsim/pkg/core/player/character"
 	"github.com/genshinsim/gcsim/pkg/core/reactions"
-	"github.com/genshinsim/gcsim/pkg/enemy"
+	"github.com/genshinsim/gcsim/pkg/core/targets"
 	"github.com/genshinsim/gcsim/pkg/modifier"
 )
 
-var reactionElementsArr map[reactions.ReactionType][]attributes.Element
-var elementToReactionsArr [][]reactions.ReactionType
-var buffArrs [][]float64
-var buffArrsNightsoul [][]float64
+var reactToElements = map[reactions.ReactionType][]attributes.Element{
+	reactions.Overload:           {attributes.Electro, attributes.Pyro},
+	reactions.Superconduct:       {attributes.Electro, attributes.Cryo},
+	reactions.Melt:               {attributes.Pyro, attributes.Cryo},
+	reactions.Vaporize:           {attributes.Pyro, attributes.Hydro},
+	reactions.Freeze:             {attributes.Cryo, attributes.Hydro},
+	reactions.ElectroCharged:     {attributes.Electro, attributes.Hydro},
+	reactions.SwirlHydro:         {attributes.Anemo, attributes.Hydro},
+	reactions.SwirlCryo:          {attributes.Anemo, attributes.Cryo},
+	reactions.SwirlElectro:       {attributes.Anemo, attributes.Electro},
+	reactions.SwirlPyro:          {attributes.Anemo, attributes.Pyro},
+	reactions.CrystallizeHydro:   {attributes.Geo, attributes.Hydro},
+	reactions.CrystallizeCryo:    {attributes.Geo, attributes.Cryo},
+	reactions.CrystallizeElectro: {attributes.Geo, attributes.Electro},
+	reactions.CrystallizePyro:    {attributes.Geo, attributes.Pyro},
+	reactions.Aggravate:          {attributes.Dendro, attributes.Electro},
+	reactions.Spread:             {attributes.Dendro},
+	reactions.Quicken:            {attributes.Dendro, attributes.Electro},
+	reactions.Bloom:              {attributes.Dendro, attributes.Hydro},
+	reactions.Hyperbloom:         {attributes.Dendro, attributes.Electro},
+	reactions.Burgeon:            {attributes.Dendro, attributes.Pyro},
+	reactions.Burning:            {attributes.Dendro, attributes.Pyro},
+}
 
 func init() {
-	reactionElementsArr = map[reactions.ReactionType][]attributes.Element{
-		reactions.Overload:           {attributes.Electro, attributes.Pyro},
-		reactions.Superconduct:       {attributes.Electro, attributes.Cryo},
-		reactions.Melt:               {attributes.Pyro, attributes.Cryo},
-		reactions.Vaporize:           {attributes.Pyro, attributes.Hydro},
-		reactions.Freeze:             {attributes.Cryo, attributes.Hydro},
-		reactions.ElectroCharged:     {attributes.Electro, attributes.Hydro},
-		reactions.SwirlHydro:         {attributes.Anemo, attributes.Hydro},
-		reactions.SwirlCryo:          {attributes.Anemo, attributes.Cryo},
-		reactions.SwirlElectro:       {attributes.Anemo, attributes.Electro},
-		reactions.SwirlPyro:          {attributes.Anemo, attributes.Pyro},
-		reactions.CrystallizeHydro:   {attributes.Geo, attributes.Hydro},
-		reactions.CrystallizeCryo:    {attributes.Geo, attributes.Cryo},
-		reactions.CrystallizeElectro: {attributes.Geo, attributes.Electro},
-		reactions.CrystallizePyro:    {attributes.Geo, attributes.Pyro},
-		reactions.Aggravate:          {attributes.Dendro, attributes.Electro},
-		reactions.Spread:             {attributes.Dendro},
-		reactions.Quicken:            {attributes.Dendro, attributes.Electro},
-		reactions.Bloom:              {attributes.Dendro, attributes.Hydro},
-		reactions.Hyperbloom:         {attributes.Dendro, attributes.Electro},
-		reactions.Burgeon:            {attributes.Dendro, attributes.Pyro},
-		reactions.Burning:            {attributes.Dendro, attributes.Pyro},
-	}
-
-	elementToReactionsArr = make([][]reactions.ReactionType, attributes.EndEleType)
-	for i, j := range reactionElementsArr {
-		for _, elem := range j {
-			elementToReactionsArr[elem] = append(elementToReactionsArr[elem], i)
-		}
-	}
-
-	buffArrs = make([][]float64, attributes.EndEleType)
-	for i := range buffArrs {
-		ele := attributes.Element(i)
-		buffArrs[ele] = make([]float64, attributes.EndStatType)
-		stat := attributes.EleToDmgP(ele)
-		if stat >= 0 {
-			buffArrs[ele][stat] = 0.12
-		}
-	}
-
-	buffArrsNightsoul = make([][]float64, attributes.EndEleType)
-	for i := range buffArrsNightsoul {
-		ele := attributes.Element(i)
-		buffArrsNightsoul[ele] = make([]float64, attributes.EndStatType)
-		stat := attributes.EleToDmgP(ele)
-		if stat >= 0 {
-			buffArrsNightsoul[ele][stat] = 0.28
-		}
-	}
-
 	core.RegisterSetFunc(keys.ScrollOfTheHeroOfCinderCity, NewSet)
 }
 
 type Set struct {
 	Index int
 	Count int
+
+	c    *core.Core
+	char *character.CharWrapper
+	buff []float64
 }
 
 func (s *Set) SetIndex(idx int) { s.Index = idx }
 func (s *Set) GetCount() int    { return s.Count }
+func (s *Set) Init() error      { return nil }
 
-func (s *Set) Init() error {
-	return nil
-}
-
-func reactionElements(r reactions.ReactionType) []attributes.Element {
-	return reactionElementsArr[r]
-}
-
-func elementToReactions(e attributes.Element) []reactions.ReactionType {
-	return elementToReactionsArr[e]
-}
-
-func reactionToEvent(r reactions.ReactionType) event.Event {
-	switch r {
-	case reactions.Overload:
-		return event.OnOverload
-	case reactions.Superconduct:
-		return event.OnSuperconduct
-	case reactions.Melt:
-		return event.OnMelt
-	case reactions.Vaporize:
-		return event.OnVaporize
-	case reactions.Freeze:
-		return event.OnFrozen
-	case reactions.ElectroCharged:
-		return event.OnElectroCharged
-	case reactions.SwirlHydro:
-		return event.OnSwirlHydro
-	case reactions.SwirlCryo:
-		return event.OnSwirlCryo
-	case reactions.SwirlElectro:
-		return event.OnSwirlElectro
-	case reactions.SwirlPyro:
-		return event.OnSwirlPyro
-	case reactions.CrystallizeHydro:
-		return event.OnCrystallizeHydro
-	case reactions.CrystallizeCryo:
-		return event.OnCrystallizeCryo
-	case reactions.CrystallizeElectro:
-		return event.OnCrystallizeElectro
-	case reactions.CrystallizePyro:
-		return event.OnCrystallizePyro
-	case reactions.Aggravate:
-		return event.OnAggravate
-	case reactions.Spread:
-		return event.OnSpread
-	case reactions.Quicken:
-		return event.OnQuicken
-	case reactions.Bloom:
-		return event.OnBloom
-	case reactions.Hyperbloom:
-		return event.OnHyperbloom
-	case reactions.Burgeon:
-		return event.OnBurgeon
-	case reactions.Burning:
-		return event.OnBurning
-	case reactions.Shatter:
-		return event.OnShatter
-	default:
-		return event.ReactionEventEndDelim
-	}
-}
-
-func reactionEventToReaction(e event.Event) reactions.ReactionType {
-	switch e {
-	case event.OnOverload:
-		return reactions.Overload
-	case event.OnSuperconduct:
-		return reactions.Superconduct
-	case event.OnMelt:
-		return reactions.Melt
-	case event.OnVaporize:
-		return reactions.Vaporize
-	case event.OnFrozen:
-		return reactions.Freeze
-	case event.OnElectroCharged:
-		return reactions.ElectroCharged
-	case event.OnSwirlHydro:
-		return reactions.SwirlHydro
-	case event.OnSwirlCryo:
-		return reactions.SwirlCryo
-	case event.OnSwirlElectro:
-		return reactions.SwirlElectro
-	case event.OnSwirlPyro:
-		return reactions.SwirlPyro
-	case event.OnCrystallizeHydro:
-		return reactions.CrystallizeHydro
-	case event.OnCrystallizeCryo:
-		return reactions.CrystallizeCryo
-	case event.OnCrystallizeElectro:
-		return reactions.CrystallizeElectro
-	case event.OnCrystallizePyro:
-		return reactions.CrystallizePyro
-	case event.OnAggravate:
-		return reactions.Aggravate
-	case event.OnSpread:
-		return reactions.Spread
-	case event.OnQuicken:
-		return reactions.Quicken
-	case event.OnBloom:
-		return reactions.Bloom
-	case event.OnHyperbloom:
-		return reactions.Hyperbloom
-	case event.OnBurgeon:
-		return reactions.Burgeon
-	case event.OnBurning:
-		return reactions.Burning
-	case event.OnShatter:
-		return reactions.Shatter
-	default:
-		return reactions.NoReaction
-	}
-}
-
-func Map[T, V any](ts []T, fn func(T) V) []V {
-	result := make([]V, len(ts))
-	for i, t := range ts {
-		result[i] = fn(t)
-	}
-	return result
-}
-
-func make4pcCB(c *core.Core, char *character.CharWrapper, react reactions.ReactionType) func(args ...interface{}) bool {
+func (s *Set) buffCB(react reactions.ReactionType, gadgetEmit bool) func(args ...interface{}) bool {
 	return func(args ...interface{}) bool {
-		_, ok := args[0].(*enemy.Enemy)
-
-		// Hyperbloom and Burgeon do not do enemy check
-		if !ok && react != reactions.Hyperbloom && react != reactions.Burgeon {
+		trg := args[0].(combat.Target)
+		if gadgetEmit && trg.Type() != targets.TargettableGadget {
 			return false
 		}
-		c.Log.NewEvent("scroll 4pc proc'd", glog.LogArtifactEvent, char.Index).
-			Write("react", react)
+		if !gadgetEmit && trg.Type() != targets.TargettableEnemy {
+			return false
+		}
 
-		for _, ele := range reactionElements(react) {
-			// Apply mod to all characters
-			ele := ele // make a copy
-			for _, ch := range c.Player.Chars() {
-				ch.AddStatMod(character.StatMod{
-					Base:         modifier.NewBaseWithHitlag(fmt.Sprintf("scroll-4pc-%s", attributes.ElementString[ele]), 15*60),
-					AffectedStat: attributes.EleToDmgP(ele),
+		ae := args[1].(*combat.AttackEvent)
+		if ae.Info.ActorIndex != s.char.Index {
+			return false
+		}
+
+		hasNightsoul := s.char.StatusIsActive(nightsoul.NightsoulBlessingStatus)
+		for _, other := range s.c.Player.Chars() {
+			elements := reactToElements[react]
+			if !slices.Contains(elements, other.Base.Element) {
+				continue
+			}
+			for _, ele := range elements {
+				stat := attributes.EleToDmgP(ele)
+
+				other.AddStatMod(character.StatMod{
+					Base:         modifier.NewBaseWithHitlag(fmt.Sprintf("scroll-4pc-%v", ele), 15*60),
+					AffectedStat: stat,
 					Amount: func() ([]float64, bool) {
-						return buffArrs[ele], true
+						clear(s.buff)
+						s.buff[stat] = 0.12
+						return s.buff, true
 					},
 				})
-			}
-		}
 
-		if char.CharZone != info.ZoneNatlan {
-			return false
-		}
-
-		if !char.StatusIsActive(nightsoul.NightsoulBlessingStatus) {
-			return false
-		}
-
-		for _, ele := range reactionElements(react) {
-			// Apply mod to all characters
-			ele := ele // make a copy
-			for _, ch := range c.Player.Chars() {
-				ch.AddStatMod(character.StatMod{
-					Base:         modifier.NewBaseWithHitlag(fmt.Sprintf("scroll-4pc-nightsoul-%s", attributes.ElementString[ele]), 20*60),
-					AffectedStat: attributes.EleToDmgP(ele),
+				if !hasNightsoul {
+					continue
+				}
+				other.AddStatMod(character.StatMod{
+					Base:         modifier.NewBaseWithHitlag(fmt.Sprintf("scroll-4pc-nightsoul-%v", ele), 20*60),
+					AffectedStat: stat,
 					Amount: func() ([]float64, bool) {
-						return buffArrsNightsoul[ele], true
+						clear(s.buff)
+						s.buff[stat] = 0.28
+						return s.buff, true
 					},
 				})
 			}
@@ -257,7 +111,12 @@ func make4pcCB(c *core.Core, char *character.CharWrapper, react reactions.Reacti
 }
 
 func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[string]int) (info.Set, error) {
-	s := Set{Count: count}
+	s := Set{
+		Count: count,
+		c:     c,
+		char:  char,
+		buff:  make([]float64, attributes.EndStatType),
+	}
 	// 2 Piece: When a nearby party member triggers a Nightsoul Burst, the equipping
 	// character regenerates 6 Elemental Energy.
 	if count >= 2 {
@@ -275,11 +134,35 @@ func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[stri
 	// equipping character can trigger this effect while off-field, and the DMG bonus
 	// from Artifact Sets with the same name do not stack.
 	if count >= 4 {
-		reactionList := elementToReactions(char.Base.Element)
-		eventList := Map(reactionList, reactionToEvent)
-		for _, evt := range eventList {
-			react := reactionEventToReaction(evt)
-			c.Combat.Events.Subscribe(evt, make4pcCB(c, char, react), fmt.Sprintf("scroll-4pc-%v-%v", react, char.Base.Key.String()))
+		for evt, react := range map[event.Event]reactions.ReactionType{
+			event.OnOverload:           reactions.Overload,
+			event.OnSuperconduct:       reactions.Superconduct,
+			event.OnMelt:               reactions.Melt,
+			event.OnVaporize:           reactions.Vaporize,
+			event.OnFrozen:             reactions.Freeze,
+			event.OnElectroCharged:     reactions.ElectroCharged,
+			event.OnSwirlHydro:         reactions.SwirlHydro,
+			event.OnSwirlCryo:          reactions.SwirlCryo,
+			event.OnSwirlElectro:       reactions.SwirlElectro,
+			event.OnSwirlPyro:          reactions.SwirlPyro,
+			event.OnCrystallizeHydro:   reactions.CrystallizeHydro,
+			event.OnCrystallizeCryo:    reactions.CrystallizeCryo,
+			event.OnCrystallizeElectro: reactions.CrystallizeElectro,
+			event.OnCrystallizePyro:    reactions.CrystallizePyro,
+			event.OnAggravate:          reactions.Aggravate,
+			event.OnSpread:             reactions.Spread,
+			event.OnQuicken:            reactions.Quicken,
+			event.OnBloom:              reactions.Bloom,
+			event.OnHyperbloom:         reactions.Hyperbloom,
+			event.OnBurgeon:            reactions.Burgeon,
+			event.OnBurning:            reactions.Burning,
+		} {
+			gadgetEmit := false
+			switch react {
+			case reactions.Burgeon, reactions.Hyperbloom:
+				gadgetEmit = true
+			}
+			c.Combat.Events.Subscribe(evt, s.buffCB(react, gadgetEmit), fmt.Sprintf("scroll-4pc-%v-%v", react, char.Base.Key.String()))
 		}
 	}
 
