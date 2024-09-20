81b7c1b9c1fb5d5b50d6bb5bc7ff855dcdaad712
Shizuka <shizuka.alpha@gmail.com>
2024-09-21 03:32:34 +0400

    Change stack gain/lose to occur only if buff is active
    - fix event names
    - remove enemy check from OnSkill

--- internal/weapons/catalyst/surfsup/surfsup.go
+++ internal/weapons/catalyst/surfsup/surfsup.go
@@ -16,6 +16,13 @@ import (
 	"github.com/genshinsim/gcsim/pkg/modifier"
 )
 
+const (
+	icdKey       = "surfs-up-icd"
+	buffKey      = "surfs-up-buff"
+	loseStackIcd = "surfs-up-stack-loss-icd"
+	gainStackIcd = "surfs-up-stack-gain-icd"
+)
+
 func init() {
 	core.RegisterWeaponFunc(keys.SurfsUp, NewWeapon)
 }
@@ -26,12 +33,6 @@ type Weapon struct {
 
 func (w *Weapon) SetIndex(idx int) { w.Index = idx }
 func (w *Weapon) Init() error      { return nil }
-
-const (
-	icdKey       = "surfs-up-icd"
-	loseStackIcd = "surfs-up-stack-loss-icd"
-	gainStackIcd = "surfs-up-stack-gain-icd"
-)
 
 // Max HP increased by 40%.
 // Once every 15s, for the 14s after using an Elemental Skill:
@@ -58,11 +59,8 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 	})
 
 	scorchingSummerStacks := 0
-
+	mNA := make([]float64, attributes.EndStatType)
 	c.Events.Subscribe(event.OnSkill, func(args ...interface{}) bool {
-		if _, ok := args[0].(*enemy.Enemy); !ok {
-			return false
-		}
 		if c.Player.Active() != char.Index {
 			return false
 		}
@@ -71,9 +69,9 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 		}
 		char.AddStatus(icdKey, 15*60, true)
 		scorchingSummerStacks = 4
-		mNA := make([]float64, attributes.EndStatType)
+
 		char.AddAttackMod(character.AttackMod{
-			Base: modifier.NewBaseWithHitlag("surfs-up-na-dmg", 14*60),
+			Base: modifier.NewBaseWithHitlag(buffKey, 14*60),
 			Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
 				if atk.Info.AttackTag == attacks.AttackTagNormal {
 					mNA[attributes.DmgP] = dmgPerStack * float64(scorchingSummerStacks)
@@ -84,49 +82,61 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 		})
 
 		return false
-	}, fmt.Sprintf("surfs-up-%v", char.Base.Key.String()))
+	}, fmt.Sprintf("surfs-up-skill-%v", char.Base.Key.String()))
 
 	c.Events.Subscribe(event.OnVaporize, func(args ...interface{}) bool {
-		atk := args[1].(*combat.AttackEvent)
 		if _, ok := args[0].(*enemy.Enemy); !ok {
 			return false
 		}
+
+		atk := args[1].(*combat.AttackEvent)
 		if atk.Info.ActorIndex != char.Index {
 			return false
 		}
 		if c.Player.Active() != char.Index {
+			return false
+		}
+
+		if !char.StatModIsActive(buffKey) {
 			return false
 		}
 		if char.StatusIsActive(gainStackIcd) {
 			return false
 		}
+
 		scorchingSummerStacks = min(4, scorchingSummerStacks+1)
 		c.Log.NewEvent("Surf's Up gained stack", glog.LogWeaponEvent, char.Index)
 		char.AddStatus(gainStackIcd, 1.5*60, true)
 
 		return false
-	}, fmt.Sprintf("surfs-up-%v", char.Base.Key.String()))
+	}, fmt.Sprintf("surfs-up-vape-%v", char.Base.Key.String()))
 
 	c.Events.Subscribe(event.OnEnemyDamage, func(args ...interface{}) bool {
-		atk := args[1].(*combat.AttackEvent)
 		if _, ok := args[0].(*enemy.Enemy); !ok {
 			return false
 		}
+
+		atk := args[1].(*combat.AttackEvent)
 		if atk.Info.ActorIndex != char.Index {
 			return false
 		}
 		if c.Player.Active() != char.Index {
 			return false
 		}
+
+		if !char.StatModIsActive(buffKey) {
+			return false
+		}
 		if char.StatusIsActive(loseStackIcd) {
 			return false
 		}
+
 		scorchingSummerStacks = max(0, scorchingSummerStacks-1)
 		c.Log.NewEvent("Surf's Up lost stack", glog.LogWeaponEvent, char.Index)
 		char.AddStatus(loseStackIcd, 1.5*60, true)
 
 		return false
-	}, fmt.Sprintf("surfs-up-%v", char.Base.Key.String()))
+	}, fmt.Sprintf("surfs-up-dmg-%v", char.Base.Key.String()))
 
 	return w, nil
 }
