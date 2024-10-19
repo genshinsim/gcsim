c33f4e398b55fc4b4477e12affde769bbf4864c3
Charlie Zheng <charlie.x.3000@gmail.com>
2024-10-18 21:17:47 -0600

    Added burst frames and offsets

--- internal/characters/xilonen/burst.go
+++ internal/characters/xilonen/burst.go
@@ -14,31 +14,34 @@ var burstFrames []int
 const burstStart = 96
 
 func init() {
-	burstFrames = frames.InitAbilSlice(93) // Q -> D/J
-	// burstFrames[action.ActionAttack] = 88  // Q -> N1
-	// burstFrames[action.ActionSkill] = 89   // Q -> E
-	// burstFrames[action.ActionSwap] = 88    // Q -> Swap
+	burstFrames = frames.InitAbilSlice(101) // Q -> W
+	burstFrames[action.ActionAttack] = 94   // Q -> N1
+	burstFrames[action.ActionSkill] = 94    // Q -> E
+	burstFrames[action.ActionDash] = 95     // Q -> D
+	burstFrames[action.ActionJump] = 94     // Q -> J
+	burstFrames[action.ActionSwap] = 92     // Q -> Swap
 }
 
 func (c *char) Burst(p map[string]int) (action.Info, error) {
 	ai := combat.AttackInfo{
-		ActorIndex: c.Index,
-		Abil:       "Ocelotlicue Point",
-		AttackTag:  attacks.AttackTagElementalBurst,
-		ICDTag:     attacks.ICDTagElementalBurst,
-		ICDGroup:   attacks.ICDGroupDefault,
-		StrikeType: attacks.StrikeTypeDefault,
-		Element:    attributes.Geo,
-		Durability: 25,
-		Mult:       burstDMG[c.TalentLvlBurst()],
-		UseDef:     true,
+		ActorIndex:     c.Index,
+		Abil:           "Ocelotlicue Point",
+		AttackTag:      attacks.AttackTagElementalBurst,
+		AdditionalTags: []attacks.AdditionalTag{attacks.AdditionalTagNightsoul},
+		ICDTag:         attacks.ICDTagElementalBurst,
+		ICDGroup:       attacks.ICDGroupDefault,
+		StrikeType:     attacks.StrikeTypeDefault,
+		Element:        attributes.Geo,
+		Durability:     25,
+		Mult:           burstDMG[c.TalentLvlBurst()],
+		UseDef:         true,
 	}
 
 	// initial hit at 15f after burst start
 	c.Core.QueueAttack(ai, combat.NewCircleHitOnTarget(c.Core.Combat.Player(), nil, 7), burstStart, burstStart)
 
 	if c.samplersConverted >= 2 {
-		for i := 0; i < 8; i++ {
+		for i := 1; i <= 8; i++ {
 			hpplus := c.Stat(attributes.Heal)
 			heal := burstHealBase[c.TalentLvlBurst()] + c.TotalDef()*burstHealPer[c.TalentLvlBurst()]
 			c.Core.Tasks.Add(func() {
@@ -49,17 +52,17 @@ func (c *char) Burst(p map[string]int) (action.Info, error) {
 					Src:     heal,
 					Bonus:   hpplus,
 				})
-			}, burstStart+i*90+8)
+			}, burstStart+int(88.5*float64(i))) // alternate between 88 and 89 frames
 		}
 	} else {
 		ai.Abil = "Ocelotlicue Point Ardent Rhythm"
 		for i := 1; i <= 2; i++ {
-			c.Core.QueueAttack(ai, combat.NewCircleHitOnTarget(c.Core.Combat.Player(), nil, 6), burstStart+i*60, burstStart+i*60)
+			c.Core.QueueAttack(ai, combat.NewCircleHitOnTarget(c.Core.Combat.Player(), nil, 6), burstStart+i*33, burstStart+i*33)
 		}
 	}
 
-	c.ConsumeEnergy(2)
-	c.SetCDWithDelay(action.ActionBurst, 15*60, 2)
+	c.ConsumeEnergy(14)
+	c.SetCDWithDelay(action.ActionBurst, 15*60, 0)
 
 	return action.Info{
 		Frames:          frames.NewAbilFunc(burstFrames),
