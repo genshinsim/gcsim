cd0557a41575c7d3ae8cfe3b54d029c5316851aa
srl <906239+srliao@users.noreply.github.com>
2024-10-26 13:40:14 -0400

    fix enka import re builds (#2261)
    * fix enka import re builds
    
    * refactor Item to behave with old data
    
    * put back "Imported"

--- ui/packages/ui/src/Stores/userDataSlice.ts
+++ ui/packages/ui/src/Stores/userDataSlice.ts
@@ -1,25 +1,33 @@
-import { Character } from '@gcsim/types';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {Character} from '@gcsim/types';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 export interface UserData {
-  GOODImport: { [key: string]: Character };
+  GOODImport: {[key: string]: Character};
 }
 
 const initialState: UserData = {
   GOODImport: {},
 };
 
+type ImportSource = 'enka' | 'good';
+
 export const userDataSlice = createSlice({
   name: 'user_data',
   initialState: initialState,
   reducers: {
-    loadFromGOOD: (state, action: PayloadAction<{ data: Character[] }>) => {
+    loadFromGOOD: (
+      state,
+      action: PayloadAction<{data: Character[]; source: ImportSource}>,
+    ) => {
       // if there are characters, do something
       if (action.payload.data.length > 0) {
         //make it
         state.GOODImport = {};
         action.payload.data.forEach((c) => {
-          state.GOODImport[c.name + (c.buildName??"")] = c;
+          c.source = action.payload.source;
+          //unique key should be name + source + optional enka build name
+          const key = `${c.name}-${c.source}-${c.enka_build_name ?? 'none'}`;
+          state.GOODImport[key] = c;
         });
       }
       return state;
@@ -30,5 +38,5 @@ export const userDataSlice = createSlice({
 export const userDataActions = userDataSlice.actions;
 
 export type UserDataSlice = {
-  [userDataSlice.name]: ReturnType<typeof userDataSlice['reducer']>;
+  [userDataSlice.name]: ReturnType<(typeof userDataSlice)['reducer']>;
 };
