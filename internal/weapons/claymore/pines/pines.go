2c1e2b29dc7ea72e42da239f9607fe982b403b4f
kengzzzz <ssiwat.keng@gmail.com>
2024-12-22 07:28:51 +0700

    fix song of broken pines atk speed

--- internal/weapons/claymore/pines/pines.go
+++ internal/weapons/claymore/pines/pines.go
@@ -87,10 +87,12 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 			stacks = 0
 			char.AddStatus(cdKey, cd, true)
 			for _, char := range c.Player.Chars() {
-				char.AddStatMod(character.StatMod{
+				char.AddAttackMod(character.AttackMod{
 					Base:         modifier.NewBaseWithHitlag("pines-proc", buffDuration),
-					AffectedStat: attributes.AtkSpd,
-					Amount: func() ([]float64, bool) {
+					Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+						if atk.Info.AttackTag != attacks.AttackTagNormal {
+							return nil, false
+						}
 						return uniqueVal, true
 					},
 				})
