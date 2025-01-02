5f47cb8038fa8ecc50e3fdb87eced5e206bf229f
Angelo <53081256+ange1o5@users.noreply.github.com>
2025-01-02 17:41:22 +0200

    Add Kinich (#2263)
    * added and tested fang of the mountain king
    
    * reverted unrelated changes.
    
    * changed shortcut; reverted another file.
    
    * changes which should in last commit; added icd tags; enemy checks; stacks duration.
    
    * modified passive: added stacktracker.
    
    * Add Kinich.
    
    * in process of debugging.
    
    * finished adding frames and testing.
    
    * fixed tests.
    
    * bugfixes pt. 1
    
    * bugfixes pt. 2
    
    * corrected max hold duration (only for hold scalespiker) and fixed tests
    .
    
    * bugfixes pt. 3
    
    * bugfixes pt. 4
    
    * small frame corrections.
    
    ---------
    
    Co-authored-by: imring <35897995+imring@users.noreply.github.com>

---
+++ internal/characters/kinich/charge.go
@@ -0,0 +1,52 @@
+package kinich
+
+import (
+	"fmt"
+
+	"github.com/genshinsim/gcsim/internal/frames"
+	"github.com/genshinsim/gcsim/pkg/core/action"
+	"github.com/genshinsim/gcsim/pkg/core/attacks"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/geometry"
+)
+
+var chargeFrames []int
+var chargeHitmarks = []int{71, 95, 119}
+
+func init() {
+	chargeFrames = frames.InitAbilSlice(87)
+}
+
+func (c *char) ChargeAttack(p map[string]int) (action.Info, error) {
+	ai := combat.AttackInfo{
+		ActorIndex:       c.Index,
+		AttackTag:        attacks.AttackTagExtra,
+		PoiseDMG:         40,
+		ICDTag:           attacks.ICDTagNormalAttack,
+		ICDGroup:         attacks.ICDGroupDefault,
+		StrikeType:       attacks.StrikeTypeBlunt,
+		Element:          attributes.Physical,
+		Durability:       25,
+		HitlagFactor:     0.01,
+		HitlagHaltFrames: 0.01 * 60,
+	}
+
+	for i := 0; i < 3; i++ {
+		ai.Mult = charge[c.TalentLvlAttack()]
+		ai.Abil = fmt.Sprintf("Charge %v", i)
+		c.Core.QueueAttack(
+			ai,
+			combat.NewBoxHitOnTarget(c.Core.Combat.Player().Pos(), geometry.Point{Y: 0.45}, 3.0, 5.0),
+			chargeHitmarks[i],
+			chargeHitmarks[i],
+		)
+	}
+
+	return action.Info{
+		Frames:          frames.NewAbilFunc(chargeFrames),
+		AnimationLength: chargeFrames[action.InvalidAction],
+		CanQueueAfter:   chargeHitmarks[len(chargeHitmarks)-1],
+		State:           action.ChargeAttackState,
+	}, nil
+}
---
+++ internal/characters/kinich/cons.go
@@ -0,0 +1,116 @@
+package kinich
+
+import (
+	"slices"
+
+	"github.com/genshinsim/gcsim/pkg/core/attacks"
+	"github.com/genshinsim/gcsim/pkg/core/attributes"
+	"github.com/genshinsim/gcsim/pkg/core/combat"
+	"github.com/genshinsim/gcsim/pkg/core/glog"
+	"github.com/genshinsim/gcsim/pkg/core/player/character"
+	"github.com/genshinsim/gcsim/pkg/enemy"
+	"github.com/genshinsim/gcsim/pkg/modifier"
+)
+
+const (
+	c4IcdKey = "kinich-c4-icd-key"
+	c6Abil   = "Scalespiker Cannon (C6)"
+)
+
+func (c *char) c1() {
+	if c.Base.Cons < 1 {
+		return
+	}
+	// "After Kinich lands from Canopy Hunter: Riding High's mid-air swing,
+	// his Movement SPD will increase by 30% for 6s." is not implemented
+	m := make([]float64, attributes.EndStatType)
+	c.AddAttackMod(character.AttackMod{
+		Base: modifier.NewBase("kinich-c1", -1),
+		Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+			switch atk.Info.AttackTag {
+			case attacks.AttackTagElementalArt, attacks.AttackTagElementalArtHold:
+			default:
+				return nil, false
+			}
+			if !slices.Contains(atk.Info.AdditionalTags, attacks.AdditionalTagKinichCannon) {
+				return nil, false
+			}
+
+			m[attributes.CD] = 1
+			return m, true
+		},
+	})
+}
+
+func (c *char) c2ResShredCB(a combat.AttackCB) {
+	if c.Base.Cons < 2 {
+		return
+	}
+	if a.Damage == 0 {
+		return
+	}
+
+	e, ok := a.Target.(*enemy.Enemy)
+	if !ok {
+		return
+	}
+	e.AddResistMod(combat.ResistMod{
+		Base:  modifier.NewBaseWithHitlag("kinich-c2", 6*60),
+		Ele:   attributes.Dendro,
+		Value: -0.3,
+	})
+}
+
+func (c *char) c2Bonus(ai *combat.AttackInfo) (combat.Snapshot, float64) {
+	s := c.Snapshot(ai)
+	if c.Base.Cons < 2 {
+		return s, 3.0
+	}
+	if c.c2AoeIncreased {
+		return s, 3.0
+	}
+	c.c2AoeIncreased = true
+	s.Stats[attributes.DmgP] += 1.0
+	c.Core.Log.NewEvent("Kinich C2 Damage Bonus", glog.LogCharacterEvent, c.Index).
+		Write("final", s.Stats[attributes.DmgP])
+	return s, 5.0
+}
+
+func (c *char) c4() {
+	if c.Base.Cons < 4 {
+		return
+	}
+	if c.StatusIsActive(c4IcdKey) {
+		return
+	}
+	c.AddStatus(c4IcdKey, 2.8*60, true)
+	c.AddEnergy("kinich-c4", 5)
+
+	m := make([]float64, attributes.EndStatType)
+	c.AddAttackMod(character.AttackMod{
+		Base: modifier.NewBase("kinich-c4-dmgp", -1),
+		Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+			if atk.Info.AttackTag != attacks.AttackTagElementalBurst {
+				return nil, false
+			}
+			m[attributes.DmgP] = 0.7
+			return m, true
+		},
+	})
+}
+
+func (c *char) c6(ai combat.AttackInfo, s *combat.Snapshot, radius float64, target combat.Target, travel int) {
+	if c.Base.Cons < 6 {
+		return
+	}
+	ai.Abil = c6Abil
+	ai.Mult = 7
+	var next combat.Target = c.Core.Combat.RandomEnemyWithinArea(combat.NewCircleHitOnTarget(target, nil, radius), func(t combat.Enemy) bool {
+		return target.Key() != t.Key()
+	})
+	if next == nil {
+		next = target
+	}
+	ap := combat.NewCircleHitOnTarget(next, nil, radius)
+	c.Core.QueueAttackWithSnap(ai, *s, ap, travel, c.particleCB, c.a1CB, c.c2ResShredCB)
+}
