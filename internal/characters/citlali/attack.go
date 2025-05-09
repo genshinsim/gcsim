97771bc1a014111a264a064936c87938b4126ef8
Charlie Zheng <charlie.x.3000@gmail.com>
2025-05-09 07:50:07 -0600

    Revert changes to attack.go in bugfixes pt. 3 (b90a8bd3d106b3a53a018ec7559b808550eeb883)

--- internal/characters/citlali/attack.go
+++ internal/characters/citlali/attack.go
@@ -13,9 +13,10 @@ import (
 const normalHitNum = 3
 
 var (
-	attackFrames   [][]int
-	attackHitmarks = []int{16, 16, 36}
-	attackRadius   = []float64{0.75, 0.75, 0.75}
+	attackFrames       [][]int
+	attackHitmarks     = []int{16, 16, 36}
+	attackRadius       = 0.8
+	attackHitlagFactor = 0.05
 )
 
 func init() {
@@ -24,29 +25,14 @@ func init() {
 	attackFrames[0] = frames.InitNormalCancelSlice(attackHitmarks[0], 38) // N1 -> Walk
 	attackFrames[0][action.ActionAttack] = 34
 	attackFrames[0][action.ActionCharge] = 33
-	attackFrames[0][action.ActionSkill] = attackHitmarks[0]
-	attackFrames[0][action.ActionBurst] = attackHitmarks[0]
-	attackFrames[0][action.ActionDash] = attackHitmarks[0]
-	attackFrames[0][action.ActionJump] = attackHitmarks[0]
-	attackFrames[0][action.ActionSwap] = attackHitmarks[0]
 
 	attackFrames[1] = frames.InitNormalCancelSlice(attackHitmarks[1], 39) // N2 -> Walk
 	attackFrames[1][action.ActionAttack] = 37
 	attackFrames[1][action.ActionCharge] = 37
-	attackFrames[1][action.ActionSkill] = attackHitmarks[1]
-	attackFrames[1][action.ActionBurst] = attackHitmarks[1]
-	attackFrames[1][action.ActionDash] = attackHitmarks[1]
-	attackFrames[1][action.ActionJump] = attackHitmarks[1]
-	attackFrames[1][action.ActionSwap] = attackHitmarks[1]
 
-	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2], 52) // N3 -> N1
-	attackFrames[2][action.ActionAttack] = 50
-	attackFrames[2][action.ActionCharge] = 49
-	attackFrames[2][action.ActionSkill] = attackHitmarks[2]
-	attackFrames[2][action.ActionBurst] = attackHitmarks[2]
-	attackFrames[2][action.ActionDash] = attackHitmarks[2]
-	attackFrames[2][action.ActionJump] = attackHitmarks[2]
-	attackFrames[2][action.ActionSwap] = attackHitmarks[2]
+	attackFrames[2] = frames.InitNormalCancelSlice(attackHitmarks[2], 52) // N3 -> Walk
+	attackFrames[2][action.ActionAttack] = 49
+	attackFrames[2][action.ActionCharge] = 50
 }
 
 func (c *char) Attack(p map[string]int) (action.Info, error) {
@@ -56,21 +42,23 @@ func (c *char) Attack(p map[string]int) (action.Info, error) {
 	}
 
 	ai := combat.AttackInfo{
-		ActorIndex: c.Index,
-		Abil:       fmt.Sprintf("Normal %v", c.NormalCounter),
-		AttackTag:  attacks.AttackTagNormal,
-		ICDTag:     attacks.ICDTagNormalAttack,
-		ICDGroup:   attacks.ICDGroupDefault,
-		StrikeType: attacks.StrikeTypeDefault,
-		Element:    attributes.Cryo,
-		Durability: 25,
-		Mult:       attack[c.NormalCounter][c.TalentLvlAttack()],
+		ActorIndex:   c.Index,
+		Abil:         fmt.Sprintf("Normal %v", c.NormalCounter),
+		AttackTag:    attacks.AttackTagNormal,
+		ICDTag:       attacks.ICDTagNormalAttack,
+		ICDGroup:     attacks.ICDGroupDefault,
+		StrikeType:   attacks.StrikeTypeDefault,
+		Element:      attributes.Cryo,
+		Durability:   25,
+		Mult:         attack[c.NormalCounter][c.TalentLvlAttack()],
+		HitlagFactor: attackHitlagFactor,
 	}
 
-	ap := combat.NewCircleHitOnTarget(
+	ap := combat.NewCircleHit(
+		c.Core.Combat.Player(),
 		c.Core.Combat.PrimaryTarget(),
 		nil,
-		attackRadius[c.NormalCounter],
+		attackRadius,
 	)
 
 	c.Core.QueueAttack(
@@ -85,7 +73,7 @@ func (c *char) Attack(p map[string]int) (action.Info, error) {
 	return action.Info{
 		Frames:          frames.NewAttackFunc(c.Character, attackFrames),
 		AnimationLength: attackFrames[c.NormalCounter][action.InvalidAction],
-		CanQueueAfter:   attackFrames[c.NormalCounter][action.ActionDash],
+		CanQueueAfter:   attackHitmarks[c.NormalCounter],
 		State:           action.NormalAttackState,
 	}, nil
 }
