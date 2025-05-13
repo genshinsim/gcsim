434cab567992fec5219535504391a88c7f180888
imring <35897995+imring@users.noreply.github.com>
2025-05-13 19:16:39 +0300

    add i18n.ts

---
+++ ui/packages/utils/src/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+import { resources } from "@gcsim/localization";
+
+// used in result tab for graph y axis text direction/offset handling
+export { specialLocales } from "@gcsim/localization";
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources,
+    defaultNS: "translation",
+    fallbackLng: "en",
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export { i18n };
