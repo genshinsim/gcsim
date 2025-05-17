d25218c17d83e249ccb9aefa243dd5608eb4fc2e
Liu Yihua <yihuajack@live.cn>
2025-05-17 22:17:27 +0800

    deep-galleries fix lint

--- internal/artifacts/finaleofthedeepgalleries/finaleofthedeepgalleries.go
+++ internal/artifacts/finaleofthedeepgalleries/finaleofthedeepgalleries.go
@@ -40,67 +40,78 @@ func NewSet(c *core.Core, char *character.CharWrapper, count int, param map[stri
 		char:  char,
 	}
 
-	if count >= 2 {
-		m := make([]float64, attributes.EndStatType)
-		m[attributes.CryoP] = 0.15
-		char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBase("deep-galleries-2pc", -1),
-			AffectedStat: attributes.CryoP,
-			Amount: func() ([]float64, bool) {
-				return m, true
-			},
-		})
-	}
-
-	if count >= 4 {
-		procDurNormal := 360 // 6s * 60
-		procDurBurst := 360  // 6s * 60
-
-		m := make([]float64, attributes.EndStatType)
-		m[attributes.DmgP] = 0.6
-
-		char.AddAttackMod(character.AttackMod{
-			Base: modifier.NewBase("deep-galleries-4pc", -1),
-			Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
-				if char.Energy != 0 {
-					return nil, false
-				}
-				if atk.Info.AttackTag != attacks.AttackTagNormal && atk.Info.AttackTag != attacks.AttackTagElementalBurst {
-					return nil, false
-				}
-				if atk.Info.AttackTag == attacks.AttackTagNormal && c.F < s.procNormalExpireF {
-					return nil, false
-				}
-				if atk.Info.AttackTag == attacks.AttackTagElementalBurst && c.F < s.procBurstExpireF {
-					return nil, false
-				}
-				return m, true
-			},
-		})
-
-		c.Events.Subscribe(event.OnEnemyDamage, func(args ...interface{}) bool {
-			// If attack does not belong to the equipped character then ignore
-			atk := args[1].(*combat.AttackEvent)
-			if atk.Info.ActorIndex != char.Index {
-				return false
-			}
-			// If this is not a normal attack then ignore
-			if atk.Info.AttackTag != attacks.AttackTagNormal && atk.Info.AttackTag != attacks.AttackTagElementalBurst {
-				return false
-			}
-
-			if atk.Info.AttackTag == attacks.AttackTagNormal {
-				s.procBurstExpireF = c.F + procDurBurst
-				c.Log.NewEvent("deep galleries 4pc stop playing", glog.LogArtifactEvent, char.Index).
-					Write("burst_buff_stop_expiry", s.procBurstExpireF)
-			} else {
-				s.procNormalExpireF = c.F + procDurNormal
-				c.Log.NewEvent("deep galleries 4pc stop playing", glog.LogArtifactEvent, char.Index).
-					Write("normal_buff_stop_expiry", s.procNormalExpireF)
-			}
-			return false
-		}, fmt.Sprintf("deep-galleries-4pc-%v", char.Base.Key.String()))
-	}
+	s.pc2()
+	s.pc4()
 
 	return &s, nil
+}
+
+func (s *Set) pc2() {
+	if s.Count < 2 {
+		return
+	}
+
+	m := make([]float64, attributes.EndStatType)
+	m[attributes.CryoP] = 0.15
+	s.char.AddStatMod(character.StatMod{
+		Base:         modifier.NewBase("deep-galleries-2pc", -1),
+		AffectedStat: attributes.CryoP,
+		Amount: func() ([]float64, bool) {
+			return m, true
+		},
+	})
+}
+
+func (s *Set) pc4() {
+	if s.Count < 4 {
+		return
+	}
+
+	procDurNormal := 360 // 6s * 60
+	procDurBurst := 360  // 6s * 60
+
+	m := make([]float64, attributes.EndStatType)
+	m[attributes.DmgP] = 0.6
+
+	s.char.AddAttackMod(character.AttackMod{
+		Base: modifier.NewBase("deep-galleries-4pc", -1),
+		Amount: func(atk *combat.AttackEvent, t combat.Target) ([]float64, bool) {
+			if s.char.Energy != 0 {
+				return nil, false
+			}
+			if atk.Info.AttackTag != attacks.AttackTagNormal && atk.Info.AttackTag != attacks.AttackTagElementalBurst {
+				return nil, false
+			}
+			if atk.Info.AttackTag == attacks.AttackTagNormal && s.c.F < s.procNormalExpireF {
+				return nil, false
+			}
+			if atk.Info.AttackTag == attacks.AttackTagElementalBurst && s.c.F < s.procBurstExpireF {
+				return nil, false
+			}
+			return m, true
+		},
+	})
+
+	s.c.Events.Subscribe(event.OnEnemyDamage, func(args ...interface{}) bool {
+		// If attack does not belong to the equipped character then ignore
+		atk := args[1].(*combat.AttackEvent)
+		if atk.Info.ActorIndex != s.char.Index {
+			return false
+		}
+		// If this is not a normal attack then ignore
+		if atk.Info.AttackTag != attacks.AttackTagNormal && atk.Info.AttackTag != attacks.AttackTagElementalBurst {
+			return false
+		}
+
+		if atk.Info.AttackTag == attacks.AttackTagNormal {
+			s.procBurstExpireF = s.c.F + procDurBurst
+			s.c.Log.NewEvent("deep galleries 4pc stop playing", glog.LogArtifactEvent, s.char.Index).
+				Write("burst_buff_stop_expiry", s.procBurstExpireF)
+		} else {
+			s.procNormalExpireF = s.c.F + procDurNormal
+			s.c.Log.NewEvent("deep galleries 4pc stop playing", glog.LogArtifactEvent, s.char.Index).
+				Write("normal_buff_stop_expiry", s.procNormalExpireF)
+		}
+		return false
+	}, fmt.Sprintf("deep-galleries-4pc-%v", s.char.Base.Key.String()))
 }
