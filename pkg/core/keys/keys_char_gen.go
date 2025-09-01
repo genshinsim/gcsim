e69e2dfe1a6b68493d3367feb3067bd20d5323e9
Vitaliy Vorobets <35897995+imring@users.noreply.github.com>
2025-09-01 22:53:31 +0300

    add varesa (#2371)
    * add varesa
    
    * add shortcut
    
    * fix varesa q short condition
    
    * fix lint
    
    * fix fiery charged hitmark and lint
    
    * multiple fixes
    - update cancel frames
    - fix c1
    - remove nightsoul state after changing the state
    
    * update varesa hitmarks/ns duration. complete varesa c4
    
    * update mode-gcsim
    
    * fix ns exit and hitmark point generation
    
    * bugfixes
    
    * varesa fixes
    
    * update attack infos & hitboxes
    
    * update docs
    
    * fix raiden resolve stacks for varesa
    
    * Updated substat optimizer to use OnEnergyBurst to calculated needed ER
    
    * Added xianyun plunge
    
    * Added docs for varesa plunge frames
    
    ---------
    
    Co-authored-by: Charlie Zheng <charlie.x.3000@gmail.com>

--- pkg/core/keys/keys_char_gen.go
+++ pkg/core/keys/keys_char_gen.go
@@ -86,6 +86,7 @@ const (
 	Tartaglia
 	Thoma
 	Tighnari
+	Varesa
 	Venti
 	Wanderer
 	Wriothesley
@@ -421,6 +422,10 @@ func init() {
 	charNames[Tighnari] = "tighnari"
 	charPrettyName[Tighnari] = "Tighnari"
 	CharKeyToEle[Tighnari] = attributes.Dendro
+
+	charNames[Varesa] = "varesa"
+	charPrettyName[Varesa] = "Varesa"
+	CharKeyToEle[Varesa] = attributes.Electro
 
 	charNames[Venti] = "venti"
 	charPrettyName[Venti] = "Venti"
