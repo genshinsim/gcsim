d6e5bd6141e5afe16c5ccfb3d22af22025f1d818
Matoba <martinbabala04@gmail.com>
2025-12-12 13:11:43 +0100

    improved readability of the lunarbloom functions

--- pkg/reactable/lunarbloom.go
+++ pkg/reactable/lunarbloom.go
@@ -11,42 +11,39 @@ const (
 	verdantDewStartFrame = 13
 	verdantDewEndFrame   = 140
 	nextDewFrameRefresh  = 150
+	LbKey                = "lunar-bloom"
 	verdantDewKey        = "verdant-dew"
 	nextDewFrameKey      = "verdant-dew-next"
-	lastLBTickKey        = "last-lb-frame"
 )
 
 func (r *Reactable) lunarBloom() {
 	if r.core.Flags.Custom[verdantDewKey] >= maxVerdantDew {
 		return
 	}
-	r.core.Tasks.Add(func() {
-		r.addDew(verdantDewEndFrame - verdantDewStartFrame)
-		r.core.Log.NewEvent("verdant dew generation extended", glog.LogElementEvent, -1).
-			Write("nextDewFrame", r.core.Flags.Custom[nextDewFrameKey])
-	}, verdantDewStartFrame)
+
+	if r.core.Status.Duration(LbKey) == 0 {
+		r.core.Tasks.Add(r.addDew(), verdantDewStartFrame)
+	}
+
+	r.core.Status.Add(LcKey, verdantDewEndFrame)
 }
 
-func (r *Reactable) addDew(amount int) {
-	if r.core.Flags.Custom[verdantDewKey] >= maxVerdantDew {
-		return
-	}
-	currentFrame := r.core.F
-	r.core.Flags.Custom[nextDewFrameKey] -= float64(min(amount, currentFrame-int(r.core.Flags.Custom[lastLBTickKey])))
-	if r.core.Flags.Custom[nextDewFrameKey] <= 0 {
-		r.core.Tasks.Add(func() {
-			r.core.Flags.Custom[verdantDewKey]++
-			if r.core.Flags.Custom[verdantDewKey] >= maxVerdantDew {
-				r.core.Flags.Custom[nextDewFrameKey] = nextDewFrameRefresh
-			}
+func (r *Reactable) addDew() func() {
+	return func() {
+		if r.core.Flags.Custom[verdantDewKey] >= maxVerdantDew {
+			return
+		}
+
+		r.core.Flags.Custom[nextDewFrameKey] -= 1
+
+		if r.core.Flags.Custom[nextDewFrameKey] <= 0 {
+			r.core.Flags.Custom[verdantDewKey] += 1
 			r.core.Log.NewEvent(fmt.Sprintf("lunar bloom dew gained: %v/%v", r.core.Flags.Custom[verdantDewKey], maxVerdantDew), glog.LogElementEvent, -1)
-		}, amount+int(r.core.Flags.Custom[nextDewFrameKey]))
-		r.core.Flags.Custom[nextDewFrameKey] += nextDewFrameRefresh
-	}
+		}
 
-	r.core.Flags.Custom[lastLBTickKey] = float64(currentFrame)
-
-	if r.core.Flags.Custom[nextDewFrameKey] < 0 {
-		r.core.Flags.Custom[nextDewFrameKey] = nextDewFrameRefresh
+		if r.core.Flags.Custom[nextDewFrameKey] < 0 {
+			r.core.Flags.Custom[nextDewFrameKey] = nextDewFrameRefresh
+		}
+		r.core.Tasks.Add(r.addDew(), 1)
 	}
 }
