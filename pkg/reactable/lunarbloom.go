931fac47e192e59e275d13fc1caf90b59623e0bf
Matoba004 <martinbabala04@gmail.com>
2026-01-07 21:46:28 +0100

    resolved issues

--- pkg/reactable/lunarbloom.go
+++ pkg/reactable/lunarbloom.go
@@ -12,12 +12,16 @@ const (
 	verdantDewEndFrame   = 140
 	nextDewFrameRefresh  = 150
 	LbKey                = "lunar-bloom"
-	verdantDewKey        = "verdant-dew"
+	VerdantDewKey        = "verdant-dew"
 	nextDewFrameKey      = "verdant-dew-next"
 )
 
+func (r *Reactable) GetVerdantDew() int {
+	return int(r.core.Flags.Custom[VerdantDewKey])
+}
+
 func (r *Reactable) lunarBloom() {
-	if r.core.Flags.Custom[verdantDewKey] >= maxVerdantDew {
+	if r.core.Flags.Custom[VerdantDewKey] >= maxVerdantDew {
 		return
 	}
 
@@ -30,7 +34,7 @@ func (r *Reactable) lunarBloom() {
 
 func (r *Reactable) addDew() func() {
 	return func() {
-		if r.core.Flags.Custom[verdantDewKey] >= maxVerdantDew {
+		if r.core.Flags.Custom[VerdantDewKey] >= maxVerdantDew {
 			return
 		}
 
@@ -38,12 +42,12 @@ func (r *Reactable) addDew() func() {
 			return
 		}
 
-		r.core.Flags.Custom[nextDewFrameKey] -= 1
+		r.core.Flags.Custom[nextDewFrameKey] += 1
 
-		if r.core.Flags.Custom[nextDewFrameKey] <= 0 {
-			r.core.Flags.Custom[verdantDewKey] += 1
-			r.core.Log.NewEvent(fmt.Sprintf("lunar bloom dew gained: %v/%v", r.core.Flags.Custom[verdantDewKey], maxVerdantDew), glog.LogElementEvent, -1)
-			r.core.Flags.Custom[nextDewFrameKey] = nextDewFrameRefresh
+		if r.core.Flags.Custom[nextDewFrameKey] >= nextDewFrameRefresh {
+			r.core.Flags.Custom[VerdantDewKey] += 1
+			r.core.Log.NewEvent(fmt.Sprintf("lunar bloom dew gained: %v/%v", r.core.Flags.Custom[VerdantDewKey], maxVerdantDew), glog.LogElementEvent, -1)
+			r.core.Flags.Custom[nextDewFrameKey] = 0
 		}
 
 		r.core.Tasks.Add(r.addDew(), 1)
