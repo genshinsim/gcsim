a770e360b859a8baead1f78f849c3b5e9c03ba74
Charlie Zheng <charlie.x.3000@gmail.com>
2026-04-18 11:04:06 -0600

    Adjusted aubade to use a status to show when buff will expire. Moved most initialization to Init() instead of NewSet

--- internal/artifacts/aubade/aubade.go
+++ internal/artifacts/aubade/aubade.go
@@ -22,21 +22,72 @@ type Set struct {
 	lastSwap int
 	core     *core.Core
 	char     *character.CharWrapper
-	buff     float64
 	Index    int
 	Count    int
 }
 
+const aubade4pcKey = "aubade-4pc"
+
 func (s *Set) SetIndex(idx int) { s.Index = idx }
 func (s *Set) GetCount() int    { return s.Count }
+
 func (s *Set) Init() error {
-	m := 0.2
+	// NewSet is called before all characters added, so we don't initialize buffs there due to MoonsignLevel being incorrect
+	// Init gets called after all characters added, so MoonsignLevel is correct.
+
+	if s.Count < 2 {
+		return nil
+	}
+
+	m := make([]float64, attributes.EndStatType)
+	m[attributes.EM] = 80
+	s.char.AddStatMod(character.StatMod{
+		Base:         modifier.NewBase("aubade-2pc", -1),
+		AffectedStat: attributes.EM,
+		Amount: func() []float64 {
+			return m
+		},
+	})
+
+	if s.Count < 4 {
+		return nil
+	}
+
+	buff := 0.2
 	if s.core.Player.GetMoonsignLevel() >= 2 {
-		m += 0.4
+		buff += 0.4
 	}
-	if s.Count >= 4 && s.core.Player.Active() != s.char.Index() {
-		s.gainBuff(m)
+
+	if s.core.Player.Active() != s.char.Index() {
+		s.gainBuff()
 	}
+
+	s.core.Events.Subscribe(event.OnCharacterSwap, func(args ...any) {
+		prev := args[0].(int)
+		next := args[1].(int)
+		if prev == s.char.Index() {
+			s.lastSwap = -1
+			s.gainBuff()
+		} else if next == s.char.Index() {
+			s.lastSwap = s.core.F
+			s.char.AddStatus(aubade4pcKey, 3*60, false)
+			s.core.Tasks.Add(s.clearBuff(s.core.F), 3*60)
+		}
+	}, fmt.Sprintf("aubade-4pc-%v", s.char.Base.Key.String()))
+
+	s.char.AddReactBonusMod(character.ReactBonusMod{
+		Base: modifier.NewBase("aubade-4pc-dmg", -1),
+		Amount: func(ai info.AttackInfo) float64 {
+			if !s.char.StatusIsActive(aubade4pcKey) {
+				return 0
+			}
+			if attacks.LunarReactionStartDelim < ai.AttackTag && ai.AttackTag < attacks.DirectLunarReactionEndDelim {
+				return buff
+			}
+			return 0
+		},
+	})
+
 	return nil
 }
 
@@ -47,51 +98,11 @@ func NewSet(core *core.Core, char *character.CharWrapper, count int, param map[s
 		lastSwap: -1,
 		Count:    count,
 	}
-	if count >= 2 {
-		m := make([]float64, attributes.EndStatType)
-		m[attributes.EM] = 80
-		char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBase("aubade-2pc", -1),
-			AffectedStat: attributes.EM,
-			Amount: func() []float64 {
-				return m
-			},
-		})
-	}
-	if count >= 4 {
-		m := 0.2
-		if core.Player.GetMoonsignLevel() >= 2 {
-			m += 0.4
-		}
-
-		core.Events.Subscribe(event.OnCharacterSwap, func(args ...any) {
-			prev := args[0].(int)
-			next := args[1].(int)
-			if prev == char.Index() {
-				s.lastSwap = -1
-				s.gainBuff(m)
-			} else if next == char.Index() {
-				s.lastSwap = core.F
-				core.Tasks.Add(s.clearBuff(core.F), 3*60)
-			}
-		}, fmt.Sprintf("aubade-4pc-%v", char.Base.Key.String()))
-
-		char.AddReactBonusMod(character.ReactBonusMod{
-			Base: modifier.NewBase("aubade-4pc", -1),
-			Amount: func(ai info.AttackInfo) float64 {
-				if ai.AttackTag > attacks.LunarReactionStartDelim && ai.AttackTag < attacks.DirectLunarReactionEndDelim {
-					return s.buff
-				}
-				return 0
-			},
-		})
-	}
-
 	return &s, nil
 }
 
-func (s *Set) gainBuff(m float64) {
-	s.buff = m
+func (s *Set) gainBuff() {
+	s.char.AddStatus(aubade4pcKey, -1, false)
 	s.core.Log.NewEvent("aubade of morningstar and moon 4pc proc'd", glog.LogArtifactEvent, s.char.Index())
 }
 
@@ -104,7 +115,6 @@ func (s *Set) clearBuff(src int) func() {
 			return
 		}
 
-		s.buff = 0
 		s.core.Log.NewEvent("aubade of morningstar and moon 4pc lost", glog.LogArtifactEvent, s.char.Index())
 	}
 }
