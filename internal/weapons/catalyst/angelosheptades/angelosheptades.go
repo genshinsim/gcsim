48d01105475340f12e57bd643874071ac51138d8
Matoba004 <martinbabala04@gmail.com>
2026-06-13 10:05:03 +0200

    fixed issues

--- internal/weapons/catalyst/angelosheptades/angelosheptades.go
+++ internal/weapons/catalyst/angelosheptades/angelosheptades.go
@@ -6,6 +6,7 @@ import (
 	"github.com/genshinsim/gcsim/pkg/core"
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
 	"github.com/genshinsim/gcsim/pkg/core/event"
+	"github.com/genshinsim/gcsim/pkg/core/glog"
 	"github.com/genshinsim/gcsim/pkg/core/info"
 	"github.com/genshinsim/gcsim/pkg/core/keys"
 	"github.com/genshinsim/gcsim/pkg/core/player/character"
@@ -23,18 +24,26 @@ func init() {
 }
 
 type Weapon struct {
-	Index int
+	Index   int
+	BuffSrc int
+	BuffAmt float64
+	Refine  int
 }
 
 func (w *Weapon) SetIndex(idx int) { w.Index = idx }
 func (w *Weapon) Init() error      { return nil }
 
 func NewWeapon(core *core.Core, char *character.CharWrapper, p info.WeaponProfile) (info.Weapon, error) {
-	w := &Weapon{}
+	w := &Weapon{
+		BuffSrc: -1,
+		BuffAmt: 0,
+	}
 	r := p.Refine
 
 	m := make([]float64, attributes.EndStatType)
 	m[attributes.ATK] = 0.09 + float64(r)*0.03
+
+	n := make([]float64, attributes.EndStatType)
 
 	char.AddStatMod(character.StatMod{
 		Base:         modifier.NewBase("angelos-heptades-atk", -1),
@@ -50,21 +59,25 @@ func NewWeapon(core *core.Core, char *character.CharWrapper, p info.WeaponProfil
 			return
 		}
 		// TODO: Not sure if the character needs to be on the field
-		if core.Player.Active() != char.Index() {
-			return
-		}
+		// if core.Player.Active() != char.Index() {
+		// 	return
+		// }
 
-		buff := 0.07 + float64(r)*0.03
-		buffCap := 0.18 + float64(r)*0.08
+		w.BuffSrc = core.F
+
+		w.updateBuff(char, core, r, w.BuffSrc)
 
 		for _, c := range core.Player.Chars() {
-			c.AddStatMod(character.StatMod{
-				Base:         modifier.NewBase("pathfinders-light", 20*60),
-				AffectedStat: attributes.DmgP,
-				Amount: func() []float64 {
-					n := make([]float64, attributes.EndStatType)
-					n[attributes.DmgP] = min(char.TotalAtk()/1000.0*buff, buffCap)
+			c.AddAttackMod(character.AttackMod{
+				Base: modifier.NewBaseWithHitlag("pathfinders-light", 20*60),
+				Amount: func(atk *info.AttackEvent, t info.Target) []float64 {
+					n[attributes.DmgP] = w.BuffAmt
 					if c.Index() != core.Player.Active() {
+						if c.IsHexerei {
+							n[attributes.DmgP] *= 0.5
+							return n
+						}
+
 						return nil
 					}
 
@@ -79,8 +92,32 @@ func NewWeapon(core *core.Core, char *character.CharWrapper, p info.WeaponProfil
 
 		energyAmount := 13.0 + float64(r)
 
-		char.AddStatus(angelosHeptadesEnergyICDKey, 14*60, false)
+		char.AddStatus(angelosHeptadesEnergyICDKey, 14*60, true)
 		char.AddEnergy(angelosHeptadesEnergyKey, energyAmount)
 	}, fmt.Sprintf("angelos-heptades-shielded-%v", char.Base.Key.String()))
 	return w, nil
+}
+
+func (w *Weapon) updateBuff(char *character.CharWrapper, core *core.Core, r, src int) {
+	if w.BuffSrc != src {
+		return
+	}
+
+	buff := 0.07 + float64(r)*0.03
+	buffCap := 0.18 + float64(r)*0.08
+	w.BuffAmt = min(char.NonExtraStat(attributes.ATK)/1000.0*buff, buffCap)
+
+	if core.F-w.BuffSrc >= 20*60 {
+		w.BuffAmt = 0
+		return
+	}
+
+	core.Log.NewEvent("angelos heptades buff updated", glog.LogWeaponEvent, char.Index()).
+		Write("src", w.BuffSrc).
+		Write("value", w.BuffAmt).
+		Write("holder-atk", char.NonExtraStat(attributes.ATK))
+
+	core.Tasks.Add(func() {
+		w.updateBuff(char, core, r, src)
+	}, 0.5*60)
 }
