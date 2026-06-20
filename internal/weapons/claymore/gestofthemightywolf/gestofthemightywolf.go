bf785f633d1ced57b6ab6a7dcd3169d5d42c91e9
Matoba004 <martinbabala04@gmail.com>
2026-06-20 18:34:20 +0200

    fixed hexerei stacks logic

--- internal/weapons/claymore/gestofthemightywolf/gestofthemightywolf.go
+++ internal/weapons/claymore/gestofthemightywolf/gestofthemightywolf.go
@@ -17,17 +17,16 @@ func init() {
 }
 
 type Weapon struct {
-	Index     int
-	char      *character.CharWrapper
-	refine    int
-	stacks    int
-	hexStacks int
+	Index  int
+	char   *character.CharWrapper
+	refine int
+	stacks int
+	mod    []float64
 }
 
 const (
 	gestAttackSpeedKey = "gest-of-the-mighty-wolf-atkspd"
 	gestStacksKey      = "gest-of-the-mighty-wolf-stacks"
-	gestHexStacksKey   = "gest-of-the-mighty-wolf-stacks-hexerei"
 )
 
 func (w *Weapon) SetIndex(idx int) { w.Index = idx }
@@ -37,8 +36,8 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 	w := &Weapon{
 		char:   char,
 		refine: p.Refine,
+		mod:    make([]float64, attributes.EndStatType),
 	}
-	r := p.Refine
 
 	m := make([]float64, attributes.EndStatType)
 	m[attributes.AtkSpd] = 0.1
@@ -50,69 +49,40 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 		},
 	})
 
-	m2 := make([]float64, attributes.EndStatType)
-	char.AddStatMod(character.StatMod{
-		Base:         modifier.NewBase(gestHexStacksKey, -1),
-		AffectedStat: attributes.CD,
-		Amount: func() []float64 {
-			m2[attributes.CD] = (0.055 + 0.02*float64(r)) * float64(w.hexStacks)
-
-			return m2
-		},
-	})
-
-	m3 := make([]float64, attributes.EndStatType)
-
 	c.Events.Subscribe(event.OnEnemyDamage, func(args ...any) {
-		w.addStacks(1, m3)
-		w.addHexStacks(1)
+		w.addStacks(1)
 	}, fmt.Sprintf("gest-of-the-mighty-wolf-on-normal-attack-%v", char.Base.Key.String()))
 
 	c.Events.Subscribe(event.OnChargeAttack, func(args ...any) {
-		w.addStacks(2, m3)
-		w.addHexStacks(2)
+		w.addStacks(2)
 	}, fmt.Sprintf("gest-of-the-mighty-wolf-on-charge-%v", char.Base.Key.String()))
 
 	c.Events.Subscribe(event.OnSkill, func(args ...any) {
-		w.addStacks(2, m3)
-		w.addHexStacks(2)
+		w.addStacks(2)
 	}, fmt.Sprintf("gest-of-the-mighty-wolf-on-skill-%v", char.Base.Key.String()))
 
 	return w, nil
 }
 
-func (w *Weapon) addStacks(amt int, m []float64) {
+func (w *Weapon) addStacks(amt int) {
 	if !w.char.StatModIsActive(gestStacksKey) {
 		w.stacks = 0
 	}
 
 	w.char.AddStatMod(character.StatMod{
-		Base:         modifier.NewBase(gestStacksKey, 4*60),
-		AffectedStat: attributes.DmgP,
+		Base: modifier.NewBase(gestStacksKey, 4*60),
 		Amount: func() []float64 {
-			m[attributes.DmgP] = (0.055 + 0.02*float64(w.refine)) * float64(w.stacks)
+			w.mod[attributes.DmgP] = (0.055 + 0.02*float64(w.refine)) * float64(w.stacks)
 
-			return m
+			if !w.char.IsHexerei {
+				return w.mod
+			}
+
+			w.mod[attributes.CD] = (0.055 + 0.02*float64(w.refine)) * float64(w.stacks)
+
+			return w.mod
 		},
 	})
 
 	w.stacks = min(4, w.stacks+amt)
-}
-
-func (w *Weapon) addHexStacks(amt int) {
-	addedAmt := min(4-w.hexStacks, amt)
-
-	if addedAmt == 0 {
-		return
-	}
-
-	w.hexStacks += addedAmt
-
-	w.char.QueueCharTask(w.removeHexStacks(addedAmt), 4*60)
-}
-
-func (w *Weapon) removeHexStacks(amt int) func() {
-	return func() {
-		w.hexStacks -= amt
-	}
 }
