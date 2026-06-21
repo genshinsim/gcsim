1fff462d5443bd788c32bf855eef35fd6b3c5519
Charlie Zheng <charlie.x.3000@gmail.com>
2026-06-21 15:44:18 -0600

    Fix bugs in implementation

--- internal/artifacts/celestialgift/celestialgift.go
+++ internal/artifacts/celestialgift/celestialgift.go
@@ -13,10 +13,11 @@ import (
 )
 
 const (
-	duration      = 60 * 20
-	set4Key       = "celestialgift-4pc"
-	set4KeyHolder = set4Key + "-holder"
-	set4KeyActive = set4Key + "-active"
+	duration          = 60 * 20
+	set4Key           = "celestialgift-4pc"
+	set4KeyHolder     = set4Key + "-holder"
+	set4KeyActive     = set4Key + "-active"
+	set4KeyActiveBuff = set4Key + "-active-buff"
 )
 
 func init() {
@@ -35,140 +36,140 @@ func (s *Set) SetIndex(idx int) { s.Index = idx }
 func (s *Set) GetCount() int    { return s.Count }
 func (s *Set) Init() error {
 	count := s.Count
-	c := s.core
-	char := s.char
+	core := s.core
 
-	if count < 4 || !char.IsHexerei {
+	if count < 4 || !s.char.IsHexerei {
 		return nil
 	}
 
-	holderBuffActive := func() bool {
-		for _, c := range c.Player.Chars() {
-			if c.StatusIsActive(set4KeyHolder) {
-				return true
-			}
-		}
-		return false
-	}
-
-	holderBuffDuration := func() int {
-		x := 0
-		for _, c := range c.Player.Chars() {
-			x = max(x, c.StatusDuration(set4KeyHolder))
-		}
-		return x
-	}
-
 	holderBuffIsActiveOfElement := func(e attributes.Element) bool {
-		for _, c := range c.Player.Chars() {
-			if c.StatusIsActive(set4KeyHolder) && c.Base.Element == e {
+		for _, char := range core.Player.Chars() {
+			if char.StatusIsActive(set4KeyHolder) && char.Base.Element == e {
 				return true
 			}
 		}
 		return false
 	}
 
-	holderBuffIsActiveOfElementNotFrom := func(e attributes.Element, i int) bool {
-		for _, x := range c.Player.Chars() {
-			if x.StatusIsActive(set4KeyHolder) && x.Base.Element == e && x.Index() != i {
+	// returns false if there is another party member of the same element with the 4pc buff active,
+	// otherwise returns true
+	shouldRefreshElemBuff := func() bool {
+		selfEle := s.char.Base.Element
+		selfIndex := s.char.Index()
+		for _, char := range core.Player.Chars() {
+			if char.StatusIsActive(set4KeyHolder) && char.Base.Element == selfEle && char.Index() != selfIndex {
+				return false
+			}
+		}
+		return true
+	}
+
+	// active char status is active if Hexerei Secret Rite is active and there is a party member with the 4pc buff
+	// don't need to check Secret Rite because the active char stat mod isn't added when not Secret Rite
+	activeStatusIsActive := func() bool {
+		for _, char := range core.Player.Chars() {
+			if char.StatusIsActive(set4KeyActive) {
 				return true
 			}
 		}
 		return false
 	}
 
-	activeBuffIsActive := func() bool {
-		for _, x := range c.Player.Chars() {
-			if x.StatusIsActive(set4KeyActive) {
-				return true
-			}
+	activeStatusDuration := func() int {
+		dur := 0
+		for _, char := range core.Player.Chars() {
+			dur = max(dur, char.StatusDuration(set4KeyActive))
 		}
-		return false
+		return dur
 	}
 
-	clearActiveBuff := func() {
-		for _, x := range c.Player.Chars() {
-			x.DeleteStatus(set4KeyActive)
-		}
-	}
-
-	refreshActiveBuff := func() {
-		for _, x := range c.Player.Chars() {
-			x.AddStatus(set4KeyActive, duration, true)
+	deleteActiveBuff := func() {
+		for _, char := range core.Player.Chars() {
+			char.DeleteStatus(set4KeyActiveBuff)
 		}
 	}
 
 	elems := map[attributes.Element]bool{}
-	for _, x := range c.Player.Chars() {
-		elems[x.Base.Element] = true
+	for _, char := range core.Player.Chars() {
+		elems[char.Base.Element] = true
 	}
 
 	buffStrength := 0.2
-	isHexereiSecretRite := c.Player.GetHexereiCount() >= 2
+	isHexereiSecretRite := core.Player.GetHexereiCount() >= 2
 	if isHexereiSecretRite {
 		buffStrength = 0.4
 	}
 
-	c.Events.Subscribe(event.OnSkill, func(args ...any) {
+	core.Events.Subscribe(event.OnSkill, func(args ...any) {
 		// NOTE: Description says "The equipping character can trigger this effect while off-field."
 		// but currently no characters can use skills while off field, so this is ignored
-		if c.Player.Active() != char.Index() {
+		if core.Player.Active() != s.char.Index() {
 			return
 		}
-		refreshActiveBuff()
-		elem := char.Base.Element
-		if holderBuffIsActiveOfElementNotFrom(elem, char.Index()) {
+
+		// we always refresh the duration of the active char buff
+		s.char.AddStatus(set4KeyActive, duration, true)
+
+		elem := s.char.Base.Element
+		if !shouldRefreshElemBuff() {
 			return
 		}
-		char.AddStatus(set4KeyHolder, duration, true)
+
+		s.char.AddStatus(set4KeyHolder, duration, true)
 		buffedDmgP := attributes.EleToDmgP(elem)
-		for _, x := range c.Player.Chars() {
-			x.AddStatMod(character.StatMod{
-				Base:         modifier.NewBase(set4KeyHolder+"-"+elem.String(), -1),
+		for _, otherChar := range core.Player.Chars() {
+			otherChar.AddStatMod(character.StatMod{
+				Base:         modifier.NewBaseWithHitlag(set4KeyHolder+"-"+elem.String(), duration),
 				AffectedStat: buffedDmgP,
 				Amount: func() []float64 {
 					clear(s.buff)
 					s.buff[buffedDmgP] = buffStrength
-					if holderBuffIsActiveOfElement(elem) {
-						return s.buff
-					}
-					return nil
+					return s.buff
 				},
 			})
 		}
-	}, fmt.Sprintf(set4KeyHolder+"-%v", char.Base.Key.String()))
+		// need to remove the active char buff because the holder is currently on field
+		// and is now applying a buff of their own element
+		deleteActiveBuff()
+	}, fmt.Sprintf(set4KeyHolder+"-%v", s.char.Base.Key.String()))
 
 	if !isHexereiSecretRite {
 		return nil
 	}
 
-	c.Events.Subscribe(event.OnCharacterSwap, func(args ...any) {
-		clearActiveBuff()
-		remainingDuration := holderBuffDuration()
+	core.Events.Subscribe(event.OnCharacterSwap, func(args ...any) {
+		deleteActiveBuff()
+		remainingDuration := activeStatusDuration()
 		if remainingDuration == 0 {
 			return
 		}
-		elem := c.Player.ActiveChar().Base.Element
+		elem := core.Player.ActiveChar().Base.Element
+
+		// The active char buff doesn't stack with the holder buff
+		// Even if the holder buff expires, the active char buff isn't reapplied until
+		// the next swap
 		if holderBuffIsActiveOfElement(elem) {
 			return
 		}
-		char.AddStatus(set4KeyActive, remainingDuration, true)
+
 		buffedDmgP := attributes.EleToDmgP(elem)
-		for _, x := range c.Player.Chars() {
-			x.AddStatMod(character.StatMod{
-				Base:         modifier.NewBase(set4KeyHolder+"-"+elem.String()+"-active", -1),
+		for _, otherChar := range core.Player.Chars() {
+			// This stat mod needs hitlag on both the buff reciever, and the holder
+			// in order for the stat mod to actually be extended
+			otherChar.AddStatMod(character.StatMod{
+				Base:         modifier.NewBaseWithHitlag(set4KeyActiveBuff, remainingDuration),
 				AffectedStat: buffedDmgP,
 				Amount: func() []float64 {
 					clear(s.buff)
 					s.buff[buffedDmgP] = buffStrength
-					if activeBuffIsActive() && holderBuffActive() {
+					if activeStatusIsActive() {
 						return s.buff
 					}
 					return nil
 				},
 			})
 		}
-	}, fmt.Sprintf(set4KeyActive+"-%v", char.Base.Key.String()))
+	}, set4KeyActive) // OnSwap subscription is same for every holder, so we allow it to be overwritten
 	return nil
 }
 
