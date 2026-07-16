b8a59c10adfd925fee7d4636a37e7165df5edcfa
Matoba004 <martinbabala04@gmail.com>
2026-07-16 18:29:47 +0200

    fixed issues

--- internal/characters/albedo/albedo.go
+++ internal/characters/albedo/albedo.go
@@ -20,7 +20,7 @@ type char struct {
 	c2stacks int
 }
 
-func NewChar(s *core.Core, w *character.CharWrapper, p info.CharacterProfile) error {
+func NewChar(s *core.Core, w *character.CharWrapper, _ info.CharacterProfile) error {
 	c := char{}
 	c.Character = tmpl.NewWithWrapper(s, w)
 
