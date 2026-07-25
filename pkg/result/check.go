d314bb29176a952ee32ad428f980b475e2980f0a
Charlie Zheng <charlie.x.3000@gmail.com>
2026-07-24 21:27:28 -0600

    Add Varka to incomplete chars

--- pkg/result/check.go
+++ pkg/result/check.go
@@ -9,6 +9,7 @@ import (
 // adding a character to this list will make the "incomplete warning" appear on the viewer
 var incompleteCharacters = []keys.Char{
 	keys.InvalidChar,
+	keys.Varka,
 }
 
 func IsCharacterComplete(char keys.Char) bool {
