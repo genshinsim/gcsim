5ec01bdb4cec25bc94334c9e119402b046aab28e
Shizuka <shizuka.alpha@gmail.com>
2026-07-29 22:40:58 +0400

    Remove hardcoded names in GOOD import

--- ui/packages/ui/src/Pages/Simulator/Components/GOOD/GOODToSrl.functions.ts
+++ ui/packages/ui/src/Pages/Simulator/Components/GOOD/GOODToSrl.functions.ts
@@ -184,29 +184,6 @@ export function GOODChartoSrlChar(
 }
 
 export function GOODKeytoGCSIMKey(goodKey: GOODArtifactSetKey | GOODCharacterKey | GOODWeaponKey) {
-  switch (goodKey) {
-    case 'KaedeharaKazuha':
-      return 'kazuha';
-    case 'KamisatoAyaka':
-      return 'ayaka';
-    case 'KamisatoAyato':
-      return 'ayato';
-    case 'KujouSara':
-      return 'sara';
-    case 'RaidenShogun':
-      return 'raiden';
-    case 'SangonomiyaKokomi':
-      return 'kokomi';
-    case 'YaeMiko':
-      return 'yaemiko';
-    case 'AratakiItto':
-      return 'itto';
-    case 'ShikanoinHeizou':
-      return 'heizou';
-    case 'KukiShinobu':
-      return 'kuki';
-  }
-
   const result = goodKey
     .toString()
     .replace(/[^0-9a-z]/gi, "")
