06740a35aaa3219a7e4d3058cec47de5086477d2
Matoba004 <martinbabala04@gmail.com>
2026-08-15 19:18:58 +0200

    initial commit
    2nd commit
    
    fixed other characters triggering passives
    
    fixed on action event crashing the sim
    
    fix 3
    
    fix

--- pkg/core/keys/weapon.dm.go
+++ pkg/core/keys/weapon.dm.go
@@ -77,6 +77,7 @@ const (
 	CashflowSupervision                           // cashflowsupervision
 	ChainBreaker                                  // chainbreaker
 	CinnabarSpindle                               // cinnabarspindle
+	ClashOfKings                                  // clashofkings
 	Cloudforged                                   // cloudforged
 	CompoundBow                                   // compoundbow
 	CoolSteel                                     // coolsteel
@@ -323,6 +324,7 @@ var _WeaponNames = [...]string{
 	"cashflowsupervision",
 	"chainbreaker",
 	"cinnabarspindle",
+	"clashofkings",
 	"cloudforged",
 	"compoundbow",
 	"coolsteel",
@@ -569,6 +571,7 @@ var _WeaponValues = [...]Weapon{
 	CashflowSupervision,
 	ChainBreaker,
 	CinnabarSpindle,
+	ClashOfKings,
 	Cloudforged,
 	CompoundBow,
 	CoolSteel,
