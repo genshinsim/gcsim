e93c1177eb608028da1993afcb63d5b16f14cd24
Charlie Zheng <charlie.x.3000@gmail.com>
2026-09-04 21:03:39 -0600

    Add stat buffs to Traveler from the True Moon archon quest

--- internal/characters/traveler/common/storybuffs.go
+++ internal/characters/traveler/common/storybuffs.go
@@ -7,6 +7,8 @@ import (
 	"github.com/genshinsim/gcsim/pkg/modifier"
 )
 
+const TrueMoonParam = "true_moon_story_buff"
+
 func TravelerStoryBuffs(c *character.CharWrapper, p info.CharacterProfile) {
 	// TravelerStoryBuffs applies buffs based on completed story quests
 	//
@@ -16,15 +18,21 @@ func TravelerStoryBuffs(c *character.CharWrapper, p info.CharacterProfile) {
 	// skirk_story_buff
 	// 		buffs from completing Crystallina Chapter: Act I (Skirk's Story Quest)
 	// 		+7 base atk, +15 EM, +50 base HP
-	//
+	// true_moon_buff
+	//		buffs from completing Song of the Welkin Moon: Act VIII - True Moon (Nod Krai Archon Quest)
+	//		we assume traveler has all resonances done
 	// All buffs default to enabled
 	baseAtkBuff, okBaseAtkBuff := p.Params["base_atk_buff"]
 	skirkBuff, okSkirkBuff := p.Params["skirk_story_buff"]
+	trueMoonBuff, okTrueMoonBuff := p.Params[TrueMoonParam]
 	if !okBaseAtkBuff {
 		baseAtkBuff = 1
 	}
 	if !okSkirkBuff {
-		skirkBuff = 1 // default to maximum buffs
+		skirkBuff = 1
+	}
+	if !okTrueMoonBuff {
+		trueMoonBuff = 1
 	}
 
 	m := make([]float64, attributes.EndStatType)
@@ -36,6 +44,17 @@ func TravelerStoryBuffs(c *character.CharWrapper, p info.CharacterProfile) {
 		m[attributes.EM] += 15
 		m[attributes.BaseHP] += 50
 	}
+
+	if trueMoonBuff == 1 {
+		m[attributes.CR] += 0.1
+		m[attributes.DEFP] += 0.2
+		m[attributes.ER] += 0.2
+		m[attributes.EM] += 60
+		m[attributes.HPP] += 0.2
+		m[attributes.ATKP] += 0.2
+		m[attributes.CD] += 0.2
+	}
+
 	c.AddStatMod(character.StatMod{
 		Base: modifier.NewBase("traveler-story-quest-buffs", -1),
 		Amount: func() []float64 {
@@ -43,3 +62,5 @@ func TravelerStoryBuffs(c *character.CharWrapper, p info.CharacterProfile) {
 		},
 	})
 }
+
+// the CA buffs must be added in the specific traveler
