d4dc22279e2c9e643d2c9fb3c5c738b173e88b73
Charlie Zheng <charlie.x.3000@gmail.com>
2026-09-15 18:33:55 -0600

    Fix shortcut typo

--- pkg/shortcut/artifact.dm.go
+++ pkg/shortcut/artifact.dm.go
@@ -67,7 +67,7 @@ var SetNameToKey = map[string]keys.Set{
 	"heartofdepth":                       keys.HeartOfDepth,
 	"hod":                                keys.HeartOfDepth,
 	"heartofthefurnace":                  keys.HeartOfTheFurnace,
-	"furnance":                           keys.HeartOfTheFurnace,
+	"furnace":                            keys.HeartOfTheFurnace,
 	"huskofopulentdreams":                keys.HuskOfOpulentDreams,
 	"hood":                               keys.HuskOfOpulentDreams,
 	"husk":                               keys.HuskOfOpulentDreams,
