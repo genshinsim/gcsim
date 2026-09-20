b82c0a47f5af7074834699006347bac04db8c163
Charlie Zheng <charlie.x.3000@gmail.com>
2026-09-20 14:36:52 -0600

    Fix review and nits

--- internal/weapons/claymore/forgedbythegoldenmelody/forgedbythegoldenmelody.go
+++ internal/weapons/claymore/forgedbythegoldenmelody/forgedbythegoldenmelody.go
@@ -1,8 +1,6 @@
 package forgedbythegoldenmelody
 
 import (
-	"fmt"
-
 	"github.com/genshinsim/gcsim/pkg/core"
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
 	"github.com/genshinsim/gcsim/pkg/core/event"
@@ -12,13 +10,12 @@ import (
 )
 
 const (
-	atkBuffKey           = "forged-by-the-golden-melody-atk"
-	emBuffKey            = "forged-by-the-golden-melody-em"
-	stellarBuffKey       = "forged-by-the-golden-melody-stellar"
-	atkBuffContraKey     = "forged-by-the-golden-melody-contrapuntal-atk"
-	emBuffContraKey      = "forged-by-the-golden-melody-contrapuntal-em"
-	stellarBuffContraKey = "forged-by-the-golden-melody-contrapuntal-stellar"
-	contraICDKey         = "forged-by-the-golden-melody-contrapuntal-icd"
+	weaponKey     = "golden-melody"
+	atkBuffSuffix = "-atk"
+	emBuffSuffix  = "-em"
+	stellarSuffix = "-stellar"
+
+	contraICDKey = "golden-melody-contrapuntal-icd"
 )
 
 type Weapon struct {
@@ -31,10 +28,7 @@ type Weapon struct {
 }
 
 func (w *Weapon) SetIndex(idx int) { w.Index = idx }
-func (w *Weapon) Init() error {
-	w.switchBuff()
-	return nil
-}
+func (w *Weapon) Init() error      { return nil }
 
 func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) (info.Weapon, error) {
 	w := &Weapon{
@@ -42,12 +36,7 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 	}
 	r := p.Refine
 
-	if p.Params["buff_type"] != 0 {
-		buffType := p.Params["buff_type"]
-		w.curBuff = (buffType - 1) % 3
-	} else {
-		w.curBuff = 2
-	}
+	w.curBuff = p.Params["buff_type"] % 3
 
 	w.atkBuff = make([]float64, attributes.EndStatType)
 	w.atkBuff[attributes.ATKP] = 0.135 + float64(r)*0.045
@@ -57,60 +46,46 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 
 	w.stellarBuff = 0.21 + float64(r)*0.07
 
-	onStellar := func(args ...any) {
-		if char.StatusIsActive(contraICDKey) {
-			return
-		}
+	w.applyBuff(false, 10*60)
+	w.char.QueueCharTask(w.switchBuff, 10*60)
 
+	onStellar := func(args ...any) {
 		atk := args[1].(*info.AttackEvent)
 		if atk.Info.ActorIndex != char.Index() {
 			return
 		}
 
-		switch w.curBuff {
-		case 0:
-			char.AddStatMod(character.StatMod{
-				Base:         modifier.NewBaseWithHitlag(atkBuffContraKey, 12*60),
-				AffectedStat: attributes.ATKP,
-				Amount: func() []float64 {
-					return w.atkBuff
-				},
-			})
-		case 1:
-			char.AddStatMod(character.StatMod{
-				Base:         modifier.NewBaseWithHitlag(emBuffContraKey, 12*60),
-				AffectedStat: attributes.EM,
-				Amount: func() []float64 {
-					return w.emBuff
-				},
-			})
-		case 2:
-			w.char.AddReactBonusMod(character.ReactBonusMod{
-				Base: modifier.NewBaseWithHitlag(stellarBuffContraKey, 12*60),
-				Amount: func(ai info.AttackInfo) float64 {
-					if ai.AttackTag.IsStellar() {
-						return w.stellarBuff
-					}
-					return 0
-				},
-			})
+		if char.StatusIsActive(contraICDKey) {
+			return
 		}
 
 		char.AddStatus(contraICDKey, 12*60, true)
+
+		w.applyBuff(true, 12*60)
 	}
 
-	c.Events.Subscribe(event.OnStellarConduct, onStellar, fmt.Sprintf("forged-by-the-golden-melody-on-stellar-conduct-%v", char.Base.Key.String()))
-	c.Events.Subscribe(event.OnStellarSwirl, onStellar, fmt.Sprintf("forged-by-the-golden-melody-on-stellar-swirl-%v", char.Base.Key.String()))
+	c.Events.Subscribe(event.OnStellarConduct, onStellar, "forged-by-the-golden-melody-on-stellar-conduct-"+char.Base.Key.String())
+	c.Events.Subscribe(event.OnStellarSwirl, onStellar, "forged-by-the-golden-melody-on-stellar-swirl-"+char.Base.Key.String())
 
 	return w, nil
 }
 
 func (w *Weapon) switchBuff() {
 	w.curBuff = (w.curBuff + 1) % 3
+	w.applyBuff(false, 10*60)
+	w.char.QueueCharTask(w.switchBuff, 10*60)
+}
+
+func (w *Weapon) applyBuff(contrapuntal bool, dur int) {
+	middle := ""
+	if contrapuntal {
+		middle = "-contrapuntal"
+	}
+
 	switch w.curBuff {
 	case 1:
 		w.char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBaseWithHitlag(emBuffKey, 10*60),
+			Base:         modifier.NewBaseWithHitlag(weaponKey+middle+emBuffSuffix, dur),
 			AffectedStat: attributes.EM,
 			Amount: func() []float64 {
 				return w.emBuff
@@ -118,7 +93,7 @@ func (w *Weapon) switchBuff() {
 		})
 	case 2:
 		w.char.AddReactBonusMod(character.ReactBonusMod{
-			Base: modifier.NewBaseWithHitlag(stellarBuffKey, 10*60),
+			Base: modifier.NewBaseWithHitlag(weaponKey+middle+stellarSuffix, dur),
 			Amount: func(ai info.AttackInfo) float64 {
 				if ai.AttackTag.IsStellar() {
 					return w.stellarBuff
@@ -128,12 +103,11 @@ func (w *Weapon) switchBuff() {
 		})
 	default:
 		w.char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBaseWithHitlag(atkBuffKey, 10*60),
+			Base:         modifier.NewBaseWithHitlag(weaponKey+middle+atkBuffSuffix, dur),
 			AffectedStat: attributes.ATKP,
 			Amount: func() []float64 {
 				return w.atkBuff
 			},
 		})
 	}
-	w.char.QueueCharTask(w.switchBuff, 10*60)
 }
