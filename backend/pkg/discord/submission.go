b673aa802c705217284daa1d53cc7f8947add0d1
srliao <906239+srliao@users.noreply.github.com>
2026-09-22 09:14:22 -0400

    db archival notice

--- backend/pkg/discord/submission.go
+++ backend/pkg/discord/submission.go
@@ -2,14 +2,10 @@ package discord
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/diamondburned/arikawa/v3/api"
 	"github.com/diamondburned/arikawa/v3/api/cmdroute"
 	"github.com/diamondburned/arikawa/v3/discord"
-	"github.com/diamondburned/arikawa/v3/utils/json/option"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func init() {
@@ -32,47 +28,11 @@ func init() {
 }
 
 func (b *Bot) cmdSubmit(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
-	var opts struct {
-		Link string `discord:"link"`
-		Desc string `discord:"desc"`
-	}
-	b.Log.Infow("submission received", "from", data.Event.Sender().Username)
-	if err := data.Options.Unmarshal(&opts); err != nil {
-		return errorResponse(err)
-	}
-	b.Log.Infow("submission options", "opts", opts)
+	b.Log.Infow("submission received while db archiving", "from", data.Event.Sender().Username)
 
-	if data.Event.SenderID() == 0 {
-		b.Log.Info("unexpected sender id is 0")
-		return &api.InteractionResponseData{
-			Content: &option.NullableStringData{
-				Val: "Command failed; Could not find sender information",
-			},
-		}
-	}
-
-	id, err := b.Backend.Submit(opts.Link, opts.Desc, data.Event.SenderID().String())
 	resp := discord.NewEmbed()
-	if err != nil {
-		resp.Title = "Submission failed"
-		// catch all desc
-		resp.Description = "An unexpected error occured. Please contact administrator"
-		st, ok := status.FromError(err)
-		if ok {
-			switch st.Code() {
-			case codes.InvalidArgument:
-				resp.Description = "The viewer link you have provided is either invalid or an error occured reading it."
-			case codes.NotFound:
-				resp.Description = "Could not find the viewer share provided"
-			}
-		} else {
-			b.Log.Warnw("unexpected error submitting; error not a status", "err", err)
-		}
-	} else {
-		resp.Title = fmt.Sprintf("Submission recorded successfully (id: %v)", id)
-		resp.URL = opts.Link
-		resp.Description = opts.Desc
-	}
+	resp.Title = "Submissions are closed"
+	resp.Description = "The DB is in the process of being archived and as such we are not taking any new submissions. Stay tuned for more updates."
 
 	e := []discord.Embed{*resp}
 
