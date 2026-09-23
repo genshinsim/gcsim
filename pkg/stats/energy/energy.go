1be6ed5ef374c602c4aab6a77f7b7ad0d11d0535
Charlie Zheng <charlie.x.3000@gmail.com>
2026-09-23 16:37:25 -0600

    Removed commented EnergyStatus code

--- pkg/stats/energy/energy.go
+++ pkg/stats/energy/energy.go
@@ -16,18 +16,12 @@ func init() {
 
 type buffer struct {
 	events [][]stats.EnergyEvent
-	// startEnergy []float64
 }
 
 func NewStat(core *core.Core) (stats.Collector, error) {
 	out := buffer{
 		events: make([][]stats.EnergyEvent, len(core.Player.Chars())),
-		// startEnergy: make([]float64, len(core.Player.Chars())),
 	}
-
-	// for i, char := range core.Player.Chars() {
-	// 	out.startEnergy[i] = char.Energy
-	// }
 
 	core.Events.Subscribe(event.OnEnergyChange, func(args ...any) {
 		character := args[0].(*character.CharWrapper)
@@ -58,16 +52,5 @@ func NewStat(core *core.Core) (stats.Collector, error) {
 func (b buffer) Flush(core *core.Core, result *stats.Result) {
 	for c := 0; c < len(b.events); c++ {
 		result.Characters[c].EnergyEvents = b.events[c]
-
-		// result.Characters[c].EnergyStatus = make([]float64, core.F)
-		// currEnergy := b.startEnergy[c]
-		// currEvent := 0
-		// for f := 0; f < core.F; f++ {
-		// 	if currEvent < len(b.events[c]) && b.events[c][currEvent].Frame == f {
-		// 		currEnergy = b.events[c][currEvent].Current
-		// 		currEvent++
-		// 	}
-		// 	result.Characters[c].EnergyStatus[f] = currEnergy
-		// }
 	}
 }
